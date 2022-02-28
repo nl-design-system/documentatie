376b4fcabf9ec70cb7f97bc9b6e8cb5d0605bc3a
Yolijn <yolijn@frameless.io>
2022-02-28 12:39:36 +0100

    remove docz

--- .docz/doczrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-	title: "NL Design System",
-	description: "NL Design System website",
-	files: ["**/*.{md,mdx}"],
-	ignore: ["README.md"],
-	base: "/documentatie",
-	dest: "/dist",
-};
