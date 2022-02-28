920f2c1a27033fa6e02f64d79fa8eab24663ba53
Robbert Broersma <mail@robbertbroersma.nl>
2022-02-28 11:28:39 +0100

    ci: deploy tmp branch

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,43 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---algemeen-tekst-en-taalgebruik-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/algemeen/tekst-en-taalgebruik.md"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/.docz/.cache/dev-404-page.js"))),
+  "component---components-alternate-lang-nav-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/components/alternate-lang-nav/readme.md"))),
+  "component---components-checkbox-group-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/components/checkbox-group/readme.md"))),
+  "component---components-form-input-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/components/form-input/readme.md"))),
+  "component---components-heading-1-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/components/heading-1/readme.md"))),
+  "component---components-heading-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/components/heading/readme.md"))),
+  "component---components-input-tel-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/components/input-tel/readme.md"))),
+  "component---components-radio-button-group-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/components/radio-button-group/readme.md"))),
+  "component---figma-figma-autolayout-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-autolayout.md"))),
+  "component---figma-figma-components-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-components.md"))),
+  "component---figma-figma-design-tokens-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-design-tokens.md"))),
+  "component---figma-figma-documentation-links-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-documentation-links.md"))),
+  "component---figma-figma-file-structure-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-file-structure.md"))),
+  "component---figma-figma-naming-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-naming.md"))),
+  "component---figma-figma-responsive-behaviour-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-responsive-behaviour.md"))),
+  "component---figma-figma-slots-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-slots.md"))),
+  "component---figma-figma-version-history-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-version-history.md"))),
+  "component---figma-figma-zero-frames-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/figma-zero-frames.md"))),
+  "component---figma-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/figma/README.md"))),
+  "component---gebruikersonderzoek-readme-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/gebruikersonderzoek/README.md"))),
+  "component---patronen-formulieren-afhandeling-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-afhandeling.md"))),
+  "component---patronen-formulieren-help-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-help.md"))),
+  "component---patronen-formulieren-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren.md"))),
+  "component---patronen-formulieren-naamgeving-feedback-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-naamgeving-feedback.md"))),
+  "component---patronen-formulieren-naamgeving-velden-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-naamgeving-velden.md"))),
+  "component---patronen-formulieren-overzicht-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-overzicht.md"))),
+  "component---patronen-formulieren-toegankelijkheid-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-toegankelijkheid.md"))),
+  "component---patronen-formulieren-validatie-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-validatie.md"))),
+  "component---patronen-formulieren-voorbereiden-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-voorbereiden.md"))),
+  "component---patronen-formulieren-vormgeving-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-vormgeving.md"))),
+  "component---patronen-formulieren-vragen-antwoorden-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-vragen-antwoorden.md"))),
+  "component---patronen-formulieren-wachtindicatie-md": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/patronen/formulieren-wachtindicatie.md"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/bobby/Sites/github.com/nl-design-system/documentatie/.docz/src/pages/404.js")))
+}
+
---
+++ .docz/doczrc.js
@@ -0,0 +1,8 @@
+export default {
+	title: "NL Design System",
+	description: "NL Design System website",
+	files: ["**/*.{md,mdx}"],
+	ignore: ["README.md"],
+	base: "/documentatie",
+	dest: "/dist",
+};
