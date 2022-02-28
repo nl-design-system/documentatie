bde236d14318379501a5773a61bfe75b379d72ff
Yolijn <yolijn@frameless.io>
2022-02-28 15:37:37 +0100

    feature: improve homepage

--- footerConfig.js
+++ footerConfig.js
@@ -4,32 +4,12 @@ module.exports = {
     {
       items: [
         {
-          label: "Gebruiker Centraal",
-          href: "https://www.gebruikercentraal.nl",
-        },
-        {
-          label: "GitHub",
-          href: "https://github.com/nl-design-system",
-        },
-        {
-          label: "Storybook",
-          href: "https://nl-design-system.github.io/themes",
-        },
-      ],
-    },
-    {
-      items: [
-        {
-          label: "Over ons",
-          to: "/docs/algemeen/kernteam",
-        },
-        {
-          label: "Privacy",
+          label: "Privacy verklaring",
           to: "/docs/footer/privacy",
         },
         {
-          label: "Cookies",
-          to: "/docs/footer/cookies",
+          label: "Toegankelijkheid",
+          to: "/docs/footer/toegankelijkheidsverklaring",
         },
         {
           label: "Sitemap",
@@ -40,16 +20,12 @@ module.exports = {
     {
       items: [
         {
-          label: "Nieuwsbrief",
-          href: "https://designsystem.gebruikercentraal.nl/nieuwsbrieven/",
+          label: "GitHub",
+          href: "https://github.com/nl-design-system/backlog",
         },
         {
-          label: "Blog",
-          href: "https://designsystem.gebruikercentraal.nl/blogs-nieuws/",
-        },
-        {
-          label: "LinkedIn",
-          href: "https://www.linkedin.com/showcase/nl-design-system/",
+          label: "Storybook",
+          href: "https://nl-design-system.github.io/themes",
         },
       ],
     },
