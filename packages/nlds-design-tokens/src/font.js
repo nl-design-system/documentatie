63e96049f5f29d95460a5d615b33b52cc83345e8
Robbert Broersma <git@robbertbroersma.nl>
2023-02-13 08:20:30 +0100

    feat: design token package for NL Design System theme

---
+++ packages/nlds-design-tokens/src/font.js
@@ -0,0 +1,13 @@
+import '@fontsource/fira-sans/200.css';
+import '@fontsource/fira-sans/200-italic.css';
+import '@fontsource/fira-sans/300.css';
+import '@fontsource/fira-sans/300-italic.css';
+import '@fontsource/fira-sans/400.css';
+import '@fontsource/fira-sans/400-italic.css';
+import '@fontsource/fira-sans/500.css';
+import '@fontsource/fira-sans/500-italic.css';
+import '@fontsource/fira-sans/600.css';
+import '@fontsource/fira-sans/600-italic.css';
+import '@fontsource/fira-sans/700.css';
+import '@fontsource/fira-sans/700-italic.css';
+import 'firacode/distr/fira_code.css';
