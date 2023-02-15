7ba9fd1f339efc49211b456cacd9c7640e79e491
Robbert Broersma <git@robbertbroersma.nl>
2023-02-15 11:39:58 +0100

    docs: project

--- docs/project/sidebar.js
+++ docs/project/sidebar.js
@@ -5,6 +5,8 @@ const sidebar = [
     items: [
       { type: 'doc', id: 'project/contact' },
       { type: 'doc', id: 'project/blijf-op-de-hoogte' },
+      { type: 'doc', id: 'project/geschiedenis' },
+      { type: 'doc', id: 'project/links' },
     ],
   },
 ];
