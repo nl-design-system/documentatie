aff69419d2ab32c58afe15fb978cc062bffbba9d
Yolijn <yolijn@frameless.io>
2023-05-22 13:16:52 +0200

    docs: add docs for accessible form-labels

---
+++ src/components/Guideline.tsx
@@ -0,0 +1,43 @@
+import React, { HTMLAttributes, PropsWithChildren, ReactNode } from 'react';
+import { IconMoodSad, IconMoodHappy } from '@tabler/icons-react';
+import style from './Guideline.module.css';
+import clsx from 'clsx';
+import { Paragraph } from '@utrecht/component-library-react';
+
+interface GuidelineProps extends HTMLAttributes<HTMLDivElement> {
+  title?: string;
+  description?: ReactNode;
+  appearance: 'do' | 'dont';
+}
+
+export const Guideline = ({ title, appearance, description, children }: PropsWithChildren<GuidelineProps>) => {
+  const getBadgeTitle = {
+    do: 'Doen',
+    dont: 'Niet doen',
+  };
+
+  const badgeTitle = title ? `${getBadgeTitle[appearance]}: ${title}` : getBadgeTitle[appearance];
+
+  return (
+    <div
+      className={clsx(style['nlds-guideline'], style[`nlds-guideline--${appearance}`])}
+      id={title?.toLowerCase().replace(/\s/g, '-')}
+    >
+      <div className={clsx(style['nlds-guideline__description'])}>
+        {appearance === 'dont' ? (
+          <Paragraph className={clsx(style['nlds-guideline__badge'], style[`nlds-guideline__badge--${appearance}`])}>
+            <IconMoodSad className={style['nlds-guideline__icon']} />
+            <span className={style['nlds-guideline__title']}>{badgeTitle}</span>
+          </Paragraph>
+        ) : (
+          <Paragraph className={clsx(style['nlds-guideline__badge'], style[`nlds-guideline__badge--${appearance}`])}>
+            <IconMoodHappy className={style['nlds-guideline__icon']} />
+            <span className={style['nlds-guideline__title']}>{badgeTitle}</span>
+          </Paragraph>
+        )}
+        {description}
+      </div>
+      <div className={clsx(style['nlds-guideline__example'])}>{children}</div>
+    </div>
+  );
+};
--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import '@nl-design-system-unstable/nlds-design-tokens/src/font';
+import '@nl-design-system-unstable/example-theme/dist/design-tokens.css';
+import '@nl-design-system-unstable/example-theme/dist/custom.css';
+import '@fontsource/open-sans/300.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
+import '@fontsource/open-sans/800.css';
 
 // Package with CSS for white-label components
 import '@utrecht/component-library-css';
