9f8cbfc2b28ff429811e910b493beb69d2428244
Yolijn <yolijn@frameless.io>
2023-06-02 15:42:05 +0200

    refactor: linear-gradient

--- src/components/HomepageHero.tsx
+++ src/components/HomepageHero.tsx
@@ -11,24 +11,23 @@ interface HomepageHeroProps {
   cta: string;
 }
 
-export const HomepageHero = ({ title, ctaLink, cta, children }: PropsWithChildren<HomepageHeroProps>) => {
-  return (
-    <div className={clsx(style['homepage-hero'])}>
-      <div className="container">
-        <Heading1 className={clsx(style['homepage-hero__title'])}>{title}</Heading1>
-        <div className={clsx(style['homepage-hero__description'])}>{children}</div>
-        <Paragraph className={clsx(style['homepage-hero__call-to-action'])}>
-          <Link className={clsx('utrecht-link', style['homepage-hero__call-to-action-link'])} to={ctaLink}>
-            {cta}
-            <IconChevronRight
-              className={clsx('utrecht-icon', style['homepage-hero__call-to-action-icon'])}
-              style={{ verticalAlign: 'middle' }}
-            />
-          </Link>
-        </Paragraph>
-      </div>
+export const HomepageHero = ({ title, ctaLink, cta, children }: PropsWithChildren<HomepageHeroProps>) => (
+  <div className={clsx(style['homepage-hero'])}>
+    <div className={clsx('container', style['homepage-hero__container'])}>
+      <Heading1 className={clsx(style['homepage-hero__title'])}>{title}</Heading1>
+      <div className={clsx(style['homepage-hero__description'])}>{children}</div>
+      <Paragraph className={clsx(style['homepage-hero__call-to-action'])}>
+        <Link className={clsx('utrecht-link', style['homepage-hero__call-to-action-link'])} to={ctaLink}>
+          {cta}
+          <IconChevronRight
+            className={clsx('utrecht-icon', style['homepage-hero__call-to-action-icon'])}
+            style={{ verticalAlign: 'middle' }}
+          />
+        </Link>
+      </Paragraph>
     </div>
-  );
-};
+    <div className={clsx(style['homepage-hero__linear-gradient'])} />
+  </div>
+);
 
 export default HomepageHero;
