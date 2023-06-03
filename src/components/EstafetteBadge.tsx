a4bf698350dca83173b77dbf9b106df8b9a057f3
Yolijn <yolijn@frameless.io>
2023-06-03 12:35:55 +0200

    feat: add estafette data-badge

---
+++ src/components/EstafetteBadge.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { DataBadge } from '@utrecht/component-library-react';
+import { COMPONENT_STATES } from '@nl-design-system/component-index';
+import style from './EstafetteBadge.module.css';
+import clsx from 'clsx';
+
+interface EstafetteBadgeProps {
+  stateModifier: 'help-wanted' | 'community' | 'candidate' | 'hall-of-fame';
+  state: COMPONENT_STATES;
+}
+
+export const EstafetteBadge = ({ stateModifier, state }: EstafetteBadgeProps) => (
+  <DataBadge className={clsx(style['estafette-badge'], style[`estafette-badge--${stateModifier}`])}>{state}</DataBadge>
+);
