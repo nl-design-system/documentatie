1ccf9cef095b167734c517a40559b321f957bba8
Yolijn <yolijn@frameless.io>
2023-06-04 18:03:51 +0200

    chore: fix linting

--- docs/project/sidebar.js
+++ docs/project/sidebar.js
@@ -2,6 +2,6 @@ const sidebar = [
   { type: 'doc', id: 'project/kernteam' },
   { type: 'doc', id: 'project/blijf-op-de-hoogte' },
   { type: 'doc', id: 'project/links' },
-]
+];
 
-module.exports = sidebar
+module.exports = sidebar;
