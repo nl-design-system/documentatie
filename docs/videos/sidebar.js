a4c3bc2dd5c5989e345bdfe6d79a2a8266c207af
Yolijn <yolijn@frameless.io>
2023-06-07 05:55:41 +0200

    fix: videos category

--- docs/videos/sidebar.js
+++ docs/videos/sidebar.js
@@ -1,10 +1,3 @@
-const sidebar = [
-  {
-    type: 'category',
-    label: 'Videos',
-    link: { type: 'doc', id: 'videos/README' },
-    items: ['videos/design-system-week-2022', 'videos/heartbeat'],
-  },
-];
+const sidebar = ['videos/design-system-week-2022', 'videos/heartbeat'];
 
 module.exports = sidebar;
