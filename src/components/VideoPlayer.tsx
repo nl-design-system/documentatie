673558d68bb634f1c4c189601b9381261c8c1027
Hidde de Vries <hidde@hiddedevries.nl>
2023-11-20 14:29:31 +0100

    chore: Move the video player CSS to a module

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import clsx from 'clsx';
+import style from './VideoPlayer.module.css';
 
-export const VideoPlayer = ({ videoId, ...restProps }) => (
-  <ReactPlayer url={`https://youtube.com/watch?v=${videoId}&disablekb=1`} controls {...restProps} />
+export const VideoPlayer = ({ videoId, className, ...restProps }) => (
+  <ReactPlayer url={`https://youtube.com/watch?v=${videoId}&disablekb=1`} className={`${clsx(style[`video-player`])} ${className}`} width="100%" height="100%" controls {...restProps} />
 );
