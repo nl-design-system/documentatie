f62fd1fc6a757343e1eba199e5dae0738d51fb8d
Hidde de Vries <hidde@hiddedevries.nl>
2023-11-20 14:57:31 +0100

    fix: Disable keyboard shortcuts for embed

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -4,5 +4,13 @@ import clsx from 'clsx';
 import style from './VideoPlayer.module.css';
 
 export const VideoPlayer = ({ videoId, className, ...restProps }) => (
-  <ReactPlayer url={`https://youtube.com/watch?v=${videoId}&disablekb=1`} className={`${clsx(style[`video-player`])} ${className}`} width="100%" height="100%" controls {...restProps} />
+  <ReactPlayer
+    url={`https://youtube.com/watch?v=${videoId}`}
+    className={`${clsx(style[`video-player`])} ${className}`}
+    width="100%"
+    height="100%"
+    controls
+    {...restProps}
+    config={{ youtube: { playerVars: { disablekb: 1 } } }}
+  />
 );
