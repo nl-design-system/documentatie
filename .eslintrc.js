5362b9b38309c3c589fb42498935bab6e2d48044
Robbert Broersma <git@robbertbroersma.nl>
2023-12-13 21:03:52 +0100

    ci: add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+/* eslint-env node */
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: false,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      extends: ['plugin:json/recommended'],
+      files: ['*.json'],
+    },
+    {
+      extends: [
+        'plugin:react/recommended',
+        'eslint-config-prettier',
+        './.eslintrc.js.json',
+        './.eslintrc.react.json',
+        'plugin:mdx/overrides',
+      ],
+      files: ['*.mdx'],
+      plugins: ['import'],
+      settings: {
+        'mdx/code-blocks': true,
+      },
+    },
+    {
+      extends: ['plugin:react/recommended', 'eslint-config-prettier', './.eslintrc.js.json', './.eslintrc.react.json'],
+      files: ['*.js', '*.jsx'],
+      plugins: ['import'],
+    },
+    {
+      extends: ['plugin:react/recommended', 'eslint-config-prettier', './.eslintrc.js.json', './.eslintrc.react.json'],
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint', 'import'],
+    },
+    {
+      extends: ['plugin:react/recommended', 'eslint-config-prettier', './.eslintrc.js.json'],
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ['@typescript-eslint', 'import'],
+    },
+  ],
+};
