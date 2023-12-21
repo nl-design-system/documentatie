53d27c1a948ad91e5a50243f8c01624a44dfaa11
Robbert Broersma <git@robbertbroersma.nl>
2023-12-21 18:42:18 +0100

    chore: remove all swizzled components, start fresh

--- src/theme/MDXComponents/Heading.tsx
+++
@@ -1,7 +0,0 @@
-import Heading from '@theme/Heading';
-import type { Props } from '@theme/MDXComponents/Heading';
-import React from 'react';
-
-export default function MDXHeading(props: Props): React.Element {
-  return <Heading {...props} />;
-}
--- src/theme/Navbar/MobileSidebar/SecondaryMenu/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useNavbarSecondaryMenu } from '@docusaurus/theme-common/internal';
-import React from 'react';
-
-// The secondary menu slides from the right and shows contextual information
-// such as the docs sidebar
-export default function NavbarMobileSidebarSecondaryMenu(): React.Element | null {
-  const secondaryMenu = useNavbarSecondaryMenu();
-  return <>{secondaryMenu.content}</>;
-}
