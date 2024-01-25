b26029ae1283ab8b3074a1920f1cbf8c549b5d90
Robbert <mail@robbertbroersma.nl>
2024-01-25 09:30:05 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@1c2f81743c8d0c14bbfb9f8ddebd5f590964938e 🚀

---
+++ assets/js/03722200.f350ed6d.js
@@ -0,0 +1,89 @@
+"use strict";
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [4075],
+ {
+  4829: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => x, contentTitle: () => v, default: () => N, frontMatter: () => p, metadata: () => b, toc: () => f });
+   var a = t(1527),
+    r = t(7214),
+    i = t(6532),
+    s = t(6259);
+   t(959);
+   const o = { "core-team": "core-team_E52r", "core-team__card": "core-team__card_gkWz", "core-team__content": "core-team__content_lanl", "core-team__heading": "core-team__heading_wW7n", "core-team__heading-subtitle": "core-team__heading-subtitle_W6CG", "core-team__description": "core-team__description_CAF3", "core-team__illustration": "core-team__illustration_NY8V", "core-team__avatar": "core-team__avatar_c1Tj" },
+    d = { name: "Hidde de Vries", role: "Public Relations Lead & Toegankelijkheidsspecialist", slack: { mention: "@Hidde", id: "UA17NJHTR" }, Description: () => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(i.nv, { children: "Hidde is als PR lead bij NL Design System verantwoordelijk voor de communicatiestrategie, bijeenkomsten als de Heartbeat en Design Systems Week en de promotie van NL Design System bij organisaties en hun design system teams." }), (0, a.jsx)(i.nv, { children: "Hij heeft zich de afgelopen jaren intensief beziggehouden met design systems en digitale toegankelijkheid, dat laatste onder meer bij het W3C. In zijn rol van toegankelijkheidsspecialist helpt hij om NL Design System componenten, documentatie en voorbeelden zo toegankelijk mogelijk te maken." }), (0, a.jsx)(i.nv, { children: "Hij wordt blij van espresso tonics en van heldere, to-the-point uitleg die mensen duidelijkheid geeft. Maar wel met voldoende nuance." })] }) },
+    l = { name: "Jeffrey Lauwers", role: "Designer", slack: { mention: "@Jeffrey Lauwers", id: "U0412KZ9TV1" }, Description: () => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(i.nv, { children: "Jeffrey vervult binnen het NL Design System de designrol. Eerder realiseerde hij design systems voor het Ministerie van VWS, Nederlandse Spoorwegen, Nationale Nederlanden en Randstad." }), (0, a.jsx)(i.nv, { children: "Naast een passie voor Design Systems en Ajax heeft Jeffrey een passie voor toegankelijkheid." })] }) },
+    c = { name: "Peter Berrevoets", role: "Projectleider", slack: { mention: "@Peter Berrevoets", id: "U04J217KA67" }, Description: () => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(i.nv, { children: "Peter is projectleider bij NL Design System. Hij heeft de overheid al van diverse kanten kunnen zien en werkt al sinds 2001 bij ICTU in diverse rollen, projecten en programma's." }), (0, a.jsx)(i.nv, { children: "Oorspronkelijk komt hij uit Den Haag, maar hij woont inmiddels in het mooie Brabant." })] }) },
+    m = { name: "Renate Bruinenberg", role: "Communicatieadviseur", slack: { mention: "@Renate Bruinenberg", id: "U039TN2VAC8" }, Description: () => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(i.nv, { children: "Als communicatieadviseur is Renate degene die zich samen met Hidde bezighoudt met de communicatie vanuit en rondom het NL Design System. De kans is dus groot dat je haar naam regelmatig voorbij ziet komen onder artikelen en blogs. Ook maakt ze de maandelijkse nieuwsbrief waarin alle updates gedeeld worden." }), (0, a.jsx)(i.nv, { children: "Naast haar werk als communicatieadviseur is Renate ook copywriter en coach op het gebied van schrijven en zoekmachineoptimalisatie (SEO)." }), (0, a.jsx)(i.nv, { children: "In haar vrije tijd is ze druk met haar grote gezin, waarvoor ze graag in de keuken staat om lekkere gerechten te bereiden. Die passie voor gezelligheid en lekker eten komt voort uit haar Italiaanse roots." }), (0, a.jsxs)(i.nv, { children: ["Volgens Renate wordt communiceren over het NL Design System alleen maar leuker met input vanuit de community. Dus heb je iets leuks te vertellen of wil je een keer meewerken aan een blogartikel? Laat het haar dan vooral weten!", " "] })] }) },
+    g = { name: "Rian Rietveld", role: "Specialist webtoegankelijkheid", slack: { mention: "@Rian Rietveld", id: "U05UTKUNF6Y" }, Description: () => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(i.nv, { children: "Samen met het team onderzoekt en documenteert Rian de toegankelijkheid van webcomponenten. Daarnaast schrijft en onderhoudt ze de richtlijnen voor het NL Design System. Hierdoor is het een stuk makkelijker een toegankelijke website op te zetten en te onderhouden. Rian geeft gevraagd en ongevraagd advies hoe het NL Design System zo toegankelijk mogelijk te maken." }), (0, a.jsx)(i.nv, { children: "Ze vindt het belangrijk dat iedereen het web goed kan gebruiken. Daarom geeft ze ook les, presentaties, uitleg en voorlichting aan iedereen betrokken bij een website." }), (0, a.jsx)(i.nv, { children: "Als ze niet aan het schrijven, reviewen of uitleggen is, werkt Rian graag in haar tuin en klust ze in huis." })] }) },
+    h = { name: "Robbert Broersma", role: "Design System lead & Developer Relations", slack: { mention: "@Robbert", id: "U019X17U5HQ" }, Description: () => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(i.nv, { children: "Robbert is design system lead van NL Design System. Met zijn ervaring bij andere design systems kent hij zowel de valkuilen van een team overstijgend design system als de waarde van het hergebruik van stabiele, toegankelijke componenten." }), (0, a.jsx)(i.nv, { children: "In de rol van developer relations werkt hij graag online samen met developers van verschillende organisaties om dit community gedragen design system neer te zetten. Zijn passie voor keukengerei is daarbij niet te missen! RVS met een haakje? Dan kun je het waarschijnlijk op zijn achtergrond vinden." })] }) },
+    u = { name: "Yolijn van der Kolk", role: "Productmanager & Developer Relations", slack: { mention: "@Yolijn", id: "U01B7T1F24S" }, Description: () => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(i.nv, { children: "Als productmanager is Yolijn graag de schakel tussen de verschillende stakeholders en het kernteam van NL Design System. Ze combineert graag het uitwerken van een lange termijn visie met het opknippen van grote mijlpalen naar kleine, toetsbare onderdelen." }), (0, a.jsx)(i.nv, { children: "Bij eerdere projecten bleek al dat ze graag de schakel is tussen de makers van een component en de verscheidenheid aan organisaties die het white-label design system afnemen en aanpassen naar hun eigen Design Tokens. In de rol van Developer Relations ontdekt ze graag na een lekker kopje koffie wat de gemene deler is tussen componenten en stijlen bij verschillende applicaties." })] }) };
+   var j = t(5845);
+   const k = [c, u, h, l, d, g, m],
+    _ = (e) => {
+     let { headingLevel: n } = e;
+     return (0, a.jsx)(j.um, {
+      appearance: "large",
+      children: k.map((e) => {
+       let { Avatar: t, name: r, role: d, Description: l, slack: c } = e;
+       return (0, a.jsxs)(j.Zb, { appearance: "large", className: (0, s.Z)(o["core-team__card"]), component: "section", children: [t && (0, a.jsx)(j.wu, { className: (0, s.Z)(o["core-team__illustration"]), children: (0, a.jsx)(t, { className: (0, s.Z)(o["core-team__avatar"]) }) }), (0, a.jsxs)(j.aY, { className: (0, s.Z)(o["core-team__content"]), children: [(0, a.jsxs)(i.OX, { className: (0, s.Z)(o["core-team__heading"]), children: [(0, a.jsx)(i.X6, { level: n, className: (0, s.Z)(o["core-team__heading-title"]), children: r }), (0, a.jsx)(i.nv, { className: (0, s.Z)(o["core-team__heading-subtitle"]), children: d })] }), (0, a.jsx)("div", { className: (0, s.Z)(o["core-team__description"]), children: (0, a.jsx)(l, {}) }), (0, a.jsxs)(i.nv, { children: ["Slack: ", (0, a.jsx)(i.rU, { href: `https://codefornl.slack.com/team/${c.id}`, children: c.mention })] })] })] }, r);
+      }),
+     });
+    },
+    p = { title: "Kernteam", hide_title: !0, hide_table_of_contents: !0, sidebar_label: "Kernteam", pagination_label: "Kernteam", description: "Contact met NL Design System Kernteam", keywords: ["kernteam", "over ons", "nl design system", "contact"] },
+    v = "Kernteam",
+    b = { id: "project/kernteam", title: "Kernteam", description: "Contact met NL Design System Kernteam", source: "@site/docs/project/kernteam.mdx", sourceDirName: "project", slug: "/project/kernteam", permalink: "/project/kernteam", draft: !1, unlisted: !1, editUrl: "https://github.com/nl-design-system/documentatie/tree/main/docs/project/kernteam.mdx", tags: [], version: "current", frontMatter: { title: "Kernteam", hide_title: !0, hide_table_of_contents: !0, sidebar_label: "Kernteam", pagination_label: "Kernteam", description: "Contact met NL Design System Kernteam", keywords: ["kernteam", "over ons", "nl design system", "contact"] }, sidebar: "project", previous: { title: "Over NL Design System", permalink: "/project/over-nlds" }, next: { title: "Events", permalink: "/project/events/overzicht" } },
+    x = {},
+    f = [];
+   function y(e) {
+    const n = { a: "a", h1: "h1", p: "p", ...(0, r.a)(), ...e.components };
+    return (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(n.h1, { id: "kernteam", children: "Kernteam" }), "\n", (0, a.jsxs)(n.p, { children: ["Naast de community is er een enthousiast kernteam dat zich dagelijks inzet voor het NL Design System. Kennismaken met het kernteam? Neem contact op door een e-mail te sturen naar ", (0, a.jsx)(n.a, { href: "mailto:kernteam@nldesignsystem.nl", children: "kernteam@nldesignsystem.nl" })] }), "\n", (0, a.jsx)(_, { headingLevel: 2 })] });
+   }
+   function N(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, a.jsx)(n, { ...e, children: (0, a.jsx)(y, { ...e }) }) : y(e);
+   }
+  },
+  5845: (e, n, t) => {
+   t.d(n, { Zb: () => l, aY: () => d, um: () => c, wu: () => o });
+   var a = t(6532),
+    r = t(6259);
+   t(959);
+   const i = { cardgroup: "cardgroup_0VVt", "cardgroup--small": "cardgroup--small_BDzz", "cardgroup--medium": "cardgroup--medium_xS6r", "cardgroup--large": "cardgroup--large_Tjo8", cardgroup__card: "cardgroup__card_emnt", "cardgroup__card--small": "cardgroup__card--small_bxfC", "cardgroup__card--medium": "cardgroup__card--medium_xiP2", "cardgroup__card--large": "cardgroup__card--large_PGe3", cardgroup__link: "cardgroup__link_YISR", card__illustration: "card__illustration_MVXv", "card__illustration--background": "card__illustration--background_cTcZ", card__content: "card__content_LYKG" };
+   var s = t(1527);
+   const o = (e) => {
+     let { background: n, children: t, className: a, ...o } = e;
+     return (0, s.jsx)("div", { className: (0, r.Z)(i.card__illustration, n && i["card__illustration--background"], a), ...o, children: t });
+    },
+    d = (e) => (0, s.jsx)("div", { className: (0, r.Z)(i.card__content), ...e }),
+    l = (e) => {
+     let { href: n, appearance: t, className: o, component: d = "div", children: l } = e;
+     const c = (e) => ("article" === d ? (0, s.jsx)("article", { ...e }) : "section" === d ? (0, s.jsx)("section", { ...e }) : (0, s.jsx)("div", { ...e })),
+      m = (0, s.jsx)(c, { className: (0, r.Z)(i.cardgroup__card, i[`cardgroup__card--${t}`], o), children: l });
+     return n ? (0, s.jsx)(a.rU, { href: n, boxContent: !0, className: i.cardgroup__link, children: m }) : m;
+    },
+    c = (e) => {
+     let { appearance: n = "medium", children: t } = e;
+     return (0, s.jsx)("div", { className: (0, r.Z)(i.cardgroup, i[`cardgroup--${n}`]), children: t });
+    };
+  },
+  7214: (e, n, t) => {
+   t.d(n, { Z: () => o, a: () => s });
+   var a = t(959);
+   const r = {},
+    i = a.createContext(r);
+   function s(e) {
+    const n = a.useContext(i);
+    return a.useMemo(
+     function () {
+      return "function" == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function o(e) {
+    let n;
+    return (n = e.disableParentContext ? ("function" == typeof e.components ? e.components(r) : e.components || r) : s(e.components)), a.createElement(i.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
