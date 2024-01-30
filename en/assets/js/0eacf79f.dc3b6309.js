23bbf38e62191ff8626934ccdbf5816229bba675
hidde <hidde@hiddedevries.nl>
2024-01-30 13:17:00 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@afb507dc2eabe221263a0eecd07fe300048ce1c9 🚀

---
+++ en/assets/js/0eacf79f.dc3b6309.js
@@ -0,0 +1,43 @@
+"use strict";
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [4193],
+ {
+  84954: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => l, contentTitle: () => r, default: () => m, frontMatter: () => o, metadata: () => a, toc: () => d });
+   var i = t(11527),
+    s = t(47214);
+   const o = { title: "Design Open Hour", hide_title: !0, hide_table_of_contents: !1, sidebar_label: "Design Open Hour", pagination_label: "In het Design Open Hour wisselen designers informatie, inzichten en tips met elkaar uit.", slug: "/events/design-open-hour" },
+    r = "Design Open Hour",
+    a = { id: "community/events/design-open-hour/design-open-hour", title: "Design Open Hour", description: "Screenshot van een online meeting met verschillende aanwezigen vanuit de community.", source: "@site/docs/community/events/design-open-hour/design-open-hour.mdx", sourceDirName: "community/events/design-open-hour", slug: "/events/design-open-hour", permalink: "/en/events/design-open-hour", draft: !1, unlisted: !1, editUrl: "https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/design-open-hour/design-open-hour.mdx", tags: [], version: "current", frontMatter: { title: "Design Open Hour", hide_title: !0, hide_table_of_contents: !1, sidebar_label: "Design Open Hour", pagination_label: "In het Design Open Hour wisselen designers informatie, inzichten en tips met elkaar uit.", slug: "/events/design-open-hour" }, sidebar: "community", previous: { title: "Design Open Hour", permalink: "/en/events/design-open-hour/overzicht" }, next: { title: "In het Design Open Hour wisselen designers informatie, inzichten en tips met elkaar uit.", permalink: "/en/events/design-open-hour/aanmelden" } },
+    l = {},
+    d = [];
+   function u(e) {
+    const n = { a: "a", h1: "h1", img: "img", p: "p", ...(0, s.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: "design-open-hour", children: "Design Open Hour" }), "\n", (0, i.jsx)(n.p, { children: (0, i.jsx)(n.img, { src: "https://raw.githubusercontent.com/nl-design-system/documentatie/assets/design-open-hour.jpg", alt: "Screenshot van een online meeting met verschillende aanwezigen vanuit de community." }) }), "\n", (0, i.jsx)(n.p, { children: "Wees welkom bij het 2-wekelijkse (online) \u2018Design Open Hour\u2019. Een moment om kennis te delen door design gerelateerde vraagstukken met elkaar te bespreken. Maar ook het delen van work-in-progress, tips of interessante inzichten uit gebruikersonderzoek wordt gewaardeerd!" }), "\n", (0, i.jsxs)(n.p, { children: ["Deze sessies zijn publiek toegankelijk. ", (0, i.jsx)(n.a, { href: "/events/design-open-hour/aanmelden", children: "Meld je aan" }), "."] }), "\n", (0, i.jsxs)(n.p, { children: ["Heb je vragen of idee\xebn voor het Design Open Hour? Stuur ons dan een mailtje op ", (0, i.jsx)("a", { href: "mailto:kernteam@nldesignsystem.nl", children: (0, i.jsx)(n.a, { href: "mailto:kernteam@nldesignsystem.nl", children: "kernteam@nldesignsystem.nl" }) })] })] });
+   }
+   function m(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(u, { ...e }) }) : u(e);
+   }
+  },
+  47214: (e, n, t) => {
+   t.d(n, { Z: () => a, a: () => r });
+   var i = t(50959);
+   const s = {},
+    o = i.createContext(s);
+   function r(e) {
+    const n = i.useContext(o);
+    return i.useMemo(
+     function () {
+      return "function" == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ("function" == typeof e.components ? e.components(s) : e.components || s) : r(e.components)), i.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
