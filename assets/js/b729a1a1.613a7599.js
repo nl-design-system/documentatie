ef3d95bcbdbd25dc0afb873c360353b56ce93943
hidde <hidde@hiddedevries.nl>
2024-03-11 20:52:17 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@bf6fdd1c47ec704fe8422c717d9de064b455af7b 🚀

--- assets/js/b729a1a1.613a7599.js
+++ assets/js/b729a1a1.613a7599.js
@@ -1,29 +1,29 @@
 "use strict";
 (self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
- [6516],
+ [9386],
  {
-  41816: (e, n, a) => {
+  67504: (e, n, a) => {
    a.r(n), a.d(n, { assets: () => l, contentTitle: () => o, default: () => u, frontMatter: () => i, metadata: () => d, toc: () => s });
    var t = a(11527),
     r = a(27034);
-   const i = { title: "Lessen vanuit de gebruikerstest formulieren met Haagse ondernemers", slug: "gebruikerstest-ondernemers-den-haag", authors: [{ name: "Renate Bruinenberg", title: "Communicatieadviseur NL Design System", url: "https://www.linkedin.com/in/renate-bruinenberg-886019163/" }], tags: ["meta", "NL Design System"], image: "https://github.com/nl-design-system/documentatie/blob/assets/lessen-gebruikerstest.png?raw=true", hide_table_of_contents: !1, date: new Date("2024-03-06T00:00:00.000Z") },
+   const i = { title: "Lessen vanuit de gebruikerstest formulieren met Haagse ondernemers", slug: "gebruikerstest-ondernemers-den-haag", authors: [{ name: "Renate Bruinenberg", title: "Communicatieadviseur NL Design System", url: "https://www.linkedin.com/in/renate-bruinenberg-886019163/" }], tags: ["meta", "NL Design System"], image: "https://github.com/nl-design-system/documentatie/blob/assets/lessen-gebruikerstest.png?raw=true", hide_table_of_contents: !1, date: new Date("2024-03-11T00:00:00.000Z") },
     o = void 0,
     d = {
-     permalink: "/en/blog/gebruikerstest-ondernemers-den-haag",
-     source: "@site/blog/2024/20240306-gebruikerstest-ondernemers.md",
+     permalink: "/blog/gebruikerstest-ondernemers-den-haag",
+     source: "@site/blog/2024/20240311-gebruikerstest-ondernemers.md",
      title: "Lessen vanuit de gebruikerstest formulieren met Haagse ondernemers",
      description: "Veel online aanvragen verlopen niet direct soepel. Denk hierbij aan foutmeldingen, onvolledig ingevulde vergunningsaanvragen en ontbrekende bijlagen. Dit is niet alleen vervelend voor de aanvrager, maar ook voor degene die de aanvraag moet behandelen. Daarom deed de gemeente Den Haag onderzoek naar een nieuw ontwerp voor de vergunningsaanvraag voor ondernemers. Onderzoeker Margo Welling en UX-designer Ananta Mulyono vertellen over het onderzoek en delen hun belangrijkste inzichten.",
-     date: "2024-03-06T00:00:00.000Z",
-     formattedDate: "March 6, 2024",
+     date: "2024-03-11T00:00:00.000Z",
+     formattedDate: "11 maart 2024",
      tags: [
-      { label: "meta", permalink: "/en/blog/tags/meta" },
-      { label: "NL Design System", permalink: "/en/blog/tags/nl-design-system" },
+      { label: "meta", permalink: "/blog/tags/meta" },
+      { label: "NL Design System", permalink: "/blog/tags/nl-design-system" },
      ],
      hasTruncateMarker: !0,
      authors: [{ name: "Renate Bruinenberg", title: "Communicatieadviseur NL Design System", url: "https://www.linkedin.com/in/renate-bruinenberg-886019163/" }],
-     frontMatter: { title: "Lessen vanuit de gebruikerstest formulieren met Haagse ondernemers", slug: "gebruikerstest-ondernemers-den-haag", authors: [{ name: "Renate Bruinenberg", title: "Communicatieadviseur NL Design System", url: "https://www.linkedin.com/in/renate-bruinenberg-886019163/" }], tags: ["meta", "NL Design System"], image: "https://github.com/nl-design-system/documentatie/blob/assets/lessen-gebruikerstest.png?raw=true", hide_table_of_contents: !1, date: "2024-03-06T00:00:00.000Z" },
+     frontMatter: { title: "Lessen vanuit de gebruikerstest formulieren met Haagse ondernemers", slug: "gebruikerstest-ondernemers-den-haag", authors: [{ name: "Renate Bruinenberg", title: "Communicatieadviseur NL Design System", url: "https://www.linkedin.com/in/renate-bruinenberg-886019163/" }], tags: ["meta", "NL Design System"], image: "https://github.com/nl-design-system/documentatie/blob/assets/lessen-gebruikerstest.png?raw=true", hide_table_of_contents: !1, date: "2024-03-11T00:00:00.000Z" },
      unlisted: !1,
-     nextItem: { title: "Ik wil je wat vragen, maar ik heb geen WhatsApp!", permalink: "/en/blog/meerdere-manieren-contact" },
+     nextItem: { title: "Ik wil je wat vragen, maar ik heb geen WhatsApp!", permalink: "/blog/meerdere-manieren-contact" },
     },
     l = { authorsImageUrls: [void 0] },
     s = [
