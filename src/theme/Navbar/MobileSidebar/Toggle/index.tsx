7c78e25ef617d2e2ed05a98f5ef0d53fdc227f78
Bryan de Jong <bryandejong99@gmail.com>
2024-04-05 02:03:13 +0200

    fix: remove aria-label

--- src/theme/Navbar/MobileSidebar/Toggle/index.tsx
+++ src/theme/Navbar/MobileSidebar/Toggle/index.tsx
@@ -1,4 +1,3 @@
-import { translate } from '@docusaurus/Translate';
 import { useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
 import { Button } from '@utrecht/component-library-react/dist/css-module';
 import React from 'react';
@@ -9,11 +8,6 @@ export default function MobileSidebarToggle(): React.Element {
     <Button
       appearance="subtle-button"
       onClick={toggle}
-      aria-label={translate({
-        id: 'theme.docs.sidebar.toggleSidebarButtonAriaLabel',
-        message: 'Toggle navigation bar',
-        description: 'The ARIA label for hamburger menu button of mobile navigation',
-      })}
       aria-expanded={shown}
       className="navbar__toggle clean-btn"
       type="button"
