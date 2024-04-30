c4a8e69b141dd8c29f7b2e14245c1169a0034c96
Robbert <mail@robbertbroersma.nl>
2024-04-30 16:46:31 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@f873b6830810281f4e20922d987d0425ac2d05ba 🚀

---
+++ assets/js/11f70c51.45a62d95.js
@@ -0,0 +1,49 @@
+"use strict";
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [231],
+ {
+  98278: (e, t, n) => {
+   n.r(t), n.d(t, { assets: () => r, contentTitle: () => c, default: () => m, frontMatter: () => l, metadata: () => h, toc: () => a });
+   var i = n(11527),
+    s = n(80877),
+    o = n(46828);
+   const l = { title: "Switch", hide_title: !0, hide_table_of_contents: !1, sidebar_label: "Switch", pagination_label: "Switch", description: "UX richtlijnen voor Switch component", slug: "/switch", tags: ["Community"] },
+    c = "Switch",
+    h = { id: "componenten/build/switch", title: "Switch", description: "UX richtlijnen voor Switch component", source: "@site/docs/componenten/build/switch.mdx", sourceDirName: "componenten/build", slug: "/switch", permalink: "/switch", draft: !1, unlisted: !1, editUrl: "https://github.com/nl-design-system/documentatie/tree/main/docs/componenten/build/switch.mdx", tags: [{ label: "Community", permalink: "/tags/community" }], version: "current", frontMatter: { title: "Switch", hide_title: !0, hide_table_of_contents: !1, sidebar_label: "Switch", pagination_label: "Switch", description: "UX richtlijnen voor Switch component", slug: "/switch", tags: ["Community"] }, sidebar: "componenten", previous: { title: "Strong", permalink: "/strong" }, next: { title: "Table", permalink: "/table" } },
+    r = {},
+    a = [
+     { value: "Voorbeeld", id: "voorbeeld", level: 2 },
+     { value: "Implementaties", id: "implementaties", level: 2 },
+     { value: "CSS", id: "css", level: 3 },
+     { value: "Gemeente Utrecht", id: "gemeente-utrecht", level: 4 },
+     { value: "Web Component", id: "web-component", level: 3 },
+     { value: "Gemeente Utrecht", id: "gemeente-utrecht-1", level: 4 },
+     { value: "React", id: "react", level: 3 },
+     { value: "Gemeente Den Haag", id: "gemeente-den-haag", level: 4 },
+     { value: "Levende Backlog", id: "levende-backlog", level: 2 },
+    ];
+   function d(e) {
+    const t = { a: "a", h1: "h1", h2: "h2", h3: "h3", h4: "h4", li: "li", p: "p", ul: "ul", ...(0, s.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: ["\n", "\n", "\n", "\n", (0, i.jsx)(t.h1, { id: "switch", children: "Switch" }), "\n", (0, i.jsx)(t.p, { children: 'Dit component heeft de "Community" status: Heb je hem succesvol ingezet voor jouw project met jouw huisstijl? Laat ons dit weten, misschien kan dit component dan opgenomen worden in NL Design System.' }), "\n", (0, i.jsx)(t.h2, { id: "voorbeeld", children: "Voorbeeld" }), "\n", (0, i.jsx)(o.o, { label: "https://nl-design-system.github.io/themes/", href: "https://nl-design-system.github.io/themes/iframe.html?viewMode=story&id=switch--utrecht" }), "\n", (0, i.jsx)(t.h2, { id: "implementaties", children: "Implementaties" }), "\n", (0, i.jsx)(t.h3, { id: "css", children: "CSS" }), "\n", (0, i.jsx)(t.h4, { id: "gemeente-utrecht", children: "Gemeente Utrecht" }), "\n", (0, i.jsxs)(t.ul, { children: ["\n", (0, i.jsxs)(t.li, { children: ["Implementatie: ", (0, i.jsx)(t.a, { href: "https://github.com/nl-design-system/utrecht/tree/main/components/form-toggle/css", children: "Switch CSS implementatie van Gemeente Utrecht" })] }), "\n", (0, i.jsxs)(t.li, { children: ["Storybook: ", (0, i.jsx)(t.a, { href: "https://nl-design-system.github.io/utrecht/storybook/?path=/docs/css-form-toggle--default-story", children: "Switch CSS storybook van Gemeente Utrecht" })] }), "\n"] }), "\n", (0, i.jsx)(t.h3, { id: "web-component", children: "Web Component" }), "\n", (0, i.jsx)(t.h4, { id: "gemeente-utrecht-1", children: "Gemeente Utrecht" }), "\n", (0, i.jsxs)(t.ul, { children: ["\n", (0, i.jsxs)(t.li, { children: ["Implementatie: ", (0, i.jsx)(t.a, { href: "https://github.com/nl-design-system/utrecht/tree/main/components/form-toggle/web-component", children: "Switch Web Component implementatie van Gemeente Utrecht" })] }), "\n", (0, i.jsxs)(t.li, { children: ["Storybook: ", (0, i.jsx)(t.a, { href: "https://nl-design-system.github.io/utrecht/storybook/?path=/docs/web-component-form-toggle--default-story", children: "Switch Web Component storybook van Gemeente Utrecht" })] }), "\n"] }), "\n", (0, i.jsx)(t.h3, { id: "react", children: "React" }), "\n", (0, i.jsx)(t.h4, { id: "gemeente-den-haag", children: "Gemeente Den Haag" }), "\n", (0, i.jsxs)(t.ul, { children: ["\n", (0, i.jsxs)(t.li, { children: ["Implementatie: ", (0, i.jsx)(t.a, { href: "https://github.com/nl-design-system/denhaag/blob/main/components/Switch/src/index.tsx", children: "Switch React implementatie van Gemeente Den Haag" })] }), "\n", (0, i.jsxs)(t.li, { children: ["Storybook: ", (0, i.jsx)(t.a, { href: "https://nl-design-system.github.io/denhaag/?path=/story/react-input-switch--default", children: "Switch React storybook van Gemeente Den Haag" })] }), "\n"] }), "\n", (0, i.jsx)(t.h2, { id: "levende-backlog", children: "Levende Backlog" }), "\n", (0, i.jsxs)(t.p, { children: ["De Switch component staat bij NL Design System in de ", (0, i.jsx)(t.a, { href: "https://github.com/nl-design-system/backlog/issues/33", children: "levende backlog" }), ", heb je een concrete eisen of wensen voor dit component? Laat het ons dan daar weten!"] })] });
+   }
+   function m(e = {}) {
+    const { wrapper: t } = { ...(0, s.a)(), ...e.components };
+    return t ? (0, i.jsx)(t, { ...e, children: (0, i.jsx)(d, { ...e }) }) : d(e);
+   }
+  },
+  46828: (e, t, n) => {
+   n.d(t, { o: () => c });
+   var i = n(64875);
+   n(50959);
+   const s = "story_PQo9",
+    o = "story__link_f0vD";
+   var l = n(11527);
+   const c = (e) => {
+    let { label: t, href: n } = e;
+    const c = n,
+     h = n.replace("?path=/docs/", "iframe.html?id=") + "&viewMode=story";
+    return (0, l.jsxs)("div", { className: s, children: [(0, l.jsx)(i.Z, { src: h, style: { width: "1px", minWidth: "100%" }, heightCalculationMethod: "lowestElement" }), (0, l.jsx)("a", { className: o, href: c, target: "_blank", rel: "noreferrer", children: t })] });
+   };
+  },
+ },
+]);
---
+++ assets/js/e577b909.1957dadc.js
@@ -0,0 +1,47 @@
+"use strict";
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [3882],
+ {
+  78288: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => r, contentTitle: () => d, default: () => m, frontMatter: () => a, metadata: () => o, toc: () => h });
+   var i = t(11527),
+    s = t(80877),
+    l = t(46828);
+   const a = { title: "Heading 1", hide_title: !0, hide_table_of_contents: !1, sidebar_label: "Heading 1", pagination_label: "Heading 1", description: "UX richtlijnen voor Heading 1 component", slug: "/heading-1", tags: ["Community"] },
+    d = "Heading 1",
+    o = { id: "componenten/build/heading-1", title: "Heading 1", description: "UX richtlijnen voor Heading 1 component", source: "@site/docs/componenten/build/heading-1.mdx", sourceDirName: "componenten/build", slug: "/heading-1", permalink: "/heading-1", draft: !1, unlisted: !1, editUrl: "https://github.com/nl-design-system/documentatie/tree/main/docs/componenten/build/heading-1.mdx", tags: [{ label: "Community", permalink: "/tags/community" }], version: "current", frontMatter: { title: "Heading 1", hide_title: !0, hide_table_of_contents: !1, sidebar_label: "Heading 1", pagination_label: "Heading 1", description: "UX richtlijnen voor Heading 1 component", slug: "/heading-1", tags: ["Community"] }, sidebar: "componenten", previous: { title: "Form field", permalink: "/form-field" }, next: { title: "Heading 2", permalink: "/heading-2" } },
+    r = {},
+    h = [
+     { value: "Voorbeeld", id: "voorbeeld", level: 2 },
+     { value: "Implementaties", id: "implementaties", level: 2 },
+     { value: "CSS", id: "css", level: 3 },
+     { value: "Gemeente Utrecht", id: "gemeente-utrecht", level: 4 },
+     { value: "Figma", id: "figma", level: 3 },
+     { value: "NL Design System", id: "nl-design-system", level: 4 },
+     { value: "Levende Backlog", id: "levende-backlog", level: 2 },
+    ];
+   function c(e) {
+    const n = { a: "a", h1: "h1", h2: "h2", h3: "h3", h4: "h4", li: "li", p: "p", ul: "ul", ...(0, s.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: ["\n", "\n", "\n", "\n", (0, i.jsx)(n.h1, { id: "heading-1", children: "Heading 1" }), "\n", (0, i.jsx)(n.p, { children: 'Dit component heeft de "Community" status: Heb je hem succesvol ingezet voor jouw project met jouw huisstijl? Laat ons dit weten, misschien kan dit component dan opgenomen worden in NL Design System.' }), "\n", (0, i.jsx)(n.h2, { id: "voorbeeld", children: "Voorbeeld" }), "\n", (0, i.jsx)(l.o, { label: "https://nl-design-system.github.io/themes/", href: "https://nl-design-system.github.io/themes/iframe.html?viewMode=story&id=heading-1--utrecht" }), "\n", (0, i.jsx)(n.h2, { id: "implementaties", children: "Implementaties" }), "\n", (0, i.jsx)(n.h3, { id: "css", children: "CSS" }), "\n", (0, i.jsx)(n.h4, { id: "gemeente-utrecht", children: "Gemeente Utrecht" }), "\n", (0, i.jsxs)(n.ul, { children: ["\n", (0, i.jsxs)(n.li, { children: ["Implementatie: ", (0, i.jsx)(n.a, { href: "https://github.com/nl-design-system/utrecht/tree/main/components/heading/css", children: "Heading 1 CSS implementatie van Gemeente Utrecht" })] }), "\n", (0, i.jsxs)(n.li, { children: ["Storybook: ", (0, i.jsx)(n.a, { href: "https://nl-design-system.github.io/utrecht/storybook/?path=/docs/css-heading-1--default-story", children: "Heading 1 CSS storybook van Gemeente Utrecht" })] }), "\n"] }), "\n", (0, i.jsx)(n.h3, { id: "figma", children: "Figma" }), "\n", (0, i.jsx)(n.h4, { id: "nl-design-system", children: "NL Design System" }), "\n", (0, i.jsxs)(n.ul, { children: ["\n", (0, i.jsxs)(n.li, { children: ["Implementatie: ", (0, i.jsx)(n.a, { href: "https://www.figma.com/file/shhwGcqPLi2CapK0P1zz8O/NL-Design-System---Bibliotheek?type=design&node-id=153-1039", children: "Heading 1 Figma implementatie van NL Design System" })] }), "\n"] }), "\n", (0, i.jsx)(n.h2, { id: "levende-backlog", children: "Levende Backlog" }), "\n", (0, i.jsxs)(n.p, { children: ["De Heading 1 component staat bij NL Design System in de ", (0, i.jsx)(n.a, { href: "https://github.com/nl-design-system/backlog/issues/114", children: "levende backlog" }), ", heb je een concrete eisen of wensen voor dit component? Laat het ons dan daar weten!"] })] });
+   }
+   function m(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(c, { ...e }) }) : c(e);
+   }
+  },
+  46828: (e, n, t) => {
+   t.d(n, { o: () => d });
+   var i = t(64875);
+   t(50959);
+   const s = "story_PQo9",
+    l = "story__link_f0vD";
+   var a = t(11527);
+   const d = (e) => {
+    let { label: n, href: t } = e;
+    const d = t,
+     o = t.replace("?path=/docs/", "iframe.html?id=") + "&viewMode=story";
+    return (0, a.jsxs)("div", { className: s, children: [(0, a.jsx)(i.Z, { src: o, style: { width: "1px", minWidth: "100%" }, heightCalculationMethod: "lowestElement" }), (0, a.jsx)("a", { className: l, href: d, target: "_blank", rel: "noreferrer", children: n })] });
+   };
+  },
+ },
+]);
