fbdf736eb46bbdc5947cdcf684b531be5ad063b0
Robbert <mail@robbertbroersma.nl>
2024-05-28 17:06:09 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@03fd5c55fbf81c8003c802a48ef7533e246bcece 🚀

--- en/assets/js/6076374a.f006967c.js
+++ en/assets/js/6076374a.f006967c.js
@@ -3,7 +3,7 @@
  [374],
  {
   73867: (e, n, o) => {
-   o.r(n), o.d(n, { assets: () => a, contentTitle: () => r, default: () => m, frontMatter: () => s, metadata: () => l, toc: () => c });
+   o.r(n), o.d(n, { assets: () => a, contentTitle: () => r, default: () => p, frontMatter: () => s, metadata: () => l, toc: () => c });
    var t = o(52676),
     i = o(24785);
    const s = { title: "Colofon", hide_title: !0, hide_table_of_contents: !0, sidebar_label: "Colofon", pagination_label: "Colofon", description: "Colofon - opsomming van de manier waarop deze website tot stand is gekomen", slug: "/colofon" },
@@ -13,12 +13,13 @@
     c = [
      { value: "Over deze website", id: "over-deze-website", level: 2 },
      { value: "Iconen", id: "iconen", level: 2 },
+     { value: "Open source", id: "open-source", level: 2 },
     ];
    function d(e) {
-    const n = { a: "a", h1: "h1", h2: "h2", p: "p", ...(0, i.a)(), ...e.components };
-    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(n.h1, { id: "colofon", children: "Colofon" }), "\n", (0, t.jsx)(n.h2, { id: "over-deze-website", children: "Over deze website" }), "\n", (0, t.jsxs)(n.p, { children: ["De website wordt gemaakt en onderhouden door het kernteam en is nog volop in ontwikkeling. Mocht u vragen of opmerkingen hebben dan kunt u contact met ons opnemen op ons centrale e-mailadres: ", (0, t.jsx)("a", { href: "mailto:kernteam@nldesignsystem.nl", children: (0, t.jsx)(n.a, { href: "mailto:kernteam@nldesignsystem.nl", children: "kernteam@nldesignsystem.nl" }) }), "."] }), "\n", (0, t.jsx)(n.h2, { id: "iconen", children: "Iconen" }), "\n", (0, t.jsxs)(n.p, { children: ["De toptaak iconen op onze homepage zijn gemaakt door ", (0, t.jsx)("a", { href: "https://www.opengemeenten.nl/producten/iconenset", target: "_top", children: "OpenGemeenten" }), ". Deze iconenset is vrij te gebruiken en valt onder de CC BY-SA 4.0-licentie."] })] });
+    const n = { a: "a", h1: "h1", h2: "h2", li: "li", p: "p", ul: "ul", ...(0, i.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(n.h1, { id: "colofon", children: "Colofon" }), "\n", (0, t.jsx)(n.h2, { id: "over-deze-website", children: "Over deze website" }), "\n", (0, t.jsxs)(n.p, { children: ["De website wordt gemaakt en onderhouden door het kernteam en is nog volop in ontwikkeling. Mocht u vragen of opmerkingen hebben dan kunt u contact met ons opnemen op ons centrale e-mailadres: ", (0, t.jsx)("a", { href: "mailto:kernteam@nldesignsystem.nl", children: (0, t.jsx)(n.a, { href: "mailto:kernteam@nldesignsystem.nl", children: "kernteam@nldesignsystem.nl" }) }), "."] }), "\n", (0, t.jsx)(n.h2, { id: "iconen", children: "Iconen" }), "\n", (0, t.jsxs)(n.p, { children: ["De toptaak iconen op onze homepage zijn gemaakt door ", (0, t.jsx)("a", { href: "https://www.opengemeenten.nl/producten/iconenset", target: "_top", children: "OpenGemeenten" }), ". Deze iconenset is vrij te gebruiken en valt onder de CC BY-SA 4.0-licentie."] }), "\n", (0, t.jsx)(n.h2, { id: "open-source", children: "Open source" }), "\n", (0, t.jsx)(n.p, { children: "De belangrijkste onderdelen van NL Design System zijn open source. We gebruiken voor twee open source licenties:" }), "\n", (0, t.jsxs)(n.ul, { children: ["\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/en/open-source/eupl", children: "EUPL 1.2" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/en/open-source/cc0", children: "CC0 1.0 Universeel" }) }), "\n"] })] });
    }
-   function m(e = {}) {
+   function p(e = {}) {
     const { wrapper: n } = { ...(0, i.a)(), ...e.components };
     return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(d, { ...e }) }) : d(e);
    }
