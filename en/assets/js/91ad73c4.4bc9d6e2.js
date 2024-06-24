95649a20b4559578ff84000325055ee693b5d45e
hidde <hidde@hiddedevries.nl>
2024-06-24 07:21:06 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@d2f9fe2da5dca6ce94673ca7c6fbec106d0388f9 🚀

---
+++ en/assets/js/91ad73c4.4bc9d6e2.js
@@ -0,0 +1,60 @@
+"use strict";
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [94299],
+ {
+  42286: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => o, contentTitle: () => c, default: () => g, frontMatter: () => s, metadata: () => l, toc: () => d });
+   var t = i(52676),
+    r = i(24785),
+    a = i(54894);
+   const s = { title: "Introductie WCAG pagina's", hide_title: !0, hide_table_of_contents: !0, sidebar_label: "Introductie WCAG-pagina's", pagination_label: "Introductie WCAG-pagina's", description: "Introductie van de pagina met informatie over de WCAG-succescriteria.", slug: "introduction", keywords: ["WCAG"] },
+    c = "Introductie WCAG-pagina's",
+    l = { id: "wcag/introduction", title: "Introductie WCAG pagina's", description: "Introductie van de pagina met informatie over de WCAG-succescriteria.", source: "@site/docs/wcag/0-introduction.mdx", sourceDirName: "wcag", slug: "/wcag/introduction", permalink: "/en/wcag/introduction", draft: !1, unlisted: !1, editUrl: "https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/0-introduction.mdx", tags: [], version: "current", sidebarPosition: 0, frontMatter: { title: "Introductie WCAG pagina's", hide_title: !0, hide_table_of_contents: !0, sidebar_label: "Introductie WCAG-pagina's", pagination_label: "Introductie WCAG-pagina's", description: "Introductie van de pagina met informatie over de WCAG-succescriteria.", slug: "introduction", keywords: ["WCAG"] }, sidebar: "richtlijnen", previous: { title: "WCAG", permalink: "/en/wcag" }, next: { title: "WCAG-succescriterium 1.1.1 Niet-tekstuele content", permalink: "/en/wcag/1.1.1" } },
+    o = {},
+    d = [
+     { value: "Opzet", id: "opzet", level: 2 },
+     { value: "WCAG-pagina&#39;s", id: "wcag-paginas", level: 2 },
+    ];
+   function h(e) {
+    const n = { a: "a", h1: "h1", h2: "h2", li: "li", p: "p", strong: "strong", ul: "ul", ...(0, r.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(n.h1, { id: "introductie-wcag-paginas", children: "Introductie WCAG-pagina's" }), "\n", (0, t.jsxs)(n.p, { children: ["In deze pagina's vind je ", (0, t.jsx)(n.a, { href: "https://www.w3.org/Translations/WCAG22-nl/", children: (0, t.jsx)("span", { lang: "en", children: "Web Content Accessibility Guidelines" }) }), " (WCAG) beschreven en uitgelegd."] }), "\n", (0, t.jsx)(n.p, { children: "Het doel is om WCAG begrijpelijk uit te leggen en daardoor makkelijker toepasbaar te maken. Daarnaast staat beschreven hoe je webpagina's voor de verschillende succescriteria kunt testen." }), "\n", (0, t.jsxs)(n.p, { children: [(0, t.jsx)(n.strong, { children: "Let op" }), ": Deze pagina's bevatten praktische uitleg, voorbeelden en interpretatie bij de Web Content Accessibility Guidelines (WCAG). De richtlijnen in NL Design System zijn niet verplicht en geen vervanging voor ", (0, t.jsx)(n.a, { href: "https://www.w3.org/WAI/standards-guidelines/wcag/", children: "WCAG" }), ", de norm waarnaar in wetgeving wordt verwezen (zie ", (0, t.jsx)(n.a, { href: "https://www.digitoegankelijk.nl/wetgeving/wat-is-verplicht", children: "Wat is verplicht - DigiToegankelijk" }), ")."] }), "\n", (0, t.jsx)(n.h2, { id: "opzet", children: "Opzet" }), "\n", (0, t.jsx)(n.p, { children: "Elk WCAG-succescriterium heeft een eigen pagina waarin is opgenomen:" }), "\n", (0, t.jsxs)(n.ul, { children: ["\n", (0, t.jsx)(n.li, { children: "De links naar de offici\xeble richtlijnen en W3C-referenties." }), "\n", (0, t.jsx)(n.li, { children: "Een uitleg." }), "\n", (0, t.jsx)(n.li, { children: "Hoe voor een succescriterium zelf te testen op webpagina's." }), "\n", (0, t.jsx)(n.li, { children: "Links naar gerelateerde richtlijnen, zodat je weet hoe deze criteria toe te passen." }), "\n", (0, t.jsx)(n.li, { children: "Bronnen over extra uitleg of voorbeelden." }), "\n", (0, t.jsx)(n.li, { children: "Relevant gebruikersonderzoek." }), "\n", (0, t.jsx)(n.li, { children: "Veelgemaakte fouten en de oplossingen hiervoor." }), "\n"] }), "\n", (0, t.jsx)(n.h2, { id: "wcag-paginas", children: "WCAG-pagina's" }), "\n", (0, t.jsx)(n.p, { children: "Op dit moment zijn de volgende succescriteria beschreven, het doel is om eind van dit jaar de lijst compleet te hebben." }), "\n", (0, t.jsxs)(n.ul, { children: ["\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/1.1.1", children: "1.1.1 Niet-tekstuele content" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/1.3.1", children: "1.3.1 Info en relaties" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/1.3.5", children: "1.3.5 Identificeer het doel van de input" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/2.1.1", children: "2.1.1 Toetsenbord" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/2.4.1", children: "2.4.1 Blokken omzeilen" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/2.4.2", children: "2.4.2 Paginatitel" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/2.4.4", children: "2.4.4 Linkdoel (in context)" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/2.4.7", children: "2.4.7 Focus zichtbaar" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/2.4.10", children: "2.4.10 Paragraafkoppen" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/2.4.13", children: "2.4.13 Focusweergave" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/3.1.1", children: "3.1.1 Taal van de pagina" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/3.1.2", children: "3.1.2 Taal van onderdelen" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/3.2.1", children: "3.2.1 Bij focus" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/3.2.2", children: "3.2.2 Bij input" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/3.3.1", children: "3.3.1 Foutidentificatie" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/3.3.3", children: "3.3.3 Foutsuggestie" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/3.3.4", children: "3.3.4 Foutpreventie (wettelijk, financieel, gegevens)" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/4.1.1", children: "4.1.1 Parsen" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/4.1.2", children: "4.1.2 Naam, rol, waarde" }) }), "\n", (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: "/wcag/4.1.3", children: "4.1.3 Statusberichten" }) }), "\n"] }), "\n", (0, t.jsx)(a.ZP, {})] });
+   }
+   function g(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(h, { ...e }) }) : h(e);
+   }
+  },
+  54894: (e, n, i) => {
+   i.d(n, { ZP: () => s });
+   var t = i(52676),
+    r = i(24785);
+   function a(e) {
+    const n = { a: "a", h2: "h2", p: "p", ...(0, r.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(n.h2, { id: "aanvullingen-of-opmerkingen", children: "Aanvullingen of opmerkingen?" }), "\n", (0, t.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, t.jsx)(n.a, { href: "https://github.com/nl-design-system/documentatie/issues", children: "Deel je mening op GitHub" }), "."] })] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(a, { ...e }) }) : a(e);
+   }
+  },
+  24785: (e, n, i) => {
+   i.d(n, { Z: () => c, a: () => s });
+   var t = i(75271);
+   const r = {},
+    a = t.createContext(r);
+   function s(e) {
+    const n = t.useContext(a);
+    return t.useMemo(
+     function () {
+      return "function" == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function c(e) {
+    let n;
+    return (n = e.disableParentContext ? ("function" == typeof e.components ? e.components(r) : e.components || r) : s(e.components)), t.createElement(a.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
