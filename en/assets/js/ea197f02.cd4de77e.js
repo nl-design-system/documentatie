d1e4791bf540825ba18ee09c31d66e4581aeaf04
hidde <hidde@hiddedevries.nl>
2024-08-05 09:21:16 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@b2dc78760a66153e1843b91b0e7b1920c50d29e2 🚀

---
+++ en/assets/js/ea197f02.cd4de77e.js
@@ -0,0 +1,9 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [58510],
+ {
+  85061: (e) => {
+   e.exports = JSON.parse('{"title":"Design Systems Week 2024","slug":"/events/design-systems-week-2024","permalink":"/en/events/design-systems-week-2024","navigation":{"previous":{"title":"Events","permalink":"/en/community/events/overzicht"},"next":{"title":"Over","permalink":"/en/events/design-systems-week-2024"}}}');
+  },
+ },
+]);
