fa467296537dc0ca2cdfe238ea67eed6328a4392
matijs <matijs@gmail.com>
2024-08-09 15:20:38 +0200

    fix: lint-staged
    Make sure eslint and npm-pkg-json-lint still work if files that are
    commit are ignored by .eslintignore or .npmpkgjsonlintignore

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   // Automatically add Prettier fixes to the commit as long as there are no errors
   '*': ['prettier --write'],
-  '*.{js,cjs,mjs,jsx,ts,tsx,mdx}': ['eslint'],
-  'package.json': ['npmPkgJsonLint'],
+  '*.{js,cjs,mjs,jsx,ts,tsx,mdx}': ['eslint --no-error-on-unmatched-pattern'],
+  'package.json': ['npmPkgJsonLint --allowEmptyTargets'],
 };
