9463a34dae7c402590707cc6bceb4bfe9a913cf7
Yolijn <mail@yolijn.com>
2024-08-20 17:09:20 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@ece13a8fe7913a69915925b74e6140d2074a657e 🚀

--- assets/js/d8c5e634.d690fde3.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
- [50232],
- {
-  11369: (e) => {
-   e.exports = JSON.parse('{"title":"Design Systems Week 2024","slug":"/events/design-systems-week-2024","permalink":"/events/design-systems-week-2024","navigation":{"previous":{"title":"Events","permalink":"/community/events/overzicht"},"next":{"title":"Over","permalink":"/events/design-systems-week-2024"}}}');
-  },
- },
-]);
--- en/assets/js/9f18257a.74e86c60.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
- [61408],
- {
-  36766: (e) => {
-   e.exports = JSON.parse('{"title":"Events","slug":"/community/events/overzicht","permalink":"/en/community/events/overzicht","navigation":{"previous":{"title":"Sluit je aan","permalink":"/en/community/sluit-je-aan"},"next":{"title":"Design Systems Week 2024","permalink":"/en/events/design-systems-week-2024"}}}');
-  },
- },
-]);
