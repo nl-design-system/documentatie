ad5426f4e4a887b348f7357fe67c633b341eaa97
Robbert <mail@robbertbroersma.nl>
2024-08-26 13:38:37 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@f88f6eed46dddd31ac4b9a22262064ce12fe56eb 🚀

---
+++ assets/js/9f39000f.ad6311fa.js
@@ -0,0 +1,108 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [36592],
+ {
+  64499: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => p, contentTitle: () => u, default: () => v, frontMatter: () => l, metadata: () => g, toc: () => h });
+   var r = i(52676),
+    t = i(24785),
+    o = i(51515),
+    d = i(91410),
+    s = i(88435);
+   function a(e) {
+    const n = { p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.p, { children: 'Bevat je vooraf opgenomen video essenti\xeble informatie die niet wordt uitgesproken? Voeg dan een extra audiotrack toe, de audiodescriptie, die deze informatie beschrijft.' }), '\n', (0, r.jsx)(n.p, { children: 'Welke informatie \u2018essentieel\u2019 is, hangt af van de context. Denk bijvoorbeeld aan videobeeld van een huis met het bordje \u2018Verkocht\u2019 in de tuin, in de context een video waar de verkoop van dat huis een rol speelt.' }), '\n', (0, r.jsx)(n.p, { children: 'Het kan zijn dat er echter te weinig stiltes of rustmomenten in de video zijn om deze extra informatie uit te spreken. Voeg dan een verlengde audiodescriptie toe. Deze audiodescriptie pauzeert de video zodat er tijd is om een aanvullende beschrijving toe te voegen.' })] });
+   }
+   function c(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(a, { ...e }) }) : a(e);
+   }
+   const l = { title: 'WCAG-succescriterium 1.2.7 Verlengde audiodescriptie (vooraf opgenomen)', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.2.7 Verlengde audiodescriptie (vooraf opgenomen)', pagination_label: 'WCAG-succescriterium 1.2.7 Verlengde audiodescriptie (vooraf opgenomen)', description: 'Is er te weinig tijd om audiodescriptie uit te spreken, voeg dan een verlengde audiodescriptie toe. Deze pauzeert de video zodat er tijd is om de aanvullende informatie toe te voegen.', slug: '1.2.7', keywords: ['WCAG', 'Niveau AAA', 'Extended Audio Description (Prerecorded)'] },
+    u = void 0,
+    g = { id: 'wcag/1.2.07', title: 'WCAG-succescriterium 1.2.7 Verlengde audiodescriptie (vooraf opgenomen)', description: 'Is er te weinig tijd om audiodescriptie uit te spreken, voeg dan een verlengde audiodescriptie toe. Deze pauzeert de video zodat er tijd is om de aanvullende informatie toe te voegen.', source: '@site/docs/wcag/1.2.07.mdx', sourceDirName: 'wcag', slug: '/wcag/1.2.7', permalink: '/wcag/1.2.7', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/1.2.07.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 1.2.7 Verlengde audiodescriptie (vooraf opgenomen)', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.2.7 Verlengde audiodescriptie (vooraf opgenomen)', pagination_label: 'WCAG-succescriterium 1.2.7 Verlengde audiodescriptie (vooraf opgenomen)', description: 'Is er te weinig tijd om audiodescriptie uit te spreken, voeg dan een verlengde audiodescriptie toe. Deze pauzeert de video zodat er tijd is om de aanvullende informatie toe te voegen.', slug: '1.2.7', keywords: ['WCAG', 'Niveau AAA', 'Extended Audio Description (Prerecorded)'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 1.2.6 Gebarentaal (vooraf opgenomen)', permalink: '/wcag/1.2.6' }, next: { title: 'WCAG-succescriterium 1.2.8 Media-alternatief (vooraf opgenomen)', permalink: '/wcag/1.2.8' } },
+    p = {},
+    h = [
+     { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
+     { value: 'Uitleg', id: 'uitleg', level: 2 },
+     { value: 'Opgelet', id: 'opgelet', level: 2 },
+     { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
+    ];
+   function m(e) {
+    const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: ['\n', '\n', '\n', '\n', (0, r.jsx)(o.j, { level: 1, conformanceLevel: 'Niveau AAA', children: 'WCAG-succescriterium 1.2.7 Verlengde audiodescriptie (vooraf opgenomen)' }), '\n', (0, r.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#extended-audio-description-prerecorded', children: (0, r.jsx)('span', { lang: 'en', children: '1.2.7 Extended Audio Description (Prerecorded)' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#verlengde-audiodescriptie-vooraf-opgenomen', children: '1.2.7 Verlengde audiodescriptie (vooraf opgenomen)' }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, r.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#extended-audio-description-prerecorded', children: (0, r.jsx)('span', { lang: 'en', children: 'Quick Reference 1.2.7 Extended Audio Description (Prerecorded)' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/extended-audio-description-prerecorded.html', children: (0, r.jsx)('span', { lang: 'en', children: 'Understanding SC 1.2.7 Extended Audio Description (Prerecorded)' }) }), '.'] }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, r.jsx)(c, {}), '\n', (0, r.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, r.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, r.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, r.jsx)(d.ZP, {}), '\n', (0, r.jsx)(s.ZP, {})] });
+   }
+   function v(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(m, { ...e }) }) : m(e);
+   }
+  },
+  91410: (e, n, i) => {
+   i.d(n, { ZP: () => d });
+   var r = i(52676),
+    t = i(24785);
+   function o(e) {
+    const n = { a: 'a', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(n.p, { children: ['Heb je gebruikersonderzoek gedaan dat betrekking heeft op dit succescriterium en wil je dit delen? Kijk eens bij ', (0, r.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/onderzoek-delen/', children: 'Gebruikersonderzoeken delen' }), ' op gebruikersonderzoeken.nl.'] });
+   }
+   function d(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  88435: (e, n, i) => {
+   i.d(n, { ZP: () => d });
+   var r = i(52676),
+    t = i(24785);
+   function o(e) {
+    const n = { a: 'a', h2: 'h2', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.h2, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, r.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, r.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function d(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  66120: (e, n, i) => {
+   i.d(n, { V: () => d });
+   var r = i(4814);
+   i(75271);
+   const t = { 'nlds-inline-heading-group__heading': 'nlds-inline-heading-group__heading_bHAJ', 'nlds-inline-heading-group__suffix': 'nlds-inline-heading-group__suffix_ioH2' };
+   var o = i(52676);
+   const d = (e) => {
+    let { children: n, className: i, level: d = 1, suffix: s, ...a } = e;
+    return (0, o.jsxs)('hgroup', { className: (0, r.Z)(t['nlds-inline-heading-group'], `utrecht-heading-${d}`, i), ...a, children: [(0, o.jsx)('h1', { className: (0, r.Z)(t['nlds-inline-heading-group__heading']), children: n }), s && (0, o.jsxs)('p', { className: (0, r.Z)(t['nlds-inline-heading-group__suffix']), children: [s ? ' ' : '', s] })] });
+   };
+  },
+  51515: (e, n, i) => {
+   i.d(n, { j: () => a });
+   var r = i(46506),
+    t = (i(75271), i(66120));
+   const o = 'nlds-inline-heading-group--wcag-heading-group_hVFN',
+    d = 'nlds-inline-heading-group__badge_TgvT';
+   var s = i(52676);
+   const a = (e) => {
+    let { children: n, conformanceLevel: i, ...a } = e;
+    return (0, s.jsx)(t.V, { className: o, suffix: i && (0, s.jsx)(r.Ou, { className: d, children: i }), ...a, children: n });
+   };
+  },
+  24785: (e, n, i) => {
+   i.d(n, { Z: () => s, a: () => d });
+   var r = i(75271);
+   const t = {},
+    o = r.createContext(t);
+   function d(e) {
+    const n = r.useContext(o);
+    return r.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function s(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : d(e.components)), r.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
