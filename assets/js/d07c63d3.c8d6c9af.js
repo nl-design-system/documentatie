f64acf92f72395bfde9154348f3fd626248e6747
hidde <hidde@hiddedevries.nl>
2024-09-05 09:01:46 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@4f9282ccc5553553e181284ba5ebfc5afd01492a 🚀

---
+++ assets/js/d07c63d3.c8d6c9af.js
@@ -0,0 +1,107 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [86792],
+ {
+  44880: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => g, contentTitle: () => h, default: () => m, frontMatter: () => c, metadata: () => u, toc: () => p });
+   var r = i(52676),
+    t = i(24785),
+    o = i(51515),
+    s = i(91410),
+    a = i(88435);
+   function l(e) {
+    const n = { a: 'a', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.p, { children: 'Maak ten volste gebruik van de mogelijkheden van bijvoorbeeld HTML en ARIA om het doel van onderdelen van een webpagina duidelijk te maken voor software. Dan kan hulptechnologie aanvullende informatie geven en eventueel ook de vormgeving kunnen aanpassen.' }), '\n', (0, r.jsxs)(n.p, { children: ['Dit succescriterium gaat een stap verder dan ', (0, r.jsx)(n.a, { href: '/wcag/1.3.5', children: 'WCAG-succescriterium 1.3.5' }), '. Niet alleen het doel van formuliervelden, maar ook die van andere belangrijke onderdelen op een webpagina moet in de HTML-code beschreven zijn.'] }), '\n', (0, r.jsx)(n.p, { children: 'Hierdoor kan hulptechnologie een website aanpassen en beter begrijpbaar en bedienbaar maken voor mensen met een cognitieve beperking. Door bijvoorbeeld icoontjes aan te passen of toe te voegen of door te zorgen dat een gebruiker alleen de hoofdinhoud hoeft te lezen.' }), '\n', (0, r.jsx)(n.p, { children: "Een andere toepassing zou kunnen zijn om een 'schil' om de website te maken die alleen een reeks iconen laat zien, in plaats van een menu in woorden." }), '\n', (0, r.jsx)(n.p, { children: 'Dit helpt gebruikers die moeite hebben met bijvoorbeeld:' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsx)(n.li, { children: 'leren, communiceren, lezen, schrijven of rekenen;' }), '\n', (0, r.jsx)(n.li, { children: 'nieuwe of complexe informatie begrijpen of nieuwe vaardigheden leren;' }), '\n', (0, r.jsx)(n.li, { children: 'geheugen en aandacht.' }), '\n'] }), '\n', (0, r.jsx)(n.p, { children: 'Al deze technologie is volop in ontwikkeling en vergt nog veel onderzoek. Het W3C beveelt op dit moment ook nog geen technieken aan die in de praktijk gebruikt kunnen worden.' })] });
+   }
+   function d(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(l, { ...e }) }) : l(e);
+   }
+   const c = { title: 'WCAG-succescriterium 1.3.6 Identificeer het doel', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.3.6 Identificeer het doel', pagination_label: 'WCAG-succescriterium 1.3.6 Identificeer het doel', description: 'Maak ten volste gebruik van de mogelijkheden van bijvoorbeeld HTML en ARIA om het doel van onderdelen van een webpagina duidelijk te maken voor software.', slug: '1.3.6', keywords: ['WCAG'] },
+    h = void 0,
+    u = { id: 'wcag/1.3.06', title: 'WCAG-succescriterium 1.3.6 Identificeer het doel', description: 'Maak ten volste gebruik van de mogelijkheden van bijvoorbeeld HTML en ARIA om het doel van onderdelen van een webpagina duidelijk te maken voor software.', source: '@site/docs/wcag/1.3.06.mdx', sourceDirName: 'wcag', slug: '/wcag/1.3.6', permalink: '/wcag/1.3.6', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/1.3.06.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 1.3.6 Identificeer het doel', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.3.6 Identificeer het doel', pagination_label: 'WCAG-succescriterium 1.3.6 Identificeer het doel', description: 'Maak ten volste gebruik van de mogelijkheden van bijvoorbeeld HTML en ARIA om het doel van onderdelen van een webpagina duidelijk te maken voor software.', slug: '1.3.6', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 1.3.5 Identificeer het doel van de input', permalink: '/wcag/1.3.5' }, next: { title: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', permalink: '/wcag/1.4.1' } },
+    g = {},
+    p = [
+     { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
+     { value: 'Uitleg', id: 'uitleg', level: 2 },
+     { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
+    ];
+   function j(e) {
+    const n = { a: 'a', code: 'code', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: ['\n', '\n', '\n', '\n', (0, r.jsx)(o.j, { level: 1, conformanceLevel: 'Niveau AAA', children: 'WCAG-succescriterium 1.3.6 Identificeer het doel' }), '\n', (0, r.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#identify-purpose', children: (0, r.jsx)('span', { lang: 'en', children: '1.3.6 Identify Purpose' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#identificeer-het-doel', children: '1.3.6 Identificeer het doel' }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, r.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#identify-purpose', children: (0, r.jsx)('span', { lang: 'en', children: 'Quick Reference 1.3.6 Identify Purpose' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/identify-purpose.html', children: (0, r.jsx)('span', { lang: 'en', children: 'Understanding SC 1.3.6 Identify Purpose' }) }), '.'] }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, r.jsx)(d, {}), '\n', (0, r.jsx)(n.p, { children: 'Maar je kunt nu al informatie toevoegen die nuttig is voor mensen met een cognitieve beperking en hulpmiddelen.' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsx)(n.li, { children: "Zet zichtbare tekst bij een icoontje. Dus niet alleen 3 streepjes laten zien, maar zet ook het woord 'Menu' erbij." }), '\n', (0, r.jsxs)(n.li, { children: ['Maak gebruik van landmarks om de inhoud van een webpagina te groeperen. Zoals ', (0, r.jsx)(n.code, { children: '<header>' }), ', ', (0, r.jsx)(n.code, { children: '<main>' }), ', ', (0, r.jsx)(n.code, { children: '<aside>' }), ' en ', (0, r.jsx)(n.code, { children: '<footer>' }), '.'] }), '\n', (0, r.jsx)(n.li, { children: 'Geef landmarks die niet uniek zijn en ook sections een aria-label met de functie hiervan.' }), '\n'] }), '\n', (0, r.jsxs)(n.p, { children: ['Bijvoorbeeld, als er meerdere aside landmarks op een pagina staan, voeg dan aan elke een aria-label toe om het onderscheid aan te geven, zoals ', (0, r.jsx)(n.code, { children: '<aside aria-label="De laatste 3 nieuwsberichten">' }), ' en ', (0, r.jsx)(n.code, { children: '<aside aria-label="Geef je op voor de nieuwsbrief">' }), '.'] }), '\n', (0, r.jsx)(n.p, { children: 'Extra informatie over het doel van de link kan software in staat stellen om standaard iconen te tonen bij links, bijvoorbeeld:' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsxs)(n.li, { children: [(0, r.jsx)(n.code, { children: '<a href="/" rel="home">' }), ' met een huisje.'] }), '\n', (0, r.jsxs)(n.li, { children: [(0, r.jsx)(n.code, { children: '<a href="url" rel="next">' }), ' met een pijltje vooruit.'] }), '\n', (0, r.jsxs)(n.li, { children: [(0, r.jsx)(n.code, { children: '<a href="url" rel="prev">' }), ' met een pijltje achteruit.'] }), '\n', (0, r.jsxs)(n.li, { children: [(0, r.jsx)(n.code, { children: '<a href="url" rel="external">' }), ' met een herkenbaar icoon.'] }), '\n'] }), '\n', (0, r.jsx)(n.p, { children: 'Het HTML-attribuut autocomplete is ook bedoeld om hierbij te kunnen helpen.' }), '\n', (0, r.jsxs)(n.p, { children: ["Is een webpagina met tekst betekenisvol ingedeeld in landmarks en kopjes, dan kunnen browsers in de zogenaamde 'Reader mode' of in het Nederlands ", (0, r.jsx)(n.a, { href: 'https://kennisportaal.visio.org/nl-nl/documenten/internet-beter-leesbaar-maken-op-de-pc', children: 'Lezerweergave' }), ' de pagina simpeler weergeven en heeft de gebruiker ook de mogelijkheid om de weergave aan te passen. Wat fijn is voor gebruikers die snel zijn afgeleid door alle reclame en de tekst in voor hen duidelijker weergave te willen lezen.'] }), '\n', (0, r.jsxs)(n.p, { children: ['De ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/about/groups/task-forces/coga/', children: (0, r.jsx)('span', { lang: 'en', children: 'Cognitive and Learning Disabilities Accessibility Task Force' }) }), ' van het W3C is bezig technieken te onderzoeken die hiervoor gebruikt kunnen worden.'] }), '\n', (0, r.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, r.jsx)(s.ZP, {}), '\n', (0, r.jsx)(a.ZP, {})] });
+   }
+   function m(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(j, { ...e }) }) : j(e);
+   }
+  },
+  91410: (e, n, i) => {
+   i.d(n, { ZP: () => s });
+   var r = i(52676),
+    t = i(24785);
+   function o(e) {
+    const n = { a: 'a', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(n.p, { children: ['Heb je gebruikersonderzoek gedaan dat betrekking heeft op dit succescriterium en wil je dit delen? Kijk eens bij ', (0, r.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/onderzoek-delen/', children: 'Gebruikersonderzoeken delen' }), ' op gebruikersonderzoeken.nl.'] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  88435: (e, n, i) => {
+   i.d(n, { ZP: () => s });
+   var r = i(52676),
+    t = i(24785);
+   function o(e) {
+    const n = { a: 'a', h2: 'h2', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.h2, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, r.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, r.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  66120: (e, n, i) => {
+   i.d(n, { V: () => s });
+   var r = i(4814);
+   i(75271);
+   const t = { 'nlds-inline-heading-group__heading': 'nlds-inline-heading-group__heading_bHAJ', 'nlds-inline-heading-group__suffix': 'nlds-inline-heading-group__suffix_ioH2' };
+   var o = i(52676);
+   const s = (e) => {
+    let { children: n, className: i, level: s = 1, suffix: a, ...l } = e;
+    return (0, o.jsxs)('hgroup', { className: (0, r.Z)(t['nlds-inline-heading-group'], `utrecht-heading-${s}`, i), ...l, children: [(0, o.jsx)('h1', { className: (0, r.Z)(t['nlds-inline-heading-group__heading']), children: n }), a && (0, o.jsxs)('p', { className: (0, r.Z)(t['nlds-inline-heading-group__suffix']), children: [a ? ' ' : '', a] })] });
+   };
+  },
+  51515: (e, n, i) => {
+   i.d(n, { j: () => l });
+   var r = i(46506),
+    t = (i(75271), i(66120));
+   const o = 'nlds-inline-heading-group--wcag-heading-group_hVFN',
+    s = 'nlds-inline-heading-group__badge_TgvT';
+   var a = i(52676);
+   const l = (e) => {
+    let { children: n, conformanceLevel: i, ...l } = e;
+    return (0, a.jsx)(t.V, { className: o, suffix: i && (0, a.jsx)(r.Ou, { className: s, children: i }), ...l, children: n });
+   };
+  },
+  24785: (e, n, i) => {
+   i.d(n, { Z: () => a, a: () => s });
+   var r = i(75271);
+   const t = {},
+    o = r.createContext(t);
+   function s(e) {
+    const n = r.useContext(o);
+    return r.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : s(e.components)), r.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
