7acabdbdfaea6b5a4be669a2a54e1ee635785876
hidde <hidde@hiddedevries.nl>
2024-09-19 05:29:29 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@263417d236f6f1f7cce61a73f42fd53e99b4082d 🚀

---
+++ en/assets/js/d4f4f4b6.bf761ff1.js
@@ -0,0 +1,108 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [73075],
+ {
+  99099: (e, n, r) => {
+   r.r(n), r.d(n, { assets: () => h, contentTitle: () => g, default: () => f, frontMatter: () => c, metadata: () => u, toc: () => p });
+   var i = r(52676),
+    t = r(24785),
+    o = r(71595),
+    s = r(45054),
+    d = r(78836);
+   function l(e) {
+    const n = { li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.p, { children: 'Zorg ervoor dat opgenomen en uitgesproken tekst te verstaan is en niet gehinderd wordt door achtergrondgeluid.\nSlechthorende luisteraars kunnen last hebben gesproken tekst te onderscheiden van te harde achtergrondgeluiden of muziek.' }), '\n', (0, i.jsx)(n.p, { children: 'Oplossingen:' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsx)(n.li, { children: 'Voeg geen achtergrondgeluiden of muziek toe.' }), '\n', (0, i.jsx)(n.li, { children: 'Zorg bij de opname dat er weinig achtergrondgeluid te horen is, neem bijvoorbeeld een interview niet af in een drukke fabriekshal.' }), '\n', (0, i.jsx)(n.li, { children: 'Zorg ervoor dat de luisteraar achtergrondgeluiden kan uitzetten.' }), '\n', (0, i.jsx)(n.li, { children: 'Zorg ervoor dat de achtergrondgeluiden ten minste 20 decibel zachter zijn dan de uitgesproken tekst.' }), '\n'] }), '\n', (0, i.jsx)(n.p, { children: "Er is natuurlijk een uitzondering voor (rap)muziek waarin gesproken wordt door de muziek heen. En ook voor audio-CAPTCHA's waar het geluid expres lastig te verstaan gemaakt wordt. En als het geluid incidenteel is en niet langer dan een of twee seconden duurt." })] });
+   }
+   function a(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(l, { ...e }) }) : l(e);
+   }
+   const c = { title: 'WCAG-succescriterium 1.4.7 Weinig of geen achtergrondgeluid', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.7 Weinig of geen achtergrondgeluid', pagination_label: 'WCAG-succescriterium 1.4.7 Weinig of geen achtergrondgeluid', description: 'Zorg ervoor dat opgenomen en uitgesproken tekst te verstaan is en niet gehinderd wordt door achtergrondgeluid.', slug: '1.4.7', keywords: ['WCAG'] },
+    g = void 0,
+    u = { id: 'wcag/1.4.07', title: 'WCAG-succescriterium 1.4.7 Weinig of geen achtergrondgeluid', description: 'Zorg ervoor dat opgenomen en uitgesproken tekst te verstaan is en niet gehinderd wordt door achtergrondgeluid.', source: '@site/docs/wcag/1.4.07.mdx', sourceDirName: 'wcag', slug: '/wcag/1.4.7', permalink: '/en/wcag/1.4.7', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/1.4.07.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 1.4.7 Weinig of geen achtergrondgeluid', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.7 Weinig of geen achtergrondgeluid', pagination_label: 'WCAG-succescriterium 1.4.7 Weinig of geen achtergrondgeluid', description: 'Zorg ervoor dat opgenomen en uitgesproken tekst te verstaan is en niet gehinderd wordt door achtergrondgeluid.', slug: '1.4.7', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 1.4.6 Contrast (versterkt)', permalink: '/en/wcag/1.4.6' }, next: { title: 'WCAG-succescriterium 1.4.8 Visuele presentatie', permalink: '/en/wcag/1.4.8' } },
+    h = {},
+    p = [
+     { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
+     { value: 'Uitleg', id: 'uitleg', level: 2 },
+     { value: 'Opgelet', id: 'opgelet', level: 2 },
+     { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
+    ];
+   function m(e) {
+    const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: ['\n', '\n', '\n', '\n', (0, i.jsx)(o.j, { level: 1, conformanceLevel: 'Niveau AAA', children: 'WCAG-succescriterium 1.4.7 Weinig of geen achtergrondgeluid' }), '\n', (0, i.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#low-or-no-background-audio', children: (0, i.jsx)('span', { lang: 'en', children: '1.4.7 Low or No Background Audio' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#weinig-of-geen-achtergrondgeluid', children: '1.4.7 Weinig of geen achtergrondgeluid' }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, i.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#low-or-no-background-audio', children: (0, i.jsx)('span', { lang: 'en', children: 'Quick Reference 1.4.7 Low or No Background Audio' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/low-or-no-background-audio.html', children: (0, i.jsx)('span', { lang: 'en', children: 'Understanding SC 1.4.7 Low or No Background Audio' }) }), '.'] }), '\n'] }), '\n', (0, i.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, i.jsx)(a, {}), '\n', (0, i.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, i.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, i.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, i.jsx)(s.ZP, {}), '\n', (0, i.jsx)(d.ZP, {})] });
+   }
+   function f(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(m, { ...e }) }) : m(e);
+   }
+  },
+  45054: (e, n, r) => {
+   r.d(n, { ZP: () => s });
+   var i = r(52676),
+    t = r(24785);
+   function o(e) {
+    const n = { a: 'a', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, i.jsxs)(n.p, { children: ['Heb je gebruikersonderzoek gedaan dat betrekking heeft op dit succescriterium en wil je dit delen? Kijk eens bij ', (0, i.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/onderzoek-delen/', children: 'Gebruikersonderzoeken delen' }), ' op gebruikersonderzoeken.nl.'] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  78836: (e, n, r) => {
+   r.d(n, { ZP: () => s });
+   var i = r(52676),
+    t = r(24785);
+   function o(e) {
+    const n = { a: 'a', h2: 'h2', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h2, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, i.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, i.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  72657: (e, n, r) => {
+   r.d(n, { V: () => s });
+   var i = r(4814);
+   r(75271);
+   const t = { 'nlds-inline-heading-group__heading': 'nlds-inline-heading-group__heading_bHAJ', 'nlds-inline-heading-group__suffix': 'nlds-inline-heading-group__suffix_ioH2' };
+   var o = r(52676);
+   const s = (e) => {
+    let { children: n, className: r, level: s = 1, suffix: d, ...l } = e;
+    return (0, o.jsxs)('hgroup', { className: (0, i.Z)(t['nlds-inline-heading-group'], `utrecht-heading-${s}`, r), ...l, children: [(0, o.jsx)('h1', { className: (0, i.Z)(t['nlds-inline-heading-group__heading']), children: n }), d && (0, o.jsxs)('p', { className: (0, i.Z)(t['nlds-inline-heading-group__suffix']), children: [d ? ' ' : '', d] })] });
+   };
+  },
+  71595: (e, n, r) => {
+   r.d(n, { j: () => l });
+   var i = r(46506),
+    t = (r(75271), r(72657));
+   const o = 'nlds-inline-heading-group--wcag-heading-group_hVFN',
+    s = 'nlds-inline-heading-group__badge_TgvT';
+   var d = r(52676);
+   const l = (e) => {
+    let { children: n, conformanceLevel: r, ...l } = e;
+    return (0, d.jsx)(t.V, { className: o, suffix: r && (0, d.jsx)(i.Ou, { className: s, children: r }), ...l, children: n });
+   };
+  },
+  24785: (e, n, r) => {
+   r.d(n, { Z: () => d, a: () => s });
+   var i = r(75271);
+   const t = {},
+    o = i.createContext(t);
+   function s(e) {
+    const n = i.useContext(o);
+    return i.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function d(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : s(e.components)), i.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
