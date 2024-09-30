561f6a4d17bdad0f8e7a4a0f574609198efe8f93
Yolijn <mail@yolijn.com>
2024-09-30 18:35:00 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@040bd1e7cc4c126bf718d94d8b4ca7bdffe4a9a2 🚀

---
+++ en/assets/js/235c86a2.a0f62c1c.js
@@ -0,0 +1,58 @@
+/*! For license information please see 235c86a2.a0f62c1c.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [88979],
+ {
+  20069: (e, t, n) => {
+   n.r(t), n.d(t, { assets: () => h, contentTitle: () => o, default: () => u, frontMatter: () => d, metadata: () => c, toc: () => b });
+   var a = n(52676),
+    i = n(24785),
+    r = n(90441),
+    s = n(81094),
+    l = n(46506);
+   const d = { title: 'Heartbeat', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Heartbeat', pagination_label: '2 wekelijkse updates van het kernteam en community', description: 'Wat is de Heartbeat?', slug: '/events/heartbeat' },
+    o = 'Heartbeat',
+    c = { id: 'community/events/heartbeat/heartbeat', title: 'Heartbeat', description: 'Wat is de Heartbeat?', source: '@site/docs/community/events/heartbeat/heartbeat.mdx', sourceDirName: 'community/events/heartbeat', slug: '/events/heartbeat', permalink: '/en/events/heartbeat', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/heartbeat/heartbeat.mdx', tags: [], version: 'current', frontMatter: { title: 'Heartbeat', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Heartbeat', pagination_label: '2 wekelijkse updates van het kernteam en community', description: 'Wat is de Heartbeat?', slug: '/events/heartbeat' }, sidebar: 'community', previous: { title: 'Heartbeat', permalink: '/en/events/heartbeat/overzicht' }, next: { title: "Video's van de afgelopen Heartbeats", permalink: '/en/events/heartbeat/videos' } },
+    h = {},
+    b = [
+     { value: 'Planning', id: 'planning', level: 2 },
+     { value: 'Vragen of iets vertellen?', id: 'vragen-of-iets-vertellen', level: 2 },
+    ];
+   function m(e) {
+    const t = { a: 'a', h1: 'h1', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, i.a)(), ...e.components };
+    return (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(t.h1, { id: 'heartbeat', children: 'Heartbeat' }), '\n', (0, a.jsx)(t.p, { children: 'In de Heartbeat vertelt het kernteam van NL Design System elke twee weken wat de laatste stand van zaken is. Daarnaast laten we organisaties aan het woord die met/aan het NL Design System werken.' }), '\n', (0, a.jsx)(t.p, { children: 'Deze sessies zijn publiek toegankelijk.' }), '\n', (0, a.jsxs)(l.hE, { children: [(0, a.jsxs)(r.Z, { href: '/events/heartbeat/aanmelden', className: 'utrecht-button-link utrecht-button-link--primary-action', children: ['Meld je aan', (0, a.jsx)(s.Z, {})] }), (0, a.jsxs)(r.Z, { href: '/events/heartbeat/videos', className: 'utrecht-button-link utrecht-button-link--secondary-action', children: ['Kijk de Heartbeat terug', (0, a.jsx)(s.Z, {})] })] }), '\n', (0, a.jsx)(t.h2, { id: 'planning', children: 'Planning' }), '\n', (0, a.jsx)(t.p, { children: 'De Heartbeat vindt om de week plaats op dinsdag. Dit zijn de data voor 2024:' }), '\n', (0, a.jsxs)(t.ul, { children: ['\n', (0, a.jsx)(t.li, { children: '9 januari' }), '\n', (0, a.jsx)(t.li, { children: '23 januari' }), '\n', (0, a.jsx)(t.li, { children: '6 februari' }), '\n', (0, a.jsx)(t.li, { children: '20 februari' }), '\n', (0, a.jsx)(t.li, { children: '5 maart' }), '\n', (0, a.jsx)(t.li, { children: '19 maart' }), '\n', (0, a.jsx)(t.li, { children: '2 april' }), '\n', (0, a.jsx)(t.li, { children: '16 april' }), '\n', (0, a.jsx)(t.li, { children: '30 april' }), '\n', (0, a.jsx)(t.li, { children: '14 mei' }), '\n', (0, a.jsx)(t.li, { children: '28 mei' }), '\n', (0, a.jsx)(t.li, { children: '11 juni' }), '\n', (0, a.jsx)(t.li, { children: '25 juni' }), '\n', (0, a.jsx)(t.li, { children: '9 juli' }), '\n', (0, a.jsx)(t.li, { children: '3 september' }), '\n', (0, a.jsx)(t.li, { children: '17 september' }), '\n', (0, a.jsx)(t.li, { children: '1 oktober' }), '\n', (0, a.jsx)(t.li, { children: '15 oktober' }), '\n', (0, a.jsx)(t.li, { children: '29 oktober' }), '\n', (0, a.jsx)(t.li, { children: '12 november' }), '\n', (0, a.jsx)(t.li, { children: '26 november' }), '\n', (0, a.jsx)(t.li, { children: '10 december' }), '\n'] }), '\n', (0, a.jsx)(t.p, { children: (0, a.jsx)(t.a, { target: '_blank', 'data-noBrokenLinkCheck': !0, href: n(29560).Z + '', children: 'Voeg Heartbeats aan je kalender (.ics)' }) }), '\n', (0, a.jsx)(t.h2, { id: 'vragen-of-iets-vertellen', children: 'Vragen of iets vertellen?' }), '\n', (0, a.jsxs)(t.p, { children: ['Heb je vragen of wil je zelf iets vertellen of presenteren tijdens een van de heartbeats? Stuur ons dan een mailtje op ', (0, a.jsx)('a', { href: 'mailto:kernteam@nldesignsystem.nl', children: (0, a.jsx)(t.a, { href: 'mailto:kernteam@nldesignsystem.nl', children: 'kernteam@nldesignsystem.nl' }) })] })] });
+   }
+   function u(e = {}) {
+    const { wrapper: t } = { ...(0, i.a)(), ...e.components };
+    return t ? (0, a.jsx)(t, { ...e, children: (0, a.jsx)(m, { ...e }) }) : m(e);
+   }
+  },
+  29560: (e, t, n) => {
+   n.d(t, { Z: () => a });
+   const a = n.p + 'assets/files/heartbeat-6b4da003e651efaa9192a8bf111929b0.ics';
+  },
+  24785: (e, t, n) => {
+   n.d(t, { Z: () => l, a: () => s });
+   var a = n(75271);
+   const i = {},
+    r = a.createContext(i);
+   function s(e) {
+    const t = a.useContext(r);
+    return a.useMemo(
+     function () {
+      return 'function' == typeof e ? e(t) : { ...t, ...e };
+     },
+     [t, e],
+    );
+   }
+   function l(e) {
+    let t;
+    return (t = e.disableParentContext ? ('function' == typeof e.components ? e.components(i) : e.components || i) : s(e.components)), a.createElement(r.Provider, { value: t }, e.children);
+   }
+  },
+  81094: (e, t, n) => {
+   n.d(t, { Z: () => a });
+   var a = (0, n(69804).Z)('outline', 'chevron-right', 'IconChevronRight', [['path', { d: 'M9 6l6 6l-6 6', key: 'svg-0' }]]);
+  },
+ },
+]);
