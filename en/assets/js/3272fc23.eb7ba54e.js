40ab59eed347f5941967d92e6f6fe0f9952a86ba
Yolijn <mail@yolijn.com>
2024-09-30 14:36:20 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@08e77d565cd7f595493d3532d20c5606713ae2d8 🚀

---
+++ en/assets/js/3272fc23.eb7ba54e.js
@@ -0,0 +1,53 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [16562],
+ {
+  67670: (e, s, n) => {
+   n.r(s), n.d(s, { assets: () => l, contentTitle: () => o, default: () => c, frontMatter: () => d, metadata: () => a, toc: () => m });
+   var t = n(52676),
+    i = n(24785),
+    r = n(46506);
+   const d = { title: 'Design Systems Week 2024', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'Over Design Systems Week', pagination_label: 'Over Design Systems Week', sidebar_position: 1, slug: '/events/design-systems-week-2024', image: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/dsw-24.png' },
+    o = 'Design Systems Week 2024',
+    a = { id: 'community/events/design-systems-week-2024/index', title: 'Design Systems Week 2024', description: 'NL Design System organiseert dit jaar voor de 4e keer de Design Systems Week. Van 14 tot en met 17 oktober zijn er dagelijks meerdere korte sessies van diverse organisaties over het hoe en waarom van design systems te volgen.', source: '@site/docs/community/events/design-systems-week-2024/index.md', sourceDirName: 'community/events/design-systems-week-2024', slug: '/events/design-systems-week-2024', permalink: '/en/events/design-systems-week-2024', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/design-systems-week-2024/index.md', tags: [], version: 'current', sidebarPosition: 1, frontMatter: { title: 'Design Systems Week 2024', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'Over Design Systems Week', pagination_label: 'Over Design Systems Week', sidebar_position: 1, slug: '/events/design-systems-week-2024', image: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/dsw-24.png' }, sidebar: 'community', previous: { title: 'In het Design Open Hour wisselen designers informatie, inzichten en tips met elkaar uit.', permalink: '/en/community/events/estafettemodeldag' }, next: { title: 'Over Design Systems Week', permalink: '/en/events/design-systems-week-2024' } },
+    l = {},
+    m = [
+     { value: 'Organisatie', id: 'organisatie', level: 2 },
+     { value: 'Eerdere edities', id: 'eerdere-edities', level: 2 },
+     { value: 'Toegankelijkheid', id: 'toegankelijkheid', level: 2 },
+     { value: 'Code of Conduct', id: 'code-of-conduct', level: 2 },
+    ];
+   function g(e) {
+    const s = { a: 'a', h1: 'h1', h2: 'h2', hr: 'hr', li: 'li', p: 'p', strong: 'strong', ul: 'ul', ...(0, i.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(s.h1, { id: 'design-systems-week-2024', children: 'Design Systems Week 2024' }), '\n', (0, t.jsxs)(r.nv, { lead: !0, children: ['NL Design System organiseert dit jaar voor de 4e keer de Design Systems Week. Van ', (0, t.jsx)(s.strong, { children: '14 tot en met 17 oktober' }), ' zijn er dagelijks meerdere korte sessies van diverse organisaties over het ', (0, t.jsx)(s.strong, { children: 'hoe en waarom van design systems' }), ' te volgen.'] }), '\n', (0, t.jsx)(r.nv, { children: 'Dus werk jij aan digitale diensten bij de overheid? Meld je dan aan en we houden je op de hoogte van alle sessies. Alle sessies zijn gratis (online) bij te wonen en duren ongeveer 30 minuten.' }), '\n', (0, t.jsxs)(s.ul, { children: ['\n', (0, t.jsxs)(s.li, { children: [(0, t.jsx)(s.a, { target: '_blank', 'data-noBrokenLinkCheck': !0, href: n(51913).Z + '', children: 'Download kalenderbestandje' }), '; zo heb je de dagen vast in je agenda.'] }), '\n', (0, t.jsxs)(s.li, { children: [(0, t.jsx)(s.a, { href: '/events/design-systems-week/aanmelden', children: 'Meld je aan' }), ' en blijf op de hoogte.'] }), '\n', (0, t.jsxs)(s.li, { children: [(0, t.jsx)(s.a, { href: '/events/design-systems-week-2024/programma', children: 'Bekijk het programma' }), '; de eerste sprekers zijn bekend.'] }), '\n'] }), '\n', (0, t.jsx)(s.hr, {}), '\n', (0, t.jsx)(s.h2, { id: 'organisatie', children: 'Organisatie' }), '\n', (0, t.jsx)(s.p, { children: 'Design Systems Week wordt georganiseerd door het kernteam van NL Design System.' }), '\n', (0, t.jsxs)(s.p, { children: ['NL Design System is een project dat specialisten van verschillende overheidsorganisaties laat samenwerken aan concrete oplossingen voor websites en applicaties. Denk bijvoorbeeld aan code, ontwerp en richtlijnen. Het project wordt uitgevoerd door ICTU, in opdracht van het Ministerie van Binnenlandse Zaken en Koninkrijksrelaties. Zie ook: meer ', (0, t.jsx)(s.a, { href: '/project/over-nl-design-system', children: 'over NL Design System' }), ' en ', (0, t.jsx)(s.a, { href: '/project/faq', children: 'Veelgestelde vragen' }), '.'] }), '\n', (0, t.jsx)(s.h2, { id: 'eerdere-edities', children: 'Eerdere edities' }), '\n', (0, t.jsx)(s.p, { children: 'Eerdere edities die we organiseerden:' }), '\n', (0, t.jsxs)(s.ul, { children: ['\n', (0, t.jsx)(s.li, { children: (0, t.jsx)(s.a, { href: '/events/design-systems-week-2023', children: 'Design Systems Week 2023' }) }), '\n', (0, t.jsx)(s.li, { children: (0, t.jsx)(s.a, { href: '/events/design-systems-week-2022', children: 'Design Systems Week 2022' }) }), '\n', (0, t.jsx)(s.li, { children: 'Onboarding Week 2021' }), '\n'] }), '\n', (0, t.jsx)(s.h2, { id: 'toegankelijkheid', children: 'Toegankelijkheid' }), '\n', (0, t.jsxs)(s.p, { children: ['We doen ons best Design Systems Week zo toegankelijk als mogelijk te organiseren. Indien je specifieke toegankelijkheidsvragen- of behoeften hebt, schroom niet te mailen met het NL Design System kernteam op ', (0, t.jsx)(s.a, { href: 'mailto:kernteam@nldesignsystem.nl', children: 'kernteam@nldesignsystem.nl' }), '.'] }), '\n', (0, t.jsx)(s.h2, { id: 'code-of-conduct', children: 'Code of Conduct' }), '\n', (0, t.jsxs)(s.p, { children: ['Op Design Systems Week is de ', (0, t.jsx)(s.a, { href: 'https://github.com/nl-design-system/.github/blob/main/CODE_OF_CONDUCT.nl.md', children: 'NL Design System Code of Conduct' }), ' van toepassing. Je gaat daarmee akkoord als je je aanmeldt of spreekt.'] })] });
+   }
+   function c(e = {}) {
+    const { wrapper: s } = { ...(0, i.a)(), ...e.components };
+    return s ? (0, t.jsx)(s, { ...e, children: (0, t.jsx)(g, { ...e }) }) : g(e);
+   }
+  },
+  51913: (e, s, n) => {
+   n.d(s, { Z: () => t });
+   const t = n.p + 'assets/files/dsweek-2024-8ea220d16a74f3d91591ee3953b9a300.ics';
+  },
+  24785: (e, s, n) => {
+   n.d(s, { Z: () => o, a: () => d });
+   var t = n(75271);
+   const i = {},
+    r = t.createContext(i);
+   function d(e) {
+    const s = t.useContext(r);
+    return t.useMemo(
+     function () {
+      return 'function' == typeof e ? e(s) : { ...s, ...e };
+     },
+     [s, e],
+    );
+   }
+   function o(e) {
+    let s;
+    return (s = e.disableParentContext ? ('function' == typeof e.components ? e.components(i) : e.components || i) : d(e.components)), t.createElement(r.Provider, { value: s }, e.children);
+   }
+  },
+ },
+]);
---
+++ en/assets/js/7074c05a.a57afbbd.js
@@ -0,0 +1,53 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [13471],
+ {
+  10223: (e, s, n) => {
+   n.r(s), n.d(s, { assets: () => l, contentTitle: () => a, default: () => m, frontMatter: () => r, metadata: () => d, toc: () => c });
+   var t = n(52676),
+    i = n(24785),
+    o = n(46506);
+   const r = { title: 'Design Systems Week 2024', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'About', pagination_label: 'About', sidebar_position: 1, slug: '/events/design-systems-week-2024/en', image: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/dsw-24-en.png' },
+    a = 'Design Systems Week 2024',
+    d = { id: 'community/events/design-systems-week-2024/english/index', title: 'Design Systems Week 2024', description: 'From 14 to 17 October 2024, NL Design System organises the 4th edition of Design Systems Week. Join us for short talks about the how and why of design systems, from managing design systems to user research and accessibility. This year there will be talks both in Dutch and English.', source: '@site/docs/community/events/design-systems-week-2024/english/index.md', sourceDirName: 'community/events/design-systems-week-2024/english', slug: '/events/design-systems-week-2024/en', permalink: '/en/events/design-systems-week-2024/en', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/design-systems-week-2024/english/index.md', tags: [], version: 'current', sidebarPosition: 1, frontMatter: { title: 'Design Systems Week 2024', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'About', pagination_label: 'About', sidebar_position: 1, slug: '/events/design-systems-week-2024/en', image: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/dsw-24-en.png' }, sidebar: 'community', previous: { title: 'Tijdschema', permalink: '/en/events/design-systems-week-2024/tijdschema' }, next: { title: 'Program', permalink: '/en/events/design-systems-week-2024/en/program' } },
+    l = {},
+    c = [
+     { value: 'Organisation', id: 'organisation', level: 2 },
+     { value: 'Previous editions', id: 'previous-editions', level: 2 },
+     { value: 'Accessibility', id: 'accessibility', level: 2 },
+     { value: 'Code of Conduct', id: 'code-of-conduct', level: 2 },
+    ];
+   function h(e) {
+    const s = { a: 'a', h1: 'h1', h2: 'h2', hr: 'hr', li: 'li', p: 'p', strong: 'strong', ul: 'ul', ...(0, i.a)(), ...e.components };
+    return (0, t.jsxs)('div', { lang: 'en', children: [(0, t.jsx)(s.h1, { id: 'design-systems-week-2024', children: 'Design Systems Week 2024' }), (0, t.jsxs)(o.nv, { lead: !0, children: ['From 14 to 17 October 2024, NL Design System organises the 4th edition of Design Systems Week. Join us for short talks about the ', (0, t.jsx)(s.strong, { children: 'how' }), ' and ', (0, t.jsx)(s.strong, { children: 'why' }), ' of design systems, from managing design systems to user research and accessibility. This year there will be talks both in Dutch and English.'] }), (0, t.jsx)(o.nv, { children: 'Will you join us? All talks are online and free to attend. They are about 30 minutes each, including Q&A.' }), (0, t.jsxs)(s.ul, { children: ['\n', (0, t.jsxs)(s.li, { children: [(0, t.jsx)(s.a, { target: '_blank', 'data-noBrokenLinkCheck': !0, href: n(51913).Z + '', children: 'Download calendar invite' }), '; to have the days blocked in your calendar.'] }), '\n', (0, t.jsxs)(s.li, { children: [(0, t.jsx)(s.a, { href: '/events/design-systems-week/sign-up', children: 'Sign up' }), ' to receive updates.'] }), '\n', (0, t.jsxs)(s.li, { children: [(0, t.jsx)(s.a, { href: '/events/design-systems-week-2024/en/program', children: 'View the program so far' }), "; we've just announced the first speakers."] }), '\n'] }), (0, t.jsx)(s.hr, {}), (0, t.jsx)(s.h2, { id: 'organisation', children: 'Organisation' }), (0, t.jsx)(s.p, { children: 'Design Systems Week is organised by the NL Design System core team.' }), (0, t.jsx)(s.p, { children: 'At NL Design System, web professionals from across the government collaborate on building blocks for websites and applications, like code, design and guidelines. The project is managed by ICTU for the Dutch Ministry of the Interior and Kingdom Relations.' }), (0, t.jsx)(s.h2, { id: 'previous-editions', children: 'Previous editions' }), (0, t.jsx)(s.p, { children: "We've previously organised:" }), (0, t.jsxs)(s.ul, { children: ['\n', (0, t.jsx)(s.li, { children: (0, t.jsx)(s.a, { href: '/events/design-systems-week-2023', children: 'Design Systems Week 2023' }) }), '\n', (0, t.jsx)(s.li, { children: (0, t.jsx)(s.a, { href: '/events/design-systems-week-2022', children: 'Design Systems Week 2022' }) }), '\n', (0, t.jsx)(s.li, { children: 'Onboarding Week 2021' }), '\n'] }), (0, t.jsx)(s.h2, { id: 'accessibility', children: 'Accessibility' }), (0, t.jsxs)(s.p, { children: ['We do our best to organise Design Systems Week accessibly. If you have specific questions or requests, please do reach to the NL Design System core team at ', (0, t.jsx)(s.a, { href: 'mailto:kernteam@nldesignsystem.nl', children: 'kernteam@nldesignsystem.nl' }), '.'] }), (0, t.jsx)(s.h2, { id: 'code-of-conduct', children: 'Code of Conduct' }), (0, t.jsxs)(s.p, { children: ['All participants of Design Systems Week are expected to abide by our ', (0, t.jsx)(s.a, { href: 'https://github.com/nl-design-system/.github/blob/main/CODE_OF_CONDUCT.md', children: 'NL Design System Code of Conduct' }), ". By signing up for one or more sessions you've agreed to these terms."] })] });
+   }
+   function m(e = {}) {
+    const { wrapper: s } = { ...(0, i.a)(), ...e.components };
+    return s ? (0, t.jsx)(s, { ...e, children: (0, t.jsx)(h, { ...e }) }) : h(e);
+   }
+  },
+  51913: (e, s, n) => {
+   n.d(s, { Z: () => t });
+   const t = n.p + 'assets/files/dsweek-2024-8ea220d16a74f3d91591ee3953b9a300.ics';
+  },
+  24785: (e, s, n) => {
+   n.d(s, { Z: () => a, a: () => r });
+   var t = n(75271);
+   const i = {},
+    o = t.createContext(i);
+   function r(e) {
+    const s = t.useContext(o);
+    return t.useMemo(
+     function () {
+      return 'function' == typeof e ? e(s) : { ...s, ...e };
+     },
+     [s, e],
+    );
+   }
+   function a(e) {
+    let s;
+    return (s = e.disableParentContext ? ('function' == typeof e.components ? e.components(i) : e.components || i) : r(e.components)), t.createElement(o.Provider, { value: s }, e.children);
+   }
+  },
+ },
+]);
