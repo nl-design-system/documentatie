492fb7dbf3a632eb7a26362b6fd1977fe4c7ac6a
hidde <hidde@hiddedevries.nl>
2024-10-02 04:07:18 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@6f4bbe1d32e3461973eb4eb203f93e6acee6f03e 🚀

--- assets/js/63095b4c.ad17d0c6.js
+++ assets/js/63095b4c.ad17d0c6.js
@@ -3,7 +3,7 @@
  [40667],
  {
   7490: (e, n, i) => {
-   i.r(n), i.d(n, { assets: () => g, contentTitle: () => u, default: () => k, frontMatter: () => d, metadata: () => h, toc: () => m });
+   i.r(n), i.d(n, { assets: () => g, contentTitle: () => u, default: () => p, frontMatter: () => d, metadata: () => h, toc: () => m });
    var t = i(52676),
     s = i(24785),
     r = i(71595),
@@ -19,7 +19,7 @@
    }
    const d = { title: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '3.3.8 Toegankelijke authenticatie (minimum)', pagination_label: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', description: 'Maak inloggen makkelijk. Zorg dat inloggen niet afhankelijk is van een cognitieve functietest, zoals het onthouden van een wachtwoord.', slug: '3.3.8', keywords: ['WCAG'] },
     u = void 0,
-    h = { id: 'wcag/3.3.08', title: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', description: 'Maak inloggen makkelijk. Zorg dat inloggen niet afhankelijk is van een cognitieve functietest, zoals het onthouden van een wachtwoord.', source: '@site/docs/wcag/3.3.08.mdx', sourceDirName: 'wcag', slug: '/wcag/3.3.8', permalink: '/wcag/3.3.8', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/3.3.08.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '3.3.8 Toegankelijke authenticatie (minimum)', pagination_label: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', description: 'Maak inloggen makkelijk. Zorg dat inloggen niet afhankelijk is van een cognitieve functietest, zoals het onthouden van een wachtwoord.', slug: '3.3.8', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 3.3.7 Overbodige invoer', permalink: '/wcag/3.3.7' }, next: { title: 'WCAG-succescriterium 4.1.1 Parsen', permalink: '/wcag/4.1.1' } },
+    h = { id: 'wcag/3.3.08', title: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', description: 'Maak inloggen makkelijk. Zorg dat inloggen niet afhankelijk is van een cognitieve functietest, zoals het onthouden van een wachtwoord.', source: '@site/docs/wcag/3.3.08.mdx', sourceDirName: 'wcag', slug: '/wcag/3.3.8', permalink: '/wcag/3.3.8', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/3.3.08.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '3.3.8 Toegankelijke authenticatie (minimum)', pagination_label: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)', description: 'Maak inloggen makkelijk. Zorg dat inloggen niet afhankelijk is van een cognitieve functietest, zoals het onthouden van een wachtwoord.', slug: '3.3.8', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 3.3.7 Overbodige invoer', permalink: '/wcag/3.3.7' }, next: { title: 'WCAG-succescriterium 3.3.9 Toegankelijke authenticatie (uitgebreid)', permalink: '/wcag/3.3.9' } },
     g = {},
     m = [
      { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
@@ -27,13 +27,13 @@
      { value: 'Opgelet', id: 'opgelet', level: 2 },
      { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
     ];
-   function p(e) {
+   function k(e) {
     const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
     return (0, t.jsxs)(t.Fragment, { children: ['\n', '\n', '\n', '\n', (0, t.jsx)(r.j, { level: 1, conformanceLevel: 'Niveau AA', children: 'WCAG-succescriterium 3.3.8 Toegankelijke authenticatie (minimum)' }), '\n', (0, t.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, t.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#accessible-authentication-minimum', children: (0, t.jsx)('span', { lang: 'en', children: '3.3.8 Accessible Authentication (Minimum)' }) }), '.'] }), '\n', (0, t.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, t.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#accessible-authentication-minimum', children: '3.3.8 Toegankelijke authenticatie (minimum)' }), '.'] }), '\n', (0, t.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, t.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, t.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#accessible-authentication-minimum', children: (0, t.jsx)('span', { lang: 'en', children: 'Quick Reference 3.3.8 Accessible Authentication (Minimum)' }) }), '.'] }), '\n', (0, t.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, t.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/accessible-authentication-minimum.html', children: (0, t.jsx)('span', { lang: 'en', children: 'Understanding SC 3.3.8 Accessible Authentication (Minimum)' }) }), '.'] }), '\n'] }), '\n', (0, t.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, t.jsx)(c, {}), '\n', (0, t.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, t.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, t.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, t.jsx)(a.ZP, {}), '\n', (0, t.jsx)(o.ZP, {})] });
    }
-   function k(e = {}) {
+   function p(e = {}) {
     const { wrapper: n } = { ...(0, s.a)(), ...e.components };
-    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(p, { ...e }) }) : p(e);
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(k, { ...e }) }) : k(e);
    }
   },
   45054: (e, n, i) => {
--- assets/js/f45121d1.649571b6.js
+++ assets/js/f45121d1.649571b6.js
@@ -3,7 +3,7 @@
  [94846],
  {
   92377: (e, n, r) => {
-   r.r(n), r.d(n, { assets: () => g, contentTitle: () => h, default: () => f, frontMatter: () => c, metadata: () => u, toc: () => p });
+   r.r(n), r.d(n, { assets: () => u, contentTitle: () => h, default: () => m, frontMatter: () => c, metadata: () => g, toc: () => p });
    var i = r(52676),
     s = r(24785),
     t = r(71595),
@@ -11,29 +11,29 @@
     d = r(78836);
    function o(e) {
     const n = { p: 'p', ...(0, s.a)(), ...e.components };
-    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.p, { children: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen per seconde.' }), '\n', (0, i.jsx)(n.p, { children: 'Het tonen van meer dan drie flitsen per seconde kan een epileptische aanval veroorzaken bij gebruikers die hiervoor gevoelig zijn.' }), '\n', (0, i.jsx)(n.p, { children: "Dit geldt voor alle onderdelen van de website, zoals animaties, video's en de overgang tussen pagina's." })] });
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.p, { children: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen binnen \xe9\xe9n seconde of maak het gebied dat flitsen bevat klein genoeg.' }), '\n', (0, i.jsx)(n.p, { children: 'Het tonen van meer dan drie flitsen binnen \xe9\xe9n seconde kan een epileptische aanval veroorzaken bij gebruikers die hiervoor gevoelig zijn.' }), '\n', (0, i.jsx)(n.p, { children: "Dit geldt voor alle onderdelen van de website, zoals animaties, video's en de overgang tussen pagina's." })] });
    }
    function a(e = {}) {
     const { wrapper: n } = { ...(0, s.a)(), ...e.components };
     return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(o, { ...e }) }) : o(e);
    }
-   const c = { title: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '2.3.1 Drie flitsen of beneden drempelwaarde', pagination_label: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', description: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen per seconde.', slug: '2.3.1', keywords: ['WCAG'] },
+   const c = { title: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '2.3.1 Drie flitsen of beneden drempelwaarde', pagination_label: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', description: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen binnen \xe9\xe9n seconde of maak het gebied dat flitsen bevat klein genoeg.', slug: '2.3.1', keywords: ['WCAG'] },
     h = void 0,
-    u = { id: 'wcag/2.3.01', title: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', description: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen per seconde.', source: '@site/docs/wcag/2.3.01.mdx', sourceDirName: 'wcag', slug: '/wcag/2.3.1', permalink: '/wcag/2.3.1', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/2.3.01.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '2.3.1 Drie flitsen of beneden drempelwaarde', pagination_label: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', description: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen per seconde.', slug: '2.3.1', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 2.2.4 Onderbrekingen', permalink: '/wcag/2.2.4' }, next: { title: 'WCAG-succescriterium 2.4.1 Blokken omzeilen', permalink: '/wcag/2.4.1' } },
-    g = {},
+    g = { id: 'wcag/2.3.01', title: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', description: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen binnen \xe9\xe9n seconde of maak het gebied dat flitsen bevat klein genoeg.', source: '@site/docs/wcag/2.3.01.mdx', sourceDirName: 'wcag', slug: '/wcag/2.3.1', permalink: '/wcag/2.3.1', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/2.3.01.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '2.3.1 Drie flitsen of beneden drempelwaarde', pagination_label: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde', description: 'Zorg ervoor dat de website geen onderdelen bevat met meer dan drie flitsen binnen \xe9\xe9n seconde of maak het gebied dat flitsen bevat klein genoeg.', slug: '2.3.1', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 2.2.4 Onderbrekingen', permalink: '/wcag/2.2.4' }, next: { title: 'WCAG-succescriterium 2.3.2 Drie flitsen', permalink: '/wcag/2.3.2' } },
+    u = {},
     p = [
      { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
      { value: 'Uitleg', id: 'uitleg', level: 2 },
      { value: 'Opgelet', id: 'opgelet', level: 2 },
      { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
     ];
-   function m(e) {
+   function f(e) {
     const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
     return (0, i.jsxs)(i.Fragment, { children: ['\n', '\n', '\n', '\n', (0, i.jsx)(t.j, { level: 1, conformanceLevel: 'Niveau A', children: 'WCAG-succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde' }), '\n', (0, i.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#three-flashes-or-below-threshold', children: (0, i.jsx)('span', { lang: 'en', children: '2.3.1 Three Flashes or Below Threshold' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#drie-flitsen-of-beneden-drempelwaarde', children: '2.3.1 Drie flitsen of beneden drempelwaarde' }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, i.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#three-flashes-or-below-threshold', children: (0, i.jsx)('span', { lang: 'en', children: 'Quick Reference 2.3.1 Three Flashes or Below Threshold' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/three-flashes-or-below-threshold.html', children: (0, i.jsx)('span', { lang: 'en', children: 'Understanding SC 2.3.1 Three Flashes or Below Threshold' }) }), '.'] }), '\n'] }), '\n', (0, i.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, i.jsx)(a, {}), '\n', (0, i.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, i.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, i.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, i.jsx)(l.ZP, {}), '\n', (0, i.jsx)(d.ZP, {})] });
    }
-   function f(e = {}) {
+   function m(e = {}) {
     const { wrapper: n } = { ...(0, s.a)(), ...e.components };
-    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(m, { ...e }) }) : m(e);
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(f, { ...e }) }) : f(e);
    }
   },
   45054: (e, n, r) => {
