065f4c005c7ec86c48bb5e7aca4aa90c76985129
Yolijn <yolijn@frameless.io>
2024-10-10 15:59:26 +0200

    docs: add organisation community steps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -57,7 +57,7 @@ module.exports = {
       files: ['*.ts', '*.tsx'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: ['tsconfig.json'],
+        projectService: true,
         tsconfigRootDir: __dirname,
       },
       plugins: ['@typescript-eslint', 'import'],
