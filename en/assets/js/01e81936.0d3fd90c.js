89f1c5aa093b5ee95e1f3afe286e51da8e0cd003
rianrietveld <rianrietveld@users.noreply.github.com>
2024-10-10 10:40:47 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@f48aab566590df5429920f82d0e20fd84f179013 🚀

---
+++ en/assets/js/01e81936.0d3fd90c.js
@@ -0,0 +1,108 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [23164],
+ {
+  85398: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => h, contentTitle: () => g, default: () => f, frontMatter: () => d, metadata: () => u, toc: () => p });
+   var r = i(52676),
+    t = i(24785),
+    s = i(71595),
+    o = i(45054),
+    l = i(78836);
+   function a(e) {
+    const n = { p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.p, { children: 'Leg afkortingen uit. Zo help je alle gebruikers die de afkorting niet kennen.' }), '\n', (0, r.jsx)(n.p, { children: 'Het makkelijkste is om bij de eerste vermelding de afkorting volledig uit te schrijven, met daarachter de afkorting tussen haakjes.' }), '\n', (0, r.jsx)(n.p, { children: "Bijvoorbeeld: 'De gemeente geeft ondersteuning thuis via de Wet maatschappelijke ondersteuning (Wmo)'. In de rest van de tekst op die pagina kun je gewoon Wmo gebruiken. Doe dit op elke afzonderlijke pagina opnieuw." })] });
+   }
+   function c(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(a, { ...e }) }) : a(e);
+   }
+   const d = { title: 'WCAG-succescriterium 3.1.4 Afkortingen', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '3.1.4 Afkortingen', pagination_label: 'WCAG-succescriterium 3.1.4 Afkortingen', description: 'Leg afkortingen uit. Zo help je alle gebruikers die de afkorting niet kennen.', slug: '3.1.4', keywords: ['WCAG'] },
+    g = void 0,
+    u = { id: 'wcag/3.1.04', title: 'WCAG-succescriterium 3.1.4 Afkortingen', description: 'Leg afkortingen uit. Zo help je alle gebruikers die de afkorting niet kennen.', source: '@site/docs/wcag/3.1.04.mdx', sourceDirName: 'wcag', slug: '/wcag/3.1.4', permalink: '/en/wcag/3.1.4', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/3.1.04.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 3.1.4 Afkortingen', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '3.1.4 Afkortingen', pagination_label: 'WCAG-succescriterium 3.1.4 Afkortingen', description: 'Leg afkortingen uit. Zo help je alle gebruikers die de afkorting niet kennen.', slug: '3.1.4', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 3.1.3 Ongebruikelijke woorden', permalink: '/en/wcag/3.1.3' }, next: { title: 'WCAG-succescriterium 3.1.6 Uitspraak', permalink: '/en/wcag/3.1.6' } },
+    h = {},
+    p = [
+     { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
+     { value: 'Uitleg', id: 'uitleg', level: 2 },
+     { value: 'Opgelet', id: 'opgelet', level: 2 },
+     { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
+    ];
+   function k(e) {
+    const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: ['\n', '\n', '\n', '\n', (0, r.jsx)(s.j, { level: 1, conformanceLevel: 'Niveau AAA', children: 'WCAG-succescriterium 3.1.4 Afkortingen' }), '\n', (0, r.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#abbreviations', children: (0, r.jsx)('span', { lang: 'en', children: '3.1.4 Abbreviations' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#afkortingen', children: '3.1.4 Afkortingen' }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, r.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#abbreviations', children: (0, r.jsx)('span', { lang: 'en', children: 'Quick Reference 3.1.4 Abbreviations' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/abbreviations', children: (0, r.jsx)('span', { lang: 'en', children: 'Understanding SC 3.1.4: Abbreviations' }) }), '.'] }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, r.jsx)(c, {}), '\n', (0, r.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, r.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, r.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, r.jsx)(o.ZP, {}), '\n', (0, r.jsx)(l.ZP, {})] });
+   }
+   function f(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(k, { ...e }) }) : k(e);
+   }
+  },
+  45054: (e, n, i) => {
+   i.d(n, { ZP: () => o });
+   var r = i(52676),
+    t = i(24785);
+   function s(e) {
+    const n = { a: 'a', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(n.p, { children: ['Heb je gebruikersonderzoek gedaan dat betrekking heeft op dit succescriterium en wil je dit delen? Kijk eens bij ', (0, r.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/onderzoek-delen/', children: 'Gebruikersonderzoeken delen' }), ' op gebruikersonderzoeken.nl.'] });
+   }
+   function o(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(s, { ...e }) }) : s(e);
+   }
+  },
+  78836: (e, n, i) => {
+   i.d(n, { ZP: () => o });
+   var r = i(52676),
+    t = i(24785);
+   function s(e) {
+    const n = { a: 'a', h2: 'h2', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.h2, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, r.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, r.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function o(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(s, { ...e }) }) : s(e);
+   }
+  },
+  72657: (e, n, i) => {
+   i.d(n, { V: () => o });
+   var r = i(4814);
+   i(75271);
+   const t = { 'nlds-inline-heading-group__heading': 'nlds-inline-heading-group__heading_bHAJ', 'nlds-inline-heading-group__suffix': 'nlds-inline-heading-group__suffix_ioH2' };
+   var s = i(52676);
+   const o = (e) => {
+    let { children: n, className: i, level: o = 1, suffix: l, ...a } = e;
+    return (0, s.jsxs)('hgroup', { className: (0, r.Z)(t['nlds-inline-heading-group'], `utrecht-heading-${o}`, i), ...a, children: [(0, s.jsx)('h1', { className: (0, r.Z)(t['nlds-inline-heading-group__heading']), children: n }), l && (0, s.jsxs)('p', { className: (0, r.Z)(t['nlds-inline-heading-group__suffix']), children: [l ? ' ' : '', l] })] });
+   };
+  },
+  71595: (e, n, i) => {
+   i.d(n, { j: () => a });
+   var r = i(46506),
+    t = (i(75271), i(72657));
+   const s = 'nlds-inline-heading-group--wcag-heading-group_hVFN',
+    o = 'nlds-inline-heading-group__badge_TgvT';
+   var l = i(52676);
+   const a = (e) => {
+    let { children: n, conformanceLevel: i, ...a } = e;
+    return (0, l.jsx)(t.V, { className: s, suffix: i && (0, l.jsx)(r.Ou, { className: o, children: i }), ...a, children: n });
+   };
+  },
+  24785: (e, n, i) => {
+   i.d(n, { Z: () => l, a: () => o });
+   var r = i(75271);
+   const t = {},
+    s = r.createContext(t);
+   function o(e) {
+    const n = r.useContext(s);
+    return r.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function l(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : o(e.components)), r.createElement(s.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
---
+++ en/assets/js/30f1736b.54d6fb3b.js
@@ -0,0 +1,108 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [59354],
+ {
+  92035: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => g, contentTitle: () => u, default: () => m, frontMatter: () => c, metadata: () => h, toc: () => p });
+   var r = i(52676),
+    s = i(24785),
+    t = i(71595),
+    o = i(45054),
+    a = i(78836);
+   function d(e) {
+    const n = { a: 'a', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.p, { children: 'Als de betekenis van de woorden in de context dubbelzinnig is zonder kennis van de uitspraak, geef dan aan hoe deze woorden uitgesproken worden. Zo voorkom je misverstanden bij het lezen van de tekst.' }), '\n', (0, r.jsx)(n.p, { children: 'Dit is van belang voor lezers:' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsx)(n.li, { children: 'voor wie de taal van de tekst niet hun moedertaal is;' }), '\n', (0, r.jsx)(n.li, { children: 'met een cognitieve beperking, die de tekst letterlijk nemen;' }), '\n', (0, r.jsx)(n.li, { children: 'screenreadergebruikers die misschien de dubbelzinnig woorden in de verkeerde uitspraak voorgelezen krijgen.' }), '\n'] }), '\n', (0, r.jsxs)(n.p, { children: ['Hiervoor zijn ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/pronunciation#techniques', children: 'verschillende technieken' }), ', maar het makkelijkste is om dubbelzinnig woorden in informatieve teksten te vermijden.'] })] });
+   }
+   function l(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(d, { ...e }) }) : d(e);
+   }
+   const c = { title: 'WCAG-succescriterium 3.1.6 Uitspraak', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '3.1.6 Uitspraak', pagination_label: 'WCAG-succescriterium 3.1.6 Uitspraak', description: 'Als de betekenis van de woorden in de context dubbelzinnig is zonder kennis van de uitspraak, geef dan aan hoe deze woorden uitgesproken worden.', slug: '3.1.6', keywords: ['WCAG'] },
+    u = void 0,
+    h = { id: 'wcag/3.1.06', title: 'WCAG-succescriterium 3.1.6 Uitspraak', description: 'Als de betekenis van de woorden in de context dubbelzinnig is zonder kennis van de uitspraak, geef dan aan hoe deze woorden uitgesproken worden.', source: '@site/docs/wcag/3.1.06.mdx', sourceDirName: 'wcag', slug: '/wcag/3.1.6', permalink: '/en/wcag/3.1.6', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/3.1.06.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 3.1.6 Uitspraak', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '3.1.6 Uitspraak', pagination_label: 'WCAG-succescriterium 3.1.6 Uitspraak', description: 'Als de betekenis van de woorden in de context dubbelzinnig is zonder kennis van de uitspraak, geef dan aan hoe deze woorden uitgesproken worden.', slug: '3.1.6', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 3.1.4 Afkortingen', permalink: '/en/wcag/3.1.4' }, next: { title: 'WCAG-succescriterium 3.2.1 Bij focus', permalink: '/en/wcag/3.2.1' } },
+    g = {},
+    p = [
+     { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
+     { value: 'Uitleg', id: 'uitleg', level: 2 },
+     { value: 'Opgelet', id: 'opgelet', level: 2 },
+     { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
+    ];
+   function k(e) {
+    const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: ['\n', '\n', '\n', '\n', (0, r.jsx)(t.j, { level: 1, conformanceLevel: 'Niveau AAA', children: 'WCAG-succescriterium 3.1.6 Uitspraak' }), '\n', (0, r.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#pronunciation', children: (0, r.jsx)('span', { lang: 'en', children: '3.1.6 Pronunciation' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#uitspraak', children: '3.1.6 Uitspraak' }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, r.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#pronunciation', children: (0, r.jsx)('span', { lang: 'en', children: 'Quick Reference 3.1.6 Pronunciation' }) }), '.'] }), '\n', (0, r.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/pronunciation.html', children: (0, r.jsx)('span', { lang: 'en', children: 'Understanding SC: 3.1.6 Pronunciation' }) }), '.'] }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, r.jsx)(l, {}), '\n', (0, r.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, r.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, r.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, r.jsx)(o.ZP, {}), '\n', (0, r.jsx)(a.ZP, {})] });
+   }
+   function m(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(k, { ...e }) }) : k(e);
+   }
+  },
+  45054: (e, n, i) => {
+   i.d(n, { ZP: () => o });
+   var r = i(52676),
+    s = i(24785);
+   function t(e) {
+    const n = { a: 'a', p: 'p', ...(0, s.a)(), ...e.components };
+    return (0, r.jsxs)(n.p, { children: ['Heb je gebruikersonderzoek gedaan dat betrekking heeft op dit succescriterium en wil je dit delen? Kijk eens bij ', (0, r.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/onderzoek-delen/', children: 'Gebruikersonderzoeken delen' }), ' op gebruikersonderzoeken.nl.'] });
+   }
+   function o(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(t, { ...e }) }) : t(e);
+   }
+  },
+  78836: (e, n, i) => {
+   i.d(n, { ZP: () => o });
+   var r = i(52676),
+    s = i(24785);
+   function t(e) {
+    const n = { a: 'a', h2: 'h2', p: 'p', ...(0, s.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.h2, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, r.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, r.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function o(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(t, { ...e }) }) : t(e);
+   }
+  },
+  72657: (e, n, i) => {
+   i.d(n, { V: () => o });
+   var r = i(4814);
+   i(75271);
+   const s = { 'nlds-inline-heading-group__heading': 'nlds-inline-heading-group__heading_bHAJ', 'nlds-inline-heading-group__suffix': 'nlds-inline-heading-group__suffix_ioH2' };
+   var t = i(52676);
+   const o = (e) => {
+    let { children: n, className: i, level: o = 1, suffix: a, ...d } = e;
+    return (0, t.jsxs)('hgroup', { className: (0, r.Z)(s['nlds-inline-heading-group'], `utrecht-heading-${o}`, i), ...d, children: [(0, t.jsx)('h1', { className: (0, r.Z)(s['nlds-inline-heading-group__heading']), children: n }), a && (0, t.jsxs)('p', { className: (0, r.Z)(s['nlds-inline-heading-group__suffix']), children: [a ? ' ' : '', a] })] });
+   };
+  },
+  71595: (e, n, i) => {
+   i.d(n, { j: () => d });
+   var r = i(46506),
+    s = (i(75271), i(72657));
+   const t = 'nlds-inline-heading-group--wcag-heading-group_hVFN',
+    o = 'nlds-inline-heading-group__badge_TgvT';
+   var a = i(52676);
+   const d = (e) => {
+    let { children: n, conformanceLevel: i, ...d } = e;
+    return (0, a.jsx)(s.V, { className: t, suffix: i && (0, a.jsx)(r.Ou, { className: o, children: i }), ...d, children: n });
+   };
+  },
+  24785: (e, n, i) => {
+   i.d(n, { Z: () => a, a: () => o });
+   var r = i(75271);
+   const s = {},
+    t = r.createContext(s);
+   function o(e) {
+    const n = r.useContext(t);
+    return r.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(s) : e.components || s) : o(e.components)), r.createElement(t.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
