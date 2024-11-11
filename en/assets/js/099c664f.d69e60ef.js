413c8841da8c8b6d8b4c43a52b5244aed11f45df
rianrietveld <rianrietveld@users.noreply.github.com>
2024-11-11 06:43:06 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@520646e77170e93e947ebe97a783a7b7f2f79007 🚀

---
+++ en/assets/js/099c664f.d69e60ef.js
@@ -0,0 +1,157 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [77410],
+ {
+  16827: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => m, contentTitle: () => p, default: () => x, frontMatter: () => u, metadata: () => j, toc: () => v });
+   var i = t(52676),
+    r = t(24785),
+    s = t(57716),
+    l = t(71595),
+    a = t(46506),
+    o = t(45054),
+    c = t(52057),
+    d = t(78836);
+   function h(e) {
+    const n = { p: 'p', strong: 'strong', ...(0, r.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsxs)(n.p, { children: ['De gebruiker moet tekst twee keer (200%) kunnen vergroten. Het gaat hierbij ', (0, i.jsx)(n.strong, { children: 'alleen om het vergroten van tekst' }), ' en niet om bijvoorbeeld afbeeldingen of de layout.'] }), '\n', (0, i.jsx)(n.p, { children: 'Alle functies, zoals het menu, moeten werken en zichtbaar zijn. Alle tekst moet leesbaar zijn. Er mag geen inhoud buiten beeld vallen, verborgen zijn of maar gedeeltelijk zichtbaar zijn.' }), '\n', (0, i.jsx)(n.p, { children: "Maak indien nodig een uitzondering voor de tekstgrootte instellen van tekst in afbeeldingen en van ondertitels in video's." })] });
+   }
+   function g(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(h, { ...e }) }) : h(e);
+   }
+   const u = { title: 'WCAG-succescriterium 1.4.4 Herschalen van tekst', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.4 Herschalen van tekst', pagination_label: 'WCAG-succescriterium 1.4.4 Herschalen van tekst', description: 'De gebruiker moet tekst twee keer (200%) kunnen vergroten en nog steeds goed kunnen lezen.', slug: '1.4.4', keywords: ['WCAG'] },
+    p = void 0,
+    j = { id: 'wcag/1.4.04', title: 'WCAG-succescriterium 1.4.4 Herschalen van tekst', description: 'De gebruiker moet tekst twee keer (200%) kunnen vergroten en nog steeds goed kunnen lezen.', source: '@site/docs/wcag/1.4.04.mdx', sourceDirName: 'wcag', slug: '/wcag/1.4.4', permalink: '/en/wcag/1.4.4', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/1.4.04.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 1.4.4 Herschalen van tekst', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.4 Herschalen van tekst', pagination_label: 'WCAG-succescriterium 1.4.4 Herschalen van tekst', description: 'De gebruiker moet tekst twee keer (200%) kunnen vergroten en nog steeds goed kunnen lezen.', slug: '1.4.4', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 1.4.3 Contrast (minimum)', permalink: '/en/wcag/1.4.3' }, next: { title: 'WCAG-succescriterium 1.4.5 Afbeeldingen van tekst', permalink: '/en/wcag/1.4.5' } },
+    m = {},
+    v = [
+     { value: 'Uitleg', id: 'uitleg', level: 2 },
+     { value: 'Opgelet', id: 'opgelet', level: 2 },
+     { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
+     { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
+     { value: 'Help richtlijn verbeteren', id: 'help-richtlijn-verbeteren', level: 2 },
+    ];
+   function k(e) {
+    const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, r.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: ['\n', '\n', '\n', (0, i.jsx)(l.j, { level: 1, conformanceLevel: 'Niveau AA', children: 'WCAG-succescriterium 1.4.4 Herschalen van tekst' }), '\n', (0, i.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, i.jsx)(g, {}), '\n', (0, i.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, i.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, i.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, i.jsx)(o.ZP, {}), '\n', (0, i.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#resize-text', children: (0, i.jsx)('span', { lang: 'en', children: '1.4.4. Resize Text' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#herschalen-van-tekst', children: '1.4.4 Herschalen van tekst' }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, i.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#resize-text', children: (0, i.jsx)('span', { lang: 'en', children: 'Quick Reference 1.4.4. Resize Text' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/resize-text.html', children: (0, i.jsx)('span', { lang: 'en', children: 'Understanding SC 1.4.4. Resize Text' }) }), '.'] }), '\n'] }), '\n', (0, i.jsx)(a.CY, { children: (0, i.jsx)(s.U, { omitH1: !0, headinglevel: 3, children: (0, i.jsx)(c.ZP, {}) }) }), '\n', (0, i.jsx)(n.h2, { id: 'help-richtlijn-verbeteren', children: 'Help richtlijn verbeteren' }), '\n', (0, i.jsx)(s.U, { omitH1: !0, headinglevel: 3, children: (0, i.jsx)(d.ZP, {}) })] });
+   }
+   function x(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(k, { ...e }) }) : k(e);
+   }
+  },
+  45054: (e, n, t) => {
+   t.d(n, { ZP: () => l });
+   var i = t(52676),
+    r = t(24785);
+   function s(e) {
+    const n = { a: 'a', p: 'p', ...(0, r.a)(), ...e.components };
+    return (0, i.jsxs)(n.p, { children: ['Heb je gebruikersonderzoek gedaan dat betrekking heeft op dit succescriterium en wil je dit delen? Kijk eens bij ', (0, i.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/onderzoek-delen/', children: 'Gebruikersonderzoeken delen' }), ' op gebruikersonderzoeken.nl.'] });
+   }
+   function l(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(s, { ...e }) }) : s(e);
+   }
+  },
+  52057: (e, n, t) => {
+   t.d(n, { ZP: () => l });
+   var i = t(52676),
+    r = t(24785);
+   function s(e) {
+    const n = { a: 'a', h1: 'h1', p: 'p', strong: 'strong', ...(0, r.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: 'aanvulling-op-wcag', children: 'Aanvulling op WCAG' }), '\n', (0, i.jsxs)(n.p, { children: [(0, i.jsx)(n.strong, { children: 'Belangrijk:' }), ' De richtlijnen van NL Design System zijn niet verplicht en zijn geen vervanging voor de ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG21/', children: 'wettelijk geldende WCAG 2.1 specificatie' }), '.'] }), '\n', (0, i.jsxs)(n.p, { children: ['Ons doel is om praktische uitleg en voorbeelden te geven die helpen bij het toegankelijk inzetten van de NL Design System componenten, patronen en richtlijnen. We doen dat op basis van een interpretatie van de ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/', children: 'nieuwe WCAG 2.2 specificatie' }), '.'] }), '\n', (0, i.jsxs)(n.p, { children: ['Weten waar je volgens de wet aan moet voldoen? Ga dan naar ', (0, i.jsx)(n.a, { href: 'https://www.digitoegankelijk.nl/wetgeving/wat-is-verplicht', children: 'wat is verplicht van DigiToegankelijk' }), '.'] })] });
+   }
+   function l(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(s, { ...e }) }) : s(e);
+   }
+  },
+  78836: (e, n, t) => {
+   t.d(n, { ZP: () => l });
+   var i = t(52676),
+    r = t(24785);
+   function s(e) {
+    const n = { a: 'a', h1: 'h1', p: 'p', ...(0, r.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, i.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, i.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function l(e = {}) {
+    const { wrapper: n } = { ...(0, r.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(s, { ...e }) }) : s(e);
+   }
+  },
+  72657: (e, n, t) => {
+   t.d(n, { V: () => l });
+   var i = t(4814);
+   t(75271);
+   const r = { 'nlds-inline-heading-group__heading': 'nlds-inline-heading-group__heading_bHAJ', 'nlds-inline-heading-group__suffix': 'nlds-inline-heading-group__suffix_ioH2' };
+   var s = t(52676);
+   const l = (e) => {
+    let { children: n, className: t, level: l = 1, suffix: a, ...o } = e;
+    return (0, s.jsxs)('hgroup', { className: (0, i.Z)(r['nlds-inline-heading-group'], `utrecht-heading-${l}`, t), ...o, children: [(0, s.jsx)('h1', { className: (0, i.Z)(r['nlds-inline-heading-group__heading']), children: n }), a && (0, s.jsxs)('p', { className: (0, i.Z)(r['nlds-inline-heading-group__suffix']), children: [a ? ' ' : '', a] })] });
+   };
+  },
+  57716: (e, n, t) => {
+   t.d(n, { U: () => d });
+   var i = t(24785),
+    r = t(46506),
+    s = (t(75271), t(52676));
+   const l = (e, n) => {
+     const t = new URL(n, new URL(e, 'resolve://pathname/'));
+     if ('resolve:' === t.protocol) {
+      const { pathname: t, search: i, hash: r } = new URL(n, new URL(e, 'http://example.com/'));
+      return t + i + r;
+     }
+     return t.toString();
+    },
+    a = { 1: {}, 2: { h1: r.XJ, h2: r.aC, h3: r.k8, h4: r.by, h5: r.Cd }, 3: { h1: r.aC, h2: r.k8, h3: r.by, h4: r.Cd, h5: r.Cd }, 4: { h1: r.k8, h2: r.by, h3: r.Cd, h4: r.Cd, h5: r.Cd }, 5: { h1: r.by, h2: r.Cd, h3: r.Cd, h4: r.Cd, h5: r.Cd }, 6: { h1: r.Cd, h2: r.Cd, h3: r.Cd, h4: r.Cd, h5: r.Cd } },
+    o = (e) => ({
+     img: (n) => {
+      let { src: t, ...i } = n;
+      return (0, s.jsx)('img', { ...i, src: l(e, t), className: 'utrecht-img utrecht-img--fit' });
+     },
+    }),
+    c = (e, n) => {
+     if (e) {
+      const e = { ...a[n - 1] };
+      return (e.h1 = () => null), e;
+     }
+     return a[n];
+    },
+    d = (e) => {
+     let { children: n, omitH1: t = !1, headingLevel: r = 1, baseUrl: l = '' } = e;
+     return (0, s.jsx)(i.Z, { components: { ...c(t, r), ...o(l) }, children: n });
+    };
+  },
+  71595: (e, n, t) => {
+   t.d(n, { j: () => o });
+   var i = t(46506),
+    r = (t(75271), t(72657));
+   const s = 'nlds-inline-heading-group--wcag-heading-group_hVFN',
+    l = 'nlds-inline-heading-group__badge_TgvT';
+   var a = t(52676);
+   const o = (e) => {
+    let { children: n, conformanceLevel: t, ...o } = e;
+    return (0, a.jsx)(r.V, { className: s, suffix: t && (0, a.jsx)(i.Ou, { className: l, children: t }), ...o, children: n });
+   };
+  },
+  24785: (e, n, t) => {
+   t.d(n, { Z: () => a, a: () => l });
+   var i = t(75271);
+   const r = {},
+    s = i.createContext(r);
+   function l(e) {
+    const n = i.useContext(s);
+    return i.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(r) : e.components || r) : l(e.components)), i.createElement(s.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
