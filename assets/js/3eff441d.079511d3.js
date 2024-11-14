e296e44427afe67c40b311cb10591b06233e748c
rianrietveld <rianrietveld@users.noreply.github.com>
2024-11-14 09:36:26 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@e1238fbbb288ae3ded6699b82993bd1757fbe5bd 🚀

--- assets/js/3eff441d.079511d3.js
+++ assets/js/3eff441d.079511d3.js
@@ -2,8 +2,8 @@
 (self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
  [98416],
  {
-  15893: (e, n, r) => {
-   r.r(n), r.d(n, { assets: () => v, contentTitle: () => p, default: () => f, frontMatter: () => g, metadata: () => m, toc: () => k });
+  32765: (e, n, r) => {
+   r.r(n), r.d(n, { assets: () => m, contentTitle: () => g, default: () => j, frontMatter: () => u, metadata: () => p, toc: () => v });
    var i = r(52676),
     t = r(24785),
     s = r(57716),
@@ -11,33 +11,26 @@
     l = r(46506),
     a = r(45054),
     c = r(52057),
-    d = r(78836);
-   function h(e) {
-    const n = { p: 'p', ...(0, t.a)(), ...e.components };
-    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.p, { children: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.' }), '\n', (0, i.jsx)(n.p, { children: 'Bijvoorbeeld: geef een foutmelding niet alleen aan met een rood randje, maar ook in tekst en eventueel met een icoontje dat een foutmelding markeert.' })] });
-   }
-   function u(e = {}) {
-    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
-    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(h, { ...e }) }) : h(e);
-   }
-   const g = { title: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.1 Gebruik van kleur', pagination_label: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', description: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.', slug: '1.4.1', keywords: ['WCAG'] },
-    p = void 0,
-    m = { id: 'wcag/1.4.01', title: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', description: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.', source: '@site/docs/wcag/1.4.01.mdx', sourceDirName: 'wcag', slug: '/wcag/1.4.1', permalink: '/wcag/1.4.1', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/1.4.01.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.1 Gebruik van kleur', pagination_label: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', description: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.', slug: '1.4.1', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 1.3.6 Identificeer het doel', permalink: '/wcag/1.3.6' }, next: { title: 'WCAG-succescriterium 1.4.2 Geluidsbediening', permalink: '/wcag/1.4.2' } },
-    v = {},
-    k = [
+    d = r(78836),
+    h = r(57670);
+   const u = { title: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.1 Gebruik van kleur', pagination_label: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', description: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.', slug: '1.4.1', keywords: ['WCAG'] },
+    g = void 0,
+    p = { id: 'wcag/1.4.01', title: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', description: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.', source: '@site/docs/wcag/1.4.01.mdx', sourceDirName: 'wcag', slug: '/wcag/1.4.1', permalink: '/wcag/1.4.1', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/1.4.01.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.4.1 Gebruik van kleur', pagination_label: 'WCAG-succescriterium 1.4.1 Gebruik van kleur', description: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.', slug: '1.4.1', keywords: ['WCAG'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 1.3.6 Identificeer het doel', permalink: '/wcag/1.3.6' }, next: { title: 'WCAG-succescriterium 1.4.2 Geluidsbediening', permalink: '/wcag/1.4.2' } },
+    m = {},
+    v = [
      { value: 'Uitleg', id: 'uitleg', level: 2 },
      { value: 'Opgelet', id: 'opgelet', level: 2 },
      { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
      { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
      { value: 'Help richtlijn verbeteren', id: 'help-richtlijn-verbeteren', level: 2 },
     ];
-   function j(e) {
+   function k(e) {
     const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
-    return (0, i.jsxs)(i.Fragment, { children: ['\n', '\n', '\n', (0, i.jsx)(o.j, { level: 1, conformanceLevel: 'Niveau A', children: 'WCAG-succescriterium 1.4.1 Gebruik van kleur' }), '\n', (0, i.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, i.jsx)(u, {}), '\n', (0, i.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, i.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, i.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, i.jsx)(a.ZP, {}), '\n', (0, i.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#use-of-color', children: (0, i.jsx)('span', { lang: 'en', children: '1.4.1 Use of color' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#gebruik-van-kleur', children: '1.4.1 Gebruik van kleur' }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, i.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#use-of-color', children: (0, i.jsx)('span', { lang: 'en', children: 'Quick Reference 1.4.1 Use of color' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/use-of-color.html', children: (0, i.jsx)('span', { lang: 'en', children: 'Understanding SC 1.4.1 Use of color' }) }), '.'] }), '\n'] }), '\n', (0, i.jsx)(l.CY, { children: (0, i.jsx)(s.U, { omitH1: !0, headinglevel: 3, children: (0, i.jsx)(c.ZP, {}) }) }), '\n', (0, i.jsx)(n.h2, { id: 'help-richtlijn-verbeteren', children: 'Help richtlijn verbeteren' }), '\n', (0, i.jsx)(s.U, { omitH1: !0, headinglevel: 3, children: (0, i.jsx)(d.ZP, {}) })] });
+    return (0, i.jsxs)(i.Fragment, { children: ['\n', '\n', '\n', (0, i.jsx)(o.j, { level: 1, conformanceLevel: 'Niveau A', children: 'WCAG-succescriterium 1.4.1 Gebruik van kleur' }), '\n', (0, i.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, i.jsx)(h.ZP, {}), '\n', (0, i.jsx)(n.h2, { id: 'opgelet', children: 'Opgelet' }), '\n', (0, i.jsx)(n.p, { children: 'Deze inhoud wordt binnenkort aangevuld met uitgebreidere uitleg, bronnen en informatie over hoe te testen.' }), '\n', (0, i.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, i.jsx)(a.ZP, {}), '\n', (0, i.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#use-of-color', children: (0, i.jsx)('span', { lang: 'en', children: '1.4.1 Use of color' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#gebruik-van-kleur', children: '1.4.1 Gebruik van kleur' }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige informatie op ', (0, i.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#use-of-color', children: (0, i.jsx)('span', { lang: 'en', children: 'Quick Reference 1.4.1 Use of color' }) }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Engelstalige toelichting: ', (0, i.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/use-of-color.html', children: (0, i.jsx)('span', { lang: 'en', children: 'Understanding SC 1.4.1 Use of color' }) }), '.'] }), '\n'] }), '\n', (0, i.jsx)(l.CY, { children: (0, i.jsx)(s.U, { omitH1: !0, headinglevel: 3, children: (0, i.jsx)(c.ZP, {}) }) }), '\n', (0, i.jsx)(n.h2, { id: 'help-richtlijn-verbeteren', children: 'Help richtlijn verbeteren' }), '\n', (0, i.jsx)(s.U, { omitH1: !0, headinglevel: 3, children: (0, i.jsx)(d.ZP, {}) })] });
    }
-   function f(e = {}) {
+   function j(e = {}) {
     const { wrapper: n } = { ...(0, t.a)(), ...e.components };
-    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(j, { ...e }) }) : j(e);
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(k, { ...e }) }) : k(e);
    }
   },
   45054: (e, n, r) => {
@@ -73,6 +66,19 @@
    function s(e) {
     const n = { a: 'a', h1: 'h1', p: 'p', ...(0, t.a)(), ...e.components };
     return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, i.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door het NL Design System. Heb je aanvullingen of opmerkingen? ", (0, i.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function o(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(s, { ...e }) }) : s(e);
+   }
+  },
+  57670: (e, n, r) => {
+   r.d(n, { ZP: () => o });
+   var i = r(52676),
+    t = r(24785);
+   function s(e) {
+    const n = { p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.p, { children: 'Zorg ervoor dat kleur niet het enige visuele middel is om informatie over te brengen, een actie aan te geven, tot een reactie op te roepen of een visueel element te onderscheiden. Niet iedereen kan kleuren zien of verandering in kleur of kleurcontrast opmerken.' }), '\n', (0, i.jsx)(n.p, { children: 'Bijvoorbeeld: geef een foutmelding niet alleen aan met een rood randje, maar ook in tekst en eventueel met een icoontje dat een foutmelding markeert.' })] });
    }
    function o(e = {}) {
     const { wrapper: n } = { ...(0, t.a)(), ...e.components };
