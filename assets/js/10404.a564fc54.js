2b142d6fe32738a6535262cc599c84aae8d071df
Robbert <mail@robbertbroersma.nl>
2024-12-06 12:24:07 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@00a7bb7749e1f4e68a9930b0d32acf75d38942b5 🚀

---
+++ assets/js/10404.a564fc54.js
@@ -0,0 +1,22 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [10404],
+ {
+  10404: (n, e, s) => {
+   s.r(e), s.d(e, { contentTitle: () => l, default: () => a, frontMatter: () => o, toc: () => c });
+   var t = s(52676),
+    i = s(87118);
+   const o = {},
+    l = 'Anatomie',
+    c = [];
+   function r(n) {
+    const e = { h1: 'h1', li: 'li', ol: 'ol', ...(0, i.a)(), ...n.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(e.h1, { id: 'anatomie', children: 'Anatomie' }), '\n', (0, t.jsxs)(e.ol, { children: ['\n', (0, t.jsx)(e.li, { children: 'Code Block' }), '\n', (0, t.jsx)(e.li, { children: 'Code' }), '\n'] })] });
+   }
+   function a(n = {}) {
+    const { wrapper: e } = { ...(0, i.a)(), ...n.components };
+    return e ? (0, t.jsx)(e, { ...n, children: (0, t.jsx)(r, { ...n }) }) : r(n);
+   }
+  },
+ },
+]);
