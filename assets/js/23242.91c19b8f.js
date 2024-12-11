cc60cf461bd902880b7451b364b7d0073736b62a
Robbert <mail@robbertbroersma.nl>
2024-12-11 17:17:31 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@0f9d8d5f27d331e8904fe5dcb6f7d6863de0bc74 🚀

---
+++ assets/js/23242.91c19b8f.js
@@ -0,0 +1,22 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [23242],
+ {
+  23242: (n, e, t) => {
+   t.r(e), t.d(e, { contentTitle: () => c, default: () => a, frontMatter: () => o, toc: () => l });
+   var s = t(52676),
+    i = t(87118);
+   const o = {},
+    c = 'Anatomie',
+    l = [];
+   function r(n) {
+    const e = { h1: 'h1', li: 'li', ol: 'ol', ...(0, i.a)(), ...n.components };
+    return (0, s.jsxs)(s.Fragment, { children: [(0, s.jsx)(e.h1, { id: 'anatomie', children: 'Anatomie' }), '\n', (0, s.jsxs)(e.ol, { children: ['\n', (0, s.jsx)(e.li, { children: 'Data Badge' }), '\n'] })] });
+   }
+   function a(n = {}) {
+    const { wrapper: e } = { ...(0, i.a)(), ...n.components };
+    return e ? (0, s.jsx)(e, { ...n, children: (0, s.jsx)(r, { ...n }) }) : r(n);
+   }
+  },
+ },
+]);
---
+++ assets/js/63694.9e2fca4d.js
@@ -0,0 +1,22 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [63694],
+ {
+  63694: (n, e, s) => {
+   s.r(e), s.d(e, { contentTitle: () => i, default: () => o, frontMatter: () => c, toc: () => l });
+   var t = s(52676),
+    r = s(87118);
+   const c = {},
+    i = 'Aliassen',
+    l = [];
+   function a(n) {
+    const e = { h1: 'h1', p: 'p', ...(0, r.a)(), ...n.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(e.h1, { id: 'aliassen', children: 'Aliassen' }), '\n', (0, t.jsx)(e.p, { children: 'Ook bekend als: Hyperlink, url en Anchor.' })] });
+   }
+   function o(n = {}) {
+    const { wrapper: e } = { ...(0, r.a)(), ...n.components };
+    return e ? (0, t.jsx)(e, { ...n, children: (0, t.jsx)(a, { ...n }) }) : a(n);
+   }
+  },
+ },
+]);
