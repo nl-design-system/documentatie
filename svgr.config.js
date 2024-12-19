3e190f34f674548de9fade12b38812832aad56c6
Hidde de Vries <hidde@hiddedevries.nl>
2024-12-19 12:19:49 +0100

    fix: use label and description on svgs

--- svgr.config.js
+++ svgr.config.js
@@ -1,4 +1,18 @@
-// .svgrrc.js
 module.exports = {
   svgProps: { role: 'img' },
+  titleProp: true,
+  descProp: true,
+  svgoConfig: {
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupIds: false,
+            removeAttrs: false,
+          },
+        },
+      },
+    ],
+  },
 };
