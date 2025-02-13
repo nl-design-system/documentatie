3c5a7eecde3af822aa9980701faf1c1810649d40
matijs <matijs@gmail.com>
2025-02-13 14:33:14 +0100

    chore: update dependencies w/ major version bumps
    - Fix Prettier issue in src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
           </Paragraph>
 
           <Paragraph>
-            NL Design System helpt om toegankelijk, inclusief en gebruiksvriendelijk ontwikkelen makkelijk te maken.{' '}
+            NL Design System helpt om toegankelijk, inclusief en gebruiksvriendelijk ontwikkelen makkelijk te maken.
           </Paragraph>
 
           <UnorderedList>
