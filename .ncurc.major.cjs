36bf490e37658c07bf6daaaf379e92c49e9705a4
matijs <matijs@gmail.com>
2025-02-13 14:19:25 +0100

    chore: improve major update process
    Clarify why `@types/node` specifially is not updated to new major
    versions by Dependabot.
    
    Broaden package.json#engines.node to allow Dependabot to still create
    PRs until it supports Node.js 22
    
    Add react, react-dom + types to the npm-check-update configuration for
    now

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -2,6 +2,17 @@ const minorConfig = require('./.ncurc.minor.cjs');
 
 module.exports = {
   ...minorConfig,
-  reject: [...minorConfig.reject, 'eslint', 'eslint-plugin-json', 'style-dictionary'],
+  reject: [
+    ...minorConfig.reject,
+    'eslint',
+    'eslint-plugin-json',
+    'style-dictionary',
+    'react',
+    'react-dom',
+    '@types/react',
+    '@types/react-dom',
+    // we manually update @types/node whenever we start using a new LTS version of Node.js
+    '@types/node',
+  ],
   target: 'latest',
 };
