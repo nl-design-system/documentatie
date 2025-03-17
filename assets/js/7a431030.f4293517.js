73f55ad24cb1229e81f1623d471d2ea914974a25
Yolijn <mail@yolijn.com>
2025-03-17 11:19:50 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@5702c41c91f6f49bafa569c594ea2dec8ba65989 🚀

---
+++ assets/js/7a431030.f4293517.js
@@ -0,0 +1,47 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [33992],
+ {
+  88885: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => u, contentTitle: () => r, default: () => l, frontMatter: () => o, metadata: () => c, toc: () => d });
+   var s = t(52676),
+    i = t(40139);
+   const o = { title: 'Thanks for signing up! \xb7 Introduction into European Design Systems 2025', hide_title: !1, hide_table_of_contents: !1, description: 'Signed up for Introduction into European Design Systems 2025.', slug: '/community/events/introduction-european-design-systems-2025/thanks', unlisted: !0, keywords: ['nl design system', 'User Needs First Conference'] },
+    r = 'Thanks!',
+    c = { id: 'community/events/introduction-european-design-systems-success', title: 'Thanks for signing up! \xb7 Introduction into European Design Systems 2025', description: 'Signed up for Introduction into European Design Systems 2025.', source: '@site/docs/community/events/introduction-european-design-systems-success.mdx', sourceDirName: 'community/events', slug: '/community/events/introduction-european-design-systems-2025/thanks', permalink: '/community/events/introduction-european-design-systems-2025/thanks', draft: !1, unlisted: !0, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/introduction-european-design-systems-success.mdx', tags: [], version: 'current', frontMatter: { title: 'Thanks for signing up! \xb7 Introduction into European Design Systems 2025', hide_title: !1, hide_table_of_contents: !1, description: 'Signed up for Introduction into European Design Systems 2025.', slug: '/community/events/introduction-european-design-systems-2025/thanks', unlisted: !0, keywords: ['nl design system', 'User Needs First Conference'] } },
+    u = {},
+    d = [];
+   function a(e) {
+    const n = { a: 'a', h1: 'h1', li: 'li', p: 'p', ul: 'ul', ...(0, i.a)(), ...e.components };
+    return (0, s.jsxs)(s.Fragment, { children: ['\n', (0, s.jsxs)('div', { lang: 'en', children: [(0, s.jsx)(n.h1, { id: 'thanks', children: 'Thanks!' }), (0, s.jsx)(n.p, { children: 'You are now signed up for joining the session Introduction into European Design Systems on March 10th 2025 online.' }), (0, s.jsxs)(n.p, { children: ['This session is part of the International ', (0, s.jsx)(n.a, { href: 'https://international.gebruikercentraal.nl/conference2025', children: 'User Needs First Conference' }), ' which can also be attended in person.'] }), (0, s.jsxs)(n.ul, { children: ['\n', (0, s.jsx)(n.li, { children: (0, s.jsx)(n.a, { target: '_blank', 'data-noBrokenLinkCheck': !0, href: t(37848).Z + '', children: 'Add the invite to your calendar (.ics)' }) }), '\n'] })] })] });
+   }
+   function l(e = {}) {
+    const { wrapper: n } = { ...(0, i.a)(), ...e.components };
+    return n ? (0, s.jsx)(n, { ...e, children: (0, s.jsx)(a, { ...e }) }) : a(e);
+   }
+  },
+  37848: (e, n, t) => {
+   t.d(n, { Z: () => s });
+   const s = t.p + 'assets/files/2025-04-10-introduction-european-design-systems-a6cea9d18d92f7026245d9f390aa3eb0.ics';
+  },
+  40139: (e, n, t) => {
+   t.d(n, { Z: () => c, a: () => r });
+   var s = t(75271);
+   const i = {},
+    o = s.createContext(i);
+   function r(e) {
+    const n = s.useContext(o);
+    return s.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function c(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(i) : e.components || i) : r(e.components)), s.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
