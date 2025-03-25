5bd340c31f05ff9edc4d27dc624b16128e0bc59b
Renate Roke <94439479+RenateRoke@users.noreply.github.com>
2025-03-25 10:10:16 +0100

    feat: add team member Renate (#2160)
    Co-authored-by: Renate Roke <renate.roke@ictu.nl>

---
+++ src/components/CoreTeam/Renate.tsx
@@ -0,0 +1,24 @@
+import { Paragraph } from '@utrecht/component-library-react/dist/css-module';
+import type { TeamMember } from './TeamMember';
+
+const Renate: TeamMember = {
+  name: 'Renate Roke',
+  role: 'Specialist webtoegankelijkheid',
+  slack: { mention: '@Renate Roke', id: 'U08BJAEAEUU' },
+  Description: () => (
+    <>
+      <Paragraph>
+        Als Specialist Webtoegankelijkheid beschrijft Renate onder andere acceptatiecriteria voor componenten in het NL
+        Design System en helpt ze de community op weg met tips over toegankelijkheid. Hiervoor gebruikt ze haar
+        jarenlange kennis in toegankelijkheidsonderzoeken en haar passie om kennis over te dragen. Ze denkt vooral graag
+        mee over wat er wél kan.
+      </Paragraph>
+      <Paragraph>
+        In haar vrije tijd is Renate graag buiten: aan het wandelen, aan het kamperen of op het terras. Daarnaast leest
+        ze heel veel boeken en heeft ze veel creatieve hobbies.
+      </Paragraph>
+    </>
+  ),
+};
+
+export default Renate;
