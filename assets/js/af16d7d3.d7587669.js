3043d8b75cb297bedb7d19d618c620323bd8ac57
eslook <42849307+eslook@users.noreply.github.com>
2025-03-31 10:53:58 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@a727a1e67e72ef66fdc611ea37f4a192d64463b1 🚀

---
+++ assets/js/af16d7d3.d7587669.js
@@ -0,0 +1,9 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [969],
+ {
+  26338: (e) => {
+   e.exports = JSON.parse('{"title":"Voor developers","description":"Mee ontwikkelen aan NL Design System of gebruik maken van bestaande componenten? Als developer vind je hier alle documentatie die je nodig hebt om te beginnen.","keywords":["developer","overzicht"],"slug":"/handboek/developer/overzicht","permalink":"/handboek/developer/overzicht","navigation":{"previous":{"title":"Versiebeheer voor design tokens","permalink":"/handboek/designer/breaking-changes"},"next":{"title":"Aan de slag","permalink":"/handboek/developer/aan-de-slag"}}}');
+  },
+ },
+]);
