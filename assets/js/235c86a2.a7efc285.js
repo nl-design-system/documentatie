ade3f58de3fac5af58feb25da84598f9fa023849
Robbert <mail@robbertbroersma.nl>
2025-05-13 15:40:05 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@548a47293f4f1e13020a1391f99a10b76bfd4bf7 🚀

---
+++ assets/js/235c86a2.a7efc285.js
@@ -0,0 +1,68 @@
+/*! For license information please see 235c86a2.a7efc285.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [88979],
+ {
+  6826: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => h, contentTitle: () => d, default: () => b, frontMatter: () => o, metadata: () => c, toc: () => m });
+   var a = t(52676),
+    i = t(40139),
+    r = t(68257),
+    s = t(78511),
+    l = t(16167);
+   const o = { title: 'Heartbeat \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Heartbeat', pagination_label: '2 wekelijkse updates van het kernteam en community', description: 'Wat is de Heartbeat, hoe meld ik mij aan, en op welke momenten is het?', slug: '/events/heartbeat' },
+    d = 'Heartbeat',
+    c = { id: 'community/events/heartbeat/heartbeat', title: 'Heartbeat \xb7 Bijeenkomsten', description: 'Wat is de Heartbeat, hoe meld ik mij aan, en op welke momenten is het?', source: '@site/docs/community/events/heartbeat/heartbeat.mdx', sourceDirName: 'community/events/heartbeat', slug: '/events/heartbeat', permalink: '/events/heartbeat', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/heartbeat/heartbeat.mdx', tags: [], version: 'current', frontMatter: { title: 'Heartbeat \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Heartbeat', pagination_label: '2 wekelijkse updates van het kernteam en community', description: 'Wat is de Heartbeat, hoe meld ik mij aan, en op welke momenten is het?', slug: '/events/heartbeat' }, sidebar: 'community', previous: { title: 'Heartbeat', permalink: '/events/heartbeat/overzicht' }, next: { title: "Video's van de afgelopen Heartbeats", permalink: '/events/heartbeat/videos' } },
+    h = {},
+    m = [
+     { value: 'Planning', id: 'planning', level: 2 },
+     { value: 'Vragen?', id: 'vragen', level: 2 },
+    ];
+   function p(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, i.a)(), ...e.components };
+    return (0, a.jsxs)(a.Fragment, { children: [(0, a.jsx)(n.h1, { id: 'heartbeat', children: 'Heartbeat' }), '\n', (0, a.jsx)(n.p, { children: 'In de Heartbeat vertelt het kernteam van NL Design System elke twee weken wat de laatste stand van zaken is. Daarnaast laten we organisaties aan het woord die met/aan het NL Design System werken.' }), '\n', (0, a.jsx)(n.p, { children: 'Deze sessies zijn publiek toegankelijk.' }), '\n', (0, a.jsxs)(l.hE, { children: [(0, a.jsxs)(r.Z, { href: '/events/heartbeat/aanmelden', appearance: 'primary-action', children: ['Meld je aan', (0, a.jsx)(s.Z, {})] }), (0, a.jsxs)(r.Z, { href: '/events/heartbeat/videos', appearance: 'secondary-action', children: ['Kijk de Heartbeat terug', (0, a.jsx)(s.Z, {})] })] }), '\n', (0, a.jsx)(n.h2, { id: 'planning', children: 'Planning' }), '\n', (0, a.jsx)(n.p, { children: 'De Heartbeat vindt om de week plaats op dinsdag. Dit zijn de data voor 2025:' }), '\n', (0, a.jsxs)(n.ul, { children: ['\n', (0, a.jsx)(n.li, { children: '7 januari' }), '\n', (0, a.jsx)(n.li, { children: '4 februari' }), '\n', (0, a.jsx)(n.li, { children: '18 februari' }), '\n', (0, a.jsx)(n.li, { children: '4 maart' }), '\n', (0, a.jsx)(n.li, { children: '18 maart' }), '\n', (0, a.jsx)(n.li, { children: '1 april' }), '\n', (0, a.jsx)(n.li, { children: '15 april' }), '\n', (0, a.jsx)(n.li, { children: '29 april' }), '\n', (0, a.jsx)(n.li, { children: '13 mei' }), '\n', (0, a.jsx)(n.li, { children: '27 mei' }), '\n', (0, a.jsx)(n.li, { children: '10 juni' }), '\n', (0, a.jsx)(n.li, { children: '24 juni' }), '\n', (0, a.jsx)(n.li, { children: '8 juli' }), '\n', (0, a.jsx)(n.li, { children: '19 augustus' }), '\n', (0, a.jsx)(n.li, { children: '2 september' }), '\n', (0, a.jsx)(n.li, { children: '16 september' }), '\n', (0, a.jsx)(n.li, { children: '30 september' }), '\n', (0, a.jsx)(n.li, { children: '14 oktober' }), '\n', (0, a.jsx)(n.li, { children: '28 oktober' }), '\n', (0, a.jsx)(n.li, { children: '11 november' }), '\n', (0, a.jsx)(n.li, { children: '25 november' }), '\n', (0, a.jsx)(n.li, { children: '9 december' }), '\n'] }), '\n', (0, a.jsx)(n.p, { children: (0, a.jsx)(n.a, { target: '_blank', 'data-noBrokenLinkCheck': !0, href: t(29560).Z + '', children: 'Voeg Heartbeats aan je kalender (.ics)' }) }), '\n', (0, a.jsx)(n.h2, { id: 'vragen', children: 'Vragen?' }), '\n', (0, a.jsxs)(n.p, { children: ['Heb je nog vragen? Stuur ons dan een mailtje op ', (0, a.jsx)(n.a, { href: 'mailto:info@nldesignsystem.nl', children: 'info@nldesignsystem.nl' }), ' of neem contact op via ', (0, a.jsx)(n.a, { href: 'https://app.slack.com/huddle/T68FXPFQV/C7KDQRXMZ', children: 'het #nl-design-system kanaal' }), ' op ', (0, a.jsx)(n.a, { href: 'https://praatmee.codefor.nl', children: 'de Code for NL Slack' }), ', daar kun je meepraten, vragen stellen of beantwoorden of gesprekken volgen.'] })] });
+   }
+   function b(e = {}) {
+    const { wrapper: n } = { ...(0, i.a)(), ...e.components };
+    return n ? (0, a.jsx)(n, { ...e, children: (0, a.jsx)(p, { ...e }) }) : p(e);
+   }
+  },
+  68257: (e, n, t) => {
+   t.d(n, { Z: () => s });
+   var a = t(83033),
+    i = t(4814),
+    r = t(52676);
+   const s = (e) => {
+    let { appearance: n, ...t } = e;
+    return (0, r.jsx)(a.Z, { className: (0, i.W)('utrecht-button-link', `utrecht-button-link--${n}`), ...t });
+   };
+  },
+  29560: (e, n, t) => {
+   t.d(n, { Z: () => a });
+   const a = t.p + 'assets/files/heartbeat-de3d4e9cd3a5ca159b02ac021e705fc8.ics';
+  },
+  40139: (e, n, t) => {
+   t.d(n, { Z: () => l, a: () => s });
+   var a = t(75271);
+   const i = {},
+    r = a.createContext(i);
+   function s(e) {
+    const n = a.useContext(r);
+    return a.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function l(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(i) : e.components || i) : s(e.components)), a.createElement(r.Provider, { value: n }, e.children);
+   }
+  },
+  78511: (e, n, t) => {
+   t.d(n, { Z: () => a });
+   var a = (0, t(72315).Z)('outline', 'chevron-right', 'IconChevronRight', [['path', { d: 'M9 6l6 6l-6 6', key: 'svg-0' }]]);
+  },
+ },
+]);
---
+++ assets/js/323b7945.b593a96b.js
@@ -0,0 +1,77 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [31875],
+ {
+  80293: (e, t, n) => {
+   n.r(t), n.d(t, { assets: () => o, contentTitle: () => l, default: () => h, frontMatter: () => r, metadata: () => d, toc: () => m });
+   var i = n(52676),
+    a = n(40139),
+    s = n(82415);
+   const r = { title: 'Aanmelden \xb7 Heartbeat \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Aanmelden', pagination_label: '2 wekelijkse updates van het kernteam en community', slug: '/events/heartbeat/aanmelden' },
+    l = 'Meld je aan voor de Heartbeat',
+    d = { id: 'community/events/heartbeat/aanmelden', title: 'Aanmelden \xb7 Heartbeat \xb7 Bijeenkomsten', description: 'Heartbeats zijn publiek toegankelijk. Laat je emailadres achter om op de hoogte te worden gehouden van toekomstige bijeenkomsten.', source: '@site/docs/community/events/heartbeat/aanmelden.mdx', sourceDirName: 'community/events/heartbeat', slug: '/events/heartbeat/aanmelden', permalink: '/events/heartbeat/aanmelden', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/heartbeat/aanmelden.mdx', tags: [], version: 'current', frontMatter: { title: 'Aanmelden \xb7 Heartbeat \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Aanmelden', pagination_label: '2 wekelijkse updates van het kernteam en community', slug: '/events/heartbeat/aanmelden' }, sidebar: 'community', previous: { title: "Video's van de afgelopen Heartbeats", permalink: '/events/heartbeat/videos' }, next: { title: 'Wat moet ik weten als ik zelf iets wil vertellen tijdens de Heartbeat?', permalink: '/events/heartbeat/zelf-iets-vertellen' } },
+    o = {},
+    m = [];
+   function c(e) {
+    const t = { h1: 'h1', p: 'p', ...(0, a.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(t.h1, { id: 'meld-je-aan-voor-de-heartbeat', children: 'Meld je aan voor de Heartbeat' }), '\n', (0, i.jsx)(t.p, { children: 'Heartbeats zijn publiek toegankelijk. Laat je emailadres achter om op de hoogte te worden gehouden van toekomstige bijeenkomsten.' }), '\n', (0, i.jsx)(t.p, { children: 'We nodigen je op dit emailadres dan ook uit voor de daadwerkelijke meeting.' }), '\n', (0, i.jsx)(s.A, { listId: '601sv3rzai', emailFieldId: 'GivkPnqc1o', firstNameFieldId: 'p8ZNN4fpMI', thanksPage: 'https://nldesignsystem.nl/events/heartbeat/aanmelden/bedankt', laPostaId: 'iyihtuzpiq' })] });
+   }
+   function h(e = {}) {
+    const { wrapper: t } = { ...(0, a.a)(), ...e.components };
+    return t ? (0, i.jsx)(t, { ...e, children: (0, i.jsx)(c, { ...e }) }) : c(e);
+   }
+  },
+  82415: (e, t, n) => {
+   n.d(t, { A: () => d });
+   var i = n(87573),
+    a = n(41115),
+    s = n(75271),
+    r = n(25282),
+    l = n(52676);
+   const d = (e) => {
+    let { listId: t = '', laPostaId: n = '', thanksPage: d = '', emailFieldId: o = '', firstNameFieldId: m = '', orgId: c = '', interestsId: h = '', interests: u = [], workAreasId: p = '', privacyPolicyId: v = '', language: j, submitText: x = '' } = e;
+    const {
+      register: g,
+      handleSubmit: b,
+      formState: { errors: y },
+     } = (0, r.cI)(),
+     k = (0, s.useRef)(null),
+     f = '2' === j?.value,
+     { search: w } = (0, i.TH)(),
+     _ = new URLSearchParams(w),
+     I = _.get('prefillEmail'),
+     $ = _.get('prefillName');
+    return (0, l.jsxs)('form', {
+     method: 'post',
+     action: 'https://nl-design-system.email-provider.eu/subscribe/post/index.php',
+     noValidate: !0,
+     acceptCharset: 'utf-8',
+     ref: k,
+     onSubmit: b(() => {
+      k.current.submit();
+     }),
+     children: [(0, l.jsxs)(a.Wi, { type: 'email', children: [(0, l.jsx)(a.nv, { children: (0, l.jsx)(a.lX, { htmlFor: `id-${o}`, children: f ? 'Email address ' : 'E-mailadres' }) }), y[o] && (0, l.jsx)(a.w7, { children: y[o].message }), (0, l.jsx)(a.nv, { children: (0, l.jsx)(a.fE, { id: `id-${o}`, name: o, type: 'email', autoComplete: 'email', defaultValue: I, 'aria-required': 'true', ...g(`${o}`, { required: { value: !0, message: f ? 'This field is required, but it was left empty.' : 'Dit veld is verplicht, maar het is niet ingevuld.' }, pattern: { value: /\S+@\S+\.\S+/, message: f ? 'This is not a valid email address.' : 'Dit is geen correct emailadres.' } }), invalid: !!y[o] }) })] }), (0, l.jsxs)(a.Wi, { type: 'text', children: [(0, l.jsx)(a.nv, { children: (0, l.jsx)(a.lX, { htmlFor: m, children: f ? 'Name' : 'Naam' }) }), y[m] && (0, l.jsx)(a.w7, { children: y[m].message }), (0, l.jsx)(a.nv, { children: (0, l.jsx)(a.fE, { id: m, name: m, type: 'text', defaultValue: $, autoComplete: 'given-name', 'aria-required': 'true', ...g(`${m}`, { required: { value: !0, message: f ? 'This field is required, but it was left empty.' : 'Dit veld is verplicht, maar het is niet ingevuld.' } }), invalid: !!y[m] }) })] }), c && (0, l.jsxs)(a.Wi, { type: 'text', children: [(0, l.jsx)(a.nv, { children: (0, l.jsx)(a.lX, { htmlFor: c, children: f ? 'Organisation (not required)' : 'Organisatie (niet verplicht)' }) }), y[c] && (0, l.jsx)(a.w7, { children: y[c].message }), (0, l.jsx)(a.nv, { children: (0, l.jsx)(a.fE, { id: c, name: c, type: 'text' }) })] }), h && (0, l.jsxs)(a.pg, { children: [(0, l.jsx)(a.Qv, { children: 'Waar wil je NL Design System voor gebruiken? (niet verplicht)' }), (0, l.jsx)(a.ue, { children: 'Meerdere antwoorden mogelijk.' }), u.map((e, t) => (0, l.jsx)(a.Wi, { type: 'checkbox', children: (0, l.jsxs)(a.nv, { children: [(0, l.jsx)(a.XZ, { name: `${h}[]`, value: t >= 6 ? t + 2 : t + 1, id: `${h}-${t + 1}` }), (0, l.jsx)(a.lX, { htmlFor: `${h}-${t + 1}`, children: e })] }) }, e))] }), p && (0, l.jsxs)(a.Wi, { type: 'text', children: [(0, l.jsxs)(a.nv, { children: [(0, l.jsx)(a.lX, { htmlFor: p, children: 'Aan wat voor projecten werk je? (niet verplicht)' }), (0, l.jsx)(a.ue, { children: 'Denk aan mijn-omgevingen, formulieren en/of kaarten.' })] }), (0, l.jsx)(a.nv, { children: (0, l.jsx)(a.gx, { id: p, name: p }) })] }), v && (0, l.jsxs)(a.Wi, { type: 'text', children: [(0, l.jsx)(a.ue, { id: `${v}-description`, children: f ? (0, l.jsxs)('p', { children: ['The ', (0, l.jsx)('a', { href: '/privacy-policy', children: 'privacy policy' }), ' of NL Design System applies to our activities.'] }) : (0, l.jsxs)('p', { children: ['Op onze activiteiten is de ', (0, l.jsx)('a', { href: '/privacyverklaring', children: 'privacyverklaring van NL Design System' }), ' van toepassing.'] }) }), (0, l.jsxs)(a.Wi, { type: 'checkbox', children: [(0, l.jsx)(a.XZ, { value: '1', id: `${v}-1`, 'aria-describedby': `${v}-description`, 'aria-required': 'true', ...g(`${v}[]`, { required: { value: !0, message: f ? 'You can only register if you agree with the privacy policy.' : 'Je kunt je alleen aanmelden als je akkoord gaat met de privacyverklaring.' } }), invalid: !!y[`${v}[]`] }), (0, l.jsx)(a.lX, { htmlFor: `${v}-1`, children: f ? 'I agree to the use of my data in accordance with the privacy policy' : 'Ik ga akkoord met het gebruik van mijn gegevens volgens de privacyverklaring' })] }), y[v] && (0, l.jsx)(a.w7, { children: y[v].message })] }), j?.id && (0, l.jsx)('input', { type: 'hidden', name: j.id, value: j.value }), (0, l.jsx)(a.hE, { children: (0, l.jsx)(a.zx, { type: 'submit', appearance: 'primary-action-button', children: x || (f ? 'Sign up' : 'Aanmelden') }) }), (0, l.jsx)('input', { type: 'hidden', name: 'next', value: d }), (0, l.jsx)('input', { type: 'hidden', name: 'a', value: n }), (0, l.jsx)('input', { type: 'hidden', name: 'l', value: t }), (0, l.jsx)('input', { name: 'osnD9cWRI3', autoComplete: 'on', id: 'id-osnD9cWRI3', type: 'hidden', value: 'NLDS website' }), (0, l.jsx)('input', { autoComplete: 'new-password', type: 'email', id: 'email', name: 'email', placeholder: 'Your e-mail here', className: 'sr-only', 'aria-hidden': 'true' })],
+    });
+   };
+  },
+  40139: (e, t, n) => {
+   n.d(t, { Z: () => l, a: () => r });
+   var i = n(75271);
+   const a = {},
+    s = i.createContext(a);
+   function r(e) {
+    const t = i.useContext(s);
+    return i.useMemo(
+     function () {
+      return 'function' == typeof e ? e(t) : { ...t, ...e };
+     },
+     [t, e],
+    );
+   }
+   function l(e) {
+    let t;
+    return (t = e.disableParentContext ? ('function' == typeof e.components ? e.components(a) : e.components || a) : r(e.components)), i.createElement(s.Provider, { value: t }, e.children);
+   }
+  },
+ },
+]);
