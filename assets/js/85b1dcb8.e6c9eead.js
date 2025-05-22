e3e22ae4e2f6b13b2de5b2d06d39580259ad353f
eslook <42849307+eslook@users.noreply.github.com>
2025-05-22 14:04:26 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@d5f4971ad97400266bc44509ca0b1d5978dc2521 🚀

--- assets/js/85b1dcb8.e6c9eead.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
- [95077],
- {
-  4421: (e) => {
-   e.exports = JSON.parse('{"title":"Bijeenkomsten","slug":"/community/events/overzicht","permalink":"/community/events/overzicht","navigation":{"previous":{"title":"Community sprints","permalink":"/community/community-sprints"},"next":{"title":"Heartbeat","permalink":"/events/heartbeat/overzicht"}}}');
-  },
- },
-]);
