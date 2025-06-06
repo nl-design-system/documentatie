cd07fb0b6ee823d90a21015d9f20b71b895da229
RenateRoke <94439479+RenateRoke@users.noreply.github.com>
2025-06-06 09:01:23 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@f13bb3d753047c6f9fcc4935760e9d5a2d0d7bde 🚀

---
+++ assets/js/4685208b.950c7afc.js
@@ -0,0 +1,233 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [71278],
+ {
+  66968: (e, n, o) => {
+   o.d(n, { ZP: () => s });
+   var l = o(52676),
+    t = o(40139);
+   function i(e) {
+    const n = { a: 'a', em: 'em', h2: 'h2', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, l.jsxs)(l.Fragment, { children: [(0, l.jsx)(n.h2, { id: 'over-deze-richtlijnen', children: 'Over deze richtlijnen' }), '\n', (0, l.jsxs)(n.p, { children: ['Deze richtlijnen worden onderhouden door het NL Design System en zijn op dit moment in ', (0, l.jsx)(n.em, { children: 'beta' }), '.'] }), '\n', (0, l.jsxs)(n.p, { children: ['We willen graag van de community horen of ze werkbaar en nuttig zijn. Heb je vragen, aanvullingen of opmerkingen?\n', (0, l.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Open een issue op GitHub' }), ' en deel je mening.'] })] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, l.jsx)(n, { ...e, children: (0, l.jsx)(i, { ...e }) }) : i(e);
+   }
+  },
+  77012: (e, n, o) => {
+   o.r(n), o.d(n, { assets: () => d, contentTitle: () => r, default: () => h, frontMatter: () => s, metadata: () => a, toc: () => c });
+   var l = o(52676),
+    t = o(40139),
+    i = (o(43599), o(40761), o(66968));
+   const s = { title: 'Flitsen in video \xb7 Content \xb7 Richtlijnen', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Flitsen', pagination_label: 'Flitsen', description: 'Richtlijnen voor flitsen in video binnen NL Design System', slug: '/richtlijnen/content/video/flitsen', keywords: ['video', 'YouTube', 'Vimeo', 'flitsen', 'epilepsie', 'epileptische aanval'] },
+    r = 'Flitsen in video',
+    a = { id: 'richtlijnen/content/video/flitsen', title: 'Flitsen in video \xb7 Content \xb7 Richtlijnen', description: 'Richtlijnen voor flitsen in video binnen NL Design System', source: '@site/docs/richtlijnen/content/video/flitsen.md', sourceDirName: 'richtlijnen/content/video', slug: '/richtlijnen/content/video/flitsen', permalink: '/richtlijnen/content/video/flitsen', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/richtlijnen/content/video/flitsen.md', tags: [], version: 'current', frontMatter: { title: 'Flitsen in video \xb7 Content \xb7 Richtlijnen', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Flitsen', pagination_label: 'Flitsen', description: 'Richtlijnen voor flitsen in video binnen NL Design System', slug: '/richtlijnen/content/video/flitsen', keywords: ['video', 'YouTube', 'Vimeo', 'flitsen', 'epilepsie', 'epileptische aanval'] }, sidebar: 'richtlijnen', previous: { title: 'Audiodescriptie', permalink: '/richtlijnen/content/video/audiodescriptie' }, next: { title: 'Gebarentaal', permalink: '/richtlijnen/content/video/gebarentaal' } },
+    d = {},
+    c = [
+     { value: 'Wie heeft last van flitsen?', id: 'wie-heeft-last-van-flitsen', level: 2 },
+     { value: 'Voorbeelden', id: 'voorbeelden', level: 2 },
+     { value: 'Flitsen voorkomen', id: 'flitsen-voorkomen', level: 2 },
+     { value: 'Voor en tijdens het opnemen van de video', id: 'voor-en-tijdens-het-opnemen-van-de-video', level: 3 },
+     { value: 'Bij en na het monteren van de video', id: 'bij-en-na-het-monteren-van-de-video', level: 3 },
+     { value: 'Meer lezen over gerelateerde WCAG-succescriteria op NL Design System', id: 'meer-lezen-over-gerelateerde-wcag-succescriteria-op-nl-design-system', level: 2 },
+    ];
+   function v(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', h3: 'h3', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, l.jsxs)(l.Fragment, { children: [(0, l.jsx)(n.h1, { id: 'flitsen-in-video', children: 'Flitsen in video' }), '\n', (0, l.jsx)(n.p, { children: 'Een flits is een kort moment van licht dat plotseling verschijnt en weer verdwijnt. Flitsen kunnen fysieke klachten veroorzaken bij mensen die daar gevoelig voor zijn. Voorkom daarom zo veel mogelijk dat er flitsen voorkomen in je video.' }), '\n', (0, l.jsx)(n.h2, { id: 'wie-heeft-last-van-flitsen', children: 'Wie heeft last van flitsen?' }), '\n', (0, l.jsx)(n.p, { children: 'Wanneer er in een hele korte tijd meerdere flitsen voorkomen in een video, kan dit een epileptische aanval uitlokken bij mensen die daar gevoelig voor zijn. Daarnaast kunnen mensen door flitsen hoofdpijn of migraine krijgen en er misselijk van worden.' }), '\n', (0, l.jsx)(n.h2, { id: 'voorbeelden', children: 'Voorbeelden' }), '\n', (0, l.jsx)(n.p, { children: 'Een flits ontstaat door een plotselinge overgang van donker (lage helderheid) naar licht (hoge helderheid), of andersom. Voorbeelden hiervan die in een video kunnen voorkomen zijn onder andere:' }), '\n', (0, l.jsxs)(n.ul, { children: ['\n', (0, l.jsx)(n.li, { children: 'Stroboscopisch licht, bijvoorbeeld tijdens concerten of voor dramatisch effect in een film.' }), '\n', (0, l.jsx)(n.li, { children: 'Camera-flitsen.' }), '\n', (0, l.jsx)(n.li, { children: 'Explosies.' }), '\n', (0, l.jsx)(n.li, { children: 'Vuurwerk.' }), '\n', (0, l.jsx)(n.li, { children: 'Knipperende teksten.' }), '\n', (0, l.jsx)(n.li, { children: 'Reflecties en schitteringen, bijvoorbeeld in een spiegel of door de zon op stilstaand water.' }), '\n', (0, l.jsx)(n.li, { children: 'Snel wisselende overgangen van scenes met een groot kleurcontrast.' }), '\n'] }), '\n', (0, l.jsx)(n.h2, { id: 'flitsen-voorkomen', children: 'Flitsen voorkomen' }), '\n', (0, l.jsx)(n.p, { children: 'De makkelijkste manier om flitsen in een video te voorkomen is natuurlijk door geen opnames te maken waar flitsen in voor komen. Maar het is niet altijd te voorspellen of iets op beeld een flits veroorzaakt.' }), '\n', (0, l.jsx)(n.p, { children: 'Er zijn wel een aantal dingen die je zelf kunt doen om er voor te zorgen dat je video geen overgangen van donker naar licht (of andersom) bevat die schadelijk kunnen zijn voor mensen die daar gevoelig voor zijn.' }), '\n', (0, l.jsx)(n.h3, { id: 'voor-en-tijdens-het-opnemen-van-de-video', children: 'Voor en tijdens het opnemen van de video' }), '\n', (0, l.jsx)(n.p, { children: 'Let voorafgaand en tijdens het opnemen van je video op:' }), '\n', (0, l.jsxs)(n.ul, { children: ['\n', (0, l.jsx)(n.li, { children: 'Knipperende en flitsende lichtbronnen, zoals de eerder genoemde voorbeelden.' }), '\n', (0, l.jsx)(n.li, { children: 'Led-, neon- en TL-verlichting. Afhankelijk van je camera kan deze verlichting knipper-effecten in de opname veroorzaken.' }), '\n', (0, l.jsx)(n.li, { children: 'Kleur en licht. Gebruik waar mogelijk rustige achtergronden en vermijd fel licht.' }), '\n'] }), '\n', (0, l.jsx)(n.h3, { id: 'bij-en-na-het-monteren-van-de-video', children: 'Bij en na het monteren van de video' }), '\n', (0, l.jsx)(n.p, { children: 'Ook tijdens en na het monteren kun je een aantal stappen nemen om flitsen te voorkomen:' }), '\n', (0, l.jsxs)(n.ul, { children: ['\n', (0, l.jsx)(n.li, { children: 'Maak gebruik van rustige overgangen tussen sc\xe8nes die sterk van kleur verschillen, bijvoorbeeld wanneer je van een zwart scherm naar een beeld met veel licht schakelt.' }), '\n', (0, l.jsx)(n.li, { children: 'Laat beeld met mogelijke flitsen niet het hele beeld in beslag nemen. Hoe minder ruimte de flits in beslag neemt, hoe kleiner de kans dat de flits fysieke problemen veroorzaakt.' }), '\n', (0, l.jsxs)(n.li, { children: ['Controleer je video met de ', (0, l.jsx)(n.a, { href: 'https://trace.umd.edu/peat/', children: 'PEAT analyse-tool' }), '. Dit is gratis software die flitsen boven een bepaalde schadelijke grenswaarde detecteert.'] }), '\n', (0, l.jsx)(n.li, { children: 'Controleer je video in de grootst mogelijke aangeboden weergave. Kan de bezoeker de video in fullscreen modus bekijken? Dan controleer je de video in die modus.' }), '\n'] }), '\n', (0, l.jsx)(n.p, { children: 'Is het helemaal niet mogelijk om een mogelijke flits te voorkomen? Voeg dan een waarschuwing toe, zodat bezoekers ervoor kunnen kiezen om de video niet verder te kijken.' }), '\n', (0, l.jsxs)(n.p, { children: ['Let wel op: Bevat je video meerdere flitsen in een hele korte tijd? Dan is het toevoegen van een waarschuwing niet voldoende om te voldoen aan ', (0, l.jsx)(n.a, { href: '/wcag/2.3.1', children: 'succescriterium 2.3.1 Drie flitsen beneden drempelwaarde' }), '.'] }), '\n', (0, l.jsx)(n.h2, { id: 'meer-lezen-over-gerelateerde-wcag-succescriteria-op-nl-design-system', children: 'Meer lezen over gerelateerde WCAG-succescriteria op NL Design System' }), '\n', (0, l.jsx)(n.p, { children: "Op de WCAG-succescriteria pagina's op deze website lees je meer over veelgemaakte fouten en hoe je kunt testen of jouw website aan de succescriteria voldoet." }), '\n', (0, l.jsxs)(n.ul, { children: ['\n', (0, l.jsx)(n.li, { children: (0, l.jsx)(n.a, { href: '/wcag/2.3.1', children: 'Succescriterium 2.3.1 Drie flitsen of beneden drempelwaarde' }) }), '\n', (0, l.jsx)(n.li, { children: (0, l.jsx)(n.a, { href: '/wcag/2.3.2', children: 'Succescriterium 2.3.2 Drie flitsen' }) }), '\n', (0, l.jsx)(n.li, { children: (0, l.jsx)(n.a, { href: '/wcag/2.3.3', children: 'Succescriterium 2.3.3 Animatie uit interacties' }) }), '\n'] }), '\n', (0, l.jsx)(i.ZP, {})] });
+   }
+   function h(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, l.jsx)(n, { ...e, children: (0, l.jsx)(v, { ...e }) }) : v(e);
+   }
+  },
+  43599: (e, n, o) => {
+   o.d(n, { X: () => g });
+   var l = o(41179),
+    t = o(16167),
+    i = o(4814),
+    s = o(64642),
+    r = o(93872),
+    a = o(86017),
+    d = o(48231),
+    c = o(75119),
+    v = o(75271),
+    h = o(60027),
+    m = o(52676);
+   const p = (e) => {
+    let { children: n } = e;
+    return (0, m.jsx)(t.Tg, { className: 'nlds-canvas__example-surface', children: (0, m.jsx)(t.BB, { className: (0, i.Z)('utrecht-document--surface', 'nlds-canvas__example-document'), children: (0, m.jsx)(t.nv, { className: 'nlds-canvas__example-paragraph', children: n }) }) });
+   };
+   p.displayName = 'ParagraphContainer';
+   const u = (e) => {
+    let { children: n } = e;
+    return (0, m.jsx)(t.Tg, { className: 'nlds-canvas__example-surface', children: (0, m.jsx)(t.BB, { className: (0, i.Z)('utrecht-document--surface', 'nlds-canvas__example-document'), children: n }) });
+   };
+   u.displayName = 'DocumentContainer';
+   const j = (e) => {
+    let { children: n } = e;
+    return (0, m.jsx)(t.Tg, { className: 'nlds-canvas__example-surface', children: n });
+   };
+   j.displayName = 'SurfaceContainer';
+   const g = (e) => {
+    let { code: n, copy: o = !0, defaultExpandedCode: g = !1, displayCode: b = !0, children: k, container: f = 'document', language: y, designTokens: x } = e;
+    const _ = 'function' == typeof k ? k() : k,
+     w = 'function' == typeof n ? n() : (0, v.isValidElement)(n) ? n : void 0,
+     N = 'string' == typeof n ? n : h.uS(w || _),
+     [z, C] = (0, v.useState)(N),
+     [S, Z] = (0, v.useState)(g);
+    (0, v.useEffect)(() => {
+     (async () => {
+      const e = await c.ZP.format(N, { parser: y, plugins: [s.Z, r.ZP, a.ZP, d.ZP], semi: !1, singleAttributePerLine: !0, embeddedLanguageFormatting: 'off', htmlWhitespaceSensitivity: 'ignore' });
+      C(e);
+     })();
+    }, [N]);
+    const F = (0, v.useId)();
+    let D = v.Fragment;
+    return (
+     'paragraph' === f ? (D = p) : 'document' === f ? (D = u) : 'surface' === f && (D = j),
+     (0, m.jsxs)('div', {
+      className: (0, i.Z)('nlds-canvas'),
+      children: [
+       _ && (0, m.jsx)('div', { className: (0, i.Z)('nlds-canvas__example'), children: (0, m.jsx)('div', { className: 'voorbeeld-theme', style: x, children: (0, m.jsx)(D, { children: (0, m.jsx)(t.pu, { children: _ }) }) }) }),
+       b &&
+        (0, m.jsx)('div', {
+         className: (0, i.Z)('nlds-canvas__toolbar'),
+         children: (0, m.jsx)(t.zx, {
+          className: (0, i.Z)('nlds-canvas__button', 'nlds-canvas__toggle-code-button'),
+          appearance: 'subtle-button',
+          onClick: () => {
+           Z(!S);
+          },
+          'aria-expanded': S,
+          'aria-controls': F,
+          children: S ? 'Verberg code' : 'Bekijk code',
+         }),
+        }),
+       b &&
+        (0, m.jsxs)('div', {
+         className: (0, i.Z)('nlds-canvas__code-block', !o && 'nlds-canvas__code-block--user-select-none'),
+         id: F,
+         hidden: !S,
+         children: [
+          (0, m.jsx)(l.u, { codeBlockLabel: 'Codevoorbeeld', syntax: y, textContent: z, trim: !0 }),
+          o &&
+           (0, m.jsx)('div', {
+            className: (0, i.Z)('nlds-canvas__toolbar', 'nlds-canvas__toolbar--copy'),
+            children: (0, m.jsx)(t.zx, {
+             className: (0, i.Z)('nlds-canvas__button', 'nlds-canvas__copy-button'),
+             appearance: 'subtle-button',
+             onClick: () => {
+              navigator.clipboard.writeText(z).catch((e) => console.error('Copy code failed', e));
+             },
+             children: 'Kopieer',
+            }),
+           }),
+         ],
+        }),
+      ],
+     })
+    );
+   };
+  },
+  41179: (e, n, o) => {
+   o.d(n, { u: () => d });
+   var l = o(16167),
+    t = o(60648),
+    i = o(75271),
+    s = o(40761);
+   const r = {
+    plain: { color: 'var(--nlds-code-block-color)', backgroundColor: 'var(--nlds-code-block-background-color)' },
+    styles: [
+     { types: ['maybe-class-name'], style: { color: 'var(--nlds-code-block-maybe-class-name-color, var(--nlds-code-block-class-name-color))' } },
+     { types: ['comment'], style: { color: 'var(--nlds-code-block-comment-color)', fontStyle: 'italic' } },
+     { types: ['prolog'], style: { color: 'var(--nlds-code-block-prolog-color)', fontStyle: 'italic' } },
+     { types: ['cdata'], style: { color: 'var(--nlds-code-block-cdata-color)', fontStyle: 'italic' } },
+     { types: ['punctuation'], style: { color: 'var(--nlds-code-block-punctuation-color)' } },
+     { types: ['deleted'], style: { color: 'var(--nlds-code-block-deleted-color)', fontStyle: 'italic' } },
+     { types: ['symbol'], style: { color: 'var(--nlds-code-block-symbol-color)' } },
+     { types: ['property'], style: { color: 'var(--nlds-code-block-property-color)' } },
+     { types: ['tag'], style: { color: 'var(--nlds-code-block-tag-color)' } },
+     { types: ['operator'], style: { color: 'var(--nlds-code-block-operator-color)' } },
+     { types: ['keyword'], style: { color: 'var(--nlds-code-block-keyword-color)' } },
+     { types: ['boolean'], style: { color: 'var(--nlds-code-block-boolean-color)' } },
+     { types: ['number'], style: { color: 'var(--nlds-code-block-number-color)' } },
+     { types: ['constant'], style: { color: 'var(--nlds-code-block-constant-color)' } },
+     { types: ['function'], style: { color: 'var(--nlds-code-block-function-color)' } },
+     { types: ['builtin'], style: { color: 'var(--nlds-code-block-builtin-color)' } },
+     { types: ['char'], style: { color: 'var(--nlds-code-block-char-color)' } },
+     { types: ['selector'], style: { color: 'var(--nlds-code-block-selector-color)', fontStyle: 'italic' } },
+     { types: ['doctype'], style: { color: 'var(--nlds-code-block-doctype-color)', fontStyle: 'italic' } },
+     { types: ['attr-name'], style: { color: 'var(--nlds-code-block-attr-name-color)', fontStyle: 'italic' } },
+     { types: ['inserted'], style: { color: 'var(--nlds-code-block-inserted-color)', fontStyle: 'italic' } },
+     { types: ['string'], style: { color: 'var(--nlds-code-block-string-color)' } },
+     { types: ['url'], style: { color: 'var(--nlds-code-block-url-color)' } },
+     { types: ['entity'], style: { color: 'var(--nlds-code-block-entity-color)' } },
+     { types: ['class-name'], style: { color: 'var(--nlds-code-block-class-name-color)' } },
+     { types: ['atrule'], style: { color: 'var(--nlds-code-block-atrule-color)' } },
+     { types: ['attr-value'], style: { color: 'var(--nlds-code-block-attr-value-color)' } },
+     { types: ['regex'], style: { color: 'var(--nlds-code-block-regex-color)' } },
+     { types: ['important'], style: { color: 'var(--nlds-code-block-important-color)', fontWeight: 'bold' } },
+     { types: ['variable'], style: { color: 'var(--nlds-code-block-variable-color)' } },
+     { types: ['bold'], style: { fontWeight: 'bold' } },
+     { types: ['italic'], style: { fontStyle: 'italic' } },
+    ],
+   };
+   var a = o(52676);
+   function d(e) {
+    let { lineNumber: n, syntax: o, textContent: d, trim: c } = e,
+     v = d;
+    const { title: h, type: m } = (0, i.useContext)(s.n),
+     p = (0, i.useId)();
+    return (
+     c && (v = v.trim()),
+     (0, a.jsx)(t.y$, {
+      theme: r,
+      code: v,
+      language: o || '',
+      children: (e) => {
+       let { style: o, tokens: t, getLineProps: i, getTokenProps: s } = e;
+       return (0, a.jsxs)(a.Fragment, { children: [(0, a.jsxs)('span', { hidden: !0, id: p, children: ['codevoorbeeld ', m ? `\u201c${m}\u201d` : '', ' ', h ? ': ' : ' ', h] }), (0, a.jsx)(l.dn, { tabIndex: 0, role: h ? 'region' : void 0, 'aria-labelledby': p, style: o, children: t.map((e, o) => (0, a.jsxs)('span', { ...i({ line: e }), children: [n && (0, a.jsx)('span', { children: o + 1 }), e.map((e, n) => (0, a.jsx)('span', { ...s({ token: e }) }, n)), '\n'] }, o)) })] });
+      },
+     })
+    );
+   }
+  },
+  40761: (e, n, o) => {
+   o.d(n, { X: () => c, n: () => d });
+   var l = o(69244),
+    t = o(20061),
+    i = o(16167),
+    s = o(4814),
+    r = o(75271),
+    a = o(52676);
+   const d = (0, r.createContext)({}),
+    c = (e) => {
+     let { title: n, appearance: o, description: r, children: c, figure: v } = e;
+     const h = { do: 'Doen', dont: 'Niet doen' },
+      m = v ? 'figure' : 'div',
+      p = v ? 'figcaption' : 'div';
+     return (0, a.jsxs)(m, { className: (0, s.Z)('nlds-guideline', `nlds-guideline--${o}`), id: 'string' == typeof n ? n?.toLowerCase().replace(/\s/g, '-') : void 0, children: [(0, a.jsxs)(p, { className: 'nlds-guideline__description', children: ['dont' === o ? (0, a.jsxs)(a.Fragment, { children: [(0, a.jsxs)(i.nv, { className: (0, s.Z)('nlds-guideline__badge', `nlds-guideline__badge--${o}`), children: [(0, a.jsx)(l.Z, { 'aria-hidden': 'true', role: 'presentation', className: 'nlds-guideline__icon' }), (0, a.jsx)('span', { className: 'nlds-guideline__title', children: h.dont })] }), (0, a.jsx)(i.nv, { children: n })] }) : (0, a.jsxs)(a.Fragment, { children: [(0, a.jsxs)(i.nv, { className: (0, s.Z)('nlds-guideline__badge', `nlds-guideline__badge--${o}`), children: [(0, a.jsx)(t.Z, { 'aria-hidden': 'true', role: 'presentation', className: 'nlds-guideline__icon' }), (0, a.jsx)('span', { className: 'nlds-guideline__title', children: h.do })] }), (0, a.jsx)(i.nv, { children: n })] }), r] }), (0, a.jsx)('div', { className: 'nlds-guideline__example', children: (0, a.jsx)(d.Provider, { value: { title: n, type: h[o] }, children: c }) })] });
+    };
+  },
+  40139: (e, n, o) => {
+   o.d(n, { Z: () => r, a: () => s });
+   var l = o(75271);
+   const t = {},
+    i = l.createContext(t);
+   function s(e) {
+    const n = l.useContext(i);
+    return l.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function r(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : s(e.components)), l.createElement(i.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
