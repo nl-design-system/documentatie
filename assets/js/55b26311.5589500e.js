51f56c6e5ccedcf3b4b5be36f619f06407a355c4
jeffreylauwers <jeffreylauwers@gmail.com>
2025-06-24 11:23:27 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@e270217c6c7fb522797010db36d944fd5d9568a1 🚀

--- assets/js/55b26311.5589500e.js
+++ assets/js/55b26311.5589500e.js
@@ -16,7 +16,7 @@
    }
   },
   84491: (e, t, n) => {
-   n.r(t), n.d(t, { assets: () => h, contentTitle: () => g, default: () => u, frontMatter: () => c, metadata: () => j, toc: () => m });
+   n.r(t), n.d(t, { assets: () => h, contentTitle: () => g, default: () => u, frontMatter: () => c, metadata: () => j, toc: () => p });
    var o = n(52676),
     r = n(40139),
     i = n(66968);
@@ -29,7 +29,7 @@
    }
    function a(e) {
     const t = { a: 'a', code: 'code', p: 'p', ...(0, r.a)(), ...e.components };
-    return (0, o.jsxs)(o.Fragment, { children: [(0, o.jsxs)(t.p, { children: ['Gebruik een lettergrootte ofwel ', (0, o.jsx)(t.code, { children: 'font-size' }), ' die groot genoeg is voor de ', (0, o.jsx)(t.a, { href: 'https://en.wikipedia.org/wiki/Body_text', children: (0, o.jsx)(t.code, { children: 'body text' }) }), ', zodat mensen comfortabel kunnen lezen. Gebruik als richtlijn minimaal een lettergrootte van 16px.'] }), '\n', (0, o.jsxs)(t.p, { children: ['Hoewel we ', (0, o.jsx)(t.code, { children: '16px' }), ' aangeven als minimale ', (0, o.jsx)(t.code, { children: 'font-size' }), ' is het beter om g\xe9\xe9n gebruik te maken van \u2018fixed-size units\u2019 zoals ', (0, o.jsx)(t.code, { children: 'px' }), '. Gebruik liever een relatieve waarde als ', (0, o.jsx)(t.code, { children: 'em' }), ' of ', (0, o.jsx)(t.code, { children: 'rem' }), '.'] }), '\n', (0, o.jsxs)(t.p, { children: ['Waarom? Browsers bieden de mogelijkheid om de standaard ', (0, o.jsx)(t.code, { children: 'font-size' }), ' aan te passen. Handig voor iemand die slechtziend is. Wanneer je relatieve waardes gebruikt schaalt alles netjes mee. Maar een pixel blijft een pixel en zodoende verandert er niks.'] }), '\n', (0, o.jsx)(t.p, { children: 'Vanuit het NL Design System zelf kiezen we momenteel w\xe9l voor pixels. Dit komt omdat wij dezelfde design token waarde willen gebruiken in Figma en in code. Wij willen de CSS nog aanpassen om px automatisch om te zetten naar rem.' })] });
+    return (0, o.jsxs)(o.Fragment, { children: [(0, o.jsxs)(t.p, { children: ['Gebruik een lettergrootte ofwel ', (0, o.jsx)(t.code, { children: 'font-size' }), ' die groot genoeg is voor de ', (0, o.jsx)(t.a, { href: 'https://en.wikipedia.org/wiki/Body_text', children: (0, o.jsx)(t.code, { children: 'body text' }) }), ', zodat mensen comfortabel kunnen lezen. Gebruik als richtlijn minimaal een lettergrootte van 16px.'] }), '\n', (0, o.jsxs)(t.p, { children: ['Hoewel we ', (0, o.jsx)(t.code, { children: '16px' }), ' aangeven als minimale ', (0, o.jsx)(t.code, { children: 'font-size' }), ' is het beter om g\xe9\xe9n gebruik te maken van \u2018fixed-size units\u2019 zoals ', (0, o.jsx)(t.code, { children: 'px' }), '. Gebruik liever een relatieve waarde als ', (0, o.jsx)(t.code, { children: 'em' }), ' of ', (0, o.jsx)(t.code, { children: 'rem' }), '.'] }), '\n', (0, o.jsxs)(t.p, { children: ['Waarom? Browsers bieden de mogelijkheid om de standaard ', (0, o.jsx)(t.code, { children: 'font-size' }), ' aan te passen. Handig voor iemand die slechtziend is. Wanneer je relatieve waardes gebruikt schaalt alles netjes mee. Maar een pixel blijft een pixel en zodoende verandert er niks.'] })] });
    }
    function d(e = {}) {
     const { wrapper: t } = { ...(0, r.a)(), ...e.components };
@@ -39,14 +39,14 @@
     g = 'Zorg ervoor dat letters groot genoeg zijn',
     j = { id: 'richtlijnen/stijl/typography/fontsize/README', title: 'Zorg ervoor dat letters groot genoeg zijn \xb7 Typografie \xb7 Richtlijnen', description: 'Gebruik een lettergrootte ofwel die groot genoeg is voor de broodtekst, zodat mensen comfortabel kunnen lezen. Gebruik als richtlijn minimaal een lettergrootte van 16px.', source: '@site/docs/richtlijnen/stijl/typography/1-fontsize/README.mdx', sourceDirName: 'richtlijnen/stijl/typography/1-fontsize', slug: '/richtlijnen/stijl/typografie/lettergrootte', permalink: '/richtlijnen/stijl/typografie/lettergrootte', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/richtlijnen/stijl/typography/1-fontsize/README.mdx', tags: [], version: 'current', frontMatter: { title: 'Zorg ervoor dat letters groot genoeg zijn \xb7 Typografie \xb7 Richtlijnen', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Letters groot genoeg', pagination_label: 'Zorg ervoor dat letters groot genoeg zijn', description: 'Gebruik een lettergrootte ofwel die groot genoeg is voor de broodtekst, zodat mensen comfortabel kunnen lezen. Gebruik als richtlijn minimaal een lettergrootte van 16px.', slug: '/richtlijnen/stijl/typografie/lettergrootte', keywords: ['lettergrootte'] }, sidebar: 'richtlijnen', previous: { title: 'Introductie richtlijnen voor typografie', permalink: '/richtlijnen/stijl/typografie/' }, next: { title: 'Zorg voor een comfortabele regelafstand', permalink: '/richtlijnen/stijl/typografie/regelafstand' } },
     h = {},
-    m = [];
-   function p(e) {
+    p = [];
+   function m(e) {
     const t = { h1: 'h1', ...(0, r.a)(), ...e.components };
     return (0, o.jsxs)(o.Fragment, { children: ['\n', '\n', '\n', (0, o.jsx)(t.h1, { id: 'zorg-ervoor-dat-letters-groot-genoeg-zijn', children: 'Zorg ervoor dat letters groot genoeg zijn' }), '\n', (0, o.jsx)(d, {}), '\n', (0, o.jsx)(l, {}), '\n', (0, o.jsx)(i.ZP, {})] });
    }
    function u(e = {}) {
     const { wrapper: t } = { ...(0, r.a)(), ...e.components };
-    return t ? (0, o.jsx)(t, { ...e, children: (0, o.jsx)(p, { ...e }) }) : p(e);
+    return t ? (0, o.jsx)(t, { ...e, children: (0, o.jsx)(m, { ...e }) }) : m(e);
    }
   },
   40139: (e, t, n) => {
