16a3b1a1766c3ebf6931390c80e34fbf08fa7b35
eslook <42849307+eslook@users.noreply.github.com>
2025-06-26 14:41:23 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@3f4a731bc0b7af0b8b600de5a071ca8b130fb60c 🚀

---
+++ assets/js/097efb18.3e68052c.js
@@ -0,0 +1,66 @@
+/*! For license information please see 097efb18.3e68052c.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [24970],
+ {
+  64008: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => l, contentTitle: () => d, default: () => h, frontMatter: () => a, metadata: () => m, toc: () => c });
+   var i = t(52676),
+    o = t(40139),
+    s = t(86135),
+    r = t(68257);
+   const a = { title: 'Bedankt voor je aanmelding \xb7 Overheidsbrede Portalen Community \xb7 Community Sprints', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Bedankt voor je aanmelding', pagination_label: 'Bedankt voor je aanmelding', slug: '/community/community-sprints/overheidsbrede-portalen-community/aanmelden/bedankt', unlisted: !0 },
+    d = 'Bedankt!',
+    m = { id: 'community/community-sprints/overheidsbrede-portalen-community/aanmelden-success', title: 'Bedankt voor je aanmelding \xb7 Overheidsbrede Portalen Community \xb7 Community Sprints', description: 'Welkom bij de Overheidsbrede Portalen Community, fijn dat je erbij bent!', source: '@site/docs/community/community-sprints/overheidsbrede-portalen-community/aanmelden-success.mdx', sourceDirName: 'community/community-sprints/overheidsbrede-portalen-community', slug: '/community/community-sprints/overheidsbrede-portalen-community/aanmelden/bedankt', permalink: '/community/community-sprints/overheidsbrede-portalen-community/aanmelden/bedankt', draft: !1, unlisted: !0, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/community-sprints/overheidsbrede-portalen-community/aanmelden-success.mdx', tags: [], version: 'current', frontMatter: { title: 'Bedankt voor je aanmelding \xb7 Overheidsbrede Portalen Community \xb7 Community Sprints', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Bedankt voor je aanmelding', pagination_label: 'Bedankt voor je aanmelding', slug: '/community/community-sprints/overheidsbrede-portalen-community/aanmelden/bedankt', unlisted: !0 } },
+    l = {},
+    c = [];
+   function u(e) {
+    const n = { a: 'a', h1: 'h1', li: 'li', p: 'p', ul: 'ul', ...(0, o.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: 'bedankt', children: 'Bedankt!' }), '\n', (0, i.jsx)(n.p, { children: 'Welkom bij de Overheidsbrede Portalen Community, fijn dat je erbij bent!' }), '\n', (0, i.jsx)(n.p, { children: 'Handige links:' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://praatmee.codefor.nl', children: 'Word lid van het #nl-design-system-developers Slack kanaal' }), ' op CodeForNL, daar kun je meepraten, vragen stellen of beantwoorden of gesprekken volgen.'] }), '\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://github.com/nl-design-system/overheidsbrede-portalen-community', children: 'Bekijk de code op GitHub' }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Bekijk onze ', (0, i.jsx)(n.a, { href: 'https://www.youtube.com/playlist?list=PLnAxFMscDU3F1VYQxlJLpmV-Z-FQ6hCU_', children: "video's in de Overheidsbrede Portalen Community Sprint playlist op YouTube" }), ' en abonneer je op ons kanaal.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Vind alle relevante linkjes en informatie op de ', (0, i.jsx)(n.a, { href: 'https://nldesignsystem.nl/community/community-sprints/overheidsbrede-portalen-community', children: 'Overheidsbrede Portalen Community pagina' }), '.'] }), '\n'] }), '\n', (0, i.jsx)(n.p, { children: 'Elke twee weken heeft de Overheidsbrede Portalen Community een afstemming in het #nl-design-system-developers kanaal om aan de NL Design System adoptie binnen mijn omgevingen te werken.' }), '\n', (0, i.jsxs)(r.Z, { appearance: 'secondary-action', href: 'https://nldesignsystem.nl/overheidsbrede-portalen-community/overheidsbrede-portalen-community-afstemming.ics', download: 'https://nldesignsystem.nl/overheidsbrede-portalen-community/overheidsbrede-portalen-community-afstemming.ics', 'aria-label': 'iCal bestand voor de Overheidsbrede Portalen Community Afstemming (download)', children: [(0, i.jsx)(s.Z, {}), ' Download het kalenderbestand'] })] });
+   }
+   function h(e = {}) {
+    const { wrapper: n } = { ...(0, o.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(u, { ...e }) }) : u(e);
+   }
+  },
+  68257: (e, n, t) => {
+   t.d(n, { Z: () => r });
+   var i = t(83033),
+    o = t(4814),
+    s = t(52676);
+   const r = (e) => {
+    let { appearance: n, ...t } = e;
+    return (0, s.jsx)(i.Z, { className: (0, o.W)('utrecht-button-link', `utrecht-button-link--${n}`), ...t });
+   };
+  },
+  40139: (e, n, t) => {
+   t.d(n, { Z: () => a, a: () => r });
+   var i = t(75271);
+   const o = {},
+    s = i.createContext(o);
+   function r(e) {
+    const n = i.useContext(s);
+    return i.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(o) : e.components || o) : r(e.components)), i.createElement(s.Provider, { value: n }, e.children);
+   }
+  },
+  86135: (e, n, t) => {
+   t.d(n, { Z: () => i });
+   var i = (0, t(72315).Z)('outline', 'calendar-event', 'IconCalendarEvent', [
+    ['path', { d: 'M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z', key: 'svg-0' }],
+    ['path', { d: 'M16 3l0 4', key: 'svg-1' }],
+    ['path', { d: 'M8 3l0 4', key: 'svg-2' }],
+    ['path', { d: 'M4 11l16 0', key: 'svg-3' }],
+    ['path', { d: 'M8 15h2v2h-2z', key: 'svg-4' }],
+   ]);
+  },
+ },
+]);
---
+++ assets/js/4bd71969.3e11a862.js
@@ -0,0 +1,9 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [47224],
+ {
+  45006: (i) => {
+   i.exports = JSON.parse('{"title":"Rijkshuisstijl Community","slug":"community/community-sprints/rijkshuisstijl-community/overzicht","permalink":"/community/community-sprints/rijkshuisstijl-community/overzicht","navigation":{"previous":{"title":"Aanmelden","permalink":"/community/community-sprints/overheidsbrede-portalen-community/aanmelden"},"next":{"title":"Rijkshuisstijl Community","permalink":"/community/community-sprints/rijkshuisstijl-community/"}}}');
+  },
+ },
+]);
