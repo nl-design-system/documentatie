4b84fc30e6975e5c2ed384b8758953fe46b2d024
eslook <42849307+eslook@users.noreply.github.com>
2025-06-30 09:09:09 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@449500b3da21d509acc98a9d0e7039eadc1a1da3 🚀

---
+++ assets/js/9d33172e.a580f025.js
@@ -0,0 +1,70 @@
+/*! For license information please see 9d33172e.a580f025.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [54238],
+ {
+  58925: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => u, contentTitle: () => d, default: () => h, frontMatter: () => a, metadata: () => m, toc: () => j });
+   var s = i(52676),
+    t = i(40139),
+    o = i(86135),
+    l = i(68257),
+    r = i(16167);
+   const a = { title: 'Bedankt voor je aanmelding \xb7 Rijkshuisstijl Community \xb7 Community Sprints', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Bedankt voor je aanmelding', pagination_label: 'Bedankt voor je aanmelding', slug: '/community/community-sprints/rijkshuisstijl-community/aanmelden/bedankt', unlisted: !0 },
+    d = 'Bedankt!',
+    m = { id: 'community/community-sprints/rijkshuisstijl-community/aanmelden-success', title: 'Bedankt voor je aanmelding \xb7 Rijkshuisstijl Community \xb7 Community Sprints', description: 'Welkom bij de Rijkshuisstijl Community, fijn dat je erbij bent. We hopen je snel (weer) eens te zien in het #rijkshuisstijl-community Slack kanaal of bij de tweewekelijkse Rijkshuisstijl Community Open Hour!', source: '@site/docs/community/community-sprints/rijkshuisstijl-community/aanmelden-success.mdx', sourceDirName: 'community/community-sprints/rijkshuisstijl-community', slug: '/community/community-sprints/rijkshuisstijl-community/aanmelden/bedankt', permalink: '/community/community-sprints/rijkshuisstijl-community/aanmelden/bedankt', draft: !1, unlisted: !0, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/community-sprints/rijkshuisstijl-community/aanmelden-success.mdx', tags: [], version: 'current', frontMatter: { title: 'Bedankt voor je aanmelding \xb7 Rijkshuisstijl Community \xb7 Community Sprints', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Bedankt voor je aanmelding', pagination_label: 'Bedankt voor je aanmelding', slug: '/community/community-sprints/rijkshuisstijl-community/aanmelden/bedankt', unlisted: !0 } },
+    u = {},
+    j = [
+     { value: 'Open Hour', id: 'open-hour', level: 2 },
+     { value: 'Sprints', id: 'sprints', level: 2 },
+    ];
+   function c(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, s.jsxs)(s.Fragment, { children: [(0, s.jsx)(n.h1, { id: 'bedankt', children: 'Bedankt!' }), '\n', (0, s.jsxs)(n.p, { children: ['Welkom bij de Rijkshuisstijl Community, fijn dat je erbij bent. We hopen je snel (weer) eens te zien in het #rijkshuisstijl-community Slack kanaal of bij de tweewekelijkse ', (0, s.jsx)(n.a, { href: 'https://nldesignsystem.nl/events/rijkshuisstijl-community-open-hour/', children: 'Rijkshuisstijl Community Open Hour' }), '!'] }), '\n', (0, s.jsx)(n.p, { children: 'Handige links:' }), '\n', (0, s.jsxs)(n.ul, { children: ['\n', (0, s.jsxs)(n.li, { children: [(0, s.jsx)(n.a, { href: 'https://praatmee.codefor.nl', children: 'Word lid van het #rijkshuisstijl-community Slack kanaal' }), ' op CodeForNL, daar kun je meepraten, vragen stellen of beantwoorden of gesprekken volgen.'] }), '\n', (0, s.jsxs)(n.li, { children: [(0, s.jsx)(n.a, { href: 'https://github.com/nl-design-system/rijkshuisstijl-community', children: 'Bekijk de code op GitHub' }), '.'] }), '\n', (0, s.jsxs)(n.li, { children: ['Bekijk onze ', (0, s.jsx)(n.a, { href: 'https://www.youtube.com/playlist?list=PLnAxFMscDU3FHVDYR9WvX3d84JdQ5Q3tJ', children: "video's in de Rijkshuisstijl Community Sprint playlist op YouTube" }), ' en abonneer je op ons kanaal.'] }), '\n', (0, s.jsxs)(n.li, { children: ['Vind alle relevante linkjes en informatie op de ', (0, s.jsx)(n.a, { href: 'https://rijkshuisstijl-community.nl/', children: 'Rijkshuisstijl Community homepage' }), '.'] }), '\n'] }), '\n', (0, s.jsx)(n.h2, { id: 'open-hour', children: 'Open Hour' }), '\n', (0, s.jsxs)(n.p, { children: ['Voor iedereen die interesse heeft in de Rijkshuisstijl is er op de oneven weken, iedere dinsdag om 16:00 uur, een ', (0, s.jsx)(n.a, { href: 'https://nldesignsystem.nl/events/rijkshuisstijl-community-open-hour', children: 'Rijkshuisstijl Community Open Hour' }), '. Dit is een moment om kennis te delen en samen vraagstukken rond de Rijkshuisstijl te bespreken. Ook het delen van work-in-progress, tips of inzichten uit gebruikersonderzoek wordt gewaardeerd!'] }), '\n', (0, s.jsx)(r.hE, { children: (0, s.jsxs)(l.Z, { appearance: 'secondary-action', href: 'https://nldesignsystem.nl/rijkshuisstijl-community/rijkshuisstijl-community-open-hour.ics', download: 'https://nldesignsystem.nl/rijkshuisstijl-community/rijkshuisstijl-community-open-hour.ics', 'aria-label': 'iCal bestand voor de Rijkshuisstijl Community Open Hour (download)', children: [(0, s.jsx)(o.Z, {}), ' Download het kalenderbestand voor de Open Hour'] }) }), '\n', (0, s.jsx)(n.h2, { id: 'sprints', children: 'Sprints' }), '\n', (0, s.jsx)(n.p, { children: "Wil je meedoen aan de sprints? Voeg dan de volgende kalenderbestanden toe aan je agenda, zodat je op de hoogte blijft van de sprint planningen en retro's:" }), '\n', (0, s.jsxs)(r.hE, { children: [(0, s.jsxs)(l.Z, { appearance: 'secondary-action', href: 'https://nldesignsystem.nl/rijkshuisstijl-community/rijkshuisstijl-community-retro.ics', download: 'https://nldesignsystem.nl/rijkshuisstijl-community/rijkshuisstijl-community-retro.ics', 'aria-label': 'iCal bestand voor de Rijkshuisstijl Community retro (download)', children: [(0, s.jsx)(o.Z, {}), ' Download het kalenderbestand voor de retro'] }), (0, s.jsxs)(l.Z, { appearance: 'secondary-action', href: 'https://nldesignsystem.nl/rijkshuisstijl-community/rijkshuisstijl-community-sprint-planning.ics', download: 'https://nldesignsystem.nl/rijkshuisstijl-community/rijkshuisstijl-community-sprint-planning.ics', 'aria-label': 'iCal bestand voor de Rijkshuisstijl Community sprint planning (download)', children: [(0, s.jsx)(o.Z, {}), ' Download het kalenderbestand voor de sprint planning'] })] })] });
+   }
+   function h(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, s.jsx)(n, { ...e, children: (0, s.jsx)(c, { ...e }) }) : c(e);
+   }
+  },
+  68257: (e, n, i) => {
+   i.d(n, { Z: () => l });
+   var s = i(83033),
+    t = i(4814),
+    o = i(52676);
+   const l = (e) => {
+    let { appearance: n, ...i } = e;
+    return (0, o.jsx)(s.Z, { className: (0, t.W)('utrecht-button-link', `utrecht-button-link--${n}`), ...i });
+   };
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => r, a: () => l });
+   var s = i(75271);
+   const t = {},
+    o = s.createContext(t);
+   function l(e) {
+    const n = s.useContext(o);
+    return s.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function r(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : l(e.components)), s.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+  86135: (e, n, i) => {
+   i.d(n, { Z: () => s });
+   var s = (0, i(72315).Z)('outline', 'calendar-event', 'IconCalendarEvent', [
+    ['path', { d: 'M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z', key: 'svg-0' }],
+    ['path', { d: 'M16 3l0 4', key: 'svg-1' }],
+    ['path', { d: 'M8 3l0 4', key: 'svg-2' }],
+    ['path', { d: 'M4 11l16 0', key: 'svg-3' }],
+    ['path', { d: 'M8 15h2v2h-2z', key: 'svg-4' }],
+   ]);
+  },
+ },
+]);
