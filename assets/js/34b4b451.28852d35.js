e59c112e51b3eb50175578545fad2c791adf1fc5
jeffreylauwers <jeffreylauwers@gmail.com>
2025-07-03 13:32:48 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@7908fd711e5692aa73128416d11f9f65f1346790 🚀

---
+++ assets/js/34b4b451.28852d35.js
@@ -0,0 +1,43 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [72053],
+ {
+  57784: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => d, contentTitle: () => a, default: () => h, frontMatter: () => s, metadata: () => r, toc: () => l });
+   var t = i(52676),
+    o = i(40139);
+   const s = { title: 'Figma changelog \xb7 Designer \xb7 Handboek', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'Figma changelog', sidebar_position: 6, pagination_label: 'Figma changelog', description: 'Overzicht van de wijzigingen in de Figma bibliotheken.', slug: '/handboek/designer/figma-changelog', keywords: ['versiebeheer', 'changelog', 'semantic versioning', 'major', 'minor', 'patch', 'figma bibliotheek'] },
+    a = 'Figma changelog',
+    r = { id: 'handboek/designer/werken-met-nl-design-system/figma-changelog', title: 'Figma changelog \xb7 Designer \xb7 Handboek', description: 'Overzicht van de wijzigingen in de Figma bibliotheken.', source: '@site/docs/handboek/designer/werken-met-nl-design-system/figma-changelog.mdx', sourceDirName: 'handboek/designer/werken-met-nl-design-system', slug: '/handboek/designer/figma-changelog', permalink: '/handboek/designer/figma-changelog', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/handboek/designer/werken-met-nl-design-system/figma-changelog.mdx', tags: [], version: 'current', sidebarPosition: 6, frontMatter: { title: 'Figma changelog \xb7 Designer \xb7 Handboek', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'Figma changelog', sidebar_position: 6, pagination_label: 'Figma changelog', description: 'Overzicht van de wijzigingen in de Figma bibliotheken.', slug: '/handboek/designer/figma-changelog', keywords: ['versiebeheer', 'changelog', 'semantic versioning', 'major', 'minor', 'patch', 'figma bibliotheek'] }, sidebar: 'handboek', previous: { title: 'Werken met de Figma changelog', permalink: '/handboek/designer/werken-met-figma-changelog' }, next: { title: 'Design tokens versiebeheer', permalink: '/handboek/designer/design-tokens-versiebeheer' } },
+    d = {},
+    l = [{ value: '2.0.0', id: '200', level: 2 }];
+   function g(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, o.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(n.h1, { id: 'figma-changelog', children: 'Figma changelog' }), '\n', (0, t.jsx)(n.p, { children: 'Op deze pagina vind je een overzicht van alle wijzigingen in onze Figma bibliotheken.' }), '\n', (0, t.jsx)(n.p, { children: 'Ben je afnemer van de bibliotheken? Dan zie je hier welke updates zijn doorgevoerd, zodat je ze waar nodig ook kunt doorvoeren in de bibliotheek van jouw organisatie. Zo blijf je up-to-date met de laatste versie van de bibliotheek.' }), '\n', (0, t.jsxs)(n.p, { children: ['Wil je meer weten over hoe je deze changelog gebruikt? Bekijk dan de\xa0', (0, t.jsx)(n.a, { href: 'https://nldesignsystem.nl/handboek/designer/nieuwe-versie-publiceren/werken-met-changelog-voor-figma', children: 'uitleg over de changelog voor Figma bibliotheken' }), '. En wil je weten wat de versienummers precies betekenen? Lees dan over\xa0', (0, t.jsx)(n.a, { href: 'https://nldesignsystem.nl/handboek/designer/nieuwe-versie-publiceren/versiebeheer-voor-design-tokens', children: 'versiebeheer' }), '.'] }), '\n', (0, t.jsx)(n.h2, { id: '200', children: '2.0.0' }), '\n', (0, t.jsx)(n.p, { children: '3 juli 2025' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: 'Meer dan 20 nieuwe componenten, inclusief de Candidate componenten.' }), '\n', (0, t.jsxs)(n.li, { children: ['Componenten verdeling op orde.', '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: '\u2018NL Design System - Bibliotheek\u2019 componenten met de status \u2018Community\u2019 en \u2018Candidate\u2019.' }), '\n', (0, t.jsx)(n.li, { children: '\u2018NL Design System - ToDo Bibliotheek\u2019 componenten met de status \u2018Help Wanted\u2019 of \u2018Experimental\u2019.' }), '\n'] }), '\n'] }), '\n', (0, t.jsx)(n.li, { children: 'Community componenten hebben een stuk minder todo tokens. Voor de todo tokens die er nog wel zijn, zijn issues aangemaakt bij de betreffende organisatie.' }), '\n', (0, t.jsx)(n.li, { children: 'Figma hygi\xebne doorgevoerd op elk niveau (Pages, Layers, Properties, etcetera.)' }), '\n', (0, t.jsx)(n.li, { children: 'W3C DTCG format doorgevoerd op de tokens in JSON.' }), '\n', (0, t.jsx)(n.li, { children: "W3C DTCG conventie voor design token type 'Dimension' doorgevoerd op tokens van het type Border width, Border radius, Sizing en Spacing." }), '\n', (0, t.jsx)(n.li, { children: 'Nieuwe \u2018Cover\u2019 op basis van vernieuwde NL Design System huisstijl.' }), '\n', (0, t.jsx)(n.li, { children: '\u2018Read me\u2019 pagina\u2019s toegevoegd.' }), '\n', (0, t.jsx)(n.li, { children: '\u2018Estafettemodel badges\u2019 op basis van vernieuwde NL Design System huisstijl.' }), '\n', (0, t.jsx)(n.li, { children: 'Design tokens gedocumenteerd met nieuw \u2018Design Token Documentation Item\u2019.' }), '\n', (0, t.jsx)(n.li, { children: '25 nieuwe toptaak iconen vanuit OpenGemeenten.' }), '\n', (0, t.jsx)(n.li, { children: 'Met deze release komen ook het \u2018Start-thema\u2019 en de \u2018basis-tokens\u2019 mee.' }), '\n'] })] });
+   }
+   function h(e = {}) {
+    const { wrapper: n } = { ...(0, o.a)(), ...e.components };
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(g, { ...e }) }) : g(e);
+   }
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => r, a: () => a });
+   var t = i(75271);
+   const o = {},
+    s = t.createContext(o);
+   function a(e) {
+    const n = t.useContext(s);
+    return t.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function r(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(o) : e.components || o) : a(e.components)), t.createElement(s.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
