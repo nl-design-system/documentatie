36c7a4c4258957a3173b9813e07952896e4f761d
jeffreylauwers <jeffreylauwers@gmail.com>
2025-07-07 10:15:35 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@77b25490f1383a8d7cb3ba14c39c8ee816675956 🚀

---
+++ assets/js/ba4a9952.cccc1ff5.js
@@ -0,0 +1,65 @@
+/*! For license information please see ba4a9952.cccc1ff5.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [2850],
+ {
+  94158: (e, n, o) => {
+   o.r(n), o.d(n, { assets: () => u, contentTitle: () => d, default: () => h, frontMatter: () => s, metadata: () => p, toc: () => v });
+   var r = o(52676),
+    i = o(40139),
+    t = o(68257),
+    l = o(78511),
+    a = o(16167);
+   const s = { title: 'Developer Open Hour \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Developer Open Hour', pagination_label: 'In de Developer Open Hour wisselen developers informatie, inzichten en tips met elkaar uit.', description: 'Tijdens Developer Open Hour kun je laagdrempelig vragen over development in de NL Design System community bespreken. Hier meld je je aan en zie je de planning.', slug: '/events/developer-open-hour' },
+    d = 'Developer Open Hour',
+    p = { id: 'community/events/developer-open-hour/developer-open-hour', title: 'Developer Open Hour \xb7 Bijeenkomsten', description: 'Tijdens Developer Open Hour kun je laagdrempelig vragen over development in de NL Design System community bespreken. Hier meld je je aan en zie je de planning.', source: '@site/docs/community/events/developer-open-hour/developer-open-hour.mdx', sourceDirName: 'community/events/developer-open-hour', slug: '/events/developer-open-hour', permalink: '/events/developer-open-hour', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/developer-open-hour/developer-open-hour.mdx', tags: [], version: 'current', frontMatter: { title: 'Developer Open Hour \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Developer Open Hour', pagination_label: 'In de Developer Open Hour wisselen developers informatie, inzichten en tips met elkaar uit.', description: 'Tijdens Developer Open Hour kun je laagdrempelig vragen over development in de NL Design System community bespreken. Hier meld je je aan en zie je de planning.', slug: '/events/developer-open-hour' }, sidebar: 'community', previous: { title: 'Developer Open Hour', permalink: '/events/developer-open-hour/overzicht' }, next: { title: 'In de Developer Open Hour wisselen developers informatie, inzichten en tips met elkaar uit.', permalink: '/events/developer-open-hour/aanmelden' } },
+    u = {},
+    v = [
+     { value: 'Over de Developer Open Hour', id: 'over-de-developer-open-hour', level: 2 },
+     { value: 'Ben je erbij?', id: 'ben-je-erbij', level: 2 },
+     { value: 'Vragen of idee\xebn', id: 'vragen-of-idee\xebn', level: 2 },
+    ];
+   function c(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, i.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.h1, { id: 'developer-open-hour', children: 'Developer Open Hour' }), '\n', (0, r.jsx)(n.h2, { id: 'over-de-developer-open-hour', children: 'Over de Developer Open Hour' }), '\n', (0, r.jsx)(n.p, { children: 'Voor developers is er elke donderdag een Developer Open Hour. Op even weken is dit op 11:00 uur, op oneven weken om 13:00 uur.' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsx)(n.li, { children: 'Heb je support nodig? Dit is het ideale moment om hulp te krijgen!' }), '\n', (0, r.jsx)(n.li, { children: 'Wil je een handige techniek laten zien die je voor NL Design System gebruikt? Deel je idee voor een demo hier.' }), '\n', (0, r.jsx)(n.li, { children: 'Ben je benieuwd of jouw ontwikkelingen handig zijn voor andere developers? Vraag het hier!' }), '\n', (0, r.jsx)(n.li, { children: 'Elk onderwerp kunnen we een aantal minuten bespreken, daarna kunnen we een vervolg plannen met de benodigde mensen.' }), '\n'] }), '\n', (0, r.jsx)(n.p, { children: 'We raden je aan om in het chatkanaal je idee voor de agenda delen, om alvast interesse te wekken!' }), '\n', (0, r.jsxs)(n.p, { children: ['Anders dan de ', (0, r.jsx)(n.a, { href: '/events/heartbeat', children: 'Heartbeat' }), ', worden Developer Open Hours niet opgenomen.'] }), '\n', (0, r.jsx)(n.h2, { id: 'ben-je-erbij', children: 'Ben je erbij?' }), '\n', (0, r.jsx)(n.p, { children: 'Iedereen kan aansluiten! Wekelijks, of incidenteel, wanneer je vragen hebt of iets wilt bespreken.' }), '\n', (0, r.jsxs)(n.p, { children: ['De Developer Open Hour vindt plaats in het ', (0, r.jsx)(n.a, { href: 'https://codefornl.slack.com/archives/C01DAT4TRPF', children: '#nl-design-system-developers' }), '-kanaal op de Slack van Code for NL. We gebruiken de Huddle-functie van dat kanaal om elkaar te spreken.'] }), '\n', (0, r.jsx)(n.p, { children: 'Door je aan te melden kunnen wij je op de hoogte houden van Developer Open Hours. Je ontvangt dan ook direct een kalenderbestandje, zodat je de Developer Open Hour makkelijk aan je agenda kan toevoegen.' }), '\n', (0, r.jsxs)(a.hE, { children: [(0, r.jsxs)(t.Z, { href: '/events/developer-open-hour/aanmelden', appearance: 'primary-action', children: ['Meld je aan', (0, r.jsx)(l.Z, {})] }), (0, r.jsxs)(t.Z, { href: 'https://praatmee.codefor.nl/', appearance: 'secondary-action', children: ['Doe mee op Slack', (0, r.jsx)(l.Z, {})] })] }), '\n', (0, r.jsx)(n.h2, { id: 'vragen-of-idee\xebn', children: 'Vragen of idee\xebn' }), '\n', (0, r.jsxs)(n.p, { children: ['Heb je vragen of idee\xebn voor de Developer Open Hour? Stuur ons dan een mailtje op ', (0, r.jsx)('a', { href: 'mailto:info@nldesignsystem.nl', children: (0, r.jsx)(n.a, { href: 'mailto:info@nldesignsystem.nl', children: 'info@nldesignsystem.nl' }) })] })] });
+   }
+   function h(e = {}) {
+    const { wrapper: n } = { ...(0, i.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(c, { ...e }) }) : c(e);
+   }
+  },
+  68257: (e, n, o) => {
+   o.d(n, { Z: () => l });
+   var r = o(83033),
+    i = o(4814),
+    t = o(52676);
+   const l = (e) => {
+    let { appearance: n, ...o } = e;
+    return (0, t.jsx)(r.Z, { className: (0, i.W)('utrecht-button-link', `utrecht-button-link--${n}`), ...o });
+   };
+  },
+  40139: (e, n, o) => {
+   o.d(n, { Z: () => a, a: () => l });
+   var r = o(75271);
+   const i = {},
+    t = r.createContext(i);
+   function l(e) {
+    const n = r.useContext(t);
+    return r.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(i) : e.components || i) : l(e.components)), r.createElement(t.Provider, { value: n }, e.children);
+   }
+  },
+  78511: (e, n, o) => {
+   o.d(n, { Z: () => r });
+   var r = (0, o(72315).Z)('outline', 'chevron-right', 'IconChevronRight', [['path', { d: 'M9 6l6 6l-6 6', key: 'svg-0' }]]);
+  },
+ },
+]);
