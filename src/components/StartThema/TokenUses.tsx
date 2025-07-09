f1103a12e29555d4ac0bc4b5e831e82d21131b91
Menno van Slooten <menno.vanslooten@gmail.com>
2025-07-09 15:06:30 +0200

    feat: Add token docs for Start-thema

---
+++ src/components/StartThema/TokenUses.tsx
@@ -0,0 +1,50 @@
+// import tokens from '@nl-design-system-unstable/start-design-tokens/dist/tokens.json';
+// import tokenList from '@nl-design-system-unstable/start-design-tokens/dist/list.json';
+import tokens from './tokens.json';
+import tokenList from './list.json';
+
+export const TokenUses = ({ tokenType }) => {
+  const tokenNames = tokenList
+    .filter((token) => token.path[0] === 'basis' && token.path[1] === tokenType)
+    .map((token) => token.path.join('.'))
+    .sort();
+
+  const tokenUses = {};
+  for (const tokenName of tokenNames) {
+    const uses = Array.from(findUses(tokenName, tokens));
+    if (uses.length) {
+      tokenUses[tokenName] = uses;
+    }
+  }
+
+  return Object.keys(tokenUses).map((tokenName) => (
+    <div key={tokenName}>
+      <h4 className="utrecht-heading-4">{tokenName}</h4>
+      <ul role="list" className="utrecht-unordered-list">
+        {tokenUses[tokenName]?.map((tokenUse, index) => (
+          <li key={tokenName + tokenUse + index} className="utrecht-unordered-list__item">
+            <code className="utrecht-code">{tokenUse}</code>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ));
+};
+
+const findUses = (tokenName, obj, path = [], result = []) => {
+  if (Array.isArray(obj) || typeof obj !== 'object') {
+    return result;
+  }
+
+  for (const key of Object.keys(obj)) {
+    const deeperPath = path.concat(key);
+
+    if (key === '$value' && obj.$value === `{${tokenName}}`) {
+      result.push(path.slice(1).join('.'));
+    } else {
+      findUses(tokenName, obj[key], deeperPath, result);
+    }
+  }
+
+  return result;
+};
