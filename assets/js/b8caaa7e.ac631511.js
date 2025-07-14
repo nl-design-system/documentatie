e530fe11407a20dee5205df74537afe84b6ac244
matijs <github@matijsbrinkhuis.com>
2025-07-14 07:05:22 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@d4b67e9357d596c282729df32c015b0612c5d7b8 🚀

---
+++ assets/js/b8caaa7e.ac631511.js
@@ -0,0 +1,43 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [81115],
+ {
+  52446: (e, t, n) => {
+   n.r(t), n.d(t, { assets: () => h, contentTitle: () => o, default: () => m, frontMatter: () => i, metadata: () => l, toc: () => d });
+   var s = n(52676),
+    a = n(40139);
+   const i = { title: "Thema's \xb7 Huisstijl vastleggen \xb7 Handboek", hide_title: !0, hide_table_of_contents: !0, sidebar_label: "Thema's", sidebar_position: 1, pagination_label: "Thema's", description: "Introductie van thema's binnen NL Design System.", slug: '/handboek/huisstijl/themas', keywords: ['componenten', 'design system', 'design tokens', "thema's"] },
+    o = "Thema's",
+    l = { id: 'handboek/huisstijl-vastleggen/themas/themas', title: "Thema's \xb7 Huisstijl vastleggen \xb7 Handboek", description: "Introductie van thema's binnen NL Design System.", source: '@site/docs/handboek/huisstijl-vastleggen/themas/themas.mdx', sourceDirName: 'handboek/huisstijl-vastleggen/themas', slug: '/handboek/huisstijl/themas', permalink: '/handboek/huisstijl/themas', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/handboek/huisstijl-vastleggen/themas/themas.mdx', tags: [], version: 'current', sidebarPosition: 1, frontMatter: { title: "Thema's \xb7 Huisstijl vastleggen \xb7 Handboek", hide_title: !0, hide_table_of_contents: !0, sidebar_label: "Thema's", sidebar_position: 1, pagination_label: "Thema's", description: "Introductie van thema's binnen NL Design System.", slug: '/handboek/huisstijl/themas', keywords: ['componenten', 'design system', 'design tokens', "thema's"] }, sidebar: 'handboek', previous: { title: 'Design tokens - Introductie', permalink: '/handboek/huisstijl/design-tokens' }, next: { title: 'Start-thema', permalink: '/handboek/huisstijl/themas/start-thema' } },
+    h = {},
+    d = [{ value: 'Zelf aan de slag', id: 'zelf-aan-de-slag', level: 2 }];
+   function r(e) {
+    const t = { a: 'a', h1: 'h1', h2: 'h2', img: 'img', p: 'p', ...(0, a.a)(), ...e.components };
+    return (0, s.jsxs)(s.Fragment, { children: [(0, s.jsx)(t.h1, { id: 'themas', children: "Thema's" }), '\n', (0, s.jsxs)(t.p, { children: ['De componenten van NL Design System hebben van zichzelf geen specifieke huisstijl. Iedere organisatie kan zijn eigen huisstijl op de componenten toepassen. Om dat voor elkaar te krijgen, maken we gebruik van \u2018', (0, s.jsx)(t.a, { href: '/handboek/huisstijl/design-tokens', children: 'design tokens' }), '\u2019.'] }), '\n', (0, s.jsx)(t.p, { children: (0, s.jsx)(t.img, { src: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/meedoen_design-tokens_no-style-and-style.png', alt: 'Drie dezelfde interfaces. E\xe9n zonder stijling. Twee met een eigen huisstijl' }) }), '\n', (0, s.jsxs)(t.p, { children: ["Vanuit NL Design System bieden we het '", (0, s.jsx)(t.a, { href: '/handboek/huisstijl/themas/start-thema/', children: 'Start-thema' }), "' aan. De stijl van dit Start-thema hebben we vastgelegd in een set aan 'basis-tokens'. Deze basis-tokens leven op het ", (0, s.jsx)(t.a, { href: '/handboek/huisstijl/design-tokens#common-tokens', children: "'Common' niveau" }), '.'] }), '\n', (0, s.jsx)(t.p, { children: (0, s.jsx)(t.img, { src: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/img_start-thema_stijl.png', alt: 'Stijl van het Start-thema: een donkerblauw vlak met witte letters \u2018Start-thema. Eenvoudig en neutraal.\u2019 Rechts een illustratie van Nederland in dezelfde kleur.' }) }), '\n', (0, s.jsxs)(t.p, { children: ['Op basis van het ', (0, s.jsx)(t.a, { href: '/handboek/huisstijl/themas/start-thema', children: 'Start-thema' }), " hebben we ook een '", (0, s.jsx)(t.a, { href: '/handboek/huisstijl/themas/voorbeeld-thema', children: 'Voorbeeld-thema' }), "' gemaakt. Hiermee laten we zien hoe je een eigen thema kunt opzetten. Dit dient als voorbeeld en inspiratie."] }), '\n', (0, s.jsx)(t.p, { children: (0, s.jsx)(t.img, { src: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/img_voorbeeld-thema_stijl.png', alt: 'Stijl van het Voorbeeld-thema uitgebeeld door middel van een violet vlak waarop met witte letters staat geschreven: Voorbeeld-thema. Uitgesproken en vriendelijk. Rechts staat in dezelfde violet kleur Nederland afgebeeld als illustratie.' }) }), '\n', (0, s.jsx)(t.h2, { id: 'zelf-aan-de-slag', children: 'Zelf aan de slag' }), '\n', (0, s.jsxs)(t.p, { children: [(0, s.jsx)(t.a, { href: '/handboek/developer/aan-de-slag', children: 'Als developer aan de slag met een eigen thema' }), '.'] }), '\n', (0, s.jsxs)(t.p, { children: ['Als designer aan de slag met een eigen thema? Volg dan het ', (0, s.jsx)(t.a, { href: '/handboek/designer/figma-stappenplan', children: 'Figma Stappenplan' }), '. Daarin leggen we stap voor stap uit hoe je het Start-thema en de basis-tokens kunt aanpassen naar de huisstijl van jouw organisatie.'] })] });
+   }
+   function m(e = {}) {
+    const { wrapper: t } = { ...(0, a.a)(), ...e.components };
+    return t ? (0, s.jsx)(t, { ...e, children: (0, s.jsx)(r, { ...e }) }) : r(e);
+   }
+  },
+  40139: (e, t, n) => {
+   n.d(t, { Z: () => l, a: () => o });
+   var s = n(75271);
+   const a = {},
+    i = s.createContext(a);
+   function o(e) {
+    const t = s.useContext(i);
+    return s.useMemo(
+     function () {
+      return 'function' == typeof e ? e(t) : { ...t, ...e };
+     },
+     [t, e],
+    );
+   }
+   function l(e) {
+    let t;
+    return (t = e.disableParentContext ? ('function' == typeof e.components ? e.components(a) : e.components || a) : o(e.components)), s.createElement(i.Provider, { value: t }, e.children);
+   }
+  },
+ },
+]);
