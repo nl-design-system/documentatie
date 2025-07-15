89430913d013134ff5065d1f02a112396ba4ff35
Menno van Slooten <menno.vanslooten@gmail.com>
2025-07-15 16:14:25 +0200

    fix(breadcrumbs): cleanup commented code

--- src/theme/DocBreadcrumbs/index.tsx
+++ src/theme/DocBreadcrumbs/index.tsx
@@ -4,13 +4,6 @@ import { BreadcrumbNav } from '@site/src/components/BreadcrumbNav';
 import type { ReactElement } from 'react';
 
 export default function DocBreadcrumbs(): ReactElement | null {
-  const sidebarBreadcrumbs =
-    useSidebarBreadcrumbs()?.map((item) => {
-      // if (item.type === 'category') {
-      //   return { ...item, ...item.items.find((i) => i.type === 'link'), label: item.label };
-      // }
-      return { href: item.href, label: item.label };
-    }) || [];
-
+  const sidebarBreadcrumbs = useSidebarBreadcrumbs()?.map((item) => ({ href: item.href, label: item.label })) || [];
   return <BreadcrumbNav breadcrumbs={sidebarBreadcrumbs} className={ThemeClassNames.docs.docBreadcrumbs} />;
 }
