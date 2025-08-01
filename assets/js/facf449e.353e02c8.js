e6af104eb6a99b01587d98566e47ea0352b77d4c
matijs <github@matijsbrinkhuis.com>
2025-08-01 12:17:01 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@e4303975519750496fefe09c40f8b16acc0dc713 🚀

---
+++ assets/js/facf449e.353e02c8.js
@@ -0,0 +1,9 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [64474],
+ {
+  77040: (e) => {
+   e.exports = JSON.parse('{"title":"Voor organisaties","keywords":["organisaties","overzicht"],"slug":"/handboek/organisatie/overzicht","permalink":"/handboek/organisatie/overzicht","navigation":{"previous":{"title":"GitHub Actions","permalink":"/handboek/developer/github-actions"},"next":{"title":"Meedoen","permalink":"/handboek/organisatie/meedoen"}}}');
+  },
+ },
+]);
