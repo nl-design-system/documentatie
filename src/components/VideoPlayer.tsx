fb7940beecd3c8a983224b026b9c2fdb6952a560
Yolijn <mail@yolijn.com>
2025-08-09 19:24:24 +0200

    fix: urls with slash

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,7 +1,8 @@
 import clsx from 'clsx';
 import type { PropsWithChildren } from 'react';
-import ReactPlayer, { type ReactPlayerProps } from 'react-player';
+import ReactPlayer from 'react-player';
 import './VideoPlayer.css';
+import type { ReactPlayerProps } from 'react-player/types';
 
 export const VideoPlayer = ({
   videoId,
@@ -9,12 +10,12 @@ export const VideoPlayer = ({
   ...restProps
 }: PropsWithChildren<ReactPlayerProps & { videoId: string; className?: string }>) => (
   <ReactPlayer
-    url={`https://youtube.com/watch?v=${videoId}`}
+    src={`https://youtube.com/watch?v=${videoId}`}
     className={clsx('video-player', className)}
     width="100%"
     height="100%"
     controls
     {...restProps}
-    config={{ youtube: { playerVars: { disablekb: 1 } } }}
+    config={{ youtube: { disablekb: 1 } }}
   />
 );
