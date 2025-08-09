5c71d8c41968e4fc6038cf74d6955f67ea22c6e5
Yolijn <mail@yolijn.com>
2025-08-09 20:17:38 +0200

    fix: improve styling for smaller and shorter pages

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,13 +1,20 @@
-import { Document } from '@utrecht/component-library-react/dist/css-module';
 import type { PropsWithChildren } from 'react';
 import '@nl-design-system-unstable/nlds-design-tokens/src/font.mjs';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/theme.css';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/custom.css';
 import '@nl-design-system-community/ma-design-tokens/dist/theme.css';
+import { PageLayout } from '@utrecht/page-layout-react/css';
+
+import '@utrecht/root-css/dist/html/index.css';
+import '@utrecht/document-css/dist/html/index.css';
 
 // Default implementation, that you can customize
 function Root({ children }: PropsWithChildren<object>) {
-  return <Document className="ma-theme main-layout">{children}</Document>;
+  return (
+    <>
+      <PageLayout className="ma-theme">{children}</PageLayout>
+    </>
+  );
 }
 
 export default Root;
