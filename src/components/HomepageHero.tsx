385498ca0394c880c86f5d4e1879ed8ad2f471e5
Yolijn <mail@yolijn.com>
2025-08-20 10:21:15 +0200

    docs: improve homepage with heartbeat and better content

--- src/components/HomepageHero.tsx
+++ src/components/HomepageHero.tsx
@@ -1,31 +1,10 @@
-import { Link } from '@site/src/components/Link';
-import { IconChevronRight } from '@tabler/icons-react';
-import { Heading1, Paragraph } from '@utrecht/component-library-react/dist/css-module';
 import clsx from 'clsx';
 import type { PropsWithChildren } from 'react';
 import './HomepageHero.css';
 
-interface HomepageHeroProps {
-  title: string;
-  ctaLink: string;
-  cta: string;
-}
-
-export const HomepageHero = ({ title, ctaLink, cta, children }: PropsWithChildren<HomepageHeroProps>) => (
+export const HomepageHero = ({ children }: PropsWithChildren) => (
   <div className="homepage-hero">
-    <div className={clsx('container', 'homepage-hero__container')}>
-      <Heading1 className="homepage-hero__title">{title}</Heading1>
-      <div className="homepage-hero__description">{children}</div>
-      <Paragraph className="homepage-hero__call-to-action">
-        <Link className={clsx('utrecht-link', 'homepage-hero__call-to-action-link')} to={ctaLink}>
-          {cta}
-          <IconChevronRight
-            className={clsx('utrecht-icon', 'homepage-hero__call-to-action-icon')}
-            style={{ verticalAlign: 'middle' }}
-          />
-        </Link>
-      </Paragraph>
-    </div>
+    <div className={clsx('container', 'homepage-hero__container')}>{children}</div>
   </div>
 );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import {
+  ButtonGroup,
+  ButtonLink,
+  Heading1,
   Heading2,
   Heading3,
   Image,
   Paragraph,
+  Strong,
   UnorderedList,
   UnorderedListItem,
 } from '@utrecht/component-library-react/dist/css-module';
@@ -12,6 +16,7 @@ import { Card, CardContent, CardGroup, CardIllustration } from '../components/Ca
 import HomepageHero from '../components/HomepageHero';
 import HomepageUpdates from '../components/HomepageUpdates';
 import { Link } from '../components/Link';
+import { HeartbeatEpisode } from '../components/HeartbeatEpisode';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -23,54 +28,39 @@ export default function Home() {
       wrapperClassName="container--narrow"
     >
       <main>
-        <HomepageHero
-          title="Doe mee met NL Design System"
-          ctaLink="handboek/introductie"
-          cta="Lees de NL Design System introductie"
-        >
+        <HomepageHero>
+          <Heading1>NL Design System</Heading1>
           <Paragraph lead>
-            Met een actieve community van specialisten verzamelt het kernteam de beste{' '}
-            <Link to="/richtlijnen">richtlijnen</Link>, <Link to="/componenten">componenten</Link> en voorbeelden om
-            robuuste websites en webapplicaties voor de digitale overheid te bouwen.
+            Samen met diverse overheidsorganisaties ontwikkelen we herbruikbare bouwblokken voor toegankelijke en
+            gebruiksvriendelijke webapplicaties en websites.
           </Paragraph>
-
-          <Paragraph>
-            NL Design System helpt om toegankelijk, inclusief en gebruiksvriendelijk ontwikkelen makkelijk te maken.
-          </Paragraph>
-
           <UnorderedList>
-            <UnorderedListItem>Gebouwd en gebruikt door de community (voor en door de overheid).</UnorderedListItem>
-            <UnorderedListItem>Platformonafhankelijk en voor iedere huisstijl.</UnorderedListItem>
-            <UnorderedListItem>Uitbreidbaar en publiek beschikbaar.</UnorderedListItem>
+            <UnorderedListItem>
+              <Strong>Actieve community</Strong> deelt regelmatig kennis, ervaring, componenten en verbeteringen
+            </UnorderedListItem>
+            <UnorderedListItem>
+              <Strong>Componentenbibliotheek</Strong> beschikbaar voor efficiënt hergebruik
+            </UnorderedListItem>
+            <UnorderedListItem>
+              <Strong>Herbruikbaar</Strong> voor iedere huisstijl. Open Source en platform-onafhankelijk
+            </UnorderedListItem>
+            <UnorderedListItem>
+              <Strong>Goed onderbouwd</Strong> met praktijkervaring, richtlijnen en gebruikersonderzoek
+            </UnorderedListItem>
           </UnorderedList>
+          <Link href="/handboek/introductie">Lees meer over NL Design System</Link>
         </HomepageHero>
         <div className="container" style={{ paddingBlockEnd: '40px' }}>
-          <Heading2>Meedoen als...</Heading2>
+          <Heading2>Doe mee</Heading2>
           <CardGroup appearance="medium">
             <Card component="section" appearance="medium">
               <CardContent>
                 <div>
-                  <Heading3>Manager</Heading3>
-                </div>
-                <Paragraph>
-                  <Link to="/handboek/manager/introductie">Meer informatie voor managers</Link>
-                </Paragraph>
-              </CardContent>
-            </Card>
-            <Card component="section" appearance="medium">
-              <CardContent>
-                <div>
-                  <Heading3>Leverancier</Heading3>
-                </div>
-                <Paragraph>
-                  <Link to="/handboek/leverancier/introductie">Meer informatie voor leveranciers</Link>
-                </Paragraph>
-              </CardContent>
-            </Card>
-            <Card component="section" appearance="medium">
-              <CardContent>
-                <div>
-                  <Heading3>Developer</Heading3>
+                  <Heading3>Developers</Heading3>
+                  <Paragraph>
+                    Meedoen als developer? In het Handboek vind je alle informatie die je nodig hebt om aan de slag te
+                    gaan met de componenten, Storybook en design tokens.
+                  </Paragraph>
                 </div>
                 <Paragraph>
                   <Link to="/handboek/developer/overzicht">Meer informatie voor developers</Link>
@@ -80,14 +70,56 @@ export default function Home() {
             <Card component="section" appearance="medium">
               <CardContent>
                 <div>
-                  <Heading3>Designer</Heading3>
+                  <Heading3>Designers</Heading3>
+                  <Paragraph>
+                    Meedoen als designer? In het Handboek vind je alle informatie die je nodig hebt om aan de slag te
+                    gaan met de componenten, Figma en design tokens.
+                  </Paragraph>
                 </div>
                 <Paragraph>
                   <Link to="/handboek/designer/overzicht">Meer informatie voor designers</Link>
                 </Paragraph>
               </CardContent>
             </Card>
+            <Card component="section" appearance="medium">
+              <CardContent>
+                <div>
+                  <Heading3>Managers</Heading3>
+                  <Paragraph>
+                    Meedoen als manager? In de introductie voor managers in het Handboek kun je lezen wat meedoen
+                    betekent
+                  </Paragraph>
+                </div>
+                <Paragraph>
+                  <Link to="/handboek/manager/introductie">Meer informatie voor managers</Link>
+                </Paragraph>
+              </CardContent>
+            </Card>
+            <Card component="section" appearance="medium">
+              <CardContent>
+                <div>
+                  <Heading3>Contentmakers</Heading3>
+                  <Paragraph>
+                    Meedoen als contentmaker? Gebruik vandaag nog onze content richtlijnen bij het maken van
+                    toegankelijke webcontent.
+                  </Paragraph>
+                </div>
+                <Paragraph>
+                  <Link to="/richtlijnen/content">Richtlijnen voor contentmakers.</Link>
+                </Paragraph>
+              </CardContent>
+            </Card>
           </CardGroup>
+          <Heading2>Bekijk de laatste updates</Heading2>
+          <HeartbeatEpisode headingLevel={3} />
+          <ButtonGroup>
+            <ButtonLink href="/events/heartbeat/aanmelden" appearance="primary-action-button">
+              Meld je aan voor de Heartbeat
+            </ButtonLink>
+            <ButtonLink href="/events/heartbeat/videos" appearance="secondary-action-button">
+              Bekijk alle Heartbeat videos
+            </ButtonLink>
+          </ButtonGroup>
           <Heading2>Wie doen er al mee?</Heading2>
           <CardGroup appearance="small">
             <Card appearance="small" href="https://github.com/nl-design-system/utrecht">
@@ -161,7 +193,8 @@ export default function Home() {
             System.
           </Paragraph>
         </div>
-        <div className="container" style={{ paddingBlockStart: '40px', paddingBlockEnd: '40px' }}>
+        <div className="container" style={{ paddingBlockEnd: '40px' }}>
+          <Heading2>Gebruik NL Design System</Heading2>
           <CardGroup>
             <Card component="section" appearance="medium">
               <CardContent>
@@ -182,8 +215,8 @@ export default function Home() {
                 <div>
                   <Heading2>Componenten</Heading2>
                   <Paragraph>
-                    Een overzicht van herbruikbare, toegankelijke componenten voor formulieren, navigatie, tabellen en
-                    meer. Inclusief code en documentatie.
+                    Herbruikbare, toegankelijke componenten voor formulieren, navigatie, tabellen en meer. Inclusief
+                    code en documentatie.
                   </Paragraph>
                 </div>
                 <Paragraph>
@@ -195,14 +228,14 @@ export default function Home() {
             <Card component="section" appearance="medium">
               <CardContent>
                 <div>
-                  <Heading2>Voorbeelden</Heading2>
+                  <Heading2>Handboek</Heading2>
                   <Paragraph>
-                    Een overzicht van patronen en templates om vaak voorkomende scenario's op een consistente,
-                    weloverwogen manier op te lossen.
+                    Algemene informatie over het Estafettemodel en bijdragen van componenten en handleidingen voor
+                    developers en designers.
                   </Paragraph>
                 </div>
                 <Paragraph>
-                  <Link to="/voorbeelden">Bekijk de voorbeelden</Link>
+                  <Link to="/handboek">Bekijk het handboek</Link>
                 </Paragraph>
               </CardContent>
             </Card>
@@ -210,14 +243,14 @@ export default function Home() {
             <Card component="section" appearance="medium">
               <CardContent>
                 <div>
-                  <Heading2>Bijeenkomsten</Heading2>
+                  <Heading2>Voorbeelden</Heading2>
                   <Paragraph>
-                    Benieuwd naar de bijeenkomsten van NL Design System? Of niet gelukt om bij de Heartbeat te zijn?
-                    Bekijk de aankondigingen en opnames hier.
+                    Patronen en templates om vaak voorkomende scenario's op een consistente, weloverwogen manier op te
+                    lossen.
                   </Paragraph>
                 </div>
                 <Paragraph>
-                  <Link to="/community/events/overzicht">Bekijk de events</Link>
+                  <Link to="/voorbeelden">Bekijk de voorbeelden</Link>
                 </Paragraph>
               </CardContent>
             </Card>
@@ -226,34 +259,6 @@ export default function Home() {
         <HomepageUpdates>
           <Heading2>Blijf op de hoogte</Heading2>
           <CardGroup>
-            <Card component="section" appearance="medium">
-              <CardContent>
-                <div>
-                  <Heading3>Heartbeat</Heading3>
-                  <Paragraph>
-                    Sluit aan bij de 2-wekelijkse online bijeenkomst waarbij ontwikkelingen rondom het NL Design System
-                    worden besproken.
-                  </Paragraph>
-                </div>
-                <Paragraph>
-                  <Link href="/events/heartbeat/aanmelden">Meld je aan voor de Heartbeat</Link>
-                </Paragraph>
-              </CardContent>
-            </Card>
-            <Card component="section" appearance="medium">
-              <CardContent>
-                <div>
-                  <Heading3>Design Open Hour</Heading3>
-                  <Paragraph>
-                    Wees welkom bij de 2-wekelijkse online bijeenkomst waar designers informatie, inzichten en tips uit
-                    te wisselen.
-                  </Paragraph>
-                </div>
-                <Paragraph>
-                  <Link href="/events/design-open-hour/aanmelden">Meld je aan voor de Design Open Hour</Link>
-                </Paragraph>
-              </CardContent>
-            </Card>
             <Card component="section" appearance="medium">
               <CardContent>
                 <div>
@@ -282,7 +287,39 @@ export default function Home() {
                 </Paragraph>
               </CardContent>
             </Card>
+            <Card component="section" appearance="medium">
+              <CardContent>
+                <div>
+                  <Heading3>Design Open Hour</Heading3>
+                  <Paragraph>
+                    Wees welkom bij de 2-wekelijkse online bijeenkomst waar designers informatie, inzichten en tips uit
+                    te wisselen.
+                  </Paragraph>
+                </div>
+                <Paragraph>
+                  <Link href="/events/design-open-hour/aanmelden">Meld je aan voor Design Open Hour</Link>
+                </Paragraph>
+              </CardContent>
+            </Card>
+            <Card component="section" appearance="medium">
+              <CardContent>
+                <div>
+                  <Heading3>Developer Open Hour</Heading3>
+                  <Paragraph>
+                    Wees welkom bij de 2-wekelijkse online bijeenkomst waar developers vragen stellen en tips uit te
+                    wisselen.
+                  </Paragraph>
+                </div>
+                <Paragraph>
+                  <Link href="/events/developer-open-hour/aanmelden">Meld je aan voor Developer Open Hour</Link>
+                </Paragraph>
+              </CardContent>
+            </Card>
           </CardGroup>
+          <Paragraph>
+            Benieuwd wat we nog meer doen?{' '}
+            <Link to="/community/events/overzicht">Bekijk het overzicht van alle bijeenkomsten</Link>
+          </Paragraph>
         </HomepageUpdates>
       </main>
     </Layout>
