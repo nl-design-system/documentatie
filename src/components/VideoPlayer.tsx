a3f8e437850fce4fa933b5a21ea59303628b3662
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-08-22 11:27:50 +0200

    refactor: no need for children or restprops in videoplayer and videoplaylistplayer

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,23 +1,14 @@
 import clsx from 'clsx';
-import type { PropsWithChildren } from 'react';
 import ReactPlayer from 'react-player';
-import type { ComponentProps } from 'react';
 import './VideoPlayer.css';
 
-type ReactPlayerProps = ComponentProps<typeof ReactPlayer>;
-
-export const VideoPlayer = ({
-  videoId,
-  className,
-  ...restProps
-}: PropsWithChildren<ReactPlayerProps & { videoId: string; className?: string }>) => (
+export const VideoPlayer = ({ videoId, className }: { videoId: string; className?: string }) => (
   <ReactPlayer
     src={`https://youtube.com/watch?v=${videoId}`}
     className={clsx('video-player', className)}
     width="100%"
     height="100%"
     controls
-    {...restProps}
     config={{ youtube: { disablekb: 1 } }}
   />
 );
--- src/components/VideoPlaylistPlayer.tsx
+++ src/components/VideoPlaylistPlayer.tsx
@@ -1,23 +1,14 @@
 import clsx from 'clsx';
-import type { PropsWithChildren } from 'react';
 import ReactPlayer from 'react-player';
-import type { ComponentProps } from 'react';
 import './VideoPlayer.css';
 
-type ReactPlayerProps = ComponentProps<typeof ReactPlayer>;
-
-export const VideoPlaylistPlayer = ({
-  playlistId,
-  className,
-  ...restProps
-}: PropsWithChildren<ReactPlayerProps & { playlistId: string; className?: string }>) => (
+export const VideoPlaylistPlayer = ({ playlistId, className }: { playlistId: string; className?: string }) => (
   <ReactPlayer
     src={`https://www.youtube.com/playlist?list=${playlistId}`}
     className={clsx('video-player', className)}
     width="100%"
     height="100%"
     controls
-    {...restProps}
     config={{ youtube: { disablekb: 1 } }}
   />
 );
