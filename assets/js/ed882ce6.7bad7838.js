fc4064a3c94d7c003655cf7032836dc6190dd300
jeffreylauwers <jeffreylauwers@gmail.com>
2025-10-03 14:25:49 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@7ce166ba5c788148698da6b660093fb3189e74ff 🚀

--- assets/js/ed882ce6.7bad7838.js
+++ assets/js/ed882ce6.7bad7838.js
@@ -16,7 +16,7 @@
    }
   },
   48405: (e, n, t) => {
-   t.r(n), t.d(n, { assets: () => h, contentTitle: () => d, default: () => j, frontMatter: () => c, metadata: () => m, toc: () => b });
+   t.r(n), t.d(n, { assets: () => h, contentTitle: () => d, default: () => p, frontMatter: () => c, metadata: () => m, toc: () => b });
    var i = t(52676),
     r = t(40139),
     o = t(5547);
@@ -28,8 +28,8 @@
     return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(s, { ...e }) }) : s();
    }
    function a(e) {
-    const n = { a: 'a', h1: 'h1', img: 'img', li: 'li', p: 'p', ul: 'ul', ...(0, r.a)(), ...e.components };
-    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: 'plaatsing-van-een-button-binnen-een-formulier', children: 'Plaatsing van een button binnen een formulier' }), '\n', (0, i.jsx)(n.p, { children: "Plaats buttons aan het begin van de regel. Zo wordt een natuurlijke 'scan-lijn' gecre\xeberd. Hierdoor kunnen mensen hun aandacht gemakkelijk op deze lijn richten." }), '\n', (0, i.jsxs)(n.p, { children: ['Ze vormen zo \xe9\xe9n lijn. Ook is het zo voor gebruikers die inzoomen (bijvoorbeeld gebruikers met een visuele beperking) makkelijker om de button te vinden.\nDaarbij zullen gebruikers die inzoomen, een scherm vergrootglas gebruiken of een visuele beperking zoals ', (0, i.jsx)(n.a, { href: 'https://nl.wikipedia.org/wiki/Kokervisus', children: 'Kokervisus' }), ' hebben buttons goed kunnen vinden.'] }), '\n', (0, i.jsx)(n.p, { children: 'Deze richtlijn is een aanbevolen werkwijze, gebaseerd op gebruikersonderzoek:' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://www.lukew.com/ff/entry.asp?571', children: (0, i.jsx)('span', { lang: 'en', children: 'Primary & Secondary Actions in Web Forms' }) }), ', Luke Wroblewski'] }), '\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://adamsilver.io/blog/where-to-put-buttons-on-forms/', children: (0, i.jsx)('span', { lang: 'en', children: 'Where to put buttons on forms' }) }), ', Adam Silver'] }), '\n'] }), '\n', (0, i.jsxs)(n.p, { children: [(0, i.jsx)(n.img, { src: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/richtlijnen_formulier_buttons_placement--do.png', alt: 'Button uitgelijnd met formulier labels en invoervelden' }), '\n', (0, i.jsx)(n.img, { src: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/richtlijnen_formulier_buttons_placement--dont.png', alt: 'Button niet uitgelijnd met formulier labels en invoervelden' })] })] });
+    const n = { a: 'a', h1: 'h1', img: 'img', li: 'li', p: 'p', strong: 'strong', ul: 'ul', ...(0, r.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: 'plaatsing-van-een-button-binnen-een-formulier', children: 'Plaatsing van een button binnen een formulier' }), '\n', (0, i.jsx)(n.p, { children: "Plaats buttons aan het begin van de regel. Zo wordt een natuurlijke 'scan-lijn' gecre\xeberd. Hierdoor kunnen mensen hun aandacht gemakkelijk op deze lijn richten." }), '\n', (0, i.jsxs)(n.p, { children: ['Ze vormen zo \xe9\xe9n lijn. Ook is het zo voor gebruikers die inzoomen (bijvoorbeeld gebruikers met een visuele beperking) makkelijker om de button te vinden.\nDaarbij zullen gebruikers die inzoomen, een scherm vergrootglas gebruiken of een visuele beperking zoals ', (0, i.jsx)(n.a, { href: 'https://nl.wikipedia.org/wiki/Kokervisus', children: 'Kokervisus' }), ' hebben buttons goed kunnen vinden.'] }), '\n', (0, i.jsx)(n.p, { children: 'Deze richtlijn is een aanbevolen werkwijze, gebaseerd op gebruikersonderzoek:' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://www.lukew.com/ff/entry.asp?571', children: (0, i.jsx)('span', { lang: 'en', children: 'Primary & Secondary Actions in Web Forms' }) }), ', Luke Wroblewski'] }), '\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://adamsilver.io/blog/where-to-put-buttons-on-forms/', children: (0, i.jsx)('span', { lang: 'en', children: 'Where to put buttons on forms' }) }), ', Adam Silver'] }), '\n'] }), '\n', (0, i.jsxs)(n.p, { children: [(0, i.jsx)(n.strong, { children: 'Doen:' }), ' Button uitlijnen met formulier labels en invoervelden.\n', (0, i.jsx)(n.img, { src: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/richtlijnen_formulier_buttons_placement--do.png', alt: 'Button uitgelijnd met formulier labels en invoervelden' })] }), '\n', (0, i.jsxs)(n.p, { children: [(0, i.jsx)(n.strong, { children: 'Niet doen:' }), ' Button rechts uitlijnen.\n', (0, i.jsx)(n.img, { src: 'https://raw.githubusercontent.com/nl-design-system/documentatie/assets/richtlijnen_formulier_buttons_placement--dont.png', alt: 'Button niet uitgelijnd met formulier labels en invoervelden' })] })] });
    }
    function u(e = {}) {
     const { wrapper: n } = { ...(0, r.a)(), ...e.components };
@@ -40,12 +40,12 @@
     m = { id: 'richtlijnen/formulieren/button/placement/README', title: 'Plaatsing van een button binnen een formulier \xb7 Buttons in een formulier \xb7 Richtlijnen', description: 'Richtlijnen voor plaatsing van een button binnen een formulier.', source: '@site/docs/richtlijnen/formulieren/button/3-placement/README.mdx', sourceDirName: 'richtlijnen/formulieren/button/3-placement', slug: '/richtlijnen/formulieren/buttons/plaatsing', permalink: '/richtlijnen/formulieren/buttons/plaatsing', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/richtlijnen/formulieren/button/3-placement/README.mdx', tags: [], version: 'current', frontMatter: { title: 'Plaatsing van een button binnen een formulier \xb7 Buttons in een formulier \xb7 Richtlijnen', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Plaatsing button', pagination_label: 'Plaatsing button', description: 'Richtlijnen voor plaatsing van een button binnen een formulier.', slug: '/richtlijnen/formulieren/buttons/plaatsing', keywords: ['labels', 'formulier', 'design', 'code'] }, sidebar: 'richtlijnen', previous: { title: 'Verstuur niet automatisch', permalink: '/richtlijnen/formulieren/buttons/niet-automatisch-versturen' }, next: { title: 'Duidelijke buttontekst', permalink: '/richtlijnen/formulieren/buttons/duidelijk-buttontekst' } },
     h = {},
     b = [];
-   function p(e) {
+   function j(e) {
     return (0, i.jsxs)(i.Fragment, { children: ['\n', '\n', '\n', (0, i.jsx)(u, {}), '\n', (0, i.jsx)(l, {}), '\n', (0, i.jsx)(o.ZP, {})] });
    }
-   function j(e = {}) {
+   function p(e = {}) {
     const { wrapper: n } = { ...(0, r.a)(), ...e.components };
-    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(p, { ...e }) }) : p();
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(j, { ...e }) }) : j();
    }
   },
   40139: (e, n, t) => {
