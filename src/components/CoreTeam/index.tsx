a43d6a77ea24b8d63ff22c67451f9f8dc13257b0
Yolijn <mail@yolijn.com>
2025-10-16 17:42:40 +0200

    fix: remove core team cards

--- src/components/CoreTeam/index.tsx
+++ src/components/CoreTeam/index.tsx
@@ -10,36 +10,36 @@ import Renate from './Renate';
 import Peter from './Peter';
 import Roy from './Roy';
 import Sandra from './Sandra';
-import { Card, CardContent, CardGroup } from '../CardGroup';
 import './CoreTeam.css';
 
 const coreteam = [Astrid, Yolijn, Robbert, Jeffrey, Rozerin, Erik, Renate, Peter, Roy, Sandra];
 
 export const CoreTeam = ({ headingLevel }: { headingLevel: number }) => {
   return (
-    <CardGroup appearance="large">
+    <>
       {coreteam.map(({ name, role, Description, slack }) => {
         return (
-          <Card appearance="large" className={clsx('core-team__card')} component="section" key={name}>
-            <CardContent className={clsx('core-team__content')}>
-              <HeadingGroup className={clsx('core-team__heading')}>
-                <Heading level={headingLevel} className={clsx('core-team__heading-title')}>
-                  {name}
-                </Heading>
-                <Paragraph className={clsx('core-team__heading-subtitle')}>{role}</Paragraph>
-              </HeadingGroup>
-              <div className={clsx('core-team__description')}>
-                <Description />
-              </div>
-
-              <Paragraph>
-                Slack: <Link href={`https://codefornl.slack.com/team/${slack.id}`}>{slack.mention}</Link>
-              </Paragraph>
-            </CardContent>
-          </Card>
+          <>
+            <HeadingGroup className={clsx('core-team__heading')}>
+              <Heading
+                level={headingLevel}
+                className={clsx('core-team__heading-title')}
+                id={name.toLowerCase().replace(/\s/g, '-')}
+              >
+                {name}
+              </Heading>
+              <Paragraph className={clsx('core-team__heading-subtitle')}>{role}</Paragraph>
+            </HeadingGroup>
+            <div className={clsx('core-team__description')}>
+              <Description />
+            </div>
+            <Paragraph>
+              Slack: <Link href={`https://codefornl.slack.com/team/${slack.id}`}>{slack.mention}</Link>
+            </Paragraph>
+          </>
         );
       })}
-    </CardGroup>
+    </>
   );
 };
 
