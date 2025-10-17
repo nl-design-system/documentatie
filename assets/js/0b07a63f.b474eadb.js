19177ed93d0cd4644fb23c760be389222831f263
eslook <42849307+eslook@users.noreply.github.com>
2025-10-17 09:10:27 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@d27a166c09f5ae7906e866e0482d8793a9ee01bb 🚀

---
+++ assets/js/0b07a63f.b474eadb.js
@@ -0,0 +1,76 @@
+/*! For license information please see 0b07a63f.b474eadb.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [41627],
+ {
+  76294: (e, n, t) => {
+   t.r(n), t.d(n, { assets: () => d, contentTitle: () => l, default: () => p, frontMatter: () => m, metadata: () => c, toc: () => u });
+   var i = t(52676),
+    s = t(40139),
+    o = t(86135),
+    r = t(68257),
+    a = t(37943);
+   const m = { title: 'Bedankt voor je aanmelding \xb7 MijnServices Community \xb7 Community Sprints', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Bedankt voor je aanmelding', pagination_label: 'Bedankt voor je aanmelding', slug: '/community/community-sprints/mijn-services-community/aanmelden/bedankt', unlisted: !0 },
+    l = 'Bedankt!',
+    c = { id: 'community/community-sprints/mijn-services-community/aanmelden-success', title: 'Bedankt voor je aanmelding \xb7 MijnServices Community \xb7 Community Sprints', description: 'Welkom bij de MijnServices Community, fijn dat je erbij bent!', source: '@site/docs/community/community-sprints/mijn-services-community/aanmelden-success.mdx', sourceDirName: 'community/community-sprints/mijn-services-community', slug: '/community/community-sprints/mijn-services-community/aanmelden/bedankt', permalink: '/community/community-sprints/mijn-services-community/aanmelden/bedankt', draft: !1, unlisted: !0, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/community-sprints/mijn-services-community/aanmelden-success.mdx', tags: [], version: 'current', frontMatter: { title: 'Bedankt voor je aanmelding \xb7 MijnServices Community \xb7 Community Sprints', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Bedankt voor je aanmelding', pagination_label: 'Bedankt voor je aanmelding', slug: '/community/community-sprints/mijn-services-community/aanmelden/bedankt', unlisted: !0 } },
+    d = {},
+    u = [];
+   function h(e) {
+    const n = { a: 'a', h1: 'h1', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
+    return (0, i.jsxs)(i.Fragment, { children: [(0, i.jsx)(n.h1, { id: 'bedankt', children: 'Bedankt!' }), '\n', (0, i.jsx)(n.p, { children: 'Welkom bij de MijnServices Community, fijn dat je erbij bent!' }), '\n', (0, i.jsx)(n.p, { children: 'Handige links:' }), '\n', (0, i.jsxs)(n.ul, { children: ['\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://praatmee.codefor.nl', children: 'Word lid van het #nl-design-system-developers Slack kanaal' }), ' op CodeForNL, daar kun je meepraten, vragen stellen of beantwoorden of gesprekken volgen.'] }), '\n', (0, i.jsxs)(n.li, { children: [(0, i.jsx)(n.a, { href: 'https://github.com/nl-design-system/mijn-services', children: 'Bekijk de code op GitHub' }), '.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Bekijk onze ', (0, i.jsx)(n.a, { href: 'https://www.youtube.com/playlist?list=PLnAxFMscDU3F1VYQxlJLpmV-Z-FQ6hCU_', children: "video's in de MijnServices Community Sprint playlist op YouTube" }), ' en abonneer je op ons kanaal.'] }), '\n', (0, i.jsxs)(n.li, { children: ['Vind alle relevante linkjes en informatie op de ', (0, i.jsx)(n.a, { href: '/community/community-sprints/mijn-services-community/', children: 'MijnServices Community pagina' }), '.'] }), '\n'] }), '\n', (0, i.jsx)(n.p, { children: 'Elke week op donderdag van 10:00 tot 11:00 kan je meedoen aan de MijnServices Community check-in huddle en de thread volgen in het #nl-design-system-designers kanaal. Deze is voor zowel developers als designers.' }), '\n', (0, i.jsx)(a.hE, { children: (0, i.jsxs)(r.Z, { appearance: 'secondary-action', href: 'https://nldesignsystem.nl/mijn-services-community/mijn-services-community-check-in.ics', download: 'https://nldesignsystem.nl/mijn-services-community/mijn-services-community-check-in.ics', 'aria-label': 'iCal bestand voor de MijnServices Community check-in (download)', children: [(0, i.jsx)(o.Z, {}), ' Download het kalenderbestand'] }) })] });
+   }
+   function p(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, i.jsx)(n, { ...e, children: (0, i.jsx)(h, { ...e }) }) : h(e);
+   }
+  },
+  68257: (e, n, t) => {
+   t.d(n, { Z: () => r });
+   var i = t(44679),
+    s = t(4814),
+    o = t(52676);
+   const r = (e) => {
+    let { appearance: n, ...t } = e;
+    return (0, o.jsx)(i.Z, { className: (0, s.W)('utrecht-button-link', `utrecht-button-link--${n}`), ...t });
+   };
+  },
+  40139: (e, n, t) => {
+   t.d(n, { Z: () => a, a: () => r });
+   var i = t(75271);
+   const s = {},
+    o = i.createContext(s);
+   function r(e) {
+    const n = i.useContext(o);
+    return i.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(s) : e.components || s) : r(e.components)), i.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+  72315: (e, n, t) => {
+   t.d(n, { Z: () => o });
+   var i = t(75271),
+    s = { outline: { xmlns: 'http://www.w3.org/2000/svg', width: 24, height: 24, viewBox: '0 0 24 24', fill: 'none', stroke: 'currentColor', strokeWidth: 2, strokeLinecap: 'round', strokeLinejoin: 'round' }, filled: { xmlns: 'http://www.w3.org/2000/svg', width: 24, height: 24, viewBox: '0 0 24 24', fill: 'currentColor', stroke: 'none' } };
+   const o = (e, n, t, o) => {
+    const r = (0, i.forwardRef)(({ color: t = 'currentColor', size: r = 24, stroke: a = 2, title: m, className: l, children: c, ...d }, u) => (0, i.createElement)('svg', { ref: u, ...s[e], width: r, height: r, className: ['tabler-icon', `tabler-icon-${n}`, l].join(' '), ...('filled' === e ? { fill: t } : { strokeWidth: a, stroke: t }), ...d }, [m && (0, i.createElement)('title', { key: 'svg-title' }, m), ...o.map(([e, n]) => (0, i.createElement)(e, n)), ...(Array.isArray(c) ? c : [c])]));
+    return (r.displayName = `${t}`), r;
+   };
+  },
+  86135: (e, n, t) => {
+   t.d(n, { Z: () => i });
+   var i = (0, t(72315).Z)('outline', 'calendar-event', 'IconCalendarEvent', [
+    ['path', { d: 'M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z', key: 'svg-0' }],
+    ['path', { d: 'M16 3l0 4', key: 'svg-1' }],
+    ['path', { d: 'M8 3l0 4', key: 'svg-2' }],
+    ['path', { d: 'M4 11l16 0', key: 'svg-3' }],
+    ['path', { d: 'M8 15h2v2h-2z', key: 'svg-4' }],
+   ]);
+  },
+ },
+]);
