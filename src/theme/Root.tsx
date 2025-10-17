e62c9315b3d42654d906f7059e08dde980bb0653
Yolijn <mail@yolijn.com>
2025-10-17 11:47:14 +0200

    fix: search by adding ma-theme to root

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -2,8 +2,9 @@ import type { PropsWithChildren } from 'react';
 import '@nl-design-system-unstable/nlds-design-tokens/src/font.mjs';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/theme.css';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/custom.css';
-import '@nl-design-system-community/ma-design-tokens/dist/theme.css';
+import '@nl-design-system-community/ma-design-tokens/dist/variables.css';
 import { PageLayout } from '@utrecht/page-layout-react/css';
+import Head from '@docusaurus/Head';
 
 import '@utrecht/root-css/dist/html/index.css';
 import '@utrecht/document-css/dist/html/index.css';
@@ -12,6 +13,9 @@ import '@utrecht/document-css/dist/html/index.css';
 function Root({ children }: PropsWithChildren<object>) {
   return (
     <>
+      <Head>
+        <html className="ma-theme" />
+      </Head>
       <PageLayout className="ma-theme">{children}</PageLayout>
     </>
   );
