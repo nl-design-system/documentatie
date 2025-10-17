5a95c86028ee65823d57f10c01ad265c64cfebdb
erikkroes <erikkroes@users.noreply.github.com>
2025-10-17 07:11:29 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@4dd122f2056b711e973ef2ad8ac5f527e83c4f8b 🚀

---
+++ assets/js/84b5c197.fc1b63c8.js
@@ -0,0 +1,155 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [60864],
+ {
+  46174: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => j, contentTitle: () => u, default: () => x, frontMatter: () => p, metadata: () => m, toc: () => v });
+   var r = i(52676),
+    t = i(40139),
+    o = i(57716),
+    s = i(46364),
+    d = i(37943),
+    l = i(21339),
+    a = i(92828),
+    c = i(38391);
+   function h(e) {
+    const n = { a: 'a', em: 'em', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsxs)(n.p, { children: ['Opgenomen video heeft ', (0, r.jsx)(n.a, { href: '/woordenlijst/#ondertiteling', children: 'ondertiteling' }), '.'] }), '\n', (0, r.jsxs)(n.p, { children: ['Alle opgenomen video die geluid heeft moet ondertiteling hebben. Het gaat hier om ondertiteling voor doven en slechthorenden. Deze wordt in het Engels vaak ', (0, r.jsx)(n.em, { children: 'captions' }), ' genoemd. Deze ondertiteling heeft gesproken tekst, maar ook andere informatie die nodig is om de video te begrijpen. Bijvoorbeeld omschrijvingen van muziek of geluidseffecten.'] }), '\n', (0, r.jsx)(n.p, { children: 'Sommige mensen kunnen auditieve inhoud niet horen of begrijpen. Bijvoorbeeld doven en slechthorenden, maar ook anderstaligen en mensen die moeite hebben om hun aandacht te houden bij deze inhoud.' })] });
+   }
+   function g(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(h, { ...e }) }) : h(e);
+   }
+   const p = { title: 'WCAG-succescriterium 1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)', pagination_label: 'WCAG-succescriterium 1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)', description: 'Opgenomen video heeft ondertiteling.', slug: '1.2.2', keywords: ['WCAG', 'A', 'waarneembaar', 'op tijd gebaseerde media', 'perceivable', 'time-based media', 'captions (prerecorded)', 'captions', 'prerecorded', 'vooraf opgenomen'] },
+    u = void 0,
+    m = { id: 'wcag/1.2.02', title: 'WCAG-succescriterium 1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)', description: 'Opgenomen video heeft ondertiteling.', source: '@site/docs/wcag/1.2.02.mdx', sourceDirName: 'wcag', slug: '/wcag/1.2.2', permalink: '/wcag/1.2.2', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/wcag/1.2.02.mdx', tags: [], version: 'current', frontMatter: { title: 'WCAG-succescriterium 1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)', hide_title: !0, hide_table_of_contents: !1, sidebar_label: '1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)', pagination_label: 'WCAG-succescriterium 1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)', description: 'Opgenomen video heeft ondertiteling.', slug: '1.2.2', keywords: ['WCAG', 'A', 'waarneembaar', 'op tijd gebaseerde media', 'perceivable', 'time-based media', 'captions (prerecorded)', 'captions', 'prerecorded', 'vooraf opgenomen'] }, sidebar: 'richtlijnen', previous: { title: 'WCAG-succescriterium 1.2.1 Louter-geluid en louter-videobeeld (vooraf opgenomen)', permalink: '/wcag/1.2.1' }, next: { title: 'WCAG-succescriterium 1.2.3 Audiodescriptie of media-alternatief (vooraf opgenomen)', permalink: '/wcag/1.2.3' } },
+    j = {},
+    v = [
+     { value: 'Uitleg', id: 'uitleg', level: 2 },
+     { value: 'Hoe te testen', id: 'hoe-te-testen', level: 2 },
+     { value: 'Tips', id: 'tips', level: 2 },
+     { value: 'Gerelateerde NL Design System-richtlijnen', id: 'gerelateerde-nl-design-system-richtlijnen', level: 2 },
+     { value: 'Gebruikersonderzoek', id: 'gebruikersonderzoek', level: 2 },
+     { value: 'W3C referenties', id: 'w3c-referenties', level: 2 },
+     { value: 'Help richtlijn verbeteren', id: 'help-richtlijn-verbeteren', level: 2 },
+    ];
+   function f(e) {
+    const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: ['\n', '\n', '\n', (0, r.jsx)(s.j, { level: 1, conformanceLevel: 'Niveau A', children: 'WCAG-succescriterium 1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)' }), '\n', (0, r.jsx)(n.h2, { id: 'uitleg', children: 'Uitleg' }), '\n', (0, r.jsx)(g, {}), '\n', (0, r.jsxs)(n.p, { children: ['Een uitgebreide uitleg van ondertiteling vind je in de richtlijn ', (0, r.jsx)(n.a, { href: '/richtlijnen/content/video/ondertiteling/', children: 'Ondertiteling bij video' }), '.'] }), '\n', (0, r.jsx)(n.h2, { id: 'hoe-te-testen', children: 'Hoe te testen' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsx)(n.li, { children: 'Controleer bij opgenomen video of er kloppende ondertiteling aanwezig is.' }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'tips', children: 'Tips' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsx)(n.li, { children: 'Let bij de presentatie van ondertiteling goed op toegankelijkheidsprincipes als het contrast, het formaat van de tekst en een goed leesbaar lettertype.' }), '\n', (0, r.jsx)(n.li, { children: 'Automatisch gegenereerde ondertiteling is praktisch. Het laat echter vaak steken vallen. Dit gebeurt vooral met namen, als er veel jargon is, of als de taal van de audio afwijkt van de gebruikte tool. Ook wordt niet-gesproken audio niet meegenomen hierin.' }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'gerelateerde-nl-design-system-richtlijnen', children: 'Gerelateerde NL Design System-richtlijnen' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsxs)(n.li, { children: ['Content: ', (0, r.jsx)(n.a, { href: '/richtlijnen/content/video/ondertiteling/', children: 'Ondertiteling' }), '.'] }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'gebruikersonderzoek', children: 'Gebruikersonderzoek' }), '\n', (0, r.jsx)(l.ZP, {}), '\n', (0, r.jsx)(n.h2, { id: 'w3c-referenties', children: 'W3C referenties' }), '\n', (0, r.jsxs)(n.ul, { children: ['\n', (0, r.jsxs)(n.li, { children: ['\n', (0, r.jsxs)(n.p, { children: ['Engelse tekst van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/#captions-prerecorded', children: (0, r.jsx)('span', { lang: 'en', children: '1.2.2 Captions (Prerecorded)' }) }), '.'] }), '\n'] }), '\n', (0, r.jsxs)(n.li, { children: ['\n', (0, r.jsxs)(n.p, { children: ['Nederlandse vertaling van het WCAG-succescriterium: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#captions-prerecorded', children: '1.2.2 Ondertitels voor doven en slechthorenden (vooraf opgenomen)' }), '.'] }), '\n'] }), '\n', (0, r.jsxs)(n.li, { children: ['\n', (0, r.jsxs)(n.p, { children: ['Engelstalige informatie op ', (0, r.jsx)('span', { lang: 'en', children: 'How to Meet WCAG' }), ': ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/quickref/#captions-prerecorded', children: (0, r.jsx)('span', { lang: 'en', children: 'Quick Reference 1.2.2 Captions (Prerecorded)' }) }), '.'] }), '\n'] }), '\n', (0, r.jsxs)(n.li, { children: ['\n', (0, r.jsxs)(n.p, { children: ['Engelstalige toelichting: ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/WAI/WCAG22/Understanding/captions-prerecorded.html', children: (0, r.jsx)('span', { lang: 'en', children: '1.2.2 Captions (Prerecorded)' }) }), '.'] }), '\n', (0, r.jsx)(d.CY, { children: (0, r.jsx)(o.U, { omitH1: !0, headinglevel: 3, children: (0, r.jsx)(a.ZP, {}) }) }), '\n'] }), '\n'] }), '\n', (0, r.jsx)(n.h2, { id: 'help-richtlijn-verbeteren', children: 'Help richtlijn verbeteren' }), '\n', (0, r.jsx)(o.U, { omitH1: !0, headinglevel: 3, children: (0, r.jsx)(c.ZP, {}) })] });
+   }
+   function x(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(f, { ...e }) }) : f(e);
+   }
+  },
+  21339: (e, n, i) => {
+   i.d(n, { ZP: () => s });
+   var r = i(52676),
+    t = i(40139);
+   function o(e) {
+    const n = { a: 'a', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(n.p, { children: ['Heb je gebruikersonderzoek gedaan dat betrekking heeft op dit succescriterium en wil je dit delen? Kijk eens bij ', (0, r.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/onderzoek-delen/', children: 'Gebruikersonderzoeken delen' }), ' op gebruikersonderzoeken.nl.'] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  92828: (e, n, i) => {
+   i.d(n, { ZP: () => s });
+   var r = i(52676),
+    t = i(40139);
+   function o(e) {
+    const n = { a: 'a', h2: 'h2', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.h2, { id: 'belangrijk-de-richtlijnen-van-nl-design-system-zijn-geen-wettelijke-verplichting', children: 'Belangrijk: De richtlijnen van NL Design System zijn geen wettelijke verplichting' }), '\n', (0, r.jsxs)(n.p, { children: ['De richtlijnen van NL Design System zijn niet wettelijk verplicht en zijn geen vervanging voor de ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG21/', children: 'wettelijk geldende WCAG 2.1 specificatie' }), '.'] }), '\n', (0, r.jsxs)(n.p, { children: ['Ons doel is om praktische uitleg en voorbeelden te geven die helpen bij het toegankelijk inzetten van de NL Design System componenten, patronen en richtlijnen. We doen dat op basis van een interpretatie van de ', (0, r.jsx)(n.a, { href: 'https://www.w3.org/TR/WCAG22/', children: 'nieuwe WCAG 2.2 specificatie' }), '.'] }), '\n', (0, r.jsxs)(n.p, { children: ['Weten waar je volgens de wet aan moet voldoen? Ga dan naar ', (0, r.jsx)(n.a, { href: 'https://www.digitoegankelijk.nl/wetgeving/wat-is-verplicht', children: 'wat is verplicht van DigiToegankelijk' }), '.'] })] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  38391: (e, n, i) => {
+   i.d(n, { ZP: () => s });
+   var r = i(52676),
+    t = i(40139);
+   function o(e) {
+    const n = { a: 'a', h1: 'h1', p: 'p', ...(0, t.a)(), ...e.components };
+    return (0, r.jsxs)(r.Fragment, { children: [(0, r.jsx)(n.h1, { id: 'aanvullingen-of-opmerkingen', children: 'Aanvullingen of opmerkingen?' }), '\n', (0, r.jsxs)(n.p, { children: ["Deze pagina's over WCAG worden onderhouden door NL Design System. Heb je aanvullingen of opmerkingen? ", (0, r.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Deel je mening op GitHub' }), '.'] })] });
+   }
+   function s(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, r.jsx)(n, { ...e, children: (0, r.jsx)(o, { ...e }) }) : o(e);
+   }
+  },
+  41757: (e, n, i) => {
+   i.d(n, { V: () => o });
+   var r = i(4814),
+    t = i(52676);
+   const o = (e) => {
+    let { children: n, className: i, level: o = 1, suffix: s, ...d } = e;
+    return (0, t.jsxs)('hgroup', { className: (0, r.Z)('nlds-inline-heading-group', `utrecht-heading-${o}`, i), ...d, children: [(0, t.jsx)('h1', { className: 'nlds-inline-heading-group__heading', children: n }), s && (0, t.jsxs)('p', { className: 'nlds-inline-heading-group__suffix', children: [s ? ' ' : '', s] })] });
+   };
+  },
+  57716: (e, n, i) => {
+   i.d(n, { U: () => c });
+   var r = i(40139),
+    t = i(37943),
+    o = i(52676);
+   const s = (e, n) => {
+     const i = new URL(n, new URL(e, 'resolve://pathname/'));
+     if ('resolve:' === i.protocol) {
+      const { pathname: i, search: r, hash: t } = new URL(n, new URL(e, 'http://example.com/'));
+      return i + r + t;
+     }
+     return i.toString();
+    },
+    d = { 1: {}, 2: { h1: t.XJ, h2: t.aC, h3: t.k8, h4: t.by, h5: t.Cd }, 3: { h1: t.aC, h2: t.k8, h3: t.by, h4: t.Cd, h5: t.Cd }, 4: { h1: t.k8, h2: t.by, h3: t.Cd, h4: t.Cd, h5: t.Cd }, 5: { h1: t.by, h2: t.Cd, h3: t.Cd, h4: t.Cd, h5: t.Cd }, 6: { h1: t.Cd, h2: t.Cd, h3: t.Cd, h4: t.Cd, h5: t.Cd } },
+    l = (e) => ({
+     img: (n) => {
+      let { src: i, ...r } = n;
+      return (0, o.jsx)('img', { ...r, src: s(e, i), className: 'utrecht-img utrecht-img--fit' });
+     },
+    }),
+    a = (e, n) => {
+     if (e) {
+      const e = { ...d[n - 1] };
+      return (e.h1 = () => null), e;
+     }
+     return d[n];
+    },
+    c = (e) => {
+     let { children: n, omitH1: i = !1, headingLevel: t = 1, baseUrl: s = '', components: d = {} } = e;
+     return (0, o.jsx)(r.Z, { components: { ...a(i, t), ...l(s), ...d }, children: n });
+    };
+  },
+  46364: (e, n, i) => {
+   i.d(n, { j: () => s });
+   var r = i(37943),
+    t = i(41757),
+    o = i(52676);
+   const s = (e) => {
+    let { children: n, conformanceLevel: i, ...s } = e;
+    return (0, o.jsx)(t.V, { className: 'nlds-inline-heading-group--wcag-heading-group', suffix: i && (0, o.jsx)(r.Ou, { className: 'nlds-inline-heading-group__badge', children: i }), ...s, children: n });
+   };
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => d, a: () => s });
+   var r = i(75271);
+   const t = {},
+    o = r.createContext(t);
+   function s(e) {
+    const n = r.useContext(o);
+    return r.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function d(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : s(e.components)), r.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
