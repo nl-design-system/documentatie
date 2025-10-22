13123afe31363ded435979e138acf10c67f8dbf0
Johan Huijkman <johan@q42.nl>
2025-10-22 17:02:36 +0200

    WIP: wizzard stijl setup

---
+++ src/components/AltTextHelper/HelperText.tsx
@@ -0,0 +1,23 @@
+import { Heading2, Paragraph, SpotlightSection } from '@utrecht/component-library-react/dist/css-module';
+import { Texts } from './helper-text';
+
+const currentHelperText = (id) => {
+  const text = Texts.find((o) => o.id === id);
+
+  return text;
+};
+
+interface HelperTextProps {
+  helperTextId: string;
+}
+
+export default function HelperText({ helperTextId }: HelperTextProps) {
+  if (currentHelperText(helperTextId)) {
+    return (
+      <SpotlightSection type="info" role="alert">
+        <Heading2>{currentHelperText(helperTextId).heading}</Heading2>
+        <Paragraph dangerouslySetInnerHTML={{ __html: currentHelperText(helperTextId).paragraph }}></Paragraph>
+      </SpotlightSection>
+    );
+  }
+}
