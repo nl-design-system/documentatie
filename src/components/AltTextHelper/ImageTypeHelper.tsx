4551f0595a629990fefe864bc2eb14b6c1a6e809
Johan Huijkman <johan@q42.nl>
2025-10-29 11:03:42 +0100

    changed visual layout

--- src/components/AltTextHelper/ImageTypeHelper.tsx
+++ src/components/AltTextHelper/ImageTypeHelper.tsx
@@ -3,7 +3,6 @@ import {
   FieldsetLegend,
   FormFieldDescription,
   FormField,
-  Paragraph,
   FormLabel,
   RadioButton,
 } from '@utrecht/component-library-react/dist/css-module';
@@ -24,68 +23,85 @@ export default function ImageTypeHelper({ onOptionChange, checked }: ImageTypeHe
       <FieldsetLegend>Wat is het doel van de afbeelding?</FieldsetLegend>
       <FormFieldDescription id="image-type-group-description">Kies de optie die het beste past:</FormFieldDescription>
       <FormField type="radio">
-        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="image-type-context" type="radio">
-            <RadioButton
-              className="utrecht-form-field__input"
-              id="image-type-context"
-              name="image-type"
-              value="image-type-context-help"
-              checked={checked === 'image-type-context-help'}
-            />
+        <FormLabel
+          className="utrecht-form-field__label utrecht-form-field__label--radio"
+          htmlFor="image-type-context"
+          type="radio"
+        >
+          <RadioButton
+            className="utrecht-form-field__input"
+            id="image-type-context"
+            name="image-type"
+            value="image-type-context-help"
+            checked={checked === 'image-type-context-help'}
+          />
+          <span>
             <strong>De afbeelding helpt om de tekst op de pagina beter te begrijpen.</strong>
             <br />
             Dit heet een informatieve afbeelding. Voorbeelden hiervan zijn een grafiek, een illustratie of een
             stappenplan.
-          </FormLabel>
-        </Paragraph>
+          </span>
+        </FormLabel>
       </FormField>
       <FormField type="radio">
-        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="image-type-decorative" type="radio">
-            <RadioButton
-              className="utrecht-form-field__input"
-              id="image-type-decorative"
-              name="image-type"
-              value="image-type-decorative-help"
-              checked={checked === 'image-type-decorative-help'}
-            />
+        <FormLabel
+          className="utrecht-form-field__label utrecht-form-field__label--radio"
+          htmlFor="image-type-decorative"
+          type="radio"
+        >
+          <RadioButton
+            className="utrecht-form-field__input"
+            id="image-type-decorative"
+            name="image-type"
+            value="image-type-decorative-help"
+            checked={checked === 'image-type-decorative-help'}
+          />
+          <span>
             <strong>De afbeelding is alleen toegevoegd om de pagina er mooier uit te laten zien.</strong>
             <br />
             Dit heet een decoratieve afbeelding, bijvoorbeeld een sfeerbeeld zonder betekenis.
-          </FormLabel>
-        </Paragraph>
+          </span>
+        </FormLabel>
       </FormField>
       <FormField type="radio">
-        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="image-type-text" type="radio">
-            <RadioButton
-              className="utrecht-form-field__input"
-              id="image-type-text"
-              name="image-type"
-              value="image-type-text-help"
-              checked={checked === 'image-type-text-help'}
-            />
+        <FormLabel
+          className="utrecht-form-field__label utrecht-form-field__label--radio"
+          htmlFor="image-type-text"
+          type="radio"
+        >
+          <RadioButton
+            className="utrecht-form-field__input"
+            id="image-type-text"
+            name="image-type"
+            value="image-type-text-help"
+            checked={checked === 'image-type-text-help'}
+          />
+          <span>
             <strong>De afbeelding bestaat hoofdzakelijk uit tekst die bedoeld is om gelezen te worden</strong>
             <br />
             Bijvoorbeeld: banners, afbeeldingen van flyers of de verkoopprijs van een product.
-          </FormLabel>
-        </Paragraph>
+          </span>
+        </FormLabel>
       </FormField>
       <FormField type="radio">
-        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="image-type-functional" type="radio">
-            <RadioButton
-              className="utrecht-form-field__input"
-              id="image-type-functional"
-              name="image-type"
-              value="image-type-functional-help"
-              checked={checked === 'image-type-functional-help'}
-            />
+        <FormLabel
+          className="utrecht-form-field__label utrecht-form-field__label--radio"
+          htmlFor="image-type-functional"
+          type="radio"
+        >
+          <RadioButton
+            className="utrecht-form-field__input"
+            id="image-type-functional"
+            name="image-type"
+            value="image-type-functional-help"
+            checked={checked === 'image-type-functional-help'}
+          />
+          <span>
             <strong>De afbeelding wordt gebruikt als een link of knop.</strong>
+            <br />
             Dit heet een functionele afbeelding.
-          </FormLabel>
-        </Paragraph>
+          </span>
+        </FormLabel>
       </FormField>
     </Fieldset>
   );
