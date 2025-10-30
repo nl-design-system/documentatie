ccba045c0a4ff8fe4cf03b6f468133963f4fa5dd
Johan Huijkman <johan@q42.nl>
2025-10-30 14:44:27 +0100

    Nieuwe opzet, die werkt.

--- src/components/AltTextHelper/ContextTypeHelper.tsx
+++ src/components/AltTextHelper/ContextTypeHelper.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Fieldset,
   FieldsetLegend,
@@ -5,59 +6,91 @@ import {
   FormField,
   FormLabel,
   RadioButton,
+  Button,
 } from '@utrecht/component-library-react/dist/css-module';
 
 interface ContextTypeHelperProps {
   onOptionChange;
-  checked;
+  onGroupChange;
+  activeGroup;
 }
 
-export default function ContextTypeHelper({ onOptionChange, checked }: ContextTypeHelperProps) {
-  return (
-    <Fieldset
-      id="context-type-group"
-      aria-describedby="image-type-group-description"
-      role="radiogroup"
-      onChange={onOptionChange}
-    >
-      <FieldsetLegend>Wat voor soort afbeelding is het?</FieldsetLegend>
-      <FormFieldDescription id="context-type-group-description">Kies de optie die het beste past:</FormFieldDescription>
-      <FormField type="radio">
-        <FormLabel
-          className="utrecht-form-field__label utrecht-form-field__label--radio"
-          htmlFor="context-complex-image"
-          type="radio"
-        >
-          <RadioButton
-            className="utrecht-form-field__input"
-            id="context-complex-image"
-            name="context-type"
-            value="context-complex-image-help"
-            defaultChecked={checked === 'context-complex-image-help'}
-          />
-          <span>
-            <strong>Het is een grafiek, diagram of een andere complexe afbeelding.</strong>
-          </span>
-        </FormLabel>
-      </FormField>
-      <FormField type="radio">
-        <FormLabel
-          className="utrecht-form-field__label utrecht-form-field__label--radio"
-          htmlFor="context-simple-image"
-          type="radio"
-        >
-          <RadioButton
-            className="utrecht-form-field__input"
-            id="context-simple-image"
-            name="context-type"
-            value="context-simple-image-help"
-            defaultChecked={checked === 'context-simple-image-help'}
-          />
-          <span>
-            <strong>Het is een foto of een eenvoudige illustratie.</strong>
-          </span>
-        </FormLabel>
-      </FormField>
-    </Fieldset>
-  );
+const groupName = 'context-type';
+
+export default function ContextTypeHelper({ onOptionChange, onGroupChange, activeGroup }: ContextTypeHelperProps) {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const changeSelected = (e) => {
+    const { value } = e.target;
+    setSelectedOption(value);
+  };
+
+  const nextStep = () => {
+    onGroupChange('');
+    onOptionChange(selectedOption);
+  };
+
+  const prevStep = () => {
+    onGroupChange('image-type');
+    onOptionChange('');
+  };
+
+  if (activeGroup === groupName) {
+    return (
+      <Fieldset id="context-type-group" aria-describedby="image-type-group-description" role="radiogroup">
+        <FieldsetLegend>Wat voor soort afbeelding is het?</FieldsetLegend>
+        <FormFieldDescription id="context-type-group-description">
+          Kies de optie die het beste past:
+        </FormFieldDescription>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="context-complex-image"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="context-complex-image"
+              name={groupName}
+              value="context-complex-image-help"
+              defaultChecked={selectedOption === 'context-complex-image-help'}
+              onChange={changeSelected}
+            />
+            <span>
+              <strong>Het is een grafiek, diagram of een andere complexe afbeelding.</strong>
+            </span>
+          </FormLabel>
+        </FormField>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="context-simple-image"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="context-simple-image"
+              name={groupName}
+              value="context-simple-image-help"
+              defaultChecked={selectedOption === 'context-simple-image-help'}
+              onChange={changeSelected}
+            />
+            <span>
+              <strong>Het is een foto of een eenvoudige illustratie.</strong>
+            </span>
+          </FormLabel>
+        </FormField>
+
+        <div className="button-bar">
+          <Button appearance="secondary-action-button" onClick={prevStep}>
+            Vorige
+          </Button>
+
+          <Button appearance="primary-action-button" disabled={selectedOption === '' ? true : false} onClick={nextStep}>
+            Volgende
+          </Button>
+        </div>
+      </Fieldset>
+    );
+  }
 }
