c4775fd3e4c753d2d3416e758c61ded0fc8b5dbf
Johan Huijkman <johan@q42.nl>
2025-11-03 16:05:15 +0100

    kleine stijl aanpassingen

--- src/components/AltTextHelper/helper-text.js
+++ src/components/AltTextHelper/helper-text.js
@@ -5,7 +5,7 @@ export const Texts = [
     heading: 'Als de afbeelding een link of knop is',
     paragraph: `Geef dan in de alt-tekst duidelijk aan wat er gebeurt als iemand erop klikt.
       <br/>
-      Bijvoorbeeld: “Bekijk productinformatie” of “Ga naar contactpagina”.`,
+      <span class="bold-text">Bijvoorbeeld:</span> “Bekijk productinformatie” of “Ga naar contactpagina”.`,
   },
   {
     id: 'image-type-decorative-help',
@@ -20,7 +20,7 @@ export const Texts = [
     heading: 'Als de afbeelding veel informatie bevat die niet in één korte beschrijving past',
     paragraph: `Voeg dan een uitgebreide beschrijving toe naast of onder de afbeelding. Zet in het veld voor alternatieve tekst een korte samenvatting, met een verwijzing naar de plek waar de langere beschrijving staat.
     <br/>
-    Voorbeeld: “Samenvatting van de grafiek. Uitgebreide uitleg staat onder de afbeelding.”`,
+    <span class="bold-text">Bijvoorbeeld:</span> “Samenvatting van de grafiek. Uitgebreide uitleg staat onder de afbeelding.”`,
   },
   {
     id: 'context-simple-image-help',
@@ -28,7 +28,7 @@ export const Texts = [
     heading: 'Als het een foto of eenvoudige illustratie is',
     paragraph: `Geef dan in het veld voor alternatieve tekst een korte beschrijving van wat er te zien is.
       <br/>
-      Bijvoorbeeld: “Twee mensen in gesprek” of “Logo van gemeente X”.`,
+      <span class="bold-text">Bijvoorbeeld:</span> “Twee mensen in gesprek” of “Logo van gemeente X”.`,
   },
   {
     id: 'text-near-image-help',
@@ -54,7 +54,7 @@ export const Texts = [
     heading: 'Als de afbeelding een functie heeft, zoals een icoon',
     paragraph: `Beschrijf dan in het veld voor alternatieve tekst wat de afbeelding doet of betekent.
     <br/>
-    Bijvoorbeeld: “Download PDF” of “Let op”.`,
+    <span class="bold-text">Bijvoorbeeld:</span> “Download PDF” of “Let op”.`,
   },
   {
     id: 'text-complex-image-help',
@@ -62,6 +62,6 @@ export const Texts = [
     heading: 'Als de afbeelding veel informatie bevat die niet in één korte beschrijving past',
     paragraph: `Voeg dan een uitgebreide beschrijving toe naast of onder de afbeelding. Zet in het veld voor alternatieve tekst een korte samenvatting, met een verwijzing naar de plek waar de langere beschrijving staat.
     <br/>
-    Voorbeeld: “Samenvatting van de grafiek. Uitgebreide uitleg staat onder de afbeelding.”`,
+    <span class="bold-text">Bijvoorbeeld:</span> “Samenvatting van de grafiek. Uitgebreide uitleg staat onder de afbeelding.”`,
   },
 ];
