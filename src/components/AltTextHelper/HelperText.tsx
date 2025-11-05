f0e6ee5483e38e28f32993c5e0e69e4e2950a238
Johan Huijkman <johan@q42.nl>
2025-11-05 17:08:26 +0100

    Aanvullende helper teksten toegevoegd

--- src/components/AltTextHelper/HelperText.tsx
+++ src/components/AltTextHelper/HelperText.tsx
@@ -1,4 +1,10 @@
-import { Heading2, Paragraph, SpotlightSection, Button } from '@utrecht/component-library-react/dist/css-module';
+import {
+  Heading2,
+  Heading3,
+  Paragraph,
+  SpotlightSection,
+  Button,
+} from '@utrecht/component-library-react/dist/css-module';
 import { Texts } from './helper-text';
 
 const currentHelperText = (id) => {
@@ -21,11 +27,14 @@ export default function HelperText({ id, changeOption, changeGroup }: HelperText
   };
 
   if (currentHelperText(id)) {
+    const helper = currentHelperText(id);
+
     return (
       <>
         <SpotlightSection type="info" role="alert" ref={autoFocus} tabIndex={-1}>
-          <Heading2>{currentHelperText(id).heading}</Heading2>
-          <Paragraph dangerouslySetInnerHTML={{ __html: currentHelperText(id).paragraph }}></Paragraph>
+          <Heading2>{helper.heading}</Heading2>
+          <Paragraph dangerouslySetInnerHTML={{ __html: helper.paragraph }}></Paragraph>
+          {helper.additionals && !!helper.additionals.length && <AdditionalHelpers helpers={helper.additionals} />}
         </SpotlightSection>
 
         <div className="button-bar">
@@ -36,4 +45,21 @@ export default function HelperText({ id, changeOption, changeGroup }: HelperText
       </>
     );
   }
+}
+
+interface AdditionalHelpersProps {
+  helpers;
+}
+
+export function AdditionalHelpers({ helpers }: AdditionalHelpersProps) {
+  return helpers.map((helper) => {
+    const item = currentHelperText(helper);
+
+    return (
+      <>
+        <Heading3>{item.heading}</Heading3>
+        <Paragraph dangerouslySetInnerHTML={{ __html: item.paragraph }}></Paragraph>
+      </>
+    );
+  });
 }
