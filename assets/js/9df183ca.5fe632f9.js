56fbf82cc1dc28ff17e54795014c77daf0e94caa
richadr <richard@vhvs.nl>
2025-11-12 10:18:06 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@863496a07574fc658475499736a78fa849e0f87b 🚀

---
+++ assets/js/9df183ca.5fe632f9.js
@@ -0,0 +1,61 @@
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [58895],
+ {
+  6138: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => a, contentTitle: () => d, default: () => m, frontMatter: () => o, metadata: () => r, toc: () => l });
+   var t = i(52676),
+    s = i(40139);
+   const o = { title: 'Figma-bestanden overzicht \xb7 Designer \xb7 Handboek', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'Figma-bestanden overzicht', sidebar_position: 5, pagination_label: 'Figma-bestanden overzicht', description: 'Overzicht van Figma-bestanden die worden beheerd door het kernteam \xe9n de community.', slug: '/figma', keywords: ['design', 'figma', 'bibliotheek', 'prototypes', 'open source'] },
+    d = 'Figma-bestanden overzicht',
+    r = { id: 'handboek/designer/figma-bestanden-overzicht', title: 'Figma-bestanden overzicht \xb7 Designer \xb7 Handboek', description: 'Overzicht van Figma-bestanden die worden beheerd door het kernteam \xe9n de community.', source: '@site/docs/handboek/designer/figma-bestanden-overzicht.mdx', sourceDirName: 'handboek/designer', slug: '/figma', permalink: '/figma', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/handboek/designer/figma-bestanden-overzicht.mdx', tags: [], version: 'current', sidebarPosition: 5, frontMatter: { title: 'Figma-bestanden overzicht \xb7 Designer \xb7 Handboek', hide_title: !0, hide_table_of_contents: !0, sidebar_label: 'Figma-bestanden overzicht', sidebar_position: 5, pagination_label: 'Figma-bestanden overzicht', description: 'Overzicht van Figma-bestanden die worden beheerd door het kernteam \xe9n de community.', slug: '/figma', keywords: ['design', 'figma', 'bibliotheek', 'prototypes', 'open source'] }, sidebar: 'handboek', previous: { title: 'Zelf componenten maken', permalink: '/handboek/designer/zelf-componenten-maken' }, next: { title: 'Voor developers', permalink: '/handboek/developer/overzicht' } },
+    a = {},
+    l = [
+     { value: 'Figma bibliotheken', id: 'figma-bibliotheken', level: 2 },
+     { value: 'Kernteam', id: 'kernteam', level: 3 },
+     { value: 'Rijkshuisstijl Community', id: 'rijkshuisstijl-community', level: 3 },
+     { value: 'Amsterdam', id: 'amsterdam', level: 3 },
+     { value: 'Den Haag', id: 'den-haag', level: 3 },
+     { value: 'Groningen', id: 'groningen', level: 3 },
+     { value: 'Nijmegen', id: 'nijmegen', level: 3 },
+     { value: 'Rotterdam', id: 'rotterdam', level: 3 },
+     { value: 'RVO', id: 'rvo', level: 3 },
+     { value: 'Utrecht', id: 'utrecht', level: 3 },
+     { value: 'Figma bibliotheek toevoegen?', id: 'figma-bibliotheek-toevoegen', level: 3 },
+     { value: 'Figma bestanden', id: 'figma-bestanden', level: 2 },
+     { value: 'Kernteam', id: 'kernteam-1', level: 3 },
+     { value: 'MijnServices Community', id: 'mijnservices-community', level: 3 },
+     { value: 'Figma bestanden toevoegen?', id: 'figma-bestanden-toevoegen', level: 3 },
+     { value: 'Help deze documentatie te verbeteren', id: 'help-deze-documentatie-te-verbeteren', level: 2 },
+     { value: 'Vragen', id: 'vragen', level: 2 },
+    ];
+   function h(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', h3: 'h3', hr: 'hr', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(n.h1, { id: 'figma-bestanden-overzicht', children: 'Figma-bestanden overzicht' }), '\n', (0, t.jsx)(n.p, { children: 'Een overzicht van Figma-bestanden die worden beheerd door het kernteam \xe9n de community.' }), '\n', (0, t.jsx)(n.h2, { id: 'figma-bibliotheken', children: 'Figma bibliotheken' }), '\n', (0, t.jsx)(n.h3, { id: 'kernteam', children: 'Kernteam' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/FqAr99wvrlHxTJYAHkFRQN/NL-Design-System---Bibliotheek?node-id=197-664&t=t25LHoMjEkUZyEi1-1', children: 'NL Design System - Bibliotheek - Start' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/42KfIBw43pwLxv2tE7oRzg/NL-Design-System---ToDo-Bibliotheek?node-id=3047-14&t=fpA32yYLXZpVB6tb-1', children: 'NL Design System - ToDo Bibliotheek - Start' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/0J3EiRpZH3LJ0cx396XLNC/NL-Design-System---Bibliotheek---Voorbeeld?node-id=197-664&t=6q27i1PaV5ZFstxx-1', children: 'NL Design System - Bibliotheek - Voorbeeld' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/ig29egqH8dyyWY1oe03OC0/NL-Design-System---ToDo-Bibliotheek---Voorbeeld?node-id=0-1&t=0JUFZYgSsC4NLwHu-1', children: 'NL Design System - ToDo Bibliotheek - Voorbeeld' }) }), '\n'] }), '\n', (0, t.jsx)(n.h3, { id: 'rijkshuisstijl-community', children: 'Rijkshuisstijl Community' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/Q5Imc7Xi9KnBQhcYI3Hytj?node-id=', children: 'NL Design System - Bibliotheek - Rijkshuisstijl Community' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/JGhvvERXErVs8VmPfkqW3w?node-id=', children: 'NL Design System - Rijkshuisstijl - Iconen en elementen' }) }), '\n'] }), '\n', (0, t.jsx)(n.h3, { id: 'amsterdam', children: 'Amsterdam' }), '\n', (0, t.jsx)(n.p, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/9IGm6IdPUYizBNGsUnueBd/Amsterdam-Design-System?m=auto&node-id=741-19633&t=t3hRY5jP655DS4Dv-1', children: 'Bibliotheek - Amsterdam' }) }), '\n', (0, t.jsx)(n.h3, { id: 'den-haag', children: 'Den Haag' }), '\n', (0, t.jsx)(n.p, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/JpoY3waVoQGlLQzQXTL9nn/%F0%9F%8C%BB-Den-Haag-Design-System?node-id=13-105&t=nLXo455uVCYvxlbi-1', children: 'Bibliotheek - Den Haag' }) }), '\n', (0, t.jsx)(n.h3, { id: 'groningen', children: 'Groningen' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/5NLJVsVgqJ4Fcos6eDEKLn/NL-Design-System---Bibliotheek---Gemeente-Groningen?t=GlTaMphFXM6XruqU-0', children: 'NL Design System - Bibliotheek - Groningen' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/nncQk7kdiJh4Dh9fDfWwmp/NL-Design-System---ToDo-Bibliotheek---Gemeente-Groningen?t=8hIrBKQJFUarY4Rk-0', children: 'NL Design System - ToDo Bibliotheek - Groningen' }) }), '\n'] }), '\n', (0, t.jsx)(n.h3, { id: 'nijmegen', children: 'Nijmegen' }), '\n', (0, t.jsx)(n.p, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/qFdecrgSdnaOEGE3oy9OIY/NL-Design-System---Bibliotheek---Nijmegen?m=auto&t=oftQPxvdr4PVobw8-6', children: 'NL Design System - Bibliotheek - Nijmegen' }) }), '\n', (0, t.jsx)(n.h3, { id: 'rotterdam', children: 'Rotterdam' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/jzlKh1VbBDvRc3fYfVotsW/RODS-Foundation?m=auto&node-id=3427-315&t=Ak6YnZB8MCnKfeOs-1', children: 'Foundation Bibliotheek - Rotterdam' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/iWuQsxelJzXVfCge87Og81/RODS-Components?m=auto&node-id=0-1&t=oiry4vTg8OlJ2M4f-1', children: 'Components Bibliotheek - Rotterdam' }) }), '\n'] }), '\n', (0, t.jsx)(n.h3, { id: 'rvo', children: 'RVO' }), '\n', (0, t.jsx)(n.p, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/Sj6myBL1Fvot5M1qGxzvEo/ROOS--RVO-Design-System-?node-id=282-5324&t=ApdrxmOPYa6H0ogG-1', children: 'Bibliotheek - RVO' }) }), '\n', (0, t.jsx)(n.h3, { id: 'utrecht', children: 'Utrecht' }), '\n', (0, t.jsx)(n.p, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/xV2HiqedQ8M43ifBYj3B4D/%F0%9F%86%95-NL-Design-System---Bibliotheek---Utrecht?m=auto&t=Z2fEQJskMB14KcJ9-6', children: 'NL Design System - Bibliotheek - Utrecht' }) }), '\n', (0, t.jsx)(n.h3, { id: 'figma-bibliotheek-toevoegen', children: 'Figma bibliotheek toevoegen?' }), '\n', (0, t.jsxs)(n.p, { children: ['Heeft jouw organisatie ook een Figma bibliotheek die open source te bekijken is? Dan voegen we deze graag toe aan de lijst. ', (0, t.jsx)(n.a, { href: '/project/kernteam/', children: 'Neem contact op met het kernteam' }), '.'] }), '\n', (0, t.jsx)(n.h2, { id: 'figma-bestanden', children: 'Figma bestanden' }), '\n', (0, t.jsx)(n.h3, { id: 'kernteam-1', children: 'Kernteam' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/fy08SZpZmqx6ljLwvA3Woe/NL-Design-System---Schetsboek?node-id=231-73&t=f7kKKaXvqOj1q3Ey-1', children: 'NL Design System - Schetsboek' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/taAnsV55PVP0cmw18BnMDk/NL-Design-System---Templates---Voorbeeld?node-id=1-3&t=3lZPNWC5lsZzFZKO-1', children: 'NL Design System - Templates - Voorbeeld' }) }), '\n'] }), '\n', (0, t.jsx)(n.h3, { id: 'mijnservices-community', children: 'MijnServices Community' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/pB5d6RlVSa1B088Xpm1sSo/2025---MijnServices---Templates--Voorheen--Overheidsbrede-portalen-?node-id=1-3&t=y30BoRawbogrzpo6-1', children: 'MijnServices Templates 2025' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/iZgSIuU8hvH9nw3h7WO1ZY/2024---MijnServices---Templates--Voorheen--Overheidsbrede-portalen-?node-id=1-3&t=evI8SVvoKpvULAxi-1', children: 'MijnServices Templates 2024' }) }), '\n'] }), '\n', (0, t.jsx)(n.h3, { id: 'figma-bestanden-toevoegen', children: 'Figma bestanden toevoegen?' }), '\n', (0, t.jsxs)(n.p, { children: ['Heeft jouw organisatie ook een Figma bestand dat open source te bekijken is? Dan voegen we het graag toe aan de lijst. ', (0, t.jsx)(n.a, { href: '/project/kernteam/', children: 'Neem contact op met het kernteam' }), '.'] }), '\n', (0, t.jsx)(n.hr, {}), '\n', (0, t.jsx)(n.h2, { id: 'help-deze-documentatie-te-verbeteren', children: 'Help deze documentatie te verbeteren' }), '\n', (0, t.jsxs)(n.p, { children: ['Om ervoor te zorgen dat deze documentatie nuttig, relevant en up-to-date is, kun je een wijziging voorstellen via ', (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie', children: 'GitHub' }), '.'] }), '\n', (0, t.jsx)(n.h2, { id: 'vragen', children: 'Vragen' }), '\n', (0, t.jsxs)(n.p, { children: ['Heb je een vraag? Twijfel niet en ', (0, t.jsx)(n.a, { href: '/project/kernteam/', children: 'neem contact op met het kernteam' }), '.'] })] });
+   }
+   function m(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(h, { ...e }) }) : h(e);
+   }
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => r, a: () => d });
+   var t = i(75271);
+   const s = {},
+    o = t.createContext(s);
+   function d(e) {
+    const n = t.useContext(o);
+    return t.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function r(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(s) : e.components || s) : d(e.components)), t.createElement(o.Provider, { value: n }, e.children);
+   }
+  },
+ },
+]);
