16cf46ca099b8d52ca4463248dfe448cd0df9a7a
Johan Huijkman <johan@q42.nl>
2025-11-21 15:30:33 +0100

    Alt-tekst helper nieuwe opzet

--- src/components/AltTextHelper/ContextTypeHelper.tsx
+++ src/components/AltTextHelper/ContextTypeHelper.tsx
@@ -6,106 +6,91 @@ import {
   FormField,
   FormLabel,
   RadioButton,
-  Button,
-  PreHeading,
 } from '@utrecht/component-library-react/dist/css-module';
 
 const groupName = 'context-type';
 
-const autoFocus = (element: HTMLElement | null) => element?.focus();
+// const autoFocus = (element: HTMLElement | null) => element?.focus();
 
-interface ContextTypeHelperProps {
-  changeOption;
-  changeGroup;
-  activeGroup: string;
+interface ImageTypeHelperProps {
+  onChangeOption;
 }
 
-export default function ContextTypeHelper({ changeOption, changeGroup, activeGroup }: ContextTypeHelperProps) {
+export default function ContextTypeHelper({ onChangeOption }: ImageTypeHelperProps) {
   const [selectedOption, setSelectedOption] = useState('');
 
-  const nextStep = () => {
-    changeGroup('');
-    changeOption(selectedOption);
-  };
+  return (
+    <>
+      <Fieldset
+        id="context-type-group"
+        aria-describedby="image-type-group-description"
+        role="radiogroup"
+        // ref={autoFocus}
+        // tabIndex={-1}
+      >
+        <FieldsetLegend>Wat voor soort afbeelding is het?</FieldsetLegend>
+        <FormFieldDescription id="context-type-group-description">
+          Kies de optie die het beste past:
+        </FormFieldDescription>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="image-complex"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="image-complex"
+              name={groupName}
+              value="image-complex"
+              defaultChecked={selectedOption === 'image-complex'}
+              onChange={(e) => {
+                setSelectedOption(e.target.value);
+                onChangeOption('complex', true);
+              }}
+            />
+            <div>
+              <span className="bold-text">Het is een grafiek, diagram of een andere complexe afbeelding.</span>
+            </div>
+          </FormLabel>
+        </FormField>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="image-simple"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="image-simple"
+              name={groupName}
+              value="image-simple"
+              defaultChecked={selectedOption === 'image-simple'}
+              onChange={(e) => {
+                setSelectedOption(e.target.value);
+                onChangeOption('complex', false);
+              }}
+            />
+            <div>
+              <span className="bold-text">Het is een foto of een eenvoudige illustratie.</span>
+            </div>
+          </FormLabel>
+        </FormField>
 
-  const prevStep = () => {
-    changeGroup('image-type');
-    changeOption('');
-  };
+        {/* <div className="nlds-button-bar">
+          <Button appearance="secondary-action-button" onClick={prevStep}>
+            Vorige stap
+          </Button>
 
-  if (activeGroup === groupName) {
-    return (
-      <>
-        <PreHeading>Stap 2 van 3</PreHeading>
-        <Fieldset
-          id="context-type-group"
-          aria-describedby="image-type-group-description"
-          role="radiogroup"
-          ref={autoFocus}
-          tabIndex={-1}
-        >
-          <FieldsetLegend>Wat voor soort afbeelding is het?</FieldsetLegend>
-          <FormFieldDescription id="context-type-group-description">
-            Kies de optie die het beste past:
-          </FormFieldDescription>
-          <FormField type="radio">
-            <FormLabel
-              className="utrecht-form-field__label utrecht-form-field__label--radio"
-              htmlFor="context-complex-image"
-              type="radio"
-            >
-              <RadioButton
-                className="utrecht-form-field__input"
-                id="context-complex-image"
-                name={groupName}
-                value="context-complex-image-help"
-                defaultChecked={selectedOption === 'context-complex-image-help'}
-                onChange={(e) => {
-                  setSelectedOption(e.target.value);
-                }}
-              />
-              <div>
-                <span className="bold-text">Het is een grafiek, diagram of een andere complexe afbeelding.</span>
-              </div>
-            </FormLabel>
-          </FormField>
-          <FormField type="radio">
-            <FormLabel
-              className="utrecht-form-field__label utrecht-form-field__label--radio"
-              htmlFor="context-simple-image"
-              type="radio"
-            >
-              <RadioButton
-                className="utrecht-form-field__input"
-                id="context-simple-image"
-                name={groupName}
-                value="context-simple-image-help"
-                defaultChecked={selectedOption === 'context-simple-image-help'}
-                onChange={(e) => {
-                  setSelectedOption(e.target.value);
-                }}
-              />
-              <div>
-                <span className="bold-text">Het is een foto of een eenvoudige illustratie.</span>
-              </div>
-            </FormLabel>
-          </FormField>
-
-          <div className="nlds-button-bar">
-            <Button appearance="secondary-action-button" onClick={prevStep}>
-              Vorige stap
-            </Button>
-
-            <Button
-              appearance="primary-action-button"
-              disabled={selectedOption === '' ? true : false}
-              onClick={nextStep}
-            >
-              Toon advies
-            </Button>
-          </div>
-        </Fieldset>
-      </>
-    );
-  }
+          <Button
+            appearance="primary-action-button"
+            disabled={selectedOption === '' ? true : false}
+            onClick={nextStep}
+          >
+            Toon advies
+          </Button>
+        </div> */}
+      </Fieldset>
+    </>
+  );
 }
--- src/components/AltTextHelper/TexTypeHelper.tsx
+++ src/components/AltTextHelper/TexTypeHelper.tsx
@@ -6,176 +6,139 @@ import {
   FormField,
   FormLabel,
   RadioButton,
-  Button,
-  PreHeading,
 } from '@utrecht/component-library-react/dist/css-module';
 
 const groupName = 'text-type';
 
-const autoFocus = (element: HTMLElement | null) => element?.focus();
+// const autoFocus = (element: HTMLElement | null) => element?.focus();
 
-interface TextTypeHelperProps {
-  changeOption;
-  changeGroup;
-  activeGroup: string;
+interface ImageTypeHelperProps {
+  onChangeOption;
 }
 
-export default function TextTypeHelper({ changeOption, changeGroup, activeGroup }: TextTypeHelperProps) {
+export default function TextTypeHelper({ onChangeOption }: ImageTypeHelperProps) {
   const [selectedOption, setSelectedOption] = useState('');
 
-  const nextStep = () => {
-    changeGroup('');
-    changeOption(selectedOption);
-  };
+  return (
+    <>
+      <Fieldset
+        id="text-type-group"
+        aria-describedby="text-type-group-description"
+        role="radiogroup"
+        // ref={autoFocus}
+        // tabIndex={-1}
+      >
+        <FieldsetLegend>Waarvoor is de tekst in de afbeelding bedoeld?</FieldsetLegend>
+        <FormFieldDescription id="text-type-group-description">Kies de optie die het beste past:</FormFieldDescription>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="text-has-function"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="text-has-function"
+              name={groupName}
+              value="text-has-function"
+              defaultChecked={selectedOption === 'text-has-function-help'}
+              onChange={(e) => {
+                setSelectedOption(e.target.value);
+                onChangeOption('content', e.target.value);
+              }}
+            />
+            <div>
+              <span className="bold-text">De tekst heeft een functionele rol.</span>
+              <br />
+              Bijvoorbeeld: een icoon met de letters PDF dat “Download PDF” of een uitroepteken dat “Let op” aanduidt.
+            </div>
+          </FormLabel>
+        </FormField>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="text-visual-effect"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="text-visual-effect"
+              name={groupName}
+              value="text-visual-effect"
+              defaultChecked={selectedOption === 'text-visual-effect-help'}
+              onChange={(e) => {
+                setSelectedOption(e.target.value);
+                onChangeOption('content', e.target.value);
+              }}
+            />
+            <div>
+              <span className="bold-text">
+                De tekst is alleen voor het visuele effect en heeft geen inhoudelijke betekenis
+              </span>
+              <br />
+              Bijvoorbeeld een watermerk, tekst in een screenshot, een afbeelding van een verpakking of een stapel
+              boeken.
+            </div>
+          </FormLabel>
+        </FormField>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="text-near-image"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="text-near-image"
+              name={groupName}
+              value="text-near-image"
+              defaultChecked={selectedOption === 'text-near-image-help'}
+              onChange={(e) => {
+                setSelectedOption(e.target.value);
+                onChangeOption('content', e.target.value);
+              }}
+            />
+            <div>
+              <span className="bold-text">Dezelfde tekst staat ook naast of vlak bij de afbeelding.</span>
+            </div>
+          </FormLabel>
+        </FormField>
+        <FormField type="radio">
+          <FormLabel
+            className="utrecht-form-field__label utrecht-form-field__label--radio"
+            htmlFor="text-only-image"
+            type="radio"
+          >
+            <RadioButton
+              className="utrecht-form-field__input"
+              id="text-only-image"
+              name={groupName}
+              value="text-only-image-help"
+              // defaultChecked={selectedOption === 'text-only-image-help'}
+              // onChange={(e) => {
+              //   setSelectedOption(e.target.value);
+              // }}
+            />
+            <div>
+              <span className="bold-text">De tekst staat nergens anders op de pagina.</span>
+            </div>
+          </FormLabel>
+        </FormField>
 
-  const prevStep = () => {
-    changeGroup('image-type');
-    changeOption('');
-  };
+        {/* <div className="nlds-button-bar">
+          <Button appearance="secondary-action-button" onClick={prevStep}>
+            Terug
+          </Button>
 
-  if (activeGroup === groupName) {
-    return (
-      <>
-        <PreHeading>Stap 2 van 3</PreHeading>
-        <Fieldset
-          id="text-type-group"
-          aria-describedby="text-type-group-description"
-          role="radiogroup"
-          ref={autoFocus}
-          tabIndex={-1}
-        >
-          <FieldsetLegend>Waarvoor is de tekst in de afbeelding bedoeld?</FieldsetLegend>
-          <FormFieldDescription id="text-type-group-description">
-            Kies de optie die het beste past:
-          </FormFieldDescription>
-          <FormField type="radio">
-            <FormLabel
-              className="utrecht-form-field__label utrecht-form-field__label--radio"
-              htmlFor="text-complex-image"
-              type="radio"
-            >
-              <RadioButton
-                className="utrecht-form-field__input"
-                id="text-complex-image"
-                name={groupName}
-                value="text-complex-image-help"
-                defaultChecked={selectedOption === 'text-complex-image-help'}
-                onChange={(e) => {
-                  setSelectedOption(e.target.value);
-                }}
-              />
-              <div>
-                <span className="bold-text">Het gaat om een grafiek, tabel of andere complexe informatie.</span>
-              </div>
-            </FormLabel>
-          </FormField>
-          <FormField type="radio">
-            <FormLabel
-              className="utrecht-form-field__label utrecht-form-field__label--radio"
-              htmlFor="text-has-function"
-              type="radio"
-            >
-              <RadioButton
-                className="utrecht-form-field__input"
-                id="text-has-function"
-                name={groupName}
-                value="text-has-function-help"
-                defaultChecked={selectedOption === 'text-has-function-help'}
-                onChange={(e) => {
-                  setSelectedOption(e.target.value);
-                }}
-              />
-              <div>
-                <span className="bold-text">De tekst heeft een functionele rol.</span>
-                <br />
-                Bijvoorbeeld: een icoon met de letters PDF dat “Download PDF” of een uitroepteken dat “Let op” aanduidt.
-              </div>
-            </FormLabel>
-          </FormField>
-          <FormField type="radio">
-            <FormLabel
-              className="utrecht-form-field__label utrecht-form-field__label--radio"
-              htmlFor="text-visual-effect"
-              type="radio"
-            >
-              <RadioButton
-                className="utrecht-form-field__input"
-                id="text-visual-effect"
-                name={groupName}
-                value="text-visual-effect-help"
-                defaultChecked={selectedOption === 'text-visual-effect-help'}
-                onChange={(e) => {
-                  setSelectedOption(e.target.value);
-                }}
-              />
-              <div>
-                <span className="bold-text">
-                  De tekst is alleen voor het visuele effect en heeft geen inhoudelijke betekenis
-                </span>
-                <br />
-                Bijvoorbeeld een watermerk, tekst in een screenshot, een afbeelding van een verpakking of een stapel
-                boeken.
-              </div>
-            </FormLabel>
-          </FormField>
-          <FormField type="radio">
-            <FormLabel
-              className="utrecht-form-field__label utrecht-form-field__label--radio"
-              htmlFor="text-near-image"
-              type="radio"
-            >
-              <RadioButton
-                className="utrecht-form-field__input"
-                id="text-near-image"
-                name={groupName}
-                value="text-near-image-help"
-                defaultChecked={selectedOption === 'text-near-image-help'}
-                onChange={(e) => {
-                  setSelectedOption(e.target.value);
-                }}
-              />
-              <div>
-                <span className="bold-text">Dezelfde tekst staat ook naast of vlak bij de afbeelding.</span>
-              </div>
-            </FormLabel>
-          </FormField>
-          <FormField type="radio">
-            <FormLabel
-              className="utrecht-form-field__label utrecht-form-field__label--radio"
-              htmlFor="text-only-image"
-              type="radio"
-            >
-              <RadioButton
-                className="utrecht-form-field__input"
-                id="text-only-image"
-                name={groupName}
-                value="text-only-image-help"
-                defaultChecked={selectedOption === 'text-only-image-help'}
-                onChange={(e) => {
-                  setSelectedOption(e.target.value);
-                }}
-              />
-              <div>
-                <span className="bold-text">De tekst staat nergens anders op de pagina.</span>
-              </div>
-            </FormLabel>
-          </FormField>
-
-          <div className="nlds-button-bar">
-            <Button appearance="secondary-action-button" onClick={prevStep}>
-              Terug
-            </Button>
-
-            <Button
-              appearance="primary-action-button"
-              disabled={selectedOption === '' ? true : false}
-              onClick={nextStep}
-            >
-              Toon advies
-            </Button>
-          </div>
-        </Fieldset>
-      </>
-    );
-  }
+          <Button
+            appearance="primary-action-button"
+            disabled={selectedOption === '' ? true : false}
+            onClick={nextStep}
+          >
+            Toon advies
+          </Button>
+        </div> */}
+      </Fieldset>
+    </>
+  );
 }
