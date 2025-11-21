5019d98efb184fcea754f0bd24cd8e118b969a43
Johan Huijkman <johan@q42.nl>
2025-11-21 15:30:33 +0100

    Alt-tekst helper nieuwe opzet

--- src/components/AltTextHelper/AltTextHelper.tsx
+++ src/components/AltTextHelper/AltTextHelper.tsx
@@ -1,43 +1,33 @@
 import { useState } from 'react';
-import HelperText from './HelperText';
 import ImageTypeHelper from './ImageTypeHelper';
-import TextTypeHelper from './TexTypeHelper';
 import ContextTypeHelper from './ContextTypeHelper';
-import { Button } from '@utrecht/component-library-react/dist/css-module';
+import TextTypeHelper from './TexTypeHelper';
+import AdditionalChecks from './AdditionalChecks';
 import './AltTextHelper.css';
 
 const AltTextHelper = () => {
-  const [activeSelection, setActiveSelection] = useState('');
-  const [activeGroup, setActiveGroup] = useState('start');
+  const [image, setImage] = useState({
+    decorative: false,
+    complex: false,
+    text: false,
+    clickable: false,
+    content: '',
+  });
 
-  const changeOption = (val: string) => {
-    setActiveSelection(val);
-  };
-
-  const changeGroup = (val: string) => {
-    setActiveGroup(val);
+  const changeProperty = (name, val) => {
+    setImage({
+      ...image,
+      [name]: val,
+    });
+    // console.log(image);
   };
 
   return (
     <form className="nlds-alt-text-helper">
-      {activeGroup === 'start' && (
-        <Button
-          appearance="primary-action-button"
-          onClick={() => {
-            setActiveGroup('image-type');
-          }}
-        >
-          Start de alt-tekst keuzehulp
-        </Button>
-      )}
-
-      <ImageTypeHelper changeOption={changeOption} changeGroup={changeGroup} activeGroup={activeGroup} />
-
-      <TextTypeHelper changeOption={changeOption} changeGroup={changeGroup} activeGroup={activeGroup} />
-
-      <ContextTypeHelper changeOption={changeOption} changeGroup={changeGroup} activeGroup={activeGroup} />
-
-      <HelperText id={activeSelection} changeOption={changeOption} changeGroup={changeGroup} />
+      <ImageTypeHelper onChangeOption={changeProperty} />
+      <ContextTypeHelper onChangeOption={changeProperty} />
+      <AdditionalChecks onChangeOption={changeProperty} />
+      <TextTypeHelper onChangeOption={changeProperty} />
     </form>
   );
 };
