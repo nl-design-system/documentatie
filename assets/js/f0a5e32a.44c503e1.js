f47807d8f3c2136ec3d19875d9d99cade17f28ef
eslook <42849307+eslook@users.noreply.github.com>
2025-11-21 14:51:33 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@e156efc5adaf526fef98c2d4b8247fe056739344 🚀

---
+++ assets/js/f0a5e32a.44c503e1.js
@@ -0,0 +1,183 @@
+/*! For license information please see f0a5e32a.44c503e1.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [73098],
+ {
+  17771: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => m, contentTitle: () => o, default: () => u, frontMatter: () => a, metadata: () => l, toc: () => c });
+   var t = i(52676),
+    s = i(40139),
+    r = i(97233);
+   const a = { title: 'MijnServices Community \xb7 Community Sprints \xb7 Community', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'MijnServices Community', pagination_label: 'MijnServices Community', description: 'Informatie over de NL Design System MijnServices Community Sprint', keywords: ['nl design system', 'community sprints', 'mijn services community', 'mijnservices community', 'mijn services community afstemming', 'mijn services community check-in', 'overheidsbrede portalen community'] },
+    o = 'Community Sprint: MijnServices Community',
+    l = { id: 'community/community-sprints/mijn-services-community/mijn-services-community', title: 'MijnServices Community \xb7 Community Sprints \xb7 Community', description: 'Informatie over de NL Design System MijnServices Community Sprint', source: '@site/docs/community/community-sprints/mijn-services-community/mijn-services-community.mdx', sourceDirName: 'community/community-sprints/mijn-services-community', slug: '/community/community-sprints/mijn-services-community/', permalink: '/community/community-sprints/mijn-services-community/', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/community-sprints/mijn-services-community/mijn-services-community.mdx', tags: [], version: 'current', frontMatter: { title: 'MijnServices Community \xb7 Community Sprints \xb7 Community', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'MijnServices Community', pagination_label: 'MijnServices Community', description: 'Informatie over de NL Design System MijnServices Community Sprint', keywords: ['nl design system', 'community sprints', 'mijn services community', 'mijnservices community', 'mijn services community afstemming', 'mijn services community check-in', 'overheidsbrede portalen community'] }, sidebar: 'community', previous: { title: 'MijnServices Community', permalink: '/community/community-sprints/mijn-services-community/overzicht' }, next: { title: 'Aanmelden', permalink: '/community/community-sprints/mijn-services-community/aanmelden' } },
+    m = {},
+    c = [
+     { value: 'Waar gaat deze sprint over?', id: 'waar-gaat-deze-sprint-over', level: 2 },
+     { value: 'Templates', id: 'templates', level: 3 },
+     { value: 'Thema&#39;s', id: 'themas', level: 3 },
+     { value: 'Research en Bronnen', id: 'research-en-bronnen', level: 3 },
+     { value: 'Documentatie', id: 'documentatie', level: 3 },
+     { value: 'Werk en resultaten', id: 'werk-en-resultaten', level: 2 },
+     { value: 'Voor wie is dit relevant?', id: 'voor-wie-is-dit-relevant', level: 2 },
+     { value: 'Hoe kun je bijdragen of meedoen?', id: 'hoe-kun-je-bijdragen-of-meedoen', level: 2 },
+     { value: 'Meepraten of input geven?', id: 'meepraten-of-input-geven', level: 3 },
+     { value: 'Zelf iets aandragen of checken?', id: 'zelf-iets-aandragen-of-checken', level: 3 },
+     { value: 'Vragen of idee\xebn', id: 'vragen-of-idee\xebn', level: 3 },
+    ];
+   function d(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', h3: 'h3', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: ['\n', '\n', '\n', (0, t.jsx)(n.h1, { id: 'community-sprint-mijnservices-community', children: 'Community Sprint: MijnServices Community' }), '\n', (0, t.jsx)(n.p, { children: 'Vanaf september 2024 is de tweede NL Design System Community Sprint van start gegaan. Dit keer werken teams samen aan het onderwerp MijnServices, van ontwerp en gebruikersonderzoek tot templates en implementatie.' }), '\n', (0, t.jsxs)(n.p, { children: ['Dit is een samenwerking van onder andere Ministerie van Binnenlandse Zaken en Koninkrijksrelaties, MijnOverheid van Logius, ', (0, t.jsx)(n.a, { href: 'https://vng.nl/omnichannel', children: 'VNG' }), ', Ministerie van Justitie en Veiligheid, verschillende gemeenten, NLPortal, OIP en OpenPDD.'] }), '\n', (0, t.jsx)(n.p, { children: 'Werk je aan een portaal voor een overheidsorganisatie en wil je samenwerken? Sluit je dan aan bij de MijnServices community. Deel kennis, kijk wat er al beschikbaar is voor hergebruik en werk samen aan nieuwe oplossingen.' }), '\n', (0, t.jsx)(n.h2, { id: 'waar-gaat-deze-sprint-over', children: 'Waar gaat deze sprint over?' }), '\n', (0, t.jsx)(n.p, { children: 'In deze Community Sprint ontwikkelen we aan een uitgebreide set van huisstijl-onafhankelijke templates voor Mijn Omgevingen en Formulieren die direct te gebruiken zijn voor websites en applicaties binnen de overheid.' }), '\n', (0, t.jsx)(n.h3, { id: 'templates', children: 'Templates' }), '\n', (0, t.jsx)(n.p, { children: 'Gebruik de templates van de MijnServices Community als een sterke basis voor verschillende soorten pagina\u2019s binnen je project. De templates zijn ontworpen met focus op schaalbaarheid en een intu\xeftieve gebruikerservaring. Of je nu werkt aan een pagina, een formulier of een specifieke flow, met deze templates werken we toe naar een \xe9\xe9nduidige beleving voor inwoners en ondernemers.' }), '\n', (0, t.jsx)(n.p, { children: 'Bekijk de templates:' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsxs)(n.li, { children: ['in ', (0, t.jsx)(n.a, { href: 'https://nl-design-system.github.io/mijn-services/', children: 'Storybook' }), '.'] }), '\n', (0, t.jsxs)(n.li, { children: ['in ', (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/pB5d6RlVSa1B088Xpm1sSo/2025---MijnServices---Templates--Voorheen--Overheidsbrede-portalen-', children: 'Figma' }), '.'] }), '\n'] }), '\n', (0, t.jsx)(n.h3, { id: 'themas', children: "Thema's" }), '\n', (0, t.jsx)(n.p, { children: 'Naast de templates bieden we vier thema\u2019s aan die elk een unieke visuele stijl introduceren zonder de layout of structuur van de templates te wijzigen. Elk thema biedt een onderscheidende look & feel, die gemakkelijk aan te passen is aan jouw huisstijl of projectbehoeften. Of je nu kiest voor een formele, moderne, speelse of neutrale uitstraling, de thema\u2019s passen zich visueel aan, terwijl de toegankelijkheid en gebruiksvriendelijkheid centraal blijven staan. Denk aan aanpassingen in kleurenschema\u2019s, typografie en iconografie\u2014allemaal afgestemd op de richtlijnen van het NL Design System.' }), '\n', (0, t.jsx)(n.h3, { id: 'research-en-bronnen', children: 'Research en Bronnen' }), '\n', (0, t.jsx)(n.p, { children: 'Bij het ontwerpen en ontwikkelen van deze templates is uitgebreid gebruik gemaakt van onderzoek en bronnen om ervoor te zorgen dat ze aansluiten bij de behoeften van gebruikers en voldoen aan de hoogste standaarden op het gebied van toegankelijkheid en gebruiksvriendelijkheid. Elk template wordt ondersteund door een aparte Research en Bronnen pagina, waarin de achtergrond, gebruikte methodologie\xebn en relevante bevindingen zijn gedocumenteerd. Deze pagina\u2019s bieden inzicht in de ontwerpprocessen en maken het eenvoudiger om verantwoorde keuzes te maken tijdens de ontwikkeling.' }), '\n', (0, t.jsx)(n.p, { children: 'Met deze templates kun je eenvoudig pagina\u2019s ontwikkelen die voldoen aan de standaarden van het NL Design System, terwijl je de flexibiliteit hebt om de visuele stijl aan te passen aan jouw project. De bijgevoegde Research en Bronnen pagina\u2019s helpen je om de thema\u2019s op de juiste manier te implementeren, zodat je snel en effici\xebnt kunt werken aan toegankelijke, betrouwbare digitale diensten.' }), '\n', (0, t.jsx)(n.h3, { id: 'documentatie', children: 'Documentatie' }), '\n', (0, t.jsx)(n.p, { children: 'De documentatie biedt gedetailleerde uitleg over de technische kant van de templates en hun componenten. Hierin wordt beschreven hoe je de componenten op de juiste manier implementeert in je codebase, inclusief voorbeelden en best practices. Dit helpt ontwikkelaars om effici\xebnt te werken en ervoor te zorgen dat de implementatie consistent blijft met de richtlijnen van het NL Design System.' }), '\n', (0, t.jsxs)(n.p, { children: ['We verzamelen alle inzichten rondom Mijn Omgevingen in ', (0, t.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/discussions/categories/mijn-omgevingen', children: 'GitHub Discussions' }), '. Dit is een belangrijk uitgangspunt. Vanuit de Figma en Storybook zal naar deze discussions verwezen worden.'] }), '\n', (0, t.jsx)(n.h2, { id: 'werk-en-resultaten', children: 'Werk en resultaten' }), '\n', (0, t.jsx)(n.p, { children: 'Bekijk de links en documentatie die horen bij deze Community Sprint:' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/mijn-services', children: 'MijnServices Community Repository op GitHub' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/discussions/categories/mijn-omgevingen', children: 'MijnServices Community Discussions rondom Mijn Omgevingen op GitHub' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/projects/34', children: 'MijnServices Community Sprintbord op GitHub' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/pB5d6RlVSa1B088Xpm1sSo/2025---MijnServices---Templates--Voorheen--Overheidsbrede-portalen-', children: 'NL Design System - MijnServices Community - Templates' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://nl-design-system.github.io/mijn-services/', children: 'MijnServices Community Storybook' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://gebruikersonderzoeken.nl/docs/thema/mijn-omgeving', children: 'Gebruikersonderzoeken getagd met "Mijn Omgeving" op Gebruikersonderzoeken.nl' }) }), '\n'] }), '\n', (0, t.jsx)(n.p, { children: "Bekijk hieronder de YouTube afspeellijst met de video's van de MijnServices Community." }), '\n', (0, t.jsx)(r.Y, { id: 'PLnAxFMscDU3F1VYQxlJLpmV-Z-FQ6hCU_', playlist: !0, thumbnail: 'https://raw.githubusercontent.com/nl-design-system/documentatie/refs/heads/assets/community-mijn-services-community-sprint-heartbeat-playlist.png', title: 'MijnServices Community updates' }), '\n', (0, t.jsx)(n.h2, { id: 'voor-wie-is-dit-relevant', children: 'Voor wie is dit relevant?' }), '\n', (0, t.jsx)(n.p, { children: 'Deze sprint is bedoeld voor iedereen die werkt aan portalen, Mijn Omgevingen en Formulieren, en in het bijzonder voor:' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: 'Designers die willen bijdragen aan een gedeelde visuele stijl' }), '\n', (0, t.jsx)(n.li, { children: 'Developers die componenten implementeren volgens MijnServices' }), '\n', (0, t.jsx)(n.li, { children: 'Product Owners die keuzes maken over ontwerpstandaarden' }), '\n', (0, t.jsx)(n.li, { children: 'Iedereen die bijdraagt aan een toegankelijk, samenhangend digitaal landschap' }), '\n'] }), '\n', (0, t.jsx)(n.h2, { id: 'hoe-kun-je-bijdragen-of-meedoen', children: 'Hoe kun je bijdragen of meedoen?' }), '\n', (0, t.jsx)(n.h3, { id: 'meepraten-of-input-geven', children: 'Meepraten of input geven?' }), '\n', (0, t.jsxs)(n.p, { children: ['Je kunt ons ook vinden op Slack. Meld je aan bij onze ', (0, t.jsx)(n.a, { href: 'https://praatmee.codefor.nl/', children: 'Slackgroep \u2018NL Design System (praatmee.codefor.nl)\u2019' }), ' en sluit aan bij #nl-design-system.'] }), '\n', (0, t.jsxs)(n.p, { children: ['Elke week op donderdag van 10:00 tot 11:00 kan je meedoen aan de ', (0, t.jsx)(n.a, { href: '/events/mijn-services-check-in/', children: 'MijnServices Community Check-in' }), ' huddle en de thread volgen in het #nl-design-system-designers kanaal. Deze is voor zowel developers als designers.'] }), '\n', (0, t.jsx)(n.h3, { id: 'zelf-iets-aandragen-of-checken', children: 'Zelf iets aandragen of checken?' }), '\n', (0, t.jsxs)(n.p, { children: ['Bekijk het ', (0, t.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/projects/34', children: 'GitHub sprintbord' }), ' of dien een ', (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/mijn-services/issues', children: 'GitHub issue' }), ' in.'] }), '\n', (0, t.jsx)(n.h3, { id: 'vragen-of-idee\xebn', children: 'Vragen of idee\xebn' }), '\n', (0, t.jsxs)(n.p, { children: ['Heb je vragen of idee\xebn voor de MijnServices Community? Stel je vraag ', (0, t.jsx)(n.a, { href: 'https://praatmee.codefor.nl/', children: 'via Slack' }), ' of stuur ons een mailtje op ', (0, t.jsx)('a', { href: 'mailto:kernteam@nldesignsystem.nl', children: (0, t.jsx)(n.a, { href: 'mailto:kernteam@nldesignsystem.nl', children: 'kernteam@nldesignsystem.nl' }) })] })] });
+   }
+   function u(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(d, { ...e }) }) : d(e);
+   }
+  },
+  97233: (e, n, i) => {
+   i.d(n, { Y: () => r });
+   var t = i(84263),
+    s = i(52676);
+   const r = (e) => {
+    let { id: n, title: i, ...r } = e;
+    return (0, s.jsx)(t.Z, { adNetwork: !1, announce: 'Bekijk', cookie: !1, id: n, title: i, poster: 'maxresdefault', ...r });
+   };
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => o, a: () => a });
+   var t = i(75271);
+   const s = {},
+    r = t.createContext(s);
+   function a(e) {
+    const n = t.useContext(r);
+    return t.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function o(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(s) : e.components || s) : a(e.components)), t.createElement(r.Provider, { value: n }, e.children);
+   }
+  },
+  84263: (e, n, i) => {
+   i.d(n, { Z: () => a });
+   var t = i(75271),
+    s = function () {
+     return (
+      (s =
+       Object.assign ||
+       function (e) {
+        for (var n, i = 1, t = arguments.length; i < t; i++) for (var s in (n = arguments[i])) Object.prototype.hasOwnProperty.call(n, s) && (e[s] = n[s]);
+        return e;
+       }),
+      s.apply(this, arguments)
+     );
+    },
+    r = { default: 120, mqdefault: 320, hqdefault: 480, sddefault: 640, maxresdefault: 1280 };
+   var a = t.forwardRef(function (e, n) {
+    var i = t.useState(!1),
+     a = i[0],
+     o = i[1],
+     l = t.useState(e.alwaysLoadIframe || !1),
+     m = l[0],
+     c = l[1],
+     d = encodeURIComponent(e.id),
+     u = 'string' == typeof e.playlistCoverId ? encodeURIComponent(e.playlistCoverId) : null,
+     h = e.title,
+     p = e.poster || 'hqdefault',
+     j = e.announce || 'Watch',
+     v = !e.alwaysLoadIframe || (e.autoplay && e.muted),
+     g = new URLSearchParams(s(s(s(s({}, e.muted ? { mute: '1' } : {}), v ? { autoplay: '1' } : {}), e.enableJsApi ? { enablejsapi: '1' } : {}), e.playlist ? { list: d } : {}));
+    e.params &&
+     new URLSearchParams(e.params.startsWith('&') ? e.params.slice(1) : e.params).forEach(function (e, n) {
+      g.append(n, e);
+     });
+    var y = e.noCookie;
+    y = e.cookie ? 'https://www.youtube.com' : 'https://www.youtube-nocookie.com';
+    var k = e.playlist ? ''.concat(y, '/embed/videoseries?').concat(g.toString()) : ''.concat(y, '/embed/').concat(d, '?').concat(g.toString()),
+     f = !e.thumbnail && !e.playlist && 'maxresdefault' === p,
+     b = e.webp ? 'webp' : 'jpg',
+     w = e.webp ? 'vi_webp' : 'vi',
+     x = f
+      ? (function (e, n, i, s) {
+         void 0 === s && (s = 'maxresdefault');
+         var a = (0, t.useState)(''),
+          o = a[0],
+          l = a[1];
+         return (
+          (0, t.useEffect)(
+           function () {
+            var t = 'https://img.youtube.com/'.concat(n, '/').concat(e, '/').concat(s, '.').concat(i),
+             a = 'https://img.youtube.com/'.concat(n, '/').concat(e, '/hqdefault.').concat(i),
+             o = r[s],
+             m = new Image();
+            (m.onload = function () {
+             m.width < o ? l(a) : l(t);
+            }),
+             (m.onerror = function () {
+              return l(a);
+             }),
+             (m.src = t);
+           },
+           [e],
+          ),
+          o
+         );
+        })(e.id, w, b, p)
+      : null,
+     S =
+      e.thumbnail ||
+      x ||
+      'https://i.ytimg.com/'
+       .concat(w, '/')
+       .concat(e.playlist ? u : d, '/')
+       .concat(p, '.')
+       .concat(b),
+     C = e.activatedClass || 'lyt-activated',
+     M = e.adNetwork || !1,
+     z = e.aspectHeight || 9,
+     D = e.aspectWidth || 16,
+     _ = e.iframeClass || '',
+     E = e.playerClass || 'lty-playbtn',
+     N = e.wrapperClass || 'yt-lite',
+     L = e.onIframeAdded || function () {},
+     O = e.rel ? 'prefetch' : 'preload',
+     B = e.containerElement || 'article',
+     I = e.style || {};
+    return (
+     t.useEffect(
+      function () {
+       m && L();
+      },
+      [m],
+     ),
+     t.createElement(
+      t.Fragment,
+      null,
+      t.createElement('link', { rel: O, href: S, as: 'image' }),
+      t.createElement(t.Fragment, null, a && t.createElement(t.Fragment, null, t.createElement('link', { rel: 'preconnect', href: y }), t.createElement('link', { rel: 'preconnect', href: 'https://www.google.com' }), M && t.createElement(t.Fragment, null, t.createElement('link', { rel: 'preconnect', href: 'https://static.doubleclick.net' }), t.createElement('link', { rel: 'preconnect', href: 'https://googleads.g.doubleclick.net' })))),
+      t.createElement(
+       B,
+       {
+        onPointerOver: function () {
+         a || o(!0);
+        },
+        onClick: function () {
+         m || c(!0);
+        },
+        className: ''.concat(N, ' ').concat(m ? C : ''),
+        'data-title': h,
+        style: s(s({ backgroundImage: 'url('.concat(S, ')') }, { '--aspect-ratio': ''.concat((z / D) * 100, '%') }), I),
+       },
+       t.createElement('button', { type: 'button', className: E, 'aria-label': ''.concat(j, ' ').concat(h) }),
+       m && t.createElement('iframe', { ref: n, className: _, title: h, width: '560', height: '315', frameBorder: '0', allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture', allowFullScreen: !0, src: k }),
+      ),
+     )
+    );
+   });
+  },
+ },
+]);
