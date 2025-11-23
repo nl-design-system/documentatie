606201669c2ff42ce15d213da032ed770c167f00
Johan Huijkman <johan@q42.nl>
2025-11-23 16:08:13 +0100

    Vorige, volgende stap toegevoegd

--- src/components/AltTextHelper/ContextTypeHelper.tsx
+++ src/components/AltTextHelper/ContextTypeHelper.tsx
@@ -6,6 +6,7 @@ import {
   FormField,
   FormLabel,
   RadioButton,
+  Button,
 } from '@utrecht/component-library-react/dist/css-module';
 
 const groupName = 'context-type';
@@ -14,83 +15,88 @@ const groupName = 'context-type';
 
 interface ImageTypeHelperProps {
   onChangeOption;
+  onPrevStep;
+  onNextStep;
+  active;
 }
 
-export default function ContextTypeHelper({ onChangeOption }: ImageTypeHelperProps) {
+export default function ContextTypeHelper({ onChangeOption, onPrevStep, onNextStep, active }: ImageTypeHelperProps) {
   const [selectedOption, setSelectedOption] = useState('');
 
-  return (
-    <>
-      <Fieldset
-        id="context-type-group"
-        aria-describedby="image-type-group-description"
-        role="radiogroup"
-        // ref={autoFocus}
-        // tabIndex={-1}
-      >
-        <FieldsetLegend>Wat voor soort afbeelding is het?</FieldsetLegend>
-        <FormFieldDescription id="context-type-group-description">
-          Kies de optie die het beste past:
-        </FormFieldDescription>
-        <FormField type="radio">
-          <FormLabel
-            className="utrecht-form-field__label utrecht-form-field__label--radio"
-            htmlFor="image-complex"
-            type="radio"
-          >
-            <RadioButton
-              className="utrecht-form-field__input"
-              id="image-complex"
-              name={groupName}
-              value="image-complex"
-              defaultChecked={selectedOption === 'image-complex'}
-              onChange={(e) => {
-                setSelectedOption(e.target.value);
-                onChangeOption('complex', true);
-              }}
-            />
-            <div>
-              <span className="bold-text">Het is een grafiek, diagram of een andere complexe afbeelding.</span>
-            </div>
-          </FormLabel>
-        </FormField>
-        <FormField type="radio">
-          <FormLabel
-            className="utrecht-form-field__label utrecht-form-field__label--radio"
-            htmlFor="image-simple"
-            type="radio"
-          >
-            <RadioButton
-              className="utrecht-form-field__input"
-              id="image-simple"
-              name={groupName}
-              value="image-simple"
-              defaultChecked={selectedOption === 'image-simple'}
-              onChange={(e) => {
-                setSelectedOption(e.target.value);
-                onChangeOption('complex', false);
-              }}
-            />
-            <div>
-              <span className="bold-text">Het is een foto of een eenvoudige illustratie.</span>
-            </div>
-          </FormLabel>
-        </FormField>
+  if (active === groupName) {
+    return (
+      <>
+        <Fieldset
+          id="context-type-group"
+          aria-describedby="image-type-group-description"
+          role="radiogroup"
+          // ref={autoFocus}
+          // tabIndex={-1}
+        >
+          <FieldsetLegend>Wat voor soort afbeelding is het?</FieldsetLegend>
+          <FormFieldDescription id="context-type-group-description">
+            Kies de optie die het beste past:
+          </FormFieldDescription>
+          <FormField type="radio">
+            <FormLabel
+              className="utrecht-form-field__label utrecht-form-field__label--radio"
+              htmlFor="image-complex"
+              type="radio"
+            >
+              <RadioButton
+                className="utrecht-form-field__input"
+                id="image-complex"
+                name={groupName}
+                value="image-complex"
+                defaultChecked={selectedOption === 'image-complex'}
+                onChange={(e) => {
+                  setSelectedOption(e.target.value);
+                  onChangeOption('complex', true);
+                }}
+              />
+              <div>
+                <span className="bold-text">Het is een grafiek, diagram of een andere complexe afbeelding.</span>
+              </div>
+            </FormLabel>
+          </FormField>
+          <FormField type="radio">
+            <FormLabel
+              className="utrecht-form-field__label utrecht-form-field__label--radio"
+              htmlFor="image-simple"
+              type="radio"
+            >
+              <RadioButton
+                className="utrecht-form-field__input"
+                id="image-simple"
+                name={groupName}
+                value="image-simple"
+                defaultChecked={selectedOption === 'image-simple'}
+                onChange={(e) => {
+                  setSelectedOption(e.target.value);
+                  onChangeOption('complex', false);
+                }}
+              />
+              <div>
+                <span className="bold-text">Het is een foto of een eenvoudige illustratie.</span>
+              </div>
+            </FormLabel>
+          </FormField>
 
-        {/* <div className="nlds-button-bar">
-          <Button appearance="secondary-action-button" onClick={prevStep}>
-            Vorige stap
-          </Button>
+          <div className="nlds-button-bar">
+            <Button appearance="secondary-action-button" onClick={() => onPrevStep(groupName)}>
+              Vorige stap
+            </Button>
 
-          <Button
-            appearance="primary-action-button"
-            disabled={selectedOption === '' ? true : false}
-            onClick={nextStep}
-          >
-            Toon advies
-          </Button>
-        </div> */}
-      </Fieldset>
-    </>
-  );
+            <Button
+              appearance="primary-action-button"
+              disabled={selectedOption === '' ? true : false}
+              onClick={() => onNextStep(groupName)}
+            >
+              Volgende stap
+            </Button>
+          </div>
+        </Fieldset>
+      </>
+    );
+  }
 }
