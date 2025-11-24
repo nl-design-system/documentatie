9849050584d7ea462ade6d071c345a592473eefb
Johan Huijkman <johan@q42.nl>
2025-11-24 12:11:00 +0100

    helpteksten

--- src/components/AltTextHelper/HelperText.tsx
+++ src/components/AltTextHelper/HelperText.tsx
@@ -1,4 +1,4 @@
-import { Heading2, Button } from '@utrecht/component-library-react/dist/css-module';
+import { Heading2, Button, Paragraph } from '@utrecht/component-library-react/dist/css-module';
 
 const groupName = 'helper-text';
 
@@ -7,14 +7,37 @@ const autoFocus = (element: HTMLElement | null) => element?.focus();
 interface HelperTextProps {
   onPrevStep;
   active;
+  image;
 }
 
-export default function HelperText({ onPrevStep, active }: HelperTextProps) {
+export default function HelperText({ onPrevStep, active, image }: HelperTextProps) {
+  // const renderHelpers = () => {
+  //   return Object.entries(image).map(([key, value], i) => {
+  //     return (
+  //       <div>
+  //         {key},{value}
+  //       </div>
+  //     )
+  //   })
+  // }
+
+  // const imblaage = {
+  //   decorative: false,
+  //   complex: false,
+  //   text: false,
+  //   clickable: false,
+  //   content: '',
+  // };
+
   if (active === groupName) {
     return (
       <>
         <div className="nlds-helper-text" ref={autoFocus} tabIndex={-1}>
           <Heading2>Help</Heading2>
+          <ImageDecorativeHelper show={image.decorative} />
+          <ImageComplexHelper show={image.complex} />
+          <ImageTextHelper show={image.text} content={image.content} />
+          <ImageClickableHelper show={image.clickable} />
         </div>
         <div className="nlds-button-bar">
           <Button appearance="secondary-action-button" onClick={() => onPrevStep(groupName)}>
@@ -23,5 +46,51 @@ export default function HelperText({ onPrevStep, active }: HelperTextProps) {
         </div>
       </>
     );
+  }
+}
+
+interface ImageDecorativeHelperProps {
+  show;
+}
+
+export function ImageDecorativeHelper({ show }: ImageDecorativeHelperProps) {
+  if (show) {
+    return <Paragraph>Decoratief</Paragraph>;
+  }
+}
+
+interface ImageComplexHelperProps {
+  show;
+}
+
+export function ImageComplexHelper({ show }: ImageComplexHelperProps) {
+  if (show) {
+    return <Paragraph>Complex</Paragraph>;
+  }
+}
+
+interface ImageTextHelperProps {
+  show;
+  content;
+}
+
+export function ImageTextHelper({ show, content }: ImageTextHelperProps) {
+  if (show) {
+    return (
+      <>
+        <Paragraph>Tekst</Paragraph>
+        <Paragraph>{content}</Paragraph>
+      </>
+    );
+  }
+}
+
+interface ImageClickableHelperProps {
+  show;
+}
+
+export function ImageClickableHelper({ show }: ImageClickableHelperProps) {
+  if (show) {
+    return <Paragraph>Klikbaar</Paragraph>;
   }
 }
