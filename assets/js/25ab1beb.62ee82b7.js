8f50ee389d2ccd961d9d793f1881879a94d92a6b
Rozerinay <84851545+Rozerinay@users.noreply.github.com>
2025-12-08 09:12:23 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@0bec8bb2854252368b0a20f17a58a80ecbc95950 🚀

---
+++ assets/js/25ab1beb.62ee82b7.js
@@ -0,0 +1,181 @@
+/*! For license information please see 25ab1beb.62ee82b7.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [95081],
+ {
+  46695: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => m, contentTitle: () => a, default: () => c, frontMatter: () => o, metadata: () => l, toc: () => d });
+   var t = i(52676),
+    s = i(40139),
+    r = i(97233);
+   const o = { title: 'Rijkshuisstijl Community \xb7 Community Sprints \xb7 Community', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Rijkshuisstijl Community', pagination_label: 'Rijkshuisstijl Community', description: 'Lees meer over de NL Design System Rijkshuisstijl Community Sprint.', keywords: ['nl design system', 'community sprints', 'rijkshuisstijl community'] },
+    a = 'Community Sprint: Rijkshuisstijl Community',
+    l = { id: 'community/community-sprints/rijkshuisstijl-community/rijkshuisstijl-community', title: 'Rijkshuisstijl Community \xb7 Community Sprints \xb7 Community', description: 'Lees meer over de NL Design System Rijkshuisstijl Community Sprint.', source: '@site/docs/community/community-sprints/rijkshuisstijl-community/rijkshuisstijl-community.mdx', sourceDirName: 'community/community-sprints/rijkshuisstijl-community', slug: '/community/community-sprints/rijkshuisstijl-community/', permalink: '/community/community-sprints/rijkshuisstijl-community/', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/community-sprints/rijkshuisstijl-community/rijkshuisstijl-community.mdx', tags: [], version: 'current', frontMatter: { title: 'Rijkshuisstijl Community \xb7 Community Sprints \xb7 Community', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Rijkshuisstijl Community', pagination_label: 'Rijkshuisstijl Community', description: 'Lees meer over de NL Design System Rijkshuisstijl Community Sprint.', keywords: ['nl design system', 'community sprints', 'rijkshuisstijl community'] }, sidebar: 'community', previous: { title: 'Rijkshuisstijl Community', permalink: '/community/community-sprints/rijkshuisstijl-community/overzicht' }, next: { title: 'Aanmelden', permalink: '/community/community-sprints/rijkshuisstijl-community/aanmelden' } },
+    m = {},
+    d = [
+     { value: 'Waar gaat deze sprint over?', id: 'waar-gaat-deze-sprint-over', level: 2 },
+     { value: 'Denk mee over designkeuzes binnen de Rijkshuisstijl', id: 'denk-mee-over-designkeuzes-binnen-de-rijkshuisstijl', level: 2 },
+     { value: 'Werk en resultaten', id: 'werk-en-resultaten', level: 2 },
+     { value: 'Voor wie is dit relevant?', id: 'voor-wie-is-dit-relevant', level: 2 },
+     { value: 'Hoe kun je bijdragen of meedoen?', id: 'hoe-kun-je-bijdragen-of-meedoen', level: 2 },
+     { value: 'Meepraten of input geven?', id: 'meepraten-of-input-geven', level: 3 },
+     { value: 'Meedoen in Figma?', id: 'meedoen-in-figma', level: 3 },
+     { value: 'Zelf iets aandragen of checken?', id: 'zelf-iets-aandragen-of-checken', level: 3 },
+     { value: 'Vragen of idee\xebn', id: 'vragen-of-idee\xebn', level: 3 },
+    ];
+   function u(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', h3: 'h3', li: 'li', p: 'p', ul: 'ul', ...(0, s.a)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: ['\n', '\n', '\n', (0, t.jsx)(n.h1, { id: 'community-sprint-rijkshuisstijl-community', children: 'Community Sprint: Rijkshuisstijl Community' }), '\n', (0, t.jsxs)(n.p, { children: ['In de zomer van 2024 gingen de eerste Community Sprints van start voor teams die werken aan webapplicaties en websites binnen de Rijkshuisstijl. Deze sprints werden afgetrapt door een kernteam van 7 developers voor de zomer beschikbaar gesteld door ', (0, t.jsx)(n.a, { href: 'https://quintor.nl', children: 'Quintor' }), ', en vormden het begin van een langdurige samenwerking tussen onder andere ', (0, t.jsx)(n.a, { href: 'https://logius.nl', children: 'Logius' }), ', Ministerie van Justitie en Veiligheid, en Rijksinstituut voor Volksgezondheid en Milieu (RIVM).'] }), '\n', (0, t.jsx)(n.h2, { id: 'waar-gaat-deze-sprint-over', children: 'Waar gaat deze sprint over?' }), '\n', (0, t.jsx)(n.p, { children: 'De Rijkshuisstijl Community Sprint maakt deel uit van een samenwerkingsverband om NL Design System-componenten te ontwikkelen voor projecten die moeten voldoen aan de Rijkshuisstijl. Organisaties van de centrale overheid van Nederland (bijvoorbeeld: Belastingdienst, DUO, Logius, SVB) en degenen die door hen zijn ingehuurd voor het ontwikkelen van websites en apps, kunnen via dit project samenwerken.' }), '\n', (0, t.jsx)(n.h2, { id: 'denk-mee-over-designkeuzes-binnen-de-rijkshuisstijl', children: 'Denk mee over designkeuzes binnen de Rijkshuisstijl' }), '\n', (0, t.jsx)(n.p, { children: 'Binnen de Rijkshuisstijl Community werken we toe naar \xe9\xe9n centrale JSON met design tokens. Daarmee kunnen componenten uit het NL Design System eenvoudig worden gestyled volgens de verschillende Rijkshuisstijl-deelidentiteiten.' }), '\n', (0, t.jsx)(n.p, { children: 'Tijdens dit proces maken we diverse designkeuzes. Om deze keuzes inzichtelijk \xe9n bespreekbaar te maken, zijn we gestart met een aantal GitHub Discussions. Iedereen die werkt met de Rijkshuisstijl of hieraan bijdraagt wordt uitgenodigd om mee te denken.' }), '\n', (0, t.jsx)(n.p, { children: 'Ook als je het eens bent met de voorgestelde keuzes, is een korte bevestiging al waardevol. Heb je zelf al keuzes gemaakt bij het stylen van componenten? Deel dan je overwegingen in de betreffende discussie. Zo brengen we alle kennis en argumentatie op \xe9\xe9n centrale plek samen.' }), '\n', (0, t.jsxs)(n.p, { children: ['Ga naar de ', (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/rijkshuisstijl-community/discussions', children: 'Rijkshuisstijl Community Discussions rondom styling keuzes op GitHub' }), '.'] }), '\n', (0, t.jsx)(n.h2, { id: 'werk-en-resultaten', children: 'Werk en resultaten' }), '\n', (0, t.jsx)(n.p, { children: 'Bekijk de links en documentatie die horen bij deze Community Sprint:' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/rijkshuisstijl-community', children: 'Rijkshuisstijl Community Repository op GitHub' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/projects/59', children: 'Rijkshuisstijl Community Sprintbord op GitHub' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/rijkshuisstijl-community/discussions', children: 'Rijkshuisstijl Community Discussions rondom styling keuzes op GitHub' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/Q5Imc7Xi9KnBQhcYI3Hytj/NL-Design-System---Bibliotheek---Rijkshuisstijl-Community', children: 'NL Design System - Bibliotheek - Rijkshuisstijl Community in Figma' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/design/H4hSqpPbvFMLklDZgswwgd/NL-Design-System---Templates---Rijkshuisstijl', children: 'NL Design System - Templates - Rijkshuisstijl Community in Figma' }) }), '\n', (0, t.jsxs)(n.li, { children: [(0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/rijkshuisstijl-community/blob/main/proprietary/design-tokens/figma/figma.tokens.json', children: 'Design tokens' }), ' (in te laden in Tokens Studio)'] }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://rijkshuisstijl-community.vercel.app/', children: 'Rijkshuisstijl Community Storybook met componenten en templates' }) }), '\n', (0, t.jsx)(n.li, { children: (0, t.jsx)(n.a, { href: 'https://www.rijkshuisstijl-community.nl/', children: 'Rijkshuisstijl Community Website' }) }), '\n'] }), '\n', (0, t.jsx)(n.p, { children: "Bekijk hieronder de YouTube afspeellijst met de video's van de Rijkshuisstijl Community." }), '\n', (0, t.jsx)(r.Y, { id: 'PLnAxFMscDU3FHVDYR9WvX3d84JdQ5Q3tJ', playlist: !0, thumbnail: 'https://raw.githubusercontent.com/nl-design-system/documentatie/refs/heads/assets/community-rijkshuisstijl-community-sprint-heartbeat-playlist.png', title: 'Rijkshuisstijl Community updates' }), '\n', (0, t.jsx)(n.h2, { id: 'voor-wie-is-dit-relevant', children: 'Voor wie is dit relevant?' }), '\n', (0, t.jsx)(n.p, { children: 'Deze sprint is bedoeld voor iedereen die werkt aan Rijksoverheidswebsites, en in het bijzonder voor:' }), '\n', (0, t.jsxs)(n.ul, { children: ['\n', (0, t.jsx)(n.li, { children: 'Designers die willen bijdragen aan een gedeelde visuele stijl' }), '\n', (0, t.jsx)(n.li, { children: 'Developers die componenten implementeren volgens Rijkshuisstijl' }), '\n', (0, t.jsx)(n.li, { children: 'Product Owners die keuzes maken over ontwerpstandaarden' }), '\n', (0, t.jsx)(n.li, { children: 'Iedereen die bijdraagt aan een toegankelijk, samenhangend digitaal landschap' }), '\n'] }), '\n', (0, t.jsx)(n.h2, { id: 'hoe-kun-je-bijdragen-of-meedoen', children: 'Hoe kun je bijdragen of meedoen?' }), '\n', (0, t.jsx)(n.h3, { id: 'meepraten-of-input-geven', children: 'Meepraten of input geven?' }), '\n', (0, t.jsxs)(n.p, { children: ['Je kunt ons ook vinden op Slack. Meld je aan bij onze ', (0, t.jsx)(n.a, { href: 'https://praatmee.codefor.nl/', children: 'Slackgroep \u2018NL Design System (praatmee.codefor.nl)\u2019' }), ' en sluit aan bij #nl-design-system.'] }), '\n', (0, t.jsxs)(n.p, { children: ['Ook kan je gedurende elke sprint de Rijkshuisstijl Community volgen en deelnemen aan de slowchat in het ', (0, t.jsx)(n.a, { href: 'https://codefornl.slack.com/archives/C05AQK9R41G', children: '#rijkshuisstijl-community' }), '-kanaal.'] }), '\n', (0, t.jsxs)(n.p, { children: ['Daarnaast organiseren we elke oneven week op dinsdag om 16:00 uur een ', (0, t.jsx)(n.a, { href: '/events/rijkshuisstijl-community-open-hour/#over-de-rijkshuisstijl-community', children: 'Rijkshuisstijl Community Open Hour' }), '.'] }), '\n', (0, t.jsx)(n.h3, { id: 'meedoen-in-figma', children: 'Meedoen in Figma?' }), '\n', (0, t.jsxs)(n.p, { children: ['Rijkshuisstijl Community gebruikt in Figma ', (0, t.jsx)(n.a, { href: '/handboek/designer/figma-structuur/#wat-zijn-connected-projects/', children: 'Connected Projects' }), ', een experimentele manier van samenwerken. Hierdoor kunnen ontwerpers van verschillende organisaties, zoals Logius en RVO, eenvoudig de nieuwste versie van componenten, iconen en elementen uit de Rijkshuisstijl bibliotheken gebruiken, zonder handmatig te hoeven updaten.'] }), '\n', (0, t.jsx)(n.p, { children: (0, t.jsx)(n.a, { href: 'https://www.figma.com/files/1448979839456618655/team/1547145874815175838', children: 'Bekijk Rijkshuisstijl Community Connected Project' }) }), '\n', (0, t.jsxs)(n.p, { children: ['Werk je als organisatie met de Rijkshuisstijl en wil je bijdragen aan de ontwikkeling van componenten? Dat kan! Laat een bericht achter in het ', (0, t.jsx)(n.a, { href: 'https://codefornl.slack.com/archives/C05AQK9R41G', children: '#rijkshuisstijl-community' }), '-kanaal in Slack, dan helpen we je op weg.'] }), '\n', (0, t.jsx)(n.h3, { id: 'zelf-iets-aandragen-of-checken', children: 'Zelf iets aandragen of checken?' }), '\n', (0, t.jsxs)(n.p, { children: ['Bekijk het ', (0, t.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/projects/59', children: 'GitHub sprintbord' }), ', dien een ', (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/rijkshuisstijl-community/issues', children: 'GitHub issue' }), ' in, of breng je onderwerp in tijdens de ', (0, t.jsx)(n.a, { href: '/events/rijkshuisstijl-community-open-hour/#over-de-rijkshuisstijl-community', children: 'Rijkshuisstijl Community Open Hour' }), '.'] }), '\n', (0, t.jsx)(n.h3, { id: 'vragen-of-idee\xebn', children: 'Vragen of idee\xebn' }), '\n', (0, t.jsxs)(n.p, { children: ['Heb je vragen of idee\xebn voor de Rijkshuisstijl Community? Stel je vraag ', (0, t.jsx)(n.a, { href: 'https://praatmee.codefor.nl/', children: 'via Slack' }), ' of stuur ons een mailtje op ', (0, t.jsx)('a', { href: 'mailto:kernteam@nldesignsystem.nl', children: (0, t.jsx)(n.a, { href: 'mailto:kernteam@nldesignsystem.nl', children: 'kernteam@nldesignsystem.nl' }) })] })] });
+   }
+   function c(e = {}) {
+    const { wrapper: n } = { ...(0, s.a)(), ...e.components };
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(u, { ...e }) }) : u(e);
+   }
+  },
+  97233: (e, n, i) => {
+   i.d(n, { Y: () => r });
+   var t = i(84263),
+    s = i(52676);
+   const r = (e) => {
+    let { id: n, title: i, ...r } = e;
+    return (0, s.jsx)(t.Z, { adNetwork: !1, announce: 'Bekijk', cookie: !1, id: n, title: i, poster: 'maxresdefault', ...r });
+   };
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => a, a: () => o });
+   var t = i(75271);
+   const s = {},
+    r = t.createContext(s);
+   function o(e) {
+    const n = t.useContext(r);
+    return t.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function a(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(s) : e.components || s) : o(e.components)), t.createElement(r.Provider, { value: n }, e.children);
+   }
+  },
+  84263: (e, n, i) => {
+   i.d(n, { Z: () => o });
+   var t = i(75271),
+    s = function () {
+     return (
+      (s =
+       Object.assign ||
+       function (e) {
+        for (var n, i = 1, t = arguments.length; i < t; i++) for (var s in (n = arguments[i])) Object.prototype.hasOwnProperty.call(n, s) && (e[s] = n[s]);
+        return e;
+       }),
+      s.apply(this, arguments)
+     );
+    },
+    r = { default: 120, mqdefault: 320, hqdefault: 480, sddefault: 640, maxresdefault: 1280 };
+   var o = t.forwardRef(function (e, n) {
+    var i = t.useState(!1),
+     o = i[0],
+     a = i[1],
+     l = t.useState(e.alwaysLoadIframe || !1),
+     m = l[0],
+     d = l[1],
+     u = encodeURIComponent(e.id),
+     c = 'string' == typeof e.playlistCoverId ? encodeURIComponent(e.playlistCoverId) : null,
+     h = e.title,
+     j = e.poster || 'hqdefault',
+     p = e.announce || 'Watch',
+     k = !e.alwaysLoadIframe || (e.autoplay && e.muted),
+     g = new URLSearchParams(s(s(s(s({}, e.muted ? { mute: '1' } : {}), k ? { autoplay: '1' } : {}), e.enableJsApi ? { enablejsapi: '1' } : {}), e.playlist ? { list: u } : {}));
+    e.params &&
+     new URLSearchParams(e.params.startsWith('&') ? e.params.slice(1) : e.params).forEach(function (e, n) {
+      g.append(n, e);
+     });
+    var y = e.noCookie;
+    y = e.cookie ? 'https://www.youtube.com' : 'https://www.youtube-nocookie.com';
+    var v = e.playlist ? ''.concat(y, '/embed/videoseries?').concat(g.toString()) : ''.concat(y, '/embed/').concat(u, '?').concat(g.toString()),
+     f = !e.thumbnail && !e.playlist && 'maxresdefault' === j,
+     b = e.webp ? 'webp' : 'jpg',
+     w = e.webp ? 'vi_webp' : 'vi',
+     x = f
+      ? (function (e, n, i, s) {
+         void 0 === s && (s = 'maxresdefault');
+         var o = (0, t.useState)(''),
+          a = o[0],
+          l = o[1];
+         return (
+          (0, t.useEffect)(
+           function () {
+            var t = 'https://img.youtube.com/'.concat(n, '/').concat(e, '/').concat(s, '.').concat(i),
+             o = 'https://img.youtube.com/'.concat(n, '/').concat(e, '/hqdefault.').concat(i),
+             a = r[s],
+             m = new Image();
+            (m.onload = function () {
+             m.width < a ? l(o) : l(t);
+            }),
+             (m.onerror = function () {
+              return l(o);
+             }),
+             (m.src = t);
+           },
+           [e],
+          ),
+          a
+         );
+        })(e.id, w, b, j)
+      : null,
+     C =
+      e.thumbnail ||
+      x ||
+      'https://i.ytimg.com/'
+       .concat(w, '/')
+       .concat(e.playlist ? c : u, '/')
+       .concat(j, '.')
+       .concat(b),
+     R = e.activatedClass || 'lyt-activated',
+     S = e.adNetwork || !1,
+     z = e.aspectHeight || 9,
+     D = e.aspectWidth || 16,
+     L = e.iframeClass || '',
+     _ = e.playerClass || 'lty-playbtn',
+     H = e.wrapperClass || 'yt-lite',
+     N = e.onIframeAdded || function () {},
+     E = e.rel ? 'prefetch' : 'preload',
+     I = e.containerElement || 'article',
+     F = e.style || {};
+    return (
+     t.useEffect(
+      function () {
+       m && N();
+      },
+      [m],
+     ),
+     t.createElement(
+      t.Fragment,
+      null,
+      t.createElement('link', { rel: E, href: C, as: 'image' }),
+      t.createElement(t.Fragment, null, o && t.createElement(t.Fragment, null, t.createElement('link', { rel: 'preconnect', href: y }), t.createElement('link', { rel: 'preconnect', href: 'https://www.google.com' }), S && t.createElement(t.Fragment, null, t.createElement('link', { rel: 'preconnect', href: 'https://static.doubleclick.net' }), t.createElement('link', { rel: 'preconnect', href: 'https://googleads.g.doubleclick.net' })))),
+      t.createElement(
+       I,
+       {
+        onPointerOver: function () {
+         o || a(!0);
+        },
+        onClick: function () {
+         m || d(!0);
+        },
+        className: ''.concat(H, ' ').concat(m ? R : ''),
+        'data-title': h,
+        style: s(s({ backgroundImage: 'url('.concat(C, ')') }, { '--aspect-ratio': ''.concat((z / D) * 100, '%') }), F),
+       },
+       t.createElement('button', { type: 'button', className: _, 'aria-label': ''.concat(p, ' ').concat(h) }),
+       m && t.createElement('iframe', { ref: n, className: L, title: h, width: '560', height: '315', frameBorder: '0', allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture', allowFullScreen: !0, src: v }),
+      ),
+     )
+    );
+   });
+  },
+ },
+]);
