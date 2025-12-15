e37288ab2e9a6d89ac52d039ed886250503eafa3
eslook <42849307+eslook@users.noreply.github.com>
2025-12-15 18:28:30 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@b5fc045049dfa53c28ee5fdf0453c6f54f211904 🚀

---
+++ assets/js/5fe3c528.916ac53a.js
@@ -0,0 +1,75 @@
+/*! For license information please see 5fe3c528.916ac53a.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [27605],
+ {
+  49315: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => u, contentTitle: () => d, default: () => c, frontMatter: () => a, metadata: () => m, toc: () => j });
+   var s = i(52676),
+    t = i(40139),
+    r = i(68257),
+    o = i(78511),
+    l = i(37943);
+   const a = { title: 'Rijkshuisstijl Community Refinement \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Rijkshuisstijl Community Refinement', pagination_label: 'Tijdens de Rijkshuisstijl Community Refinement helpen we deelnemers om hun eigen issues te verfijnen en bestaande issues te verbeteren.', description: 'Tijdens de Rijkshuisstijl Community Refinement helpen we deelnemers om hun eigen issues te verfijnen en bestaande issues te verbeteren.', slug: '/events/rijkshuisstijl-community-refinement', keywords: ['nl design system', 'community sprints', 'rijkshuisstijl community', 'rijkshuisstijl community refinement'] },
+    d = 'Rijkshuisstijl Community Refinement',
+    m = { id: 'community/events/rijkshuisstijl-community-refinement', title: 'Rijkshuisstijl Community Refinement \xb7 Bijeenkomsten', description: 'Tijdens de Rijkshuisstijl Community Refinement helpen we deelnemers om hun eigen issues te verfijnen en bestaande issues te verbeteren.', source: '@site/docs/community/events/rijkshuisstijl-community-refinement.mdx', sourceDirName: 'community/events', slug: '/events/rijkshuisstijl-community-refinement', permalink: '/events/rijkshuisstijl-community-refinement', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/rijkshuisstijl-community-refinement.mdx', tags: [], version: 'current', frontMatter: { title: 'Rijkshuisstijl Community Refinement \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Rijkshuisstijl Community Refinement', pagination_label: 'Tijdens de Rijkshuisstijl Community Refinement helpen we deelnemers om hun eigen issues te verfijnen en bestaande issues te verbeteren.', description: 'Tijdens de Rijkshuisstijl Community Refinement helpen we deelnemers om hun eigen issues te verfijnen en bestaande issues te verbeteren.', slug: '/events/rijkshuisstijl-community-refinement', keywords: ['nl design system', 'community sprints', 'rijkshuisstijl community', 'rijkshuisstijl community refinement'] }, sidebar: 'community', previous: { title: 'Tijdens de Rijkshuisstijl Community Open Hour wisselen verschillende organisaties informatie, inzichten en tips uit over de Rijkshuisstijl.', permalink: '/events/rijkshuisstijl-community-open-hour' }, next: { title: 'Tijdens de Rijkshuisstijl Community Sprint planning zijn laagdrempelige online bijeenkomsten waar designers en developers van verschillende overheidsorganisaties samen de nieuwe sprint inplannen.', permalink: '/events/rijkshuisstijl-community-sprint-planning' } },
+    u = {},
+    j = [
+     { value: 'Wat doen we?', id: 'wat-doen-we', level: 2 },
+     { value: 'Ben je erbij?', id: 'ben-je-erbij', level: 2 },
+     { value: 'Over de Rijkshuisstijl Community', id: 'over-de-rijkshuisstijl-community', level: 2 },
+     { value: 'Vragen of idee\xebn?', id: 'vragen-of-idee\xebn', level: 2 },
+    ];
+   function h(e) {
+    const n = { a: 'a', h1: 'h1', h2: 'h2', li: 'li', p: 'p', strong: 'strong', ul: 'ul', ...(0, t.a)(), ...e.components };
+    return (0, s.jsxs)(s.Fragment, { children: [(0, s.jsx)(n.h1, { id: 'rijkshuisstijl-community-refinement', children: 'Rijkshuisstijl Community Refinement' }), '\n', (0, s.jsx)(n.p, { children: 'De Rijkshuisstijl Community Refinement is voor iedereen die hulp nodig heeft met issues refinen of wilt helpen met de backlog issues te verfijnen. Dit is een moment om samen met designers en developers issues te bespreken, te prioriteren en te voorzien van de juiste details zodat ze gemakkelijk opgepakt kunnen worden door de community.' }), '\n', (0, s.jsx)(n.h2, { id: 'wat-doen-we', children: 'Wat doen we?' }), '\n', (0, s.jsx)(n.p, { children: 'Tijdens de refinements helpen we deelnemers om hun eigen issues te verfijnen en bestaande issues te verbeteren. Dit sluit aan op onze backlog, waarin alleen issues staan die we hebben beoordeeld als duidelijk, volledig en klaar om door iedereen uit de community opgepakt te worden zonder extra toelichting.' }), '\n', (0, s.jsxs)(n.p, { children: [(0, s.jsx)(n.strong, { children: '1. Eigen issue verfijnen.' }), ' Deelnemers kunnen een zelf aangemaakt issue inbrengen om samen te verbeteren.'] }), '\n', (0, s.jsxs)(n.p, { children: [(0, s.jsx)(n.strong, { children: '2. Bestaande issues verbeteren.' }), ' We bekijken daarnaast een selectie van bestaande issues om te beoordelen of:'] }), '\n', (0, s.jsxs)(n.ul, { children: ['\n', (0, s.jsx)(n.li, { children: 'de beschrijving duidelijk genoeg is;' }), '\n', (0, s.jsx)(n.li, { children: 'aanvullende informatie nodig is;' }), '\n', (0, s.jsx)(n.li, { children: 'open vragen kunnen worden beantwoord;' }), '\n', (0, s.jsx)(n.li, { children: 'het issue klaarstaat om door anderen opgepakt te worden.' }), '\n'] }), '\n', (0, s.jsxs)(n.p, { children: [(0, s.jsx)(n.strong, { children: 'Het doel van beide sporen' }), ' is om issues duidelijker en concreter te maken, zodat ze volledig, begrijpelijk en direct uitvoerbaar zijn voor iedereen in de community.'] }), '\n', (0, s.jsx)(n.p, { children: 'Ook is het mogelijk dat men opsplitst in kleinere groepen om specifieke issues te bespreken, wanneer dit niet relevant is voor de gehele groep.' }), '\n', (0, s.jsx)(n.p, { children: 'Iedereen die betrokken is bij de ontwikkeling van de Rijkshuisstijl Community is welkom, of je nu designer, developer, onderzoeker, tester of afnemer bent.' }), '\n', (0, s.jsx)(n.h2, { id: 'ben-je-erbij', children: 'Ben je erbij?' }), '\n', (0, s.jsx)(n.p, { children: 'Iedereen kan aansluiten! Regelmatig of af en toe, wanneer je vragen hebt of iets wilt bespreken.' }), '\n', (0, s.jsxs)(n.p, { children: ['De Rijkshuisstijl Community Refinement vindt plaats in de even weken, iedere donderdag om 14:00 uur, in het ', (0, s.jsx)(n.a, { href: 'https://codefornl.slack.com/archives/C05AQK9R41G', children: '#rijkshuisstijl-community' }), '-kanaal op de ', (0, s.jsx)(n.a, { href: '/slack/', children: 'Slack van Code for NL' }), '. Tijdens de sessies gebruiken we de Huddle-functie. In hetzelfde kanaal vind je ook de threads van eerdere bijeenkomsten.'] }), '\n', (0, s.jsx)(n.p, { children: 'Aanmelden is niet verplicht, maar door je aan te melden voor de Rijkshuisstijl Community kunnen we je op de hoogte houden van alle bijeenkomsten, waaronder de Rijkshuisstijl Community Refinement. Je ontvangt dan ook een kalenderbestand met alle bijeenkomsten, zodat je deze eenvoudig kunt toevoegen aan je agenda.' }), '\n', (0, s.jsxs)(l.hE, { children: [(0, s.jsxs)(r.Z, { href: '/community/community-sprints/rijkshuisstijl-community/aanmelden', appearance: 'primary-action', children: ['Meld je aan', (0, s.jsx)(o.Z, {})] }), (0, s.jsxs)(r.Z, { href: '/slack/', appearance: 'secondary-action', children: ['Doe mee op Slack', (0, s.jsx)(o.Z, {})] })] }), '\n', (0, s.jsx)(n.h2, { id: 'over-de-rijkshuisstijl-community', children: 'Over de Rijkshuisstijl Community' }), '\n', (0, s.jsx)(n.p, { children: 'De Rijkshuisstijl Community Refinement is een onderdeel van de Rijkshuisstijl Community. De Rijkshuisstijl Community is een Open Source project waaraan iedereen kan bijdragen ongeacht de organisatie waarvoor je werkt. De Rijkshuisstijl Community Sprint maakt deel uit van een samenwerkingsverband om NL Design System-componenten te ontwikkelen voor projecten die moeten voldoen aan de Rijkshuisstijl.' }), '\n', (0, s.jsx)(n.p, { children: 'Organisaties van de centrale overheid van Nederland (bijvoorbeeld: Belastingdienst, DUO, Logius, SVB) en degenen die door hen zijn ingehuurd voor het ontwikkelen van websites en apps, kunnen via dit project samenwerken.' }), '\n', (0, s.jsx)(l.hE, { children: (0, s.jsxs)(r.Z, { href: '/community/community-sprints/rijkshuisstijl-community', appearance: 'primary-action', children: ['Lees meer over de Rijkshuisstijl Community', (0, s.jsx)(o.Z, {})] }) }), '\n', (0, s.jsx)(n.h2, { id: 'vragen-of-idee\xebn', children: 'Vragen of idee\xebn?' }), '\n', (0, s.jsxs)(n.p, { children: ['Heb je vragen of idee\xebn voor de Rijkshuisstijl Community Refinement? Deel ze in het ', (0, s.jsx)(n.a, { href: 'https://codefornl.slack.com/archives/C05AQK9R41G', children: '#rijkshuisstijl-community' }), '-kanaal op de ', (0, s.jsx)(n.a, { href: '/slack/', children: 'Slack van Code for NL' }), ' of stuur een mail naar ', (0, s.jsx)('a', { href: 'mailto:info@nldesignsystem.nl', children: (0, s.jsx)(n.a, { href: 'mailto:info@nldesignsystem.nl', children: 'info@nldesignsystem.nl' }) }), '.'] })] });
+   }
+   function c(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, s.jsx)(n, { ...e, children: (0, s.jsx)(h, { ...e }) }) : h(e);
+   }
+  },
+  68257: (e, n, i) => {
+   i.d(n, { Z: () => o });
+   var s = i(44679),
+    t = i(4814),
+    r = i(52676);
+   const o = (e) => {
+    let { appearance: n, ...i } = e;
+    return (0, r.jsx)(s.Z, { className: (0, t.W)('utrecht-button-link', `utrecht-button-link--${n}`), ...i });
+   };
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => l, a: () => o });
+   var s = i(75271);
+   const t = {},
+    r = s.createContext(t);
+   function o(e) {
+    const n = s.useContext(r);
+    return s.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function l(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : o(e.components)), s.createElement(r.Provider, { value: n }, e.children);
+   }
+  },
+  72315: (e, n, i) => {
+   i.d(n, { Z: () => r });
+   var s = i(75271),
+    t = { outline: { xmlns: 'http://www.w3.org/2000/svg', width: 24, height: 24, viewBox: '0 0 24 24', fill: 'none', stroke: 'currentColor', strokeWidth: 2, strokeLinecap: 'round', strokeLinejoin: 'round' }, filled: { xmlns: 'http://www.w3.org/2000/svg', width: 24, height: 24, viewBox: '0 0 24 24', fill: 'currentColor', stroke: 'none' } };
+   const r = (e, n, i, r) => {
+    const o = (0, s.forwardRef)(({ color: i = 'currentColor', size: o = 24, stroke: l = 2, title: a, className: d, children: m, ...u }, j) => (0, s.createElement)('svg', { ref: j, ...t[e], width: o, height: o, className: ['tabler-icon', `tabler-icon-${n}`, d].join(' '), ...('filled' === e ? { fill: i } : { strokeWidth: l, stroke: i }), ...u }, [a && (0, s.createElement)('title', { key: 'svg-title' }, a), ...r.map(([e, n]) => (0, s.createElement)(e, n)), ...(Array.isArray(m) ? m : [m])]));
+    return (o.displayName = `${i}`), o;
+   };
+  },
+  78511: (e, n, i) => {
+   i.d(n, { Z: () => s });
+   var s = (0, i(72315).Z)('outline', 'chevron-right', 'IconChevronRight', [['path', { d: 'M9 6l6 6l-6 6', key: 'svg-0' }]]);
+  },
+ },
+]);
---
+++ assets/js/bca444c7.1318c656.js
@@ -0,0 +1,75 @@
+/*! For license information please see bca444c7.1318c656.js.LICENSE.txt */
+'use strict';
+(self.webpackChunk_nl_design_system_website = self.webpackChunk_nl_design_system_website || []).push([
+ [62958],
+ {
+  78843: (e, n, i) => {
+   i.r(n), i.d(n, { assets: () => j, contentTitle: () => d, default: () => p, frontMatter: () => l, metadata: () => m, toc: () => u });
+   var s = i(52676),
+    t = i(40139),
+    r = i(68257),
+    a = i(78511),
+    o = i(37943);
+   const l = { title: 'Rijkshuisstijl Community Sprint planning \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Rijkshuisstijl Community Sprint planning', pagination_label: 'Tijdens de Rijkshuisstijl Community Sprint planning zijn laagdrempelige online bijeenkomsten waar designers en developers van verschillende overheidsorganisaties samen de nieuwe sprint inplannen.', description: 'Tijdens de Rijkshuisstijl Community Sprint planning zijn laagdrempelige online bijeenkomsten waar designers en developers van verschillende overheidsorganisaties samen de nieuwe sprint inplannen.', slug: '/events/rijkshuisstijl-community-sprint-planning', keywords: ['nl design system', 'community sprints', 'rijkshuisstijl community', 'rijkshuisstijl community sprint planning', 'rijkshuisstijl community sprintplanning'] },
+    d = 'Rijkshuisstijl Community Sprint planning',
+    m = { id: 'community/events/rijkshuisstijl-community-sprint-planning', title: 'Rijkshuisstijl Community Sprint planning \xb7 Bijeenkomsten', description: 'Tijdens de Rijkshuisstijl Community Sprint planning zijn laagdrempelige online bijeenkomsten waar designers en developers van verschillende overheidsorganisaties samen de nieuwe sprint inplannen.', source: '@site/docs/community/events/rijkshuisstijl-community-sprint-planning.mdx', sourceDirName: 'community/events', slug: '/events/rijkshuisstijl-community-sprint-planning', permalink: '/events/rijkshuisstijl-community-sprint-planning', draft: !1, unlisted: !1, editUrl: 'https://github.com/nl-design-system/documentatie/tree/main/docs/community/events/rijkshuisstijl-community-sprint-planning.mdx', tags: [], version: 'current', frontMatter: { title: 'Rijkshuisstijl Community Sprint planning \xb7 Bijeenkomsten', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Rijkshuisstijl Community Sprint planning', pagination_label: 'Tijdens de Rijkshuisstijl Community Sprint planning zijn laagdrempelige online bijeenkomsten waar designers en developers van verschillende overheidsorganisaties samen de nieuwe sprint inplannen.', description: 'Tijdens de Rijkshuisstijl Community Sprint planning zijn laagdrempelige online bijeenkomsten waar designers en developers van verschillende overheidsorganisaties samen de nieuwe sprint inplannen.', slug: '/events/rijkshuisstijl-community-sprint-planning', keywords: ['nl design system', 'community sprints', 'rijkshuisstijl community', 'rijkshuisstijl community sprint planning', 'rijkshuisstijl community sprintplanning'] }, sidebar: 'community', previous: { title: 'Tijdens de Rijkshuisstijl Community Refinement helpen we deelnemers om hun eigen issues te verfijnen en bestaande issues te verbeteren.', permalink: '/events/rijkshuisstijl-community-refinement' }, next: { title: 'Tijdens de Design Open Dag komen designers bij elkaar om samen te werken aan designvraagstukken.', permalink: '/events/design-open-dag' } },
+    j = {},
+    u = [
+     { value: 'Wat doen we?', id: 'wat-doen-we', level: 2 },
+     { value: 'Ben je erbij?', id: 'ben-je-erbij', level: 2 },
+     { value: 'Over de Rijkshuisstijl Community', id: 'over-de-rijkshuisstijl-community', level: 2 },
+     { value: 'Vragen of idee\xebn?', id: 'vragen-of-idee\xebn', level: 2 },
+    ];
+   function h(e) {
+    const n = { a: 'a', br: 'br', h1: 'h1', h2: 'h2', li: 'li', ol: 'ol', p: 'p', strong: 'strong', ...(0, t.a)(), ...e.components };
+    return (0, s.jsxs)(s.Fragment, { children: [(0, s.jsx)(n.h1, { id: 'rijkshuisstijl-community-sprint-planning', children: 'Rijkshuisstijl Community Sprint planning' }), '\n', (0, s.jsx)(n.p, { children: 'De Rijkshuisstijl Community Sprint plannings zijn laagdrempelige online bijeenkomsten waar designers en developers van verschillende overheidsorganisaties samen de nieuwe sprint inplannen. Dankzij deze gedeelde planning ziet de community waar iedereen aan werkt, waar ze kunnen schakelen en hoe ze beter kunnen samenwerken. Zo voorkomen we dubbel werk en versterken we elkaar.' }), '\n', (0, s.jsx)(n.h2, { id: 'wat-doen-we', children: 'Wat doen we?' }), '\n', (0, s.jsxs)(n.p, { children: ['De Sprint planning vindt plaats direct na de ', (0, s.jsx)(n.a, { href: '/events/rijkshuisstijl-community-refinement/', children: 'Rijkshuisstijl Community Refinement' }), ', waarin issues verfijnd en verbeterd worden. Tijdens de Sprint planning plannen deelnemers issues in voor de komende sprintperiode.'] }), '\n', (0, s.jsx)(n.p, { children: 'De agenda van de bijeenkomst is als volgt:' }), '\n', (0, s.jsxs)(n.ol, { children: ['\n', (0, s.jsxs)(n.li, { children: [(0, s.jsxs)(n.strong, { children: ['Bespreken van de ', (0, s.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/projects/59/views/8', children: 'vorige sprint' }), '.'] }), (0, s.jsx)(n.br, {}), '\n', 'Zijn er nog openstaande issues? Wat moet hiermee gebeuren? Dit bespreken we per issue.'] }), '\n', (0, s.jsxs)(n.li, { children: [(0, s.jsxs)(n.strong, { children: ['Bespreken van de ', (0, s.jsx)(n.a, { href: 'https://github.com/orgs/nl-design-system/projects/59/views/7', children: 'huidige sprint' }), '.'] }), (0, s.jsx)(n.br, {}), '\n', 'Zijn er issues die nog aan niemand zijn toegewezen en wat moet hiermee gebeuren?', (0, s.jsx)(n.br, {}), '\n', 'Per persoon bespreken we kort de issues die op naam staan. Daarbij denken we aan beschikbare tijd, prioriteiten en afhankelijkheden.'] }), '\n', (0, s.jsxs)(n.li, { children: [(0, s.jsx)(n.strong, { children: 'Bespreken van open onderwerpen.' }), (0, s.jsx)(n.br, {}), '\n', 'Zijn er zaken waarvoor nog geen issue is, onderwerpen waar je met anderen over wilt schakelen, of andere mededelingen voor de community?'] }), '\n'] }), '\n', (0, s.jsx)(n.p, { children: 'Iedereen die betrokken is bij de ontwikkeling van de Rijkshuisstijl Community kan meedoen, of je nu designer, developer, onderzoeker, tester of afnemer bent.' }), '\n', (0, s.jsx)(n.h2, { id: 'ben-je-erbij', children: 'Ben je erbij?' }), '\n', (0, s.jsx)(n.p, { children: 'Iedereen kan aansluiten! Regelmatig of af en toe, wanneer je vragen hebt of iets wilt bespreken.' }), '\n', (0, s.jsxs)(n.p, { children: ['De Rijkshuisstijl Community Sprint planning vindt plaats in de even weken, iedere donderdag om 15:00 uur, in het ', (0, s.jsx)(n.a, { href: 'https://codefornl.slack.com/archives/C05AQK9R41G', children: '#rijkshuisstijl-community' }), '-kanaal op de ', (0, s.jsx)(n.a, { href: '/slack/', children: 'Slack van Code for NL' }), '. We gebruiken de Huddle-functie van dat kanaal om elkaar te spreken. In dat kanaal vind je ook de threads van eerdere sessies.'] }), '\n', (0, s.jsx)(n.p, { children: 'Aanmelden is niet verplicht, maar door je aan te melden voor de Rijkshuisstijl Community kunnen wij je op de hoogte houden van bijeenkomsten zoals de Rijkshuisstijl Community Refinement. Je ontvangt dan ook direct een kalenderbestandje voor alle bijeenkomsten, zodat je deze makkelijk aan je agenda kan toevoegen.' }), '\n', (0, s.jsxs)(o.hE, { children: [(0, s.jsxs)(r.Z, { href: '/community/community-sprints/rijkshuisstijl-community/aanmelden', appearance: 'primary-action', children: ['Meld je aan', (0, s.jsx)(a.Z, {})] }), (0, s.jsxs)(r.Z, { href: '/slack/', appearance: 'secondary-action', children: ['Doe mee op Slack', (0, s.jsx)(a.Z, {})] })] }), '\n', (0, s.jsx)(n.h2, { id: 'over-de-rijkshuisstijl-community', children: 'Over de Rijkshuisstijl Community' }), '\n', (0, s.jsx)(n.p, { children: 'De Rijkshuisstijl Community Sprint planning is een onderdeel van de Rijkshuisstijl Community. De Rijkshuisstijl Community is een Open Source project waaraan iedereen kan bijdragen ongeacht de organisatie waarvoor je werkt. De Rijkshuisstijl Community Sprint maakt deel uit van een samenwerkingsverband om NL Design System-componenten te ontwikkelen voor projecten die moeten voldoen aan de Rijkshuisstijl.' }), '\n', (0, s.jsx)(n.p, { children: 'Organisaties van de centrale overheid van Nederland (bijvoorbeeld: Belastingdienst, DUO, Logius, SVB) en degenen die door hen zijn ingehuurd voor het ontwikkelen van websites en apps, kunnen via dit project samenwerken.' }), '\n', (0, s.jsx)(o.hE, { children: (0, s.jsxs)(r.Z, { href: '/community/community-sprints/rijkshuisstijl-community', appearance: 'primary-action', children: ['Lees meer over de Rijkshuisstijl Community', (0, s.jsx)(a.Z, {})] }) }), '\n', (0, s.jsx)(n.h2, { id: 'vragen-of-idee\xebn', children: 'Vragen of idee\xebn?' }), '\n', (0, s.jsxs)(n.p, { children: ['Heb je vragen of idee\xebn voor de Rijkshuisstijl Community Sprint planning? Deel het op het ', (0, s.jsx)(n.a, { href: 'https://codefornl.slack.com/archives/C05AQK9R41G', children: '#rijkshuisstijl-community' }), '-kanaal op de ', (0, s.jsx)(n.a, { href: '/slack/', children: 'Slack van Code for NL' }), ' of stuur ons een mailtje via ', (0, s.jsx)('a', { href: 'mailto:info@nldesignsystem.nl', children: (0, s.jsx)(n.a, { href: 'mailto:info@nldesignsystem.nl', children: 'info@nldesignsystem.nl' }) }), '.'] })] });
+   }
+   function p(e = {}) {
+    const { wrapper: n } = { ...(0, t.a)(), ...e.components };
+    return n ? (0, s.jsx)(n, { ...e, children: (0, s.jsx)(h, { ...e }) }) : h(e);
+   }
+  },
+  68257: (e, n, i) => {
+   i.d(n, { Z: () => a });
+   var s = i(44679),
+    t = i(4814),
+    r = i(52676);
+   const a = (e) => {
+    let { appearance: n, ...i } = e;
+    return (0, r.jsx)(s.Z, { className: (0, t.W)('utrecht-button-link', `utrecht-button-link--${n}`), ...i });
+   };
+  },
+  40139: (e, n, i) => {
+   i.d(n, { Z: () => o, a: () => a });
+   var s = i(75271);
+   const t = {},
+    r = s.createContext(t);
+   function a(e) {
+    const n = s.useContext(r);
+    return s.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function o(e) {
+    let n;
+    return (n = e.disableParentContext ? ('function' == typeof e.components ? e.components(t) : e.components || t) : a(e.components)), s.createElement(r.Provider, { value: n }, e.children);
+   }
+  },
+  72315: (e, n, i) => {
+   i.d(n, { Z: () => r });
+   var s = i(75271),
+    t = { outline: { xmlns: 'http://www.w3.org/2000/svg', width: 24, height: 24, viewBox: '0 0 24 24', fill: 'none', stroke: 'currentColor', strokeWidth: 2, strokeLinecap: 'round', strokeLinejoin: 'round' }, filled: { xmlns: 'http://www.w3.org/2000/svg', width: 24, height: 24, viewBox: '0 0 24 24', fill: 'currentColor', stroke: 'none' } };
+   const r = (e, n, i, r) => {
+    const a = (0, s.forwardRef)(({ color: i = 'currentColor', size: a = 24, stroke: o = 2, title: l, className: d, children: m, ...j }, u) => (0, s.createElement)('svg', { ref: u, ...t[e], width: a, height: a, className: ['tabler-icon', `tabler-icon-${n}`, d].join(' '), ...('filled' === e ? { fill: i } : { strokeWidth: o, stroke: i }), ...j }, [l && (0, s.createElement)('title', { key: 'svg-title' }, l), ...r.map(([e, n]) => (0, s.createElement)(e, n)), ...(Array.isArray(m) ? m : [m])]));
+    return (a.displayName = `${i}`), a;
+   };
+  },
+  78511: (e, n, i) => {
+   i.d(n, { Z: () => s });
+   var s = (0, i(72315).Z)('outline', 'chevron-right', 'IconChevronRight', [['path', { d: 'M9 6l6 6l-6 6', key: 'svg-0' }]]);
+  },
+ },
+]);
