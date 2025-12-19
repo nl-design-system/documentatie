0603f1b4af881a0766f36b1a38400b9133987b19
Yolijn <mail@yolijn.com>
2025-12-19 14:46:56 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@a6641e5682fd13da53543049e3b94f7da1fc1215 🚀

--- assets/js/4212264a.cb2711aa.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(globalThis.webpackChunk_nl_design_system_website = globalThis.webpackChunk_nl_design_system_website || []).push([
- [49353],
- {
-  99869: (e) => {
-   e.exports = JSON.parse('{"categoryGeneratedIndex":{"title":"Voor developers","description":"Mee ontwikkelen aan NL Design System of gebruik maken van bestaande componenten? Als developer vind je hier alle documentatie die je nodig hebt om te beginnen.","keywords":["developer","overzicht"],"slug":"/handboek/developer/overzicht","permalink":"/handboek/developer/overzicht","sidebar":"handboek","navigation":{"previous":{"title":"Figma-bestanden overzicht","permalink":"/figma"},"next":{"title":"Aan de slag","permalink":"/handboek/developer/aan-de-slag"}}}}');
-  },
- },
-]);
