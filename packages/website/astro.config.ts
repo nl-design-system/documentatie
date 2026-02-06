5c6054d1a0c761d8eb15538d8b15e9d1678f5097
Peter Goes <petergoes@gmail.com>
2026-02-06 13:18:22 +0100

    chore: use existing `/static` folder for static assets

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -4,6 +4,7 @@ const siteUrl = 'https://nldesignsystem.nl';
 
 // https://astro.build/config
 export default defineConfig({
+  publicDir: '../../static',
   build: {
     inlineStylesheets: 'never',
   },
