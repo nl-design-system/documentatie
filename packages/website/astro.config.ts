2d4a3848eecf47f803ba34f8a536085824239811
Peter Goes <petergoes@gmail.com>
2026-02-06 13:17:10 +0100

    chore: add Astro project as package under `packages/website`

---
+++ packages/website/astro.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+
+const siteUrl = 'https://nldesignsystem.nl';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    inlineStylesheets: 'never',
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  site: siteUrl,
+  vite: {
+    build: {
+      // prevent vite from inlining assets as data:* attributes because it violates csp rules
+      assetsInlineLimit: 0,
+    },
+  },
+});
