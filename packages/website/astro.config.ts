247380bc4f590d34b2427c21dc317b5e4b2f6fe1
Peter Goes <petergoes@gmail.com>
2026-02-09 16:50:18 +0100

    Feat: render `/baseline/*` pages in Astro (#3687)
    De PR rendered enkel de pagina's nog geen overzichten.
    bijvoorbeeld:
    /baseline/2025-12
    
    In deze PR kan Astro overweg met MDX en React
    
    closes: #3641

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -1,21 +1,31 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
+
 const siteUrl = 'https://nldesignsystem.nl';
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: '../../static',
+
   build: {
     inlineStylesheets: 'never',
   },
+
   devToolbar: {
     enabled: false,
   },
+
   site: siteUrl,
+
   vite: {
     build: {
       // prevent vite from inlining assets as data:* attributes because it violates csp rules
       assetsInlineLimit: 0,
     },
   },
+
+  integrations: [mdx(), react()],
 });
