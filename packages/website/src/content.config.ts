d3e46995d573e53c680bb9e1e47f1fed87fc30f1
Peter Goes <petergoes@gmail.com>
2026-02-20 13:48:44 +0100

    feat: add all docs routes that render without issue

--- packages/website/src/content.config.ts
+++ packages/website/src/content.config.ts
@@ -5,7 +5,20 @@ import { z } from 'astro/zod';
 const docs = defineCollection({
   loader: glob({
     base: './../../docs',
-    pattern: ['baseline/**/!(_)(*).{md,mdx}', 'community/**/!(_)(*).{md,mdx}'],
+    pattern: [
+      'baseline/**/!(_)(*).{md,mdx}',
+      'community/**/!(_)(*).{md,mdx}',
+      // 'componenten/**/!(_)(*).{md,mdx}',
+      'footer/**/!(_)(*).{md,mdx}',
+      // 'handboek/**/!(_)(*).{md,mdx}',
+      'open-source/**/!(_)(*).{md,mdx}',
+      'private/**/!(_)(*).{md,mdx}',
+      // 'project/**/!(_)(*).{md,mdx}',
+      // 'richtlijnen/**/!(_)(*).{md,mdx}',
+      'voorbeelden/**/!(_)(*).{md,mdx}',
+      // 'wcag/**/!(_)(*).{md,mdx}',
+      'woordenlijst/**/!(_)(*).{md,mdx}',
+    ],
   }),
   schema: z.object({
     title: z.string(),
