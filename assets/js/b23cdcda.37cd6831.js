04a370847caad344c93bb4ac3d97d46b4a9bb43f
Robbert <mail@robbertbroersma.nl>
2026-02-22 10:31:25 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@fb0824bc1c12e741073fe9f405bbaa7077975c09 🚀

---
+++ assets/js/b23cdcda.37cd6831.js
@@ -0,0 +1,229 @@
+'use strict';
+(globalThis.webpackChunk_nl_design_system_website = globalThis.webpackChunk_nl_design_system_website || []).push([
+ [87915],
+ {
+  15638(e, n, o) {
+   o.d(n, { $: () => d, x: () => c });
+   var t = o(4077),
+    l = o(58315),
+    r = o(46447),
+    s = o(13526),
+    i = o(30758),
+    a = o(86070);
+   const c = (0, i.createContext)({}),
+    d = ({ title: e, appearance: n, description: o, children: i, figure: d }) => {
+     const u = { do: 'Doen', dont: 'Niet doen' },
+      p = d ? 'figure' : 'div',
+      m = d ? 'figcaption' : 'div';
+     return (0, a.jsxs)(p, { className: (0, s.A)('nlds-guideline', `nlds-guideline--${n}`), id: 'string' == typeof e ? e?.toLowerCase().replace(/\s/g, '-') : void 0, children: [(0, a.jsxs)(m, { className: 'nlds-guideline__description', children: ['dont' === n ? (0, a.jsxs)(a.Fragment, { children: [(0, a.jsxs)(r.fz, { className: (0, s.A)('nlds-guideline__badge', `nlds-guideline__badge--${n}`), children: [(0, a.jsx)(l.A, { 'aria-hidden': 'true', role: 'presentation', className: 'nlds-guideline__icon' }), (0, a.jsx)('span', { className: 'nlds-guideline__title', children: u.dont })] }), (0, a.jsx)(r.fz, { children: e })] }) : (0, a.jsxs)(a.Fragment, { children: [(0, a.jsxs)(r.fz, { className: (0, s.A)('nlds-guideline__badge', `nlds-guideline__badge--${n}`), children: [(0, a.jsx)(t.A, { 'aria-hidden': 'true', role: 'presentation', className: 'nlds-guideline__icon' }), (0, a.jsx)('span', { className: 'nlds-guideline__title', children: u.do })] }), (0, a.jsx)(r.fz, { children: e })] }), o] }), (0, a.jsx)('div', { className: 'nlds-guideline__example', children: (0, a.jsx)(c.Provider, { value: { title: e, type: u[n] }, children: i }) })] });
+    };
+  },
+  18439(e, n, o) {
+   o.d(n, { R: () => s, x: () => i });
+   var t = o(30758);
+   const l = {},
+    r = t.createContext(l);
+   function s(e) {
+    const n = t.useContext(r);
+    return t.useMemo(
+     function () {
+      return 'function' == typeof e ? e(n) : { ...n, ...e };
+     },
+     [n, e],
+    );
+   }
+   function i(e) {
+    let n;
+    return ((n = e.disableParentContext ? ('function' == typeof e.components ? e.components(l) : e.components || l) : s(e.components)), t.createElement(r.Provider, { value: n }, e.children));
+   }
+  },
+  58639(e, n, o) {
+   o.d(n, { H: () => g });
+   var t = o(69284),
+    l = o(46447),
+    r = o(13526),
+    s = o(68148),
+    i = o(9016),
+    a = o(69303),
+    c = o(91635),
+    d = o(30734),
+    u = o(30758),
+    p = o(31186),
+    m = o(86070);
+   const h = ({ children: e }) => (0, m.jsx)(l.ui, { className: 'nlds-canvas__example-surface', children: (0, m.jsx)(l.yo, { className: (0, r.A)('utrecht-document--surface', 'nlds-canvas__example-document'), children: (0, m.jsx)(l.fz, { className: 'nlds-canvas__example-paragraph', children: e }) }) });
+   h.displayName = 'ParagraphContainer';
+   const v = ({ children: e }) => (0, m.jsx)(l.ui, { className: 'nlds-canvas__example-surface', children: (0, m.jsx)(l.yo, { className: (0, r.A)('utrecht-document--surface', 'nlds-canvas__example-document'), children: e }) });
+   v.displayName = 'DocumentContainer';
+   const b = ({ children: e }) => (0, m.jsx)(l.ui, { className: 'nlds-canvas__example-surface', children: e });
+   b.displayName = 'SurfaceContainer';
+   const g = ({ code: e, copy: n = !0, defaultExpandedCode: o = !1, displayCode: g = !0, children: j, container: y = 'document', language: f, designTokens: x }) => {
+    const k = 'function' == typeof j ? j() : j,
+     _ = 'function' == typeof e ? e() : (0, u.isValidElement)(e) ? e : void 0,
+     w = 'string' == typeof e ? e : p.qV(_ || k),
+     [A, N] = (0, u.useState)(w),
+     [z, C] = (0, u.useState)(o);
+    (0, u.useEffect)(() => {
+     (async () => {
+      const e = await d.Ay.format(w, { parser: f, plugins: [s.A, i.Ay, a.Ay, c.Ay], semi: !1, singleAttributePerLine: !0, embeddedLanguageFormatting: 'off', htmlWhitespaceSensitivity: 'ignore' });
+      N(e);
+     })();
+    }, [w]);
+    const R = (0, u.useId)();
+    let S = u.Fragment;
+    return (
+     'paragraph' === y ? (S = h) : 'document' === y ? (S = v) : 'surface' === y && (S = b),
+     (0, m.jsxs)('div', {
+      className: (0, r.A)('nlds-canvas'),
+      children: [
+       k && (0, m.jsx)('div', { className: (0, r.A)('nlds-canvas__example'), children: (0, m.jsx)('div', { className: 'voorbeeld-theme', style: x, children: (0, m.jsx)(S, { children: (0, m.jsx)(l.v$, { children: k }) }) }) }),
+       g &&
+        (0, m.jsx)('div', {
+         className: (0, r.A)('nlds-canvas__toolbar'),
+         children: (0, m.jsx)(l.$n, {
+          className: (0, r.A)('nlds-canvas__button', 'nlds-canvas__toggle-code-button'),
+          appearance: 'subtle-button',
+          onClick: () => {
+           C(!z);
+          },
+          'aria-expanded': z,
+          'aria-controls': R,
+          children: z ? 'Verberg code' : 'Bekijk code',
+         }),
+        }),
+       g &&
+        (0, m.jsxs)('div', {
+         className: (0, r.A)('nlds-canvas__code-block', !n && 'nlds-canvas__code-block--user-select-none'),
+         id: R,
+         hidden: !z,
+         children: [
+          (0, m.jsx)(t.M, { codeBlockLabel: 'Codevoorbeeld', syntax: f, textContent: A, trim: !0 }),
+          n &&
+           (0, m.jsx)('div', {
+            className: (0, r.A)('nlds-canvas__toolbar', 'nlds-canvas__toolbar--copy'),
+            children: (0, m.jsx)(l.$n, {
+             className: (0, r.A)('nlds-canvas__button', 'nlds-canvas__copy-button'),
+             appearance: 'subtle-button',
+             onClick: () => {
+              navigator.clipboard.writeText(A).catch((e) => console.error('Copy code failed', e));
+             },
+             children: 'Kopieer',
+            }),
+           }),
+         ],
+        }),
+      ],
+     })
+    );
+   };
+  },
+  69284(e, n, o) {
+   o.d(n, { M: () => c });
+   var t = o(46447),
+    l = o(16194),
+    r = o(30758),
+    s = o(15638);
+   const i = {
+    plain: { color: 'var(--nlds-code-block-color)', backgroundColor: 'var(--nlds-code-block-background-color)' },
+    styles: [
+     { types: ['maybe-class-name'], style: { color: 'var(--nlds-code-block-maybe-class-name-color, var(--nlds-code-block-class-name-color))' } },
+     { types: ['comment'], style: { color: 'var(--nlds-code-block-comment-color)', fontStyle: 'italic' } },
+     { types: ['prolog'], style: { color: 'var(--nlds-code-block-prolog-color)', fontStyle: 'italic' } },
+     { types: ['cdata'], style: { color: 'var(--nlds-code-block-cdata-color)', fontStyle: 'italic' } },
+     { types: ['punctuation'], style: { color: 'var(--nlds-code-block-punctuation-color)' } },
+     { types: ['deleted'], style: { color: 'var(--nlds-code-block-deleted-color)', fontStyle: 'italic' } },
+     { types: ['symbol'], style: { color: 'var(--nlds-code-block-symbol-color)' } },
+     { types: ['property'], style: { color: 'var(--nlds-code-block-property-color)' } },
+     { types: ['tag'], style: { color: 'var(--nlds-code-block-tag-color)' } },
+     { types: ['operator'], style: { color: 'var(--nlds-code-block-operator-color)' } },
+     { types: ['keyword'], style: { color: 'var(--nlds-code-block-keyword-color)' } },
+     { types: ['boolean'], style: { color: 'var(--nlds-code-block-boolean-color)' } },
+     { types: ['number'], style: { color: 'var(--nlds-code-block-number-color)' } },
+     { types: ['constant'], style: { color: 'var(--nlds-code-block-constant-color)' } },
+     { types: ['function'], style: { color: 'var(--nlds-code-block-function-color)' } },
+     { types: ['builtin'], style: { color: 'var(--nlds-code-block-builtin-color)' } },
+     { types: ['char'], style: { color: 'var(--nlds-code-block-char-color)' } },
+     { types: ['selector'], style: { color: 'var(--nlds-code-block-selector-color)', fontStyle: 'italic' } },
+     { types: ['doctype'], style: { color: 'var(--nlds-code-block-doctype-color)', fontStyle: 'italic' } },
+     { types: ['attr-name'], style: { color: 'var(--nlds-code-block-attr-name-color)', fontStyle: 'italic' } },
+     { types: ['inserted'], style: { color: 'var(--nlds-code-block-inserted-color)', fontStyle: 'italic' } },
+     { types: ['string'], style: { color: 'var(--nlds-code-block-string-color)' } },
+     { types: ['url'], style: { color: 'var(--nlds-code-block-url-color)' } },
+     { types: ['entity'], style: { color: 'var(--nlds-code-block-entity-color)' } },
+     { types: ['class-name'], style: { color: 'var(--nlds-code-block-class-name-color)' } },
+     { types: ['atrule'], style: { color: 'var(--nlds-code-block-atrule-color)' } },
+     { types: ['attr-value'], style: { color: 'var(--nlds-code-block-attr-value-color)' } },
+     { types: ['regex'], style: { color: 'var(--nlds-code-block-regex-color)' } },
+     { types: ['important'], style: { color: 'var(--nlds-code-block-important-color)', fontWeight: 'bold' } },
+     { types: ['variable'], style: { color: 'var(--nlds-code-block-variable-color)' } },
+     { types: ['bold'], style: { fontWeight: 'bold' } },
+     { types: ['italic'], style: { fontStyle: 'italic' } },
+    ],
+   };
+   var a = o(86070);
+   function c({ lineNumber: e, syntax: n, textContent: o, trim: c }) {
+    let d = o;
+    const { title: u, type: p } = (0, r.useContext)(s.x),
+     m = (0, r.useId)();
+    return (c && (d = d.trim()), (0, a.jsx)(l.f4, { theme: i, code: d, language: n || '', children: ({ style: n, tokens: o, getLineProps: l, getTokenProps: r }) => (0, a.jsxs)(a.Fragment, { children: [(0, a.jsxs)('span', { hidden: !0, id: m, children: ['codevoorbeeld ', p ? `\u201c${p}\u201d` : '', ' ', u ? ': ' : ' ', u] }), (0, a.jsx)(t.NG, { tabIndex: 0, role: u ? 'region' : void 0, 'aria-labelledby': m, style: n, children: o.map((n, o) => (0, a.jsxs)('span', { ...l({ line: n }), children: [e && (0, a.jsx)('span', { children: o + 1 }), n.map((e, n) => (0, a.jsx)('span', { ...r({ token: e }) }, n)), '\n'] }, o)) })] }) }));
+   }
+  },
+  69982(e, n, o) {
+   (o.r(n), o.d(n, { assets: () => g, contentTitle: () => b, default: () => f, frontMatter: () => v, metadata: () => t, toc: () => j }));
+   const t = JSON.parse('{"id":"richtlijnen/formulieren/help/autocomplete/README","title":"Autocomplete in een formulier \xb7 Voorkom fouten in een formulier \xb7 Richtlijnen","description":"Richtlijnen voor het gebruik van het autocomplete-attribuut.","source":"@site/docs/richtlijnen/formulieren/help/5-autocomplete/README.mdx","sourceDirName":"richtlijnen/formulieren/help/5-autocomplete","slug":"/richtlijnen/formulieren/voorkom-fouten/autocomplete","permalink":"/richtlijnen/formulieren/voorkom-fouten/autocomplete","draft":false,"unlisted":false,"editUrl":"https://github.com/nl-design-system/documentatie/tree/main/docs/richtlijnen/formulieren/help/5-autocomplete/README.mdx","tags":[],"version":"current","frontMatter":{"title":"Autocomplete in een formulier \xb7 Voorkom fouten in een formulier \xb7 Richtlijnen","hide_title":true,"hide_table_of_contents":false,"sidebar_label":"Autocomplete","pagination_label":"Autocomplete in een formulier","description":"Richtlijnen voor het gebruik van het autocomplete-attribuut.","slug":"/richtlijnen/formulieren/voorkom-fouten/autocomplete","keywords":["informatie","formulier","design","code"]},"sidebar":"richtlijnen","previous":{"title":"Geef geldige waardes aan een invoerveld","permalink":"/richtlijnen/formulieren/voorkom-fouten/geldige-waardes"},"next":{"title":"Automatisch invullen","permalink":"/richtlijnen/formulieren/voorkom-fouten/automatisch-invullen"}}');
+   var l = o(86070),
+    r = o(18439),
+    s = o(78734),
+    i = o(58639),
+    a = o(15638);
+   function c(e) {
+    const n = { h2: 'h2', ...(0, r.R)(), ...e.components };
+    return (0, l.jsxs)(l.Fragment, { children: ['\n', '\n', (0, l.jsx)(n.h2, { id: 'voorbeelden', children: 'Voorbeelden' }), '\n', (0, l.jsx)(a.$, { appearance: 'do', title: 'Het autocomplete-attribuut gebruiken.', children: (0, l.jsx)(i.H, { language: 'html', children: () => (0, l.jsxs)(l.Fragment, { children: [(0, l.jsx)('p', { children: (0, l.jsx)('label', { htmlFor: 'voornaam', children: 'Je voornaam' }) }), (0, l.jsx)('p', { children: (0, l.jsx)('input', { id: 'voornaam', type: 'text', autoComplete: 'given-name' }) })] }) }) }), '\n', (0, l.jsx)(a.$, { appearance: 'dont', title: 'Het autocomplete-attribuut niet gebruiken.', description: 'Het input type alleen is hier niet voldoende om te voldoen aan WCAG.', children: (0, l.jsx)(i.H, { language: 'html', children: () => (0, l.jsxs)(l.Fragment, { children: [(0, l.jsx)('p', { children: (0, l.jsx)('label', { htmlFor: 'telefoon', children: 'Uw telefoonnummer' }) }), (0, l.jsx)('p', { children: (0, l.jsx)('input', { id: 'telefoon', type: 'tel' }) })] }) }) })] });
+   }
+   function d(e = {}) {
+    const { wrapper: n } = { ...(0, r.R)(), ...e.components };
+    return n ? (0, l.jsx)(n, { ...e, children: (0, l.jsx)(c, { ...e }) }) : c(e);
+   }
+   function u(e) {
+    const n = { a: 'a', code: 'code', h1: 'h1', h2: 'h2', header: 'header', p: 'p', strong: 'strong', ...(0, r.R)(), ...e.components };
+    return (0, l.jsxs)(l.Fragment, { children: [(0, l.jsx)(n.header, { children: (0, l.jsx)(n.h1, { id: 'autocomplete-in-een-formulier', children: 'Autocomplete in een formulier' }) }), '\n', (0, l.jsxs)(n.p, { children: ['Het ', (0, l.jsxs)(n.a, { href: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete', children: ['HTML-attribuut ', (0, l.jsx)(n.code, { children: 'autocomplete' })] }), ' vertelt aan de browser welke waarde verwacht wordt bij het invullen van een formulierveld. Browsers kunnen voor gebruikers onthouden wat ze eerder hebben ingevuld.'] }), '\n', (0, l.jsxs)(n.p, { children: ['Die opgeslagen waarden kunnen browsers gebruiken om velden vast in te vullen, door het ', (0, l.jsx)(n.code, { children: 'autocomplete' }), ' attribuut weten ze welke velden dat moeten zijn. Gegevens zoals namen, adres en telefoonnummer kunnen dan automatisch worden ingevuld door de browser.'] }), '\n', (0, l.jsx)(n.p, { children: 'Autocomplete is fijn voor gebruikers die moeite hebben met typen, problemen hebben met hun geheugen en voor gebruikers die moeite hebben met taal.' }), '\n', (0, l.jsx)(n.h2, { id: 'gebruik-autocomplete-als-er-een-waarde-voor-is-gedefinieerd', children: 'Gebruik autocomplete als er een waarde voor is gedefinieerd' }), '\n', (0, l.jsxs)(n.p, { children: ['WCAG bevat een volledige ', (0, l.jsx)(n.a, { href: 'https://www.w3.org/Translations/WCAG22-nl/#input-purposes', children: 'lijst van waarden voor autocomplete' }), '. Gebruik altijd een autocomplete-attribuut als er een waarde voor een formulierveld gedefinieerd is in deze lijst.'] }), '\n', (0, l.jsxs)(n.p, { children: ['Jules Ernst van 200 OK heeft van deze lijst een ', (0, l.jsx)(n.a, { href: 'https://www.200ok.nl/tips/autocomplete/', children: 'Nederlandse interpretatie' }), ' gemaakt.'] }), '\n', (0, l.jsxs)(n.p, { children: ['Belangrijk bij het gebruik van autocomplete is dat het label altijd zichtbaar moet blijven, de gebruiker moet weten of de juiste waarde in het juiste veld is ingevoerd. Zie hiervoor de richtlijn ', (0, l.jsx)(n.a, { href: '/richtlijnen/formulieren/labels/altijd-zichtbaar/', children: 'Zorg ervoor dat het label altijd zichtbaar is' }), '.'] }), '\n', (0, l.jsxs)(n.p, { children: [(0, l.jsx)(n.strong, { children: 'Let op' }), ': ', (0, l.jsx)(n.a, { href: 'https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-autocomplete', children: 'aria-autocomplete' }), ' heeft een ander doel en ander gebruik dan autocomplete bij formuliervelden. ', (0, l.jsx)(n.code, { children: 'aria-autocomplete' }), ' wordt gebruikt in webcomponenten, bijvoorbeeld om aan te geven dat er suggesties zijn bij zoekresultaten.'] })] });
+   }
+   function p(e = {}) {
+    const { wrapper: n } = { ...(0, r.R)(), ...e.components };
+    return n ? (0, l.jsx)(n, { ...e, children: (0, l.jsx)(u, { ...e }) }) : u(e);
+   }
+   function m(e) {
+    const n = { a: 'a', h2: 'h2', li: 'li', p: 'p', ul: 'ul', ...(0, r.R)(), ...e.components };
+    return (0, l.jsxs)(l.Fragment, { children: [(0, l.jsx)(n.h2, { id: 'meer-lezen-over-gerelateerde-wcag-succescriteria-op-nl-design-system', children: 'Meer lezen over gerelateerde WCAG-succescriteria op NL Design System' }), '\n', (0, l.jsx)(n.p, { children: "Op de WCAG succescriteria pagina's op deze website lees je meer over veelgemaakte fouten en hoe je kunt testen of jouw website aan de succescriteria voldoet." }), '\n', (0, l.jsxs)(n.ul, { children: ['\n', (0, l.jsx)(n.li, { children: (0, l.jsx)(n.a, { href: '/wcag/1.3.5/', children: 'Succescriterium 1.3.5 Identificeer het doel van de input' }) }), '\n'] })] });
+   }
+   function h(e = {}) {
+    const { wrapper: n } = { ...(0, r.R)(), ...e.components };
+    return n ? (0, l.jsx)(n, { ...e, children: (0, l.jsx)(m, { ...e }) }) : m(e);
+   }
+   const v = { title: 'Autocomplete in een formulier \xb7 Voorkom fouten in een formulier \xb7 Richtlijnen', hide_title: !0, hide_table_of_contents: !1, sidebar_label: 'Autocomplete', pagination_label: 'Autocomplete in een formulier', description: 'Richtlijnen voor het gebruik van het autocomplete-attribuut.', slug: '/richtlijnen/formulieren/voorkom-fouten/autocomplete', keywords: ['informatie', 'formulier', 'design', 'code'] },
+    b = void 0,
+    g = {},
+    j = [{ value: 'Gebruik autocomplete als er een waarde voor is gedefinieerd', id: 'gebruik-autocomplete-als-er-een-waarde-voor-is-gedefinieerd', level: 2 }, { value: 'Voorbeelden', id: 'voorbeelden', level: 2 }, { value: 'Meer lezen over gerelateerde WCAG-succescriteria op NL Design System', id: 'meer-lezen-over-gerelateerde-wcag-succescriteria-op-nl-design-system', level: 2 }, ...s.RM];
+   function y(e) {
+    return (0, l.jsxs)(l.Fragment, { children: ['\n', '\n', (0, l.jsx)(p, {}), '\n', (0, l.jsx)(d, {}), '\n', (0, l.jsx)(h, {}), '\n', (0, l.jsx)(s.Ay, {})] });
+   }
+   function f(e = {}) {
+    const { wrapper: n } = { ...(0, r.R)(), ...e.components };
+    return n ? (0, l.jsx)(n, { ...e, children: (0, l.jsx)(y, { ...e }) }) : y();
+   }
+  },
+  78734(e, n, o) {
+   o.d(n, { Ay: () => i, RM: () => r });
+   var t = o(86070),
+    l = o(18439);
+   const r = [{ value: 'Over deze richtlijnen', id: 'over-deze-richtlijnen', level: 2 }];
+   function s(e) {
+    const n = { a: 'a', em: 'em', h2: 'h2', p: 'p', ...(0, l.R)(), ...e.components };
+    return (0, t.jsxs)(t.Fragment, { children: [(0, t.jsx)(n.h2, { id: 'over-deze-richtlijnen', children: 'Over deze richtlijnen' }), '\n', (0, t.jsxs)(n.p, { children: ['Deze richtlijnen worden onderhouden door NL Design System en zijn op dit moment in ', (0, t.jsx)(n.em, { children: 'beta' }), '.'] }), '\n', (0, t.jsxs)(n.p, { children: ['We willen graag van de community horen of ze werkbaar en nuttig zijn. Heb je vragen, aanvullingen of opmerkingen?\n', (0, t.jsx)(n.a, { href: 'https://github.com/nl-design-system/documentatie/issues', children: 'Open een issue op GitHub' }), ' en deel je mening.'] })] });
+   }
+   function i(e = {}) {
+    const { wrapper: n } = { ...(0, l.R)(), ...e.components };
+    return n ? (0, t.jsx)(n, { ...e, children: (0, t.jsx)(s, { ...e }) }) : s(e);
+   }
+  },
+ },
+]);
