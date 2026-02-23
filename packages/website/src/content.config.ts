11687b150ef02f8e206b199d4e8cefe571667b94
Peter Goes <petergoes@gmail.com>
2026-02-23 13:34:01 +0100

    feat: render handboek pages in astro (#3798)
    closes: #3788

--- packages/website/src/content.config.ts
+++ packages/website/src/content.config.ts
@@ -5,10 +5,23 @@ import { z } from 'astro/zod';
 const docs = defineCollection({
   loader: glob({
     base: './../../docs',
-    pattern: ['baseline/**/!(_)(*).{md,mdx}', 'community/**/!(_)(*).{md,mdx}'],
+    pattern: [
+      'baseline/**/!(_)(*).{md,mdx}',
+      'community/**/!(_)(*).{md,mdx}',
+      // 'componenten/**/!(_)(*).{md,mdx}',
+      'footer/**/!(_)(*).{md,mdx}',
+      'handboek/**/!(_)(*).{md,mdx}',
+      'open-source/**/!(_)(*).{md,mdx}',
+      'private/**/!(_)(*).{md,mdx}',
+      // 'project/**/!(_)(*).{md,mdx}',
+      // 'richtlijnen/**/!(_)(*).{md,mdx}',
+      'voorbeelden/**/!(_)(*).{md,mdx}',
+      // 'wcag/**/!(_)(*).{md,mdx}',
+      'woordenlijst/**/!(_)(*).{md,mdx}',
+    ],
   }),
   schema: z.object({
-    title: z.string(),
+    title: z.string().optional(),
     slug: z.string().optional(),
     unlisted: z.boolean().optional(),
   }),
