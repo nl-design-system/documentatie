6d42deb66cf75c2ce79b8c3cb26ae93a51660658
Roy Honders <rhonders@quintor.nl>
2026-02-27 11:32:45 +0100

    fix: add community

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -278,7 +278,7 @@ export default function Home() {
                   href="/events/developer-open-hour/aanmelden"
                 ></Card>
                 <Card
-                  body="Tijdens de Rijkshuisstijl Community Open Hour wisselen verschillende organisaties informatie, inzichten en tips uit over de Rijkshuisstijl."
+                  body="Tijdens de Rijkshuisstijl Community Open Hour wisselen verschillende organisaties informatie, inzichten en tips uit over de Rijkshuisstijl community."
                   heading="Rijkshuisstijl Community Open Hour"
                   headingLevel={3}
                   href="/events/rijkshuisstijl-community-open-hour/"
