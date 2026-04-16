c8fbefcfeaf9412b5a9263a1d5b86200d2a8d2fb
RenateRoke <renate.roke@ictu.nl>
2026-04-16 11:11:38 +0200

    Changes after rebase

--- docs/componenten/form-field-error-message/_acceptatiecriteria.tsx
+++ docs/componenten/form-field-error-message/_acceptatiecriteria.tsx
@@ -6,10 +6,14 @@ import Wcag1410 from '@nl-design-system-unstable/documentation/componenten/ac/_w
 import Wcag1412 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.12.md';
 import Wcag143 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.3.md';
 import Wcag144 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.4.md';
+import Wcag211NI from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-2.1.1-noninteractives.md';
+import Wcag243NI from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-2.4.3-noninteractives.md';
 import Wcag312 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-3.1.2.md';
 import Wcag133 from '@nl-design-system-unstable/documentation/wcag/summaries/_1.3.3-summary.md';
 import Wcag141 from '@nl-design-system-unstable/documentation/wcag/summaries/_1.4.1-summary.md';
 import Wcag413Error from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-4.1.3-error.md';
+import Wcag211NLDSNI from '@nl-design-system-unstable/documentation/componenten/ac/NLDS/_wcag-2.1.1-nlds-noninteractives.md';
+import Wcag243NLDSNI from '@nl-design-system-unstable/documentation/componenten/ac/NLDS/_wcag-2.4.3-nlds-noninteractives.md';
 import Wcag413NLDS from '@nl-design-system-unstable/documentation/componenten/ac/NLDS/_wcag-4.1.3-nlds.md';
 
 export const usage = [
@@ -74,6 +78,20 @@ export const usage = [
     tags: ['developer', 'designer'],
   },
   {
+    title: 'De Form Field Error Message is niet bereikbaar en bedienbaar met het toetsenbord.',
+    sc: '2.1.1',
+    status: '',
+    component: Wcag211NI,
+    tags: ['developer'],
+  },
+  {
+    title: 'De Form Field Error Message komt niet voor in de normale focusvolgorde van de pagina.',
+    sc: '2.4.3',
+    status: '',
+    component: Wcag243NI,
+    tags: ['developer'],
+  },
+  {
     title:
       'Als de Form Field Error Message in een andere taal is dan de taal van de pagina, dan heeft het een lang-attribuut met de juiste taalcode.',
     sc: '3.1.2',
@@ -104,6 +122,20 @@ export const component = [
     sc: '1.4.12',
     status: '',
     component: Wcag1412,
+    tags: ['developer'],
+  },
+  {
+    title: 'De Form Field Error Message is standaard niet bereikbaar en bedienbaar met het toetsenbord. ',
+    sc: '2.1.1',
+    status: '',
+    component: Wcag211NLDSNI,
+    tags: ['developer'],
+  },
+  {
+    title: 'De Form Field Error Message komt standaard niet voor in de focusvolgorde van de pagina. ',
+    sc: '2.4.3',
+    status: '',
+    component: Wcag243NLDSNI,
     tags: ['developer'],
   },
   {
