bfdff2dafb5b24e8adcfa18cdbe558e817cb35d2
RenateRoke <renate.roke@ictu.nl>
2026-05-05 14:11:04 +0200

    Feedback verwerkt

--- docs/componenten/ac/NLDS/_wcag-1.3.1-nlds-ordered.md
+++ docs/componenten/ac/NLDS/_wcag-1.3.1-nlds-ordered.md
@@ -1,16 +1,17 @@
 <!-- @license CC0-1.0 -->
 
 De Ordered List is herkenbaar voor hulpsoftware als ongeordende lijst omdat de component is opgemaakt met `ol` en `li` elementen.
-In de browser Safari ([NL Design System baseline maart 2026](https://nldesignsystem.nl/baseline/2026-03/)) verdwijnt de semantiek van de lijst zodra in CSS `list-style: none` wordt gebruikt. Daarom is de ARIA rol `list` toegevoegd.
 
 ```html
-<ol role="list">
+<ol>
   <li>List-item voorbeeld</li>
   <li>List-item voorbeeld</li>
   <li>List-item voorbeeld</li>
   <li>List-item voorbeeld</li>
 </ol>
 ```
+
+Voeg `role="list"` aan het ul-element wanneer je eigen markers gebruikt en daarvoor de standaard styling in CSS overschrijft met `list-style: none`. In de browser Safari ([NL Design System baseline maart 2026](https://nldesignsystem.nl/baseline/2026-03/)) verdwijnt de semantiek van de lijst zodra in CSS `list-style: none` wordt gebruikt. Met de ARIA rol `list` voeg je die semantiek weer toe.
 
 NL Design System richtlijnen:
 
--- docs/componenten/ordered-list/_acceptatiecriteria.tsx
+++ docs/componenten/ordered-list/_acceptatiecriteria.tsx
@@ -1,6 +1,6 @@
 import Wcag131Lists from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.3.1-lists.md';
 import Wcag131NLDSOrdered from '@nl-design-system-unstable/documentation/componenten/ac/NLDS/_wcag-1.3.1-nlds-ordered.md';
-import Wcag1410 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.10.md';
+import Wcag1410Ordered from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.10-ordered.md';
 import Wcag1412 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.12.md';
 import Wcag143 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.3.md';
 import Wcag144 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-1.4.4.md';
@@ -8,7 +8,7 @@ import Wcag211NI from '@nl-design-system-unstable/documentation/componenten/ac/_
 import Wcag211NLDSNI from '@nl-design-system-unstable/documentation/componenten/ac/NLDS/_wcag-2.1.1-nlds-noninteractives.md';
 import Wcag243NLDSNI from '@nl-design-system-unstable/documentation/componenten/ac/NLDS/_wcag-2.4.3-nlds-noninteractives.md';
 import Wcag243NI from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-2.4.3-noninteractives.md';
-import Wcag312 from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-3.1.2.md';
+import Wcag312Lists from '@nl-design-system-unstable/documentation/componenten/ac/_wcag-3.1.2-lists.md';
 
 export const usage = [
   {
@@ -19,17 +19,18 @@ export const usage = [
     tags: ['developer'],
   },
   {
-    title: 'De tekst in de Ordered List heeft een contrastratio van minimaal 4,5:1 met de achtergrondkleur.',
+    title:
+      'De tekst en de tekstuele markers in de Ordered List hebben een contrastratio van minimaal 4,5:1 met de achtergrondkleur.',
     sc: '1.4.3',
     status: '',
     component: Wcag143,
     tags: ['designer'],
   },
   {
-    title: 'Als de bezoeker de Ordered List tot 400% vergroot, blijft de tekst zichtbaar en leesbaar.',
+    title: 'Als de bezoeker de Ordered List tot 400% vergroot, blijft alle tekst zichtbaar en leesbaar.',
     sc: '1.4.10',
     status: '',
-    component: Wcag1410,
+    component: Wcag1410Ordered,
     tags: ['developer', 'designer'],
   },
   {
@@ -50,7 +51,7 @@ export const usage = [
     title: 'Als de tekst in de Ordered List in een andere is dan de taal van de pagina, is dat duidelijk aangegeven.',
     sc: '3.1.2',
     status: '',
-    component: Wcag312,
+    component: Wcag312Lists,
     tags: ['developer'],
   },
 ];
