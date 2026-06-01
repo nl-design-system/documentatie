36306c41c085e43fe0fb70892e5352858a17fd3f
Peter Goes <petergoes@gmail.com>
2026-06-01 11:36:53 +0200

    chore: move template logic from `OverviewPage` to `DocCardList`
    `OverviewPage` was basically a wrapper around `DocCardList`. This change
    allows us to use both

--- packages/website/src/components/overview-page/overview-page.tsx
+++ packages/website/src/components/overview-page/overview-page.tsx
@@ -1,37 +1,15 @@
-import { isNavigationGroup, isNavigationItem, type NavigationElement } from '../../navigation';
-import { Card } from '@rijkshuisstijl-community/card-react';
-import '@rijkshuisstijl-community/card-css';
-import { UnorderedList } from '@amsterdam/design-system-react';
-import '@amsterdam/design-system-css/dist/unordered-list/unordered-list.css';
+import { DocCardList } from '../doc-card-list/doc-card-list';
+import type { NavigationElement } from '../../navigation';
 
 export interface OverviewPageProps {
   pages: NavigationElement[];
 }
 
-export const OverviewPage = (props: OverviewPageProps) => {
-  return (
-    <UnorderedList markers={false}>
-      {(props.pages || []).map((page) => {
-        let heading, description, href;
-
-        if (isNavigationItem(page)) {
-          heading = page.label;
-          description = page.description;
-          href = page.href;
-        }
-
-        if (isNavigationGroup(page)) {
-          heading = page.label;
-          description = page?.index?.description;
-          href = page.href;
-        }
-
-        return (
-          <UnorderedList.Item key={href}>
-            <Card heading={heading} description={description} href={href} />
-          </UnorderedList.Item>
-        );
-      })}
-    </UnorderedList>
-  );
-};
+/**
+ * The OverviewPage is an reexport of DocCardList. In Docusaurus the
+ * OverviewPage, exported the DocCardList but filtered the items with
+ * Docusaurus logic, in Astro, that is done on another level (Layout), but to
+ * keep the API consistent between Docusaurus and Astro, this component need to
+ * exists. An issue to unify them, is available (#4220)
+ */
+export const OverviewPage = (props: OverviewPageProps) => <DocCardList items={props.pages} />;
