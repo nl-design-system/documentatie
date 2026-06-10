a644bc957a8462bdd60bd36866d41b8fdcd9a1ab
Astrid-01 <166035020+Astrid-01@users.noreply.github.com>
2026-06-10 12:38:29 +0200

    Update Yolijn.tsx

--- src/components/CoreTeam/Yolijn.tsx
+++ src/components/CoreTeam/Yolijn.tsx
@@ -3,23 +3,7 @@ import type { TeamMember } from './TeamMember';
 
 const Yolijn: TeamMember = {
   name: 'Yolijn van der Kolk',
-  role: 'Productmanager & Developer Relations',
-  slack: { mention: '@Yolijn', id: 'U01B7T1F24S' },
-  Description: () => (
-    <>
-      <Paragraph>
-        Als productmanager is Yolijn graag de schakel tussen de verschillende stakeholders en het kernteam van NL Design
-        System. Ze combineert graag het uitwerken van een lange termijn visie met het opknippen van grote mijlpalen naar
-        kleine, toetsbare onderdelen.
-      </Paragraph>
-      <Paragraph>
-        Bij eerdere projecten bleek al dat ze graag de schakel is tussen de makers van een component en de
-        verscheidenheid aan organisaties die het white-label design system afnemen en aanpassen naar hun eigen Design
-        Tokens. In de rol van Developer Relations ontdekt ze graag na een lekker kopje koffie wat de gemene deler is
-        tussen componenten en stijlen bij verschillende applicaties.
-      </Paragraph>
-    </>
-  ),
-};
+  role: 'Product Manager',
+  slack: { mention: '@Yolijn', id: 'U01B7T1F24S' }
 
 export default Yolijn;
