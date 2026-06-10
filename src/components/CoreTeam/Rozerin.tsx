d5be4278de3a22cecda51c834506a8e42451e2e6
Astrid-01 <166035020+Astrid-01@users.noreply.github.com>
2026-06-10 12:36:20 +0200

    Update Rozerin.tsx

--- src/components/CoreTeam/Rozerin.tsx
+++ src/components/CoreTeam/Rozerin.tsx
@@ -3,18 +3,7 @@ import type { TeamMember } from './TeamMember';
 
 const Rozerin: TeamMember = {
   name: 'Rozerin Ayerdem',
-  role: 'Designer',
+  role: 'Design System Designer',
   slack: { mention: '@Rozerin', id: 'D038ABXNAG3' },
-  Description: () => (
-    <>
-      <Paragraph>
-        Als ontwerper is Rozerin bezig met het verzamelen, toevoegen en documenteren van interactiepatronen, componenten
-        en richtlijnen. Ze werkte vanuit haar eerdere rol bij de gemeente Den Haag al samen met het kernteam van NL
-        Design System.
-      </Paragraph>
-      <Paragraph>Naast haar werk is Rozerin gepassioneerd door reizen, natuur, fotografie en architectuur.</Paragraph>
-    </>
-  ),
-};
 
 export default Rozerin;
