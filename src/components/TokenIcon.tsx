c7fd6d7cb609160ac95ff87a98a9ff368dcc062a
Jeffrey Lauwers <jeffreylauwers@gmail.com>
2026-06-12 11:41:45 +0200

    docs: overview of all basis-tokens (#4270)
    Voeg een pagina met alle basis-tokens toe aan de documentatiewebsite.
    
    ---------
    
    Co-authored-by: Bart Veneman <bartveneman@gmail.com>

--- src/components/TokenIcon.tsx
+++ src/components/TokenIcon.tsx
@@ -7,17 +7,29 @@ import {
   IconRuler,
   IconTextSize,
   IconUnderline,
+  IconPointer,
+  IconNumber,
+  IconShadow,
 } from '@tabler/icons-react';
 import { Icon } from '@utrecht/component-library-react';
 import { createElement } from 'react';
 
+// This map contains a mix of DTCG-compliant types and real-world usage of types
+// which is why there are some almost-duplicates of singular and plural types.
 const tokenTypeToIconMap = {
+  boxShadow: IconShadow,
   color: IconPalette,
+  cursor: IconPointer,
   dimension: IconRuler,
+  fontFamily: IconFileTypography,
   fontFamilies: IconFileTypography,
+  fontSize: IconTextSize,
   fontSizes: IconTextSize,
+  fontWeight: IconBold,
   fontWeights: IconBold,
+  lineHeight: IconLineHeight,
   lineHeights: IconLineHeight,
+  number: IconNumber,
   other: IconFileUnknown,
   textDecoration: IconUnderline,
 } as const;
@@ -28,4 +40,8 @@ interface Props {
   type: IconType;
 }
 
-export const TokenIcon = ({ type }: Props) => <Icon>{createElement(tokenTypeToIconMap[type])}</Icon>;
+export const TokenIcon = ({ type }: Props) => {
+  // Explicit fallback to 'other' if the type doesn't match
+  const resolvedtype = Object.hasOwn(tokenTypeToIconMap, type) ? type : 'other';
+  return <Icon>{createElement(tokenTypeToIconMap[resolvedtype])}</Icon>;
+};
