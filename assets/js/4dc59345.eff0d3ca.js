2b37422a652e9671e74f04cedeaa14a8d8a7c30c
petergoes <petergoes@gmail.com>
2026-06-15 07:53:30 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@bcd50dbc0dd49b1a5746f8524991cba00a0e37dd 🚀

---
+++ assets/js/4dc59345.eff0d3ca.js
@@ -0,0 +1,9 @@
+'use strict';
+(globalThis.webpackChunk_nl_design_system_website = globalThis.webpackChunk_nl_design_system_website || []).push([
+ [32726],
+ {
+  90193(e) {
+   e.exports = JSON.parse('{"categoryGeneratedIndex":{"title":"Heartbeat","slug":"/events/heartbeat","permalink":"/events/heartbeat","sidebar":"community","navigation":{"previous":{"title":"Bijeenkomsten","permalink":"/community/events/overzicht"},"next":{"title":"Heartbeat","permalink":"/events/heartbeat"}}}}');
+  },
+ },
+]);
---
+++ assets/js/5222f8f3.57a08af6.js
@@ -0,0 +1,9 @@
+'use strict';
+(globalThis.webpackChunk_nl_design_system_website = globalThis.webpackChunk_nl_design_system_website || []).push([
+ [51933],
+ {
+  79080(e) {
+   e.exports = JSON.parse('{"categoryGeneratedIndex":{"title":"Developer Open Hour","slug":"/events/developer-open-hour","permalink":"/events/developer-open-hour","sidebar":"community","navigation":{"previous":{"title":"In de Design Open Hour wisselen designers informatie, inzichten en tips met elkaar uit.","permalink":"/events/design-open-hour/aanmelden"},"next":{"title":"In de Developer Open Hour wisselen developers informatie, inzichten en tips met elkaar uit.","permalink":"/events/developer-open-hour"}}}}');
+  },
+ },
+]);
