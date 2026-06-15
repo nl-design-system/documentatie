a6868bbd2103c394af15f650045a42b4b57528f1
richadr <richard@vhvs.nl>
2026-06-15 08:23:03 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@8bec3dfac94fdc78f9bac5e5a4eb5b8ccc4f2f6e 🚀

---
+++ assets/js/c0172415.fb92db18.js
@@ -0,0 +1,9 @@
+'use strict';
+(globalThis.webpackChunk_nl_design_system_website = globalThis.webpackChunk_nl_design_system_website || []).push([
+ [33278],
+ {
+  91307(e) {
+   e.exports = JSON.parse('{"categoryGeneratedIndex":{"title":"Bijeenkomsten","slug":"/community/events/overzicht","permalink":"/community/events/overzicht","sidebar":"community","navigation":{"previous":{"title":"Design Tokens valideren","permalink":"/project/expertteam-digitale-toegankelijkheid/design-tokens-lint"},"next":{"title":"Heartbeat","permalink":"/events/heartbeat"}}}}');
+  },
+ },
+]);
