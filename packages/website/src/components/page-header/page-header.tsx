ce5c737635a5745cbdb3756cdc766d3b6da31405
Peter Goes <petergoes@gmail.com>
2026-06-29 15:00:38 +0200

    feat: add the search form to the page header

--- packages/website/src/components/page-header/page-header.tsx
+++ packages/website/src/components/page-header/page-header.tsx
@@ -1,2 +1,29 @@
 import '@utrecht/page-header-css/dist/index.css';
-export { PageHeader } from '@utrecht/page-header-react';
+import { PageHeader as UtrechtPageHeader } from '@utrecht/page-header-react';
+import { Logo } from '@components/logo/logo';
+import { ToSearchLink } from '@components/search/to-search-link';
+import { SearchForm } from '@components/search/form';
+import { Button } from '@components/button/button';
+
+import './page-header.css';
+
+export const PageHeader = () => {
+  return (
+    <UtrechtPageHeader className="ma-page-header">
+      <div className="ma-page-header__start">
+        <Button className="ma-menu-trigger" purpose="secondary">
+          Menu
+        </Button>
+      </div>
+      <div className="ma-page-header__center">
+        <a href="/" aria-label="NL Design System logo, naar de homepage">
+          <Logo />
+        </a>
+      </div>
+      <div className="ma-page-header__end">
+        <ToSearchLink />
+        <SearchForm />
+      </div>
+    </UtrechtPageHeader>
+  );
+};
