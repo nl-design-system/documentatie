113fe75235573f34d2e44c74ecb8c3ef066a7bc9
Peter Goes <petergoes@gmail.com>
2026-08-14 15:48:10 +0200

    test: content test basis (#4459)
    closes: #4455

---
+++ test/docs/markdown-as-children.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@playwright/test';
+
+const websiteURL = 'http://localhost:4321/private/content-test/markdown-as-children';
+
+test('renders markdown as children of a component', async ({ page }) => {
+  await page.goto(websiteURL);
+
+  await expect(page.locator('h2').filter({ hasText: /This is an h2 heading/i })).toBeVisible();
+
+  await expect(page.locator('p').filter({ hasText: /This is a paragraph/i })).toBeVisible();
+
+  await expect(page.locator('li').filter({ hasText: /this is a list/i })).toBeVisible();
+});
