18ccd9a3dab4b77dca91e26f67caa34022601cc6
Peter Goes <petergoes@gmail.com>
2026-08-31 14:15:53 +0200

    chore: refactor remark-canvas-fix to inject code as prop instead of children

--- packages/website/markdown-plugins/remark-canvas-fix/index.ts
+++ packages/website/markdown-plugins/remark-canvas-fix/index.ts
@@ -1,6 +1,7 @@
 import type { Root } from 'mdast';
 import { visit } from 'unist-util-visit';
 import prettierSync from '@prettier/sync';
+import type { MdxJsxAttribute, MdxJsxFlowElement } from 'mdast-util-mdx-jsx';
 
 /**
  * The Canvas component used in Docusaurus depends on a function providing the
@@ -14,31 +15,53 @@ export function remarkCanvasFix() {
     visit(tree, 'mdxFlowExpression', (node, _, parent) => {
       if (parent.type !== 'mdxJsxFlowElement' || parent.name !== 'Canvas') return;
 
+      const parentNode = parent as MdxJsxFlowElement;
+
       if (node.value.startsWith('() => ')) {
         const code = node.value
-          .replace(/\(\)\s=>\s\(?/, '')
-          .replace(/\)$/, '')
-          .replaceAll(/\n/g, '')
+          .replace(/\(\)\s=>\s\(?/, '') // remove the arrow function from the string
+          .replace(/\)$/, '') // remove the closing `)` of the arrow function
+          .replaceAll(/\n/g, '') // remove newlines, prettier will reformat
+          .replaceAll(/\bhtmlFor=/g, 'for=') // replace jsx style attribute name `htmlFor` with `for`
+          .replaceAll(/\bclassName=/g, 'class=') // replace jsx style attribute name `className` with `class`
+          .replaceAll('<>', '') // remove jsx fragment wrapper start
+          .replaceAll('</>', '') // remove jsx fragment wrapper end
           .trim();
 
         const styleTransformedCode = styleObjectToString(code);
 
-        const formattedCode = prettierSync.format(styleTransformedCode, { parser: 'babel' });
+        const formattedCode = prettierSync
+          .format(styleTransformedCode, {
+            parser: 'html',
+            singleAttributePerLine: true,
+            embeddedLanguageFormatting: 'off',
+            htmlWhitespaceSensitivity: 'ignore',
+          })
+          // complex regex to remove multiline string literals in JSX
+          .replaceAll(/\{\s*"(?:[^"\\]|\\.)*"\s*\}/g, (m) =>
+            m
+              .replace(/^\{\s*"/, '')
+              .replace(/"\s*\}$/, '')
+              .replaceAll('\\"', '"')
+              .replaceAll("\\'", "'"),
+          );
 
-        node.value = formattedCode
-          .replaceAll(/;$/gm, '')
-          .replace('<>', '')
-          .replace('</>', '')
-          .replaceAll('{" "}', ' ')
-          .replaceAll(/^\s+$\n/gm, '\n');
+        const codeAttr: MdxJsxAttribute = {
+          type: 'mdxJsxAttribute',
+          name: 'code',
+          value: formattedCode,
+        };
 
-        // @ts-expect-error - The type html is not assignable to mdxFlowExpression. Which is true, but we want to force it to
-        node.type = 'html';
+        // only if no existing `code` attribute is present create a new one
+        if (parentNode.attributes.some((attr) => attr.type === 'mdxJsxAttribute' && attr.name === 'code') === false) {
+          parentNode.attributes.push(codeAttr);
+        }
       }
     });
   };
 }
 
+// Transform JSX (multiline) style attributes into html style attributes
 function styleObjectToString(input: string) {
   let output = input;
   const matches = output.matchAll(/style=(\{\{.+?\}\})/g);
@@ -51,7 +74,11 @@ function styleObjectToString(input: string) {
       .replace('{{', '"')
       .replace('}}', '"');
 
+    // camel case to snake case
     replacement = replacement.replaceAll(/([a-z])([A-Z])/g, (_, p1, p2) => `${p1}-${p2}`.toLowerCase());
+
+    // collapse double spaces into a single space
+    replacement = replacement.replaceAll(/\s+/g, ' ').trim();
 
     output = output.replace(match, replacement);
   });
