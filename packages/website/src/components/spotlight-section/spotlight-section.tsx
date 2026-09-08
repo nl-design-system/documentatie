8a1891d85214268f901f657c7e877e609169ca81
Peter Goes <petergoes@gmail.com>
2026-09-08 09:41:37 +0200

    fix: uppercase admonition (#4626)
    Moest (voor Astro) ook upgraden van @utrecht/spotlight-section naar
    @utrecht/note. Voor Docusaurus gelaten, omdat ik niet wist wat de
    upgrade in ma-design-tokens nog meer met zich mee brengt als dat in
    Docusaurus ook geupgrade wordt
    
    closes: #4478

--- packages/website/src/components/spotlight-section/spotlight-section.tsx
+++ packages/website/src/components/spotlight-section/spotlight-section.tsx
@@ -1,2 +1,6 @@
-export { SpotlightSection } from '@utrecht/component-library-react';
+import { Note } from '@utrecht/note-react';
+import { SpotlightSection as UtrechtSpotlightSection } from '@utrecht/component-library-react';
 import '@utrecht/spotlight-section-css/dist/index.css';
+import '@utrecht/note-css/dist/index.css';
+
+export const SpotlightSection = globalThis.isAstro ? Note : UtrechtSpotlightSection;
