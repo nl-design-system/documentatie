99e92c9ccdb532faafcb7e4e5748b24837abff7e
petergoes <petergoes@gmail.com>
2026-09-08 13:55:49 +0000

    Deploying to gh-pages from @ nl-design-system/documentatie@d1b8e9036fae9ccd600e102421c6e582bfcb6f68 🚀

---
+++ assets/js/11973.6e151cc8.js
@@ -0,0 +1,78 @@
+'use strict';
+(globalThis.webpackChunk_nl_design_system_website = globalThis.webpackChunk_nl_design_system_website || []).push([
+ [11973],
+ {
+  60529(e, n, s) {
+   s(30758);
+  },
+  83766(e) {
+   e.exports = JSON.parse('{"nl":{"heading":{"level-1":{"color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"font-family":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<family-name>","<generic-name>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontFamilies"},"font-size":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<percentage>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontSizes"},"font-weight":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<number>","nl.nldesignsystem.figma-implementation":true},"$type":"fontWeights"},"line-height":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<number>"],"nl.nldesignsystem.figma-implementation":true},"$type":"lineHeights"},"margin-block-end":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"},"margin-block-start":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"}},"level-2":{"color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"font-family":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<family-name>","<generic-name>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontFamilies"},"font-size":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<percentage>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontSizes"},"font-weight":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<number>","nl.nldesignsystem.figma-implementation":true},"$type":"fontWeights"},"line-height":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<number>"],"nl.nldesignsystem.figma-implementation":true},"$type":"lineHeights"},"margin-block-end":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"},"margin-block-start":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"}},"level-3":{"color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"font-family":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<family-name>","<generic-name>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontFamilies"},"font-size":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<percentage>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontSizes"},"font-weight":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<number>","nl.nldesignsystem.figma-implementation":true},"$type":"fontWeights"},"line-height":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<number>"],"nl.nldesignsystem.figma-implementation":true},"$type":"lineHeights"},"margin-block-end":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"},"margin-block-start":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"}},"level-4":{"color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"font-family":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<family-name>","<generic-name>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontFamilies"},"font-size":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<percentage>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontSizes"},"font-weight":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<number>","nl.nldesignsystem.figma-implementation":true},"$type":"fontWeights"},"line-height":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<number>"],"nl.nldesignsystem.figma-implementation":true},"$type":"lineHeights"},"margin-block-end":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"},"margin-block-start":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"}},"level-5":{"color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"font-family":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<family-name>","<generic-name>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontFamilies"},"font-size":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<percentage>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontSizes"},"font-weight":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<number>","nl.nldesignsystem.figma-implementation":true},"$type":"fontWeights"},"line-height":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<number>"],"nl.nldesignsystem.figma-implementation":true},"$type":"lineHeights"},"margin-block-end":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"},"margin-block-start":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"}},"level-6":{"color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"font-family":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<family-name>","<generic-name>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontFamilies"},"font-size":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<percentage>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontSizes"},"font-weight":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<number>","nl.nldesignsystem.figma-implementation":true},"$type":"fontWeights"},"line-height":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<number>"],"nl.nldesignsystem.figma-implementation":true},"$type":"lineHeights"},"margin-block-end":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"},"margin-block-start":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":false},"$type":"dimension"}}}}}');
+  },
+  95732(e, n, s) {
+   s.d(n, { $n: () => d });
+   var t = s(48171),
+    i = s(41534),
+    l = s(86070),
+    o = s(13526),
+    m = s(30758),
+    a = ['children', 'className', 'iconOnly', 'iconEnd', 'iconStart', 'purpose', 'hint', 'disabled', 'htmlDisabled', 'pressed', 'busy', 'type', 'label', 'toggle'];
+   function r(e, n) {
+    var s = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+     var t = Object.getOwnPropertySymbols(e);
+     (n &&
+      (t = t.filter(function (n) {
+       return Object.getOwnPropertyDescriptor(e, n).enumerable;
+      })),
+      s.push.apply(s, t));
+    }
+    return s;
+   }
+   function y(e) {
+    for (var n = 1; n < arguments.length; n++) {
+     var s = null != arguments[n] ? arguments[n] : {};
+     n % 2
+      ? r(Object(s), !0).forEach(function (n) {
+         (0, t.A)(e, n, s[n]);
+        })
+      : Object.getOwnPropertyDescriptors
+        ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(s))
+        : r(Object(s)).forEach(function (n) {
+           Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(s, n));
+          });
+    }
+    return e;
+   }
+   var g = function (e) {
+     return function (n) {
+      return e.includes(n);
+     };
+    },
+    p = g(['primary', 'secondary', 'subtle']),
+    d =
+     (g(['positive', 'negative']),
+     (0, m.forwardRef)(function (e, n) {
+      var s = e.children,
+       t = e.className,
+       r = e.iconOnly,
+       g = e.iconEnd,
+       d = e.iconStart,
+       c = e.purpose,
+       f = e.hint,
+       u = e.disabled,
+       x = e.htmlDisabled,
+       $ = e.pressed,
+       b = e.busy,
+       h = e.type,
+       v = void 0 === h ? 'button' : h,
+       O = e.label,
+       k = e.toggle,
+       w = (0, i.A)(e, a),
+       j = Boolean(d || g),
+       _ = (m.Children.toArray(s).some(m.isValidElement) && j) || r;
+      return (0, l.jsxs)('button', y(y({ ref: n, type: v, className: (0, o.$)('nl-button', t, { 'nl-button--pressed': $, 'nl-button--busy': b, 'nl-button--disabled': u || x, 'nl-button--primary': 'primary' === c, 'nl-button--secondary': 'secondary' === c, 'nl-button--subtle': 'subtle' === c, 'nl-button--positive': p(c) && 'positive' === f, 'nl-button--negative': p(c) && 'negative' === f, 'nl-button--icon-only': r }), 'aria-pressed': k ? String(Boolean($)) : void 0, 'aria-disabled': u || b ? 'true' : void 0, disabled: x }, w), {}, { children: [d && (0, l.jsx)('span', { className: 'nl-button__icon-start', children: d }), O && (0, l.jsx)('span', { className: 'nl-button__label', children: O }), _ ? (0, l.jsx)('span', { className: r ? 'nl-button__label' : '', children: s }) : s, g && (0, l.jsx)('span', { className: 'nl-button__icon-end', children: g })] }));
+     }));
+   d.displayName = 'Button';
+  },
+ },
+]);
---
+++ assets/js/20726.c97c738d.js
@@ -0,0 +1,78 @@
+'use strict';
+(globalThis.webpackChunk_nl_design_system_website = globalThis.webpackChunk_nl_design_system_website || []).push([
+ [20726],
+ {
+  3779(e, n, t) {
+   t(30758);
+  },
+  89019(e) {
+   e.exports = JSON.parse('{"nl":{"code-block":{"background-color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"border-radius":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length-percentage>","nl.nldesignsystem.figma-implementation":true},"$type":"dimension"},"color":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<color>","nl.nldesignsystem.figma-implementation":true},"$type":"color"},"font-family":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<family-name>","<generic-name>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontFamilies"},"font-size":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<percentage>"],"nl.nldesignsystem.figma-implementation":true},"$type":"fontSizes"},"line-height":{"$extensions":{"nl.nldesignsystem.css-property-syntax":["<length>","<number>"],"nl.nldesignsystem.figma-implementation":true},"$type":"lineHeights"},"padding-block":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":true},"$type":"dimension"},"padding-inline":{"$extensions":{"nl.nldesignsystem.css-property-syntax":"<length>","nl.nldesignsystem.figma-implementation":true},"$type":"dimension"}}}}');
+  },
+  95732(e, n, t) {
+   t.d(n, { $n: () => m });
+   var s = t(48171),
+    i = t(41534),
+    l = t(86070),
+    r = t(13526),
+    o = t(30758),
+    a = ['children', 'className', 'iconOnly', 'iconEnd', 'iconStart', 'purpose', 'hint', 'disabled', 'htmlDisabled', 'pressed', 'busy', 'type', 'label', 'toggle'];
+   function p(e, n) {
+    var t = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+     var s = Object.getOwnPropertySymbols(e);
+     (n &&
+      (s = s.filter(function (n) {
+       return Object.getOwnPropertyDescriptor(e, n).enumerable;
+      })),
+      t.push.apply(t, s));
+    }
+    return t;
+   }
+   function c(e) {
+    for (var n = 1; n < arguments.length; n++) {
+     var t = null != arguments[n] ? arguments[n] : {};
+     n % 2
+      ? p(Object(t), !0).forEach(function (n) {
+         (0, s.A)(e, n, t[n]);
+        })
+      : Object.getOwnPropertyDescriptors
+        ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+        : p(Object(t)).forEach(function (n) {
+           Object.defineProperty(e, n, Object.getOwnPropertyDescriptor(t, n));
+          });
+    }
+    return e;
+   }
+   var y = function (e) {
+     return function (n) {
+      return e.includes(n);
+     };
+    },
+    d = y(['primary', 'secondary', 'subtle']),
+    m =
+     (y(['positive', 'negative']),
+     (0, o.forwardRef)(function (e, n) {
+      var t = e.children,
+       s = e.className,
+       p = e.iconOnly,
+       y = e.iconEnd,
+       m = e.iconStart,
+       u = e.purpose,
+       b = e.hint,
+       g = e.disabled,
+       f = e.htmlDisabled,
+       h = e.pressed,
+       x = e.busy,
+       O = e.type,
+       $ = void 0 === O ? 'button' : O,
+       j = e.label,
+       v = e.toggle,
+       _ = (0, i.A)(e, a),
+       w = Boolean(m || y),
+       N = (o.Children.toArray(t).some(o.isValidElement) && w) || p;
+      return (0, l.jsxs)('button', c(c({ ref: n, type: $, className: (0, r.$)('nl-button', s, { 'nl-button--pressed': h, 'nl-button--busy': x, 'nl-button--disabled': g || f, 'nl-button--primary': 'primary' === u, 'nl-button--secondary': 'secondary' === u, 'nl-button--subtle': 'subtle' === u, 'nl-button--positive': d(u) && 'positive' === b, 'nl-button--negative': d(u) && 'negative' === b, 'nl-button--icon-only': p }), 'aria-pressed': v ? String(Boolean(h)) : void 0, 'aria-disabled': g || x ? 'true' : void 0, disabled: f }, _), {}, { children: [m && (0, l.jsx)('span', { className: 'nl-button__icon-start', children: m }), j && (0, l.jsx)('span', { className: 'nl-button__label', children: j }), N ? (0, l.jsx)('span', { className: p ? 'nl-button__label' : '', children: t }) : t, y && (0, l.jsx)('span', { className: 'nl-button__icon-end', children: y })] }));
+     }));
+   m.displayName = 'Button';
+  },
+ },
+]);
